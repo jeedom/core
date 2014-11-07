f959a5ab430c6a69163558360dc108f3ec763fb2
Loic <loicgevrey@gmail.com>
2014-11-07 11:26:20 +0100

    Fix link on design

--- core/ajax/plan.ajax.php
+++ core/ajax/plan.ajax.php
@@ -61,7 +61,7 @@ try {
                     $link = 'index.php?v=d&p=plan&plan_id=' . $plan_link->getId();
                 }
                 $html = '<span href="' . $link . '" class="plan-link-widget label label-success" data-link_id="' . $plan_link->getId() . '">';
-                $html .= '<a style="color:' . $plan->getCss('color', 'white') . ';text-decoration:none;font-size : 1.5em;">';
+                $html .= '<a href="' . $link . '" style="color:' . $plan->getCss('color', 'white') . ';text-decoration:none;font-size : 1.5em;">';
                 if ($plan->getDisplay('name') != '' || $plan->getDisplay('icon') != '') {
                     $html .=$plan->getDisplay('icon') . ' ' . $plan->getDisplay('name');
                 } else {
@@ -84,7 +84,7 @@ try {
                     $link = 'index.php?v=d&p=view&view_id=' . $view->getId();
                 }
                 $html = '<span href="' . $link . '" class="view-link-widget label label-primary" data-link_id="' . $view->getId() . '" >';
-                $html .= '<a style="color:' . $plan->getCss('color', 'white') . ';text-decoration:none;font-size : 1.5em;">';
+                $html .= '<a href="' . $link . '" style="color:' . $plan->getCss('color', 'white') . ';text-decoration:none;font-size : 1.5em;">';
                 if ($plan->getDisplay('name') != '' || $plan->getDisplay('icon') != '') {
                     $html .= $plan->getDisplay('icon') . ' ' . $plan->getDisplay('name');
                 } else {
