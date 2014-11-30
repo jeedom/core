5bf121566ffbda82e54b4fee4a14f76089bf33b5
Loic <loicgevrey@gmail.com>
2014-11-30 18:01:29 +0100

    UI improvement

--- desktop/modal/market.display.php
+++ desktop/modal/market.display.php
@@ -95,7 +95,7 @@ sendVarToJS('market_display_info', $market_array);
         <?php
         if ($market->getCost() > 0) {
             if ($market->getCost() != $market->getRealCost()) {
-                echo '<span class="label label-primary" data-l1key="rating" style="font-size: 1em;text-decoration:line-through;">' . number_format($market->getRealCost(), 2) . ' €</span> ';
+                echo '<span data-l1key="rating" style="font-size: 1em;text-decoration:line-through;">' . number_format($market->getRealCost(), 2) . ' €</span> ';
             }
             echo '<span data-l1key="rating" style="font-size: 1.5em;">' . number_format($market->getCost(), 2) . ' €</span> (TVA non applicable, article 293 B du CGI)';
         } else {
