30840e389d4bda938d16942c983f108232d1a46a
loic <lgevrey@oxya.com>
2014-12-03 19:07:15 +0100

    Improve page object on small screen

--- desktop/php/object.php
+++ desktop/php/object.php
@@ -30,15 +30,15 @@ sendVarToJS('select_id', init('id', '-1'));
             <fieldset>
                 <legend>{{Général}}</legend>
                 <div class="form-group">
-                    <label class="col-sm-2 control-label">{{Nom de l'objet}}</label>
-                    <div class="col-sm-3">
+                    <label class="col-lg-2 col-md-3 col-sm-4 col-xs-6 control-label">{{Nom de l'objet}}</label>
+                    <div class="col-lg-3 col-md-4 col-sm-5 col-xs-6">
                         <input class="form-control objectAttr" type="text" data-l1key="id" style="display : none;"/>
                         <input class="form-control objectAttr" type="text" data-l1key="name" placeholder="Nom de l'objet"/>
                     </div>
                 </div>
                 <div class="form-group">
-                    <label class="col-sm-2 control-label">{{Père}}</label>
-                    <div class="col-sm-3">
+                    <label class="col-lg-2 col-md-3 col-sm-4 col-xs-6 control-label">{{Père}}</label>
+                    <div class="col-lg-3 col-md-4 col-sm-5 col-xs-6">
                         <select class="form-control objectAttr" data-l1key="father_id">
                             <option value="">{{Aucun}}</option>
                             <?php
@@ -50,18 +50,18 @@ sendVarToJS('select_id', init('id', '-1'));
                     </div>
                 </div>
                 <div class="form-group">
-                    <label class="col-sm-2 control-label">{{Visible}}</label>
+                    <label class="col-lg-2 col-md-3 col-sm-4 col-xs-6 control-label">{{Visible}}</label>
                     <div class="col-sm-1">
                         <input class="objectAttr" type="checkbox"  data-l1key="isVisible" checked/>
                     </div>
                 </div>
                 <div class="form-group">
-                    <label class="col-sm-2 control-label">{{Icône}}</label>
-                    <div class="col-sm-2">
+                    <label class="col-lg-2 col-md-3 col-sm-4 col-xs-6 control-label">{{Icône}}</label>
+                    <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
                         <div class="objectAttr" data-l1key="display" data-l2key="icon" ></div>
                     </div>
-                    <div class="col-sm-2">
-                        <a class="btn btn-default btn-sm" id="bt_chooseIcon"><i class="fa fa-flag"></i> {{Choisir une icône}}</a>
+                    <div class="col-lg-2 col-md-3 col-sm-4 col-xs-4">
+                        <a class="btn btn-default btn-sm" id="bt_chooseIcon"><i class="fa fa-flag"></i> {{Choisir}}</a>
                     </div>
                 </div>
             </fieldset>
