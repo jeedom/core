7575c9b0e93e2d8376394c568def0b5ca2746a3b
loic <lgevrey@oxya.com>
2014-12-08 19:54:40 +0100

    Code cleaning

---
+++ desktop/modal/cmd.configure.php
@@ -0,0 +1,295 @@
+<?php
+if (!isConnect('admin')) {
+    throw new Exception('{{401 - Accès non autorisé}}');
+}
+$cmd = cmd::byId(init('cmd_id'));
+if (!is_object($cmd)) {
+    throw new Exception('Commande non trouvé : ' . init('cmd_id'));
+}
+sendVarToJS('cmdInfo', utils::o2a($cmd));
+$cmd_widgetDashboard = cmd::availableWidget('dashboard');
+$cmd_widgetMobile = cmd::availableWidget('mobile');
+?>
+<div style="display: none;" id="md_displayCmdConfigure"></div>
+<div id='div_displayCmdConfigure'>
+    <div class="row">
+        <div class="col-sm-6" >
+            <form class="form-horizontal">
+                <fieldset>
+                    <div class="form-group">
+                        <label class="col-xs-4 control-label">{{ID}}</label>
+                        <div class="col-xs-4">
+                            <span class="cmdAttr label label-primary" data-l1key="id"></span>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-xs-4 control-label">{{Logical ID}}</label>
+                        <div class="col-xs-4">
+                            <span class="cmdAttr label label-primary" data-l1key="logicalId"></span>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-xs-4 control-label">{{Nom}}</label>
+                        <div class="col-xs-4">
+                            <span class="cmdAttr label label-primary" data-l1key="name"></span>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-xs-4 control-label">{{Type}}</label>
+                        <div class="col-xs-4">
+                            <span class="cmdAttr label label-primary" data-l1key="type"></span>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-xs-4 control-label">{{Sous-type}}</label>
+                        <div class="col-xs-4">
+                            <span class="cmdAttr label label-primary" data-l1key="subType"></span>
+                        </div>
+                    </div>
+
+                </fieldset>
+            </form>
+        </div>
+        <div class="col-sm-6" >
+
+
+            <form class="form-horizontal">
+                <fieldset>
+
+                    <div class="form-group">
+                        <label class="col-xs-4 control-label">{{Unité}}</label>
+                        <div class="col-xs-4">
+                            <span class="cmdAttr label label-primary" data-l1key="unite"></span>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-xs-4 control-label">{{Commande déclenchant une mise à jour}}</label>
+                        <div class="col-xs-4">
+                            <span class="cmdAttr label label-primary" data-l1key="value"></span>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-xs-4 control-label">{{Memcache}}</label>
+                        <div class="col-xs-4">
+                            <span class="cmdAttr label label-primary tooltips" data-l1key="cache" data-l2key="enable" title="{{Actif}}"></span> 
+                            <span class="label label-default tooltips" title="{{Durée du cache}}"><span class="cmdAttr" data-l1key="cache" data-l2key="lifetime"></span> {{seconde(s)}}</span>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-xs-4 control-label">{{Evenement seulement}}</label>
+                        <div class="col-xs-4">
+                            <span class="cmdAttr label label-primary" data-l1key="eventOnly"></span>
+                        </div>
+                    </div>
+
+                </fieldset>
+            </form>
+        </div>
+    </div>
+
+    <div>
+        <legend>{{Utilisé par}}</legend>
+        <form class="form-horizontal">
+            <fieldset id="fd_cmdUsedBy">
+                <?php
+                $usedBy = $cmd->getUsedBy();
+                ?>
+                <div class="form-group">
+                    <label class="col-lg-2 col-md-3 col-sm-4 col-xs-6 control-label">{{Equipement}}</label>
+                    <div class="col-sm-4">
+                        <?php
+                        foreach ($usedBy['eqLogic'] as $usedByEqLogic) {
+                            echo '<span class="label label-primary cursor"><a href="' . $usedByEqLogic['link'] . '" style="color : white;">' . $usedByEqLogic['humanName'] . '</a></span>';
+                        }
+                        ?>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-lg-2 col-md-3 col-sm-4 col-xs-6 control-label">{{Commandes}}</label>
+                    <div class="col-sm-4">
+                        <?php
+                        foreach ($usedBy['cmd'] as $usedByCmd) {
+                            echo '<span class="label label-primary cursor"><a href="' . $usedByCmd->getEqLogic()->getLinkToConfiguration() . '" style="color : white;">' . $usedByCmd->getHumanName() . '</a></span>';
+                        }
+                        ?>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-lg-2 col-md-3 col-sm-4 col-xs-6 control-label">{{Scénario}}</label>
+                    <div class="col-sm-4">
+                        <?php
+                        foreach ($usedBy['scenario'] as $usedByScneario) {
+                            echo '<span class="label label-primary cursor"><a href="' . $usedByScneario->getLinkToConfiguration() . '" style="color : white;">' . $usedByScneario->getHumanName() . '</a></span>';
+                        }
+                        ?>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-lg-2 col-md-3 col-sm-4 col-xs-6 control-label">{{Interaction}}</label>
+                    <div class="col-sm-4">
+                        <div class="col-sm-4">
+                            <?php
+                            foreach ($usedBy['interact'] as $usedByInteract) {
+                                echo '<span class="label label-primary cursor"><a href="' . $usedByInteract->getLinkToConfiguration() . '" style="color : white;">' . $usedByInteract->getName() . '</a></span>';
+                            }
+                            ?>
+                        </div>
+                    </div>
+                </div>
+            </fieldset>
+        </form>
+
+        <legend>{{Configuration}}</legend>
+        <form class="form-horizontal">
+            <fieldset>
+                <?php if ($cmd->getType() == 'info' && $cmd->getSubType() == 'numeric') { ?>
+                    <div class="form-group">
+                        <label class="col-sm-6 control-label">{{Formule de calcul (utiliser #value# pour la valeur, ex #value# - 2)}}</label>
+                        <div class="col-sm-6">
+                            <input class="cmdAttr form-control" data-l1key="configuration" data-l2key="calculValueOffset" />
+                        </div>
+                    </div>
+                <?php } ?>
+
+                <div class="form-group">
+                    <label class="col-sm-4 control-label">{{Ne pas afficher le nom de la commande sur le dashboard}}</label>
+                    <div class="col-sm-1">
+                        <input type="checkbox" class="cmdAttr" data-l1key="display" data-l2key="doNotShowNameOnDashboard" />
+                    </div>
+                    <label class="col-sm-2 control-label">{{sur les vues}}</label>
+                    <div class="col-sm-1">
+                        <input type="checkbox" class="cmdAttr" data-l1key="display" data-l2key="doNotShowNameOnView" />
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="col-lg-2 col-md-3 col-sm-4 col-xs-6 control-label">{{Widget Desktop}}</label>
+                    <div class="col-lg-2 col-md-3 col-sm-4 col-xs-6">
+                        <select class="form-control cmdAttr" data-l1key="template" data-l2key="dashboard">
+                            <?php
+                            foreach ($cmd_widgetDashboard[$cmd->getType()][$cmd->getSubType()] as $widget) {
+                                echo '<option>' . $widget['name'] . '</option>';
+                            }
+                            ?>
+                        </select>
+                    </div>
+                    <label class="col-lg-2 col-md-3 col-sm-4 col-xs-6 control-label">{{Mobile}}</label>
+                    <div class="col-lg-2 col-md-3 col-sm-4 col-xs-6">
+                        <select class="form-control cmdAttr" data-l1key="template" data-l2key="mobile">
+                            <?php
+                            foreach ($cmd_widgetMobile[$cmd->getType()][$cmd->getSubType()] as $widget) {
+                                echo '<option>' . $widget['name'] . '</option>';
+                            }
+                            ?>
+                        </select>
+                    </div>
+                </div>
+
+                <?php if ($cmd->getIsHistorized() == 1) { ?>
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label">{{Historiser}}</label>
+                        <div class="col-sm-1">
+                            <input type="checkbox" class="cmdAttr" data-l1key="isHistorized" />
+                        </div>
+                        <label class="col-sm-2 control-label">{{Mode de lissage}}</label>
+                        <div class="col-sm-2">
+                            <select class="form-control cmdAttr" data-l1key="configuration" data-l2key="historizeMode">
+                                <option value="avg">Moyenne</option>
+                                <option value="min">Minimum</option>
+                                <option value="max">Maximum</option>
+                                <option value="none">Aucun</option>
+                            </select>
+                        </div>
+                        <label class="col-sm-2 control-label">{{Arrondi (nombre de chiffre après la virgule)}}</label>
+                        <div class="col-sm-2">
+                            <input class="cmdAttr form-control" data-l1key="configuration" data-l2key="historizeRound" />
+
+                        </div>
+                    </div>
+                <?php } ?>
+            </fieldset>
+        </form>
+
+        <legend>{{Paramètres optionnels widget}} <a class="btn btn-success btn-xs pull-right" id="bt_addWidgetParameters"><i class="fa fa-plus-circle"></i> Ajouter</a></legend>
+        <table class="table table-bordered table-condensed" id="table_widgetParameters">
+            <thead>
+                <tr>
+                    <th>Nom</th>
+                    <th>Valeur</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                if ($cmd->getDisplay('parameters') != '') {
+                    foreach ($cmd->getDisplay('parameters') as $key => $value) {
+                        echo '<tr>';
+                        echo '<td>';
+                        echo '<input class="form-control key" value="' . $key . '" />';
+                        echo '</td>';
+                        echo '<td>';
+                        echo '<input class="form-control value" value="' . $value . '" />';
+                        echo '</td>';
+                        echo '<td>';
+                        echo '<a class="btn btn-danger btn-xs removeWidgetParameter"><i class="fa fa-times"></i> Supprimer</a>';
+                        echo '</td>';
+                        echo '</tr>';
+                    }
+                }
+                ?>
+            </tbody>
+        </table>
+        <a class="btn btn-success" id="saveCmd"><i class="fa fa-check-circle"></i> {{Enregistrer}}</a>
+    </div>
+</div>
+
+<script>
+    $('#table_widgetParameters').delegate('.removeWidgetParameter', 'click', function () {
+        $(this).closest('tr').remove();
+    });
+
+    $('#bt_addWidgetParameters').off().on('click', function () {
+        var tr = '<tr>';
+        tr += '<td>';
+        tr += '<input class="form-control key" />';
+        tr += '</td>';
+        tr += '<td>';
+        tr += '<input class="form-control value" />';
+        tr += '</td>';
+        tr += '<td>';
+        tr += '<a class="btn btn-danger btn-xs removeWidgetParameter pull-right"><i class="fa fa-times"></i> Supprimer</a>';
+        tr += '</td>';
+        tr += '</tr>';
+        $('#table_widgetParameters tbody').append(tr);
+    });
+
+    $('#div_displayCmdConfigure').setValues(cmdInfo, '.cmdAttr');
+    $('#saveCmd').off().on('click', function () {
+        var cmd = $('#div_displayCmdConfigure').getValues('.cmdAttr')[0];
+        if (!isset(cmd.display)) {
+            cmd.display = {};
+        }
+        if (!isset(cmd.display.parameters)) {
+            cmd.display.parameters = {};
+        }
+        $('#table_widgetParameters tbody tr').each(function () {
+            cmd.display.parameters[$(this).find('.key').value()] = $(this).find('.value').value();
+        });
+        jeedom.cmd.save({
+            cmd: cmd,
+            error: function (error) {
+                $('#md_displayCmdConfigure').showAlert({message: error.message, level: 'danger'});
+            },
+            success: function () {
+                $('#md_displayCmdConfigure').showAlert({message: '{{Enregistrement réussi}}', level: 'success'});
+            }
+        });
+    });
+</script>
---
+++ desktop/modal/eqLogic.configure.php
@@ -0,0 +1,183 @@
+<?php
+if (!isConnect('admin')) {
+    throw new Exception('{{401 - Accès non autorisé}}');
+}
+$eqLogic = eqLogic::byId(init('eqLogic_id'));
+if (!is_object($eqLogic)) {
+    throw new Exception('EqLogic non trouvé : ' . init('eqLogic_id'));
+}
+
+sendVarToJS('eqLogicInfo', utils::o2a($eqLogic));
+?>
+<div style="display: none;" id="md_displayEqLogicConfigure"></div>
+<div id='div_displayEqLogicConfigure'>
+    <div class="row">
+        <div class="col-sm-6" >
+            <form class="form-horizontal">
+                <fieldset>
+
+                    <div class="form-group">
+                        <label class="col-sm-4 control-label">{{ID}}</label>
+                        <div class="col-sm-4">
+                            <span class="eqLogicAttr label label-primary" data-l1key="id"></span>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-sm-4 control-label">{{Nom}}</label>
+                        <div class="col-sm-4">
+                            <span class="eqLogicAttr label label-primary" data-l1key="name"></span>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-sm-4 control-label">{{Logical ID}}</label>
+                        <div class="col-sm-4">
+                            <span class="eqLogicAttr label label-primary" data-l1key="logicalId"></span>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-sm-4 control-label">{{Object ID}}</label>
+                        <div class="col-sm-4">
+                            <span class="eqLogicAttr label label-primary" data-l1key="object_id"></span>
+                        </div>
+                    </div>
+
+                </fieldset>
+            </form>
+        </div>
+        <div class="col-sm-6" >
+            <form class="form-horizontal">
+                <fieldset>
+
+                    <div class="form-group">
+                        <label class="col-sm-4 control-label">{{Type}}</label>
+                        <div class="col-sm-4">
+                            <span class="eqLogicAttr label label-primary" data-l1key="eqType_name"></span>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-sm-4 control-label">{{Activer}}</label>
+                        <div class="col-sm-4">
+                            <span class="eqLogicAttr label label-primary" data-l1key="isEnable"></span>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-sm-4 control-label">{{Visible}}</label>
+                        <div class="col-sm-4">
+                            <span class="eqLogicAttr label label-primary" data-l1key="isVisible"></span>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-sm-4 control-label">{{Tentative échouée}}</label>
+                        <div class="col-sm-4">
+                            <span class="eqLogicAttr label label-primary" data-l1key="status" data-l2key="numberTryWithoutSuccess"></span>
+                        </div>
+                    </div>
+
+                </fieldset>
+            </form>
+
+        </div>
+
+        <legend>Configuration</legend>
+        <form class="form-horizontal">
+            <fieldset>
+                <div class="form-group">
+                    <label class="col-sm-4 control-label">{{Ordre}}</label>
+                    <div class="col-sm-2">
+                        <input type="number" class="eqLogicAttr form-control" data-l1key="order" />
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="col-sm-4 control-label">{{Ne pas afficher le nom de l\'équipement sur le dashboard}}</label>
+                    <div class="col-sm-1">
+                        <input type="checkbox" class="eqLogicAttr" data-l1key="display" data-l2key="doNotShowNameOnDashboard" />
+                    </div>
+                    <label class="col-lg-2 col-md-3 col-sm-4 col-xs-6 control-label">{{sur les vues}}</label>
+                    <div class="col-sm-1">
+                        <input type="checkbox" class="eqLogicAttr" data-l1key="display" data-l2key="doNotShowNameOnView" />
+                    </div>
+                </div>
+            </fieldset>
+        </form>
+        <legend>{{Paramètres optionnels sur la tuile}} <a class="btn btn-success btn-xs pull-right" id="bt_addWidgetParameters"><i class="fa fa-plus-circle"></i> Ajouter</a></legend>
+        <table class="table table-bordered table-condensed" id="table_widgetParameters">
+            <thead>
+                <tr>
+                    <th>Nom</th>
+                    <th>Valeur</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                if ($eqLogic->getDisplay('parameters') != '') {
+                    foreach ($eqLogic->getDisplay('parameters') as $key => $value) {
+                        echo '<tr>';
+                        echo '<td>';
+                        echo '<input class="form-control key" value="' . $key . '" />';
+                        echo '</td>';
+                        echo '<td>';
+                        echo '<input class="form-control value" value="' . $value . '" />';
+                        echo '</td>';
+                        echo '<td>';
+                        echo '<a class="btn btn-danger btn-xs removeWidgetParameter"><i class="fa fa-times"></i> Supprimer</a>';
+                        echo '</td>';
+                        echo '</tr>';
+                    }
+                }
+                ?>
+            </tbody>
+        </table>
+        <a class="btn btn-success" id="saveEqLogic"><i class="fa fa-check-circle"></i> {{Enregistrer}}</a>
+    </div>
+</div>
+<script>
+    $('#div_displayEqLogicConfigure').setValues(eqLogicInfo, '.eqLogicAttr');
+    $('#table_widgetParameters').delegate('.removeWidgetParameter', 'click', function () {
+        $(this).closest('tr').remove();
+    });
+
+    $('#bt_addWidgetParameters').off().on('click', function () {
+        var tr = '<tr>';
+        tr += '<td>';
+        tr += '<input class="form-control key" />';
+        tr += '</td>';
+        tr += '<td>';
+        tr += '<input class="form-control value" />';
+        tr += '</td>';
+        tr += '<td>';
+        tr += '<a class="btn btn-danger btn-xs removeWidgetParameter pull-right"><i class="fa fa-times"></i> Supprimer</a>';
+        tr += '</td>';
+        tr += '</tr>';
+        $('#table_widgetParameters tbody').append(tr);
+    });
+
+    $('#saveEqLogic').off().on('click', function () {
+        var eqLogic = $('#div_displayEqLogicConfigure').getValues('.eqLogicAttr')[0];
+        if (!isset(eqLogic.display)) {
+            eqLogic.display = {};
+        }
+        if (!isset(eqLogic.display.parameters)) {
+            eqLogic.display.parameters = {};
+        }
+        $('#table_widgetParameters tbody tr').each(function () {
+            eqLogic.display.parameters[$(this).find('.key').value()] = $(this).find('.value').value();
+        });
+        jeedom.eqLogic.save({
+            eqLogics: [eqLogic],
+            type: eqLogic.eqType_name,
+            error: function (error) {
+                $('#md_displayEqLogicConfigure').showAlert({message: error.message, level: 'danger'});
+            },
+            success: function () {
+                $('#md_displayEqLogicConfigure').showAlert({message: '{{Enregistrement réussi}}', level: 'success'});
+            }});
+    });
+</script>
