21f5cee25b81a2a4310a26022c0ddd5c6a2168c0
loic <lgevrey@oxya.com>
2014-12-18 11:55:42 +0100

    Add option on select modal

--- desktop/modal/cmd.human.insert.php
+++ desktop/modal/cmd.human.insert.php
@@ -37,11 +37,11 @@ if (!isConnect()) {
     mod_insertCmd.options.object = {};
 
 
-    $("#table_mod_insertCmdValue_valueEqLogicToMessage").delegate("td.mod_insertCmdValue_object select", 'change', function() {
+    $("#table_mod_insertCmdValue_valueEqLogicToMessage").delegate("td.mod_insertCmdValue_object select", 'change', function () {
         mod_insertCmd.changeObjectCmd($('#table_mod_insertCmdValue_valueEqLogicToMessage td.mod_insertCmdValue_object select'), mod_insertCmd.options);
     });
 
-    mod_insertCmd.setOptions = function(_options) {
+    mod_insertCmd.setOptions = function (_options) {
         mod_insertCmd.options = _options;
         if (!isset(mod_insertCmd.options.cmd)) {
             mod_insertCmd.options.cmd = {};
@@ -58,7 +58,7 @@ if (!isConnect()) {
         mod_insertCmd.changeObjectCmd($('#table_mod_insertCmdValue_valueEqLogicToMessage td.mod_insertCmdValue_object select'), mod_insertCmd.options);
     }
 
-    mod_insertCmd.getValue = function() {
+    mod_insertCmd.getValue = function () {
         var object_name = $('#table_mod_insertCmdValue_valueEqLogicToMessage tbody tr:first .mod_insertCmdValue_object select option:selected').html();
         var equipement_name = $('#table_mod_insertCmdValue_valueEqLogicToMessage tbody tr:first .mod_insertCmdValue_eqLogic select option:selected').html();
         var cmd_name = $('#table_mod_insertCmdValue_valueEqLogicToMessage tbody tr:first .mod_insertCmdValue_cmd select option:selected').html();
@@ -68,21 +68,23 @@ if (!isConnect()) {
         return '#[' + object_name + '][' + equipement_name + '][' + cmd_name + ']#';
     }
 
-    mod_insertCmd.changeObjectCmd = function(_select) {
+    mod_insertCmd.changeObjectCmd = function (_select) {
         jeedom.object.getEqLogic({
             id: _select.value(),
-            error: function(error) {
+            error: function (error) {
                 $('#div_alert').showAlert({message: error.message, level: 'danger'});
             },
-            success: function(eqLogics) {
+            success: function (eqLogics) {
                 _select.closest('tr').find('.mod_insertCmdValue_eqLogic').empty();
                 var selectEqLogic = '<select class="form-control">';
                 for (var i in eqLogics) {
-                    selectEqLogic += '<option value="' + eqLogics[i].id + '">' + eqLogics[i].name + '</option>';
+                    if (init(mod_insertCmd.options.eqLogic.eqType_name, 'all') == 'all' || eqLogics[i].eqType_name == mod_insertCmd.options.eqLogic.eqType_name){
+                        selectEqLogic += '<option value="' + eqLogics[i].id + '">' + eqLogics[i].name + '</option>';
+                    }
                 }
                 selectEqLogic += '</select>';
                 _select.closest('tr').find('.mod_insertCmdValue_eqLogic').append(selectEqLogic);
-                _select.closest('tr').find('.mod_insertCmdValue_eqLogic select').change(function() {
+                _select.closest('tr').find('.mod_insertCmdValue_eqLogic select').change(function () {
                     mod_insertCmd.changeEqLogic($(this), mod_insertCmd.options);
                 });
                 if (isset(mod_insertCmd.options.object.id)) {
@@ -94,14 +96,14 @@ if (!isConnect()) {
 
     }
 
-    mod_insertCmd.changeEqLogic = function(_select) {
+    mod_insertCmd.changeEqLogic = function (_select) {
         jeedom.eqLogic.builSelectCmd({
             id: _select.value(),
             filter: mod_insertCmd.options.cmd,
-            error: function(error) {
+            error: function (error) {
                 $('#div_alert').showAlert({message: error.message, level: 'danger'});
             },
-            success: function(html) {
+            success: function (html) {
                 _select.closest('tr').find('.mod_insertCmdValue_cmd').empty();
                 var selectCmd = '<select class="form-control">';
                 selectCmd += html;
