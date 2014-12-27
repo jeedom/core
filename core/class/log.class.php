27db8552c0f8c30ffc9a70a8cc074fbd418edc3c
Loic <loicgevrey@gmail.com>
2014-12-27 21:13:53 +0100

    Update log chunk for new scenario log

--- core/class/log.class.php
+++ core/class/log.class.php
@@ -65,33 +65,68 @@ class log {
             $logs = ls(dirname(__FILE__) . '/../../log/', '*');
             foreach ($logs as $log) {
                 $path = dirname(__FILE__) . '/../../log/' . $log;
-                if (filesize($path) > 20000) {
-                    if (filesize($path) > 1000000) {
-                        unlink($path);
-                        touch($path);
-                        continue;
+                if(is_dir($path)){
+                    if (filesize($path) > 20000) {
+                        if (filesize($path) > 1000000) {
+                            unlink($path);
+                            touch($path);
+                            continue;
+                        }
+                        $f = @fopen($path, "r+");
+                        if ($f !== false) {
+                            ftruncate($f, 20000);
+                            fclose($f);
+                        } else {
+                            unlink($path);
+                            touch($path);
+                            continue;
+                        }
                     }
-                    $f = @fopen($path, "r+");
-                    if ($f !== false) {
-                        ftruncate($f, 20000);
-                        fclose($f);
-                    } else {
-                        unlink($path);
-                        touch($path);
-                        continue;
+                    $log_file = file($path);
+                    if (count($log_file) > config::byKey('maxLineLog')) {
+                        $log_file = array_slice($log_file, count($log_file) - config::byKey('maxLineLog'));
+                        $log_txt = implode("", $log_file);
+                        $log = fopen($path, "w+");
+                        fwrite($log, $log_txt);
+                        fclose($log);
                     }
+                    @chown($path, 'www-data');
+                    @chgrp($path, 'www-data');
+                    @chmod($path, 0777);
                 }
-                $log_file = file($path);
-                if (count($log_file) > config::byKey('maxLineLog')) {
-                    $log_file = array_slice($log_file, count($log_file) - config::byKey('maxLineLog'));
-                    $log_txt = implode("", $log_file);
-                    $log = fopen($path, "w+");
-                    fwrite($log, $log_txt);
-                    fclose($log);
+            }
+            $logs = ls(dirname(__FILE__) . '/../../log/scenarioLog', '*');
+            foreach ($logs as $log) {
+                $path = dirname(__FILE__) . '/../../log/scenarioLog/' . $log;
+                if(is_dir($path)){
+                    if (filesize($path) > 20000) {
+                        if (filesize($path) > 1000000) {
+                            unlink($path);
+                            touch($path);
+                            continue;
+                        }
+                        $f = @fopen($path, "r+");
+                        if ($f !== false) {
+                            ftruncate($f, 20000);
+                            fclose($f);
+                        } else {
+                            unlink($path);
+                            touch($path);
+                            continue;
+                        }
+                    }
+                    $log_file = file($path);
+                    if (count($log_file) > config::byKey('maxLineLog')) {
+                        $log_file = array_slice($log_file, count($log_file) - config::byKey('maxLineLog'));
+                        $log_txt = implode("", $log_file);
+                        $log = fopen($path, "w+");
+                        fwrite($log, $log_txt);
+                        fclose($log);
+                    }
+                    @chown($path, 'www-data');
+                    @chgrp($path, 'www-data');
+                    @chmod($path, 0777);
                 }
-                @chown($path, 'www-data');
-                @chgrp($path, 'www-data');
-                @chmod($path, 0777);
             }
         }
     }
