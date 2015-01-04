89193627ea9927edf1ec6ce8e186ff329ddd1a68
Loic <loicgevrey@gmail.com>
2015-01-04 14:02:41 +0100

    Update install to new zway 2.0

--- install/install.sh
+++ install/install.sh
@@ -427,7 +427,7 @@ install_razberry_zway()
 	# Check if already installed
 	if [ -f /etc/z-way/VERSION ]; then
 		# Get latest zway install
-		wget -q -O - razberry.z-wave.me/install/v1.7.2 -O zway-install
+		wget -q -O - razberry.z-wave.me/install zway-install
 		# Check version
 		ZWAY_AVAIL_VERSION="`cat zway-install | awk '/.*[0-9].[0-9].[0-9].*z-way\/VERSION$/{ print $2 }' | sed 's/["v]//g'`"
 		is_version_greater_or_equal ${ZWAY_INSTALLED_VERSION} ${ZWAY_AVAIL_VERSION} 
@@ -468,7 +468,7 @@ done
 
 	# Common install/update
 	# Download installer, if not already done
-	[ ! -f zway-install ] && wget -q -O - razberry.z-wave.me/install/v1.7.2 -O zway-install
+	[ ! -f zway-install ] && wget -q -O - razberry.z-wave.me/install -O zway-install
 
 	# actual installation
 	bash zway-install
