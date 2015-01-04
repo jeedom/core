3d77a3d938cce1526b05550f23af8a158794fff8
Loic <loicgevrey@gmail.com>
2015-01-04 11:49:48 +0100

    Add info if scenario is in speed mode on log

--- desktop/modal/scenario.log.execution.php
+++ desktop/modal/scenario.log.execution.php
@@ -2,6 +2,13 @@
 if (!isConnect()) {
     throw new Exception('{{401 - Accès non autorisé}}');
 }
+$scenario = scenario::byId(init('scenario_id'));
+if(!is_object($scenario)){
+    throw new Exception(__('Aucun scénario correspondant à : ',__FILE__).init('scenario_id'));
+}
+if($scenario->getConfiguration('speedPriority',0) == 1){
+    echo '<div class="alert alert-warning">{{Ce scénario s\'éxécute le plus rapidement possible il n\'écris donc pas de log}}</div>';
+}
 ?>
 <div style="display: none;width : 100%" id="div_alertScenarioLog"></div>
 <a class="btn btn-danger pull-right" id="bt_scenarioLogEmpty"><i class="fa fa-trash"></i> Vider les logs</a>
@@ -13,7 +20,6 @@ if (file_exists(dirname(__FILE__) . '/../../log/scenarioLog/scenario' . init('sc
     echo "Aucun log n'existe pour votre scénario : " . dirname(__FILE__) . '/../../log/scenarioLog/scenario' . init('scenario_id') . '.log.';
 }
 ?>
-
 <script>
     $('#bt_scenarioLogEmpty').on('click', function () {
         jeedom.scenario.emptyLog({
