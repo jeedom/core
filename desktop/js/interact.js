fc99997ce27692244c7033ea9de7519b3d20a86a
loic <lgevrey@oxya.com>
2015-01-06 11:36:29 +0100

    UI fix

--- desktop/js/interact.js
+++ desktop/js/interact.js
@@ -34,10 +34,11 @@ $(".li_interact").on('click', function (event) {
             $('.interactAttr').value('');
             $(".interactAttr[data-l1key=link_type]").off();
             $('.interact').setValues(data, '.interactAttr');
-            changeLinkType(data);
+            //changeLinkType(data);
             $(".interactAttr[data-l1key=link_type]").on('change', function () {
                 changeLinkType({link_type: $(this).value()});
             });
+            $(".interactAttr[data-l1key=link_type]").trigger('change');
         }
     });
     return false;
