a51a209ab4c2cd74220e02ecbc60171de5d67f55
loic <lgevrey@oxya.com>
2015-03-02 14:57:24 +0100

    Add log scenario export

--- desktop/modal/scenario.log.execution.php
+++ desktop/modal/scenario.log.execution.php
@@ -1,29 +1,30 @@
 <?php
 if (!isConnect()) {
-    throw new Exception('{{401 - Accès non autorisé}}');
+	throw new Exception('{{401 - Accès non autorisé}}');
 }
 $scenario = scenario::byId(init('scenario_id'));
-if(!is_object($scenario)){
-    throw new Exception(__('Aucun scénario correspondant à : ',__FILE__).init('scenario_id'));
+if (!is_object($scenario)) {
+	throw new Exception(__('Aucun scénario correspondant à : ', __FILE__) . init('scenario_id'));
 }
-if($scenario->getConfiguration('speedPriority',0) == 1){
-    echo '<div class="alert alert-warning">{{Ce scénario s\'éxécute le plus rapidement possible il n\'écris donc pas de log}}</div>';
+if ($scenario->getConfiguration('speedPriority', 0) == 1) {
+	echo '<div class="alert alert-warning">{{Ce scénario s\'éxécute le plus rapidement possible il n\'écris donc pas de log}}</div>';
 }
 ?>
 <div style="display: none;width : 100%" id="div_alertScenarioLog"></div>
 <a class="btn btn-danger pull-right" id="bt_scenarioLogEmpty"><i class="fa fa-trash"></i> {{Vider les logs}}</a>
+<a class="btn btn-success pull-right" id="bt_scenarioLogDownload"><i class="fa fa-cloud-download"></i> {{Télécharger}}</a>
 <br/><br/>
 <?php
 if (file_exists(dirname(__FILE__) . '/../../log/scenarioLog/scenario' . init('scenario_id') . '.log')) {
-    echo '<pre id="pre_logScenarioDisplay">' . trim(file_get_contents(dirname(__FILE__) . '/../../log/scenarioLog/scenario' . init('scenario_id') . '.log')) . '</pre>';
+	echo '<pre id="pre_logScenarioDisplay">' . trim(file_get_contents(dirname(__FILE__) . '/../../log/scenarioLog/scenario' . init('scenario_id') . '.log')) . '</pre>';
 } else {
-    echo "{{Aucun log n'existe pour votre scénario : }}" . dirname(__FILE__) . '/../../log/scenarioLog/scenario' . init('scenario_id') . '.log.';
+	echo "{{Aucun log n'existe pour votre scénario : }}" . dirname(__FILE__) . '/../../log/scenarioLog/scenario' . init('scenario_id') . '.log.';
 }
 ?>
 <script>
     $('#bt_scenarioLogEmpty').on('click', function () {
         jeedom.scenario.emptyLog({
-            id: <?php echo init('scenario_id') ?>,
+            id: <?php echo init('scenario_id')?>,
             error: function (error) {
                 $('#div_alertScenarioLog').showAlert({message: error.message, level: 'danger'});
             },
@@ -32,6 +33,11 @@ if (file_exists(dirname(__FILE__) . '/../../log/scenarioLog/scenario' . init('sc
                 $('#pre_logScenarioDisplay').empty();
             }
         });
+    });
+
+
+    $('#bt_scenarioLogDownload').click(function() {
+        window.open('core/php/downloadFile.php?pathfile=log/scenarioLog/scenario<?php echo init('scenario_id')?>.log', "_blank", null);
     });
 </script>
 
