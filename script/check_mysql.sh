638957d5129b85b8456d896a481a0778c0f13f24
loic <lgevrey@oxya.com>
2015-03-05 09:27:50 +0100

    bugfix

--- script/check_mysql.sh
+++ script/check_mysql.sh
@@ -3,7 +3,7 @@ if [ -f /tmp/watchdog-mysql ]; then
     echo "Watchdog déja en cours"
 else
     echo "" > /tmp/watchdog-mysql
-    /etc/init.d/mysql status > /dev/null 2>&1
+    sudo /etc/init.d/mysql status > /dev/null 2>&1
     if [ $? -eq 3 ]; then
         echo "Mysql arreté je le redemarre"
         sudo /etc/init.d/mysql stop
