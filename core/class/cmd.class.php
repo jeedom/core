37a673139b962774b73b4f669245c087f43c595e
loic <lgevrey@oxya.com>
2015-03-17 14:28:10 +0100

    Add option to only event change value

--- core/class/cmd.class.php
+++ core/class/cmd.class.php
@@ -930,6 +930,9 @@ class cmd {
 		}
 		$_loop++;
 		$value = $this->formatValue($_value);
+		if ($this->getConfiguration('onlyChangeEvent', 0) == 1 && $this->getEventOnly() == 1 && $value == $this->execCmd(null, 2)) {
+			return;
+		}
 		log::add('event', 'event', __('Evenement sur la commande ', __FILE__) . $this->getHumanName() . __(' valeur : ', __FILE__) . $_value);
 		cache::set('cmd' . $this->getId(), $value, $this->getCacheLifetime(), array('collectDate' => $this->getCollectDate()));
 		scenario::check($this);
