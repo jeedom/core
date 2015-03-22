36ee0c8b64d8bcc26c71b2e2bb1026e71609266f
Loic <loicgevrey@gmail.com>
2015-03-22 15:58:11 +0100

    Improve restaure

--- install/restore.php
+++ install/restore.php
@@ -94,6 +94,7 @@ try {
 	if ($return_var != 0) {
 		throw new Exception('Impossible de décompresser l\'archive');
 	}
+	@unlink($tmp . '/core/config/apache_jeedom_dynamic_rules');
 	echo "OK\n";
 	if (!file_exists($tmp . "/DB_backup.sql")) {
 		throw new Exception('Impossible de trouver le fichier de sauvegarde de la base de données dans l\'archive : DB_backup.sql');
@@ -111,22 +112,26 @@ try {
 		DB::Prepare('DROP TABLE IF EXISTS `' . $table . '`', array(), DB::FETCH_TYPE_ROW);
 		echo "OK\n";
 	}
-	echo "Réactivation des contraintes...";
-	DB::Prepare("SET foreign_key_checks = 1", array(), DB::FETCH_TYPE_ROW);
-	echo "OK\n";
 
 	echo "Restauration de la base de donnees...";
 	system("mysql --host=" . $CONFIG['db']['host'] . " --user=" . $CONFIG['db']['username'] . " --password=" . $CONFIG['db']['password'] . " " . $CONFIG['db']['dbname'] . "  < " . $tmp . "/DB_backup.sql");
 	echo "OK\n";
 
+	echo "Réactivation des contraintes...";
+	try {
+		DB::Prepare("SET foreign_key_checks = 1", array(), DB::FETCH_TYPE_ROW);
+	} catch (Exception $e) {
+
+	}
+	echo "OK\n";
+
 	echo "Restauration des fichiers...";
 	rcopy($tmp, dirname(__FILE__) . '/..', false);
-	rcopy($tmp . '/plugins', dirname(__FILE__) . '/../plugins', false);
 	echo "OK\n";
 
 	if (!file_exists($jeedom_dir . '/install')) {
 		mkdir($jeedom_dir . '/install');
-		exec('cd ' . $jeedom_dir . '/install;wget http://git.jeedom.fr/jeedom/core/raw/stable/install/backup.php;wget http://git.jeedom.fr/jeedom/core/raw/stable/install/install.php;wget http://git.jeedom.fr/jeedom/core/raw/stable/install/restore.php');
+		exec('cd ' . $jeedom_dir . '/install;wget https://raw.githubusercontent.com/jeedom/core/master/install/backup.php;wget https://raw.githubusercontent.com/jeedom/core/master/install/install.php;wget https://raw.githubusercontent.com/jeedom/core/master/install/restore.php');
 	}
 
 	foreach (plugin::listPlugin(true) as $plugin) {
