4bc13757e5dca4f46d5b575aea926a34cf2996aa
Loic <loicgevrey@gmail.com>
2015-04-19 20:21:31 +0200

    UI improvement

--- desktop/php/plugin.php
+++ desktop/php/plugin.php
@@ -58,9 +58,10 @@ foreach ($plugins_list as $category_name => $category) {
 
 <?php
 foreach (plugin::listPlugin() as $plugin) {
+	$color = ($plugin->isActive()) ? '#767676' : '#FA5858';
 	echo '<div class="pluginDisplayCard cursor" data-pluginPath="' . $plugin->getFilepath() . '" data-plugin_id="' . $plugin->getId() . '" style="background-color : #ffffff; height : 140px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >';
 	echo "<center>";
-	echo '<i class="' . $plugin->getIcon() . '" style="font-size : 4em;color:#767676;"></i>';
+	echo '<i class="' . $plugin->getIcon() . '" style="font-size : 4em;color:' . $color . ';"></i>';
 	echo "</center>";
 	echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $plugin->getName() . '</center></span>';
 	echo '</div>';
