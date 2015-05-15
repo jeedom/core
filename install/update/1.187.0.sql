2b62b41a3c3e10a02bea30ea81769002110c878c
loic <lgevrey@oxya.com>
2015-05-15 21:09:03 +0200

    Fix update

--- install/update/1.187.0.sql
+++ install/update/1.187.0.sql
@@ -1,4 +1,2 @@
-UPDATE `cron` SET `lastrun`="2015-01-01 00:00:00" WHERE `lastrun`="0000-00-00 00:00:00"
-
-ALTER TABLE `cron`
-CHANGE `deamonSleepTime` `deamonSleepTime` float NULL AFTER `deamon`;
+UPDATE `cron` SET `lastrun`="2015-01-01 00:00:00" WHERE `lastrun`="0000-00-00 00:00:00";
+ALTER TABLE `cron` CHANGE `deamonSleepTime` `deamonSleepTime` float NULL;
