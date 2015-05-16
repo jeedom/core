9d0746f908fa8899aa8837007ec8e6fca71ce989
loic <lgevrey@oxya.com>
2015-05-16 23:13:21 +0200

    Add update dependency

--- install/update/1.186.0.php
+++ install/update/1.186.0.php
@@ -1,4 +1,7 @@
 <?php
+if (!file_exists('/usr/bin/node')) {
+	exec('sudo ln -s /usr/bin/nodejs /usr/bin/node');
+}
 echo 'Installation/Mise à jour npm...';
 exec('sudo npm install -y npm > /dev/null 2>&1');
 echo "OK\n";
