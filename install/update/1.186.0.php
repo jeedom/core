e6449bb204a2ae04f59993e60f8a8cd6c565e3ed
loic <lgevrey@oxya.com>
2015-05-18 19:45:22 +0200

    Improve update

--- install/update/1.186.0.php
+++ install/update/1.186.0.php
@@ -3,6 +3,10 @@ if (!file_exists('/usr/bin/node')) {
 	exec('sudo ln -s /usr/bin/nodejs /usr/bin/node');
 }
 echo 'Installation/Mise à jour npm...';
-exec('sudo npm install -y npm > /dev/null 2>&1');
+exec('sudo apt-get install -y npm > /dev/null 2>&1');
 echo "OK\n";
+echo 'Installation/Mise à jour curl...';
+exec('sudo apt-get install -y curl > /dev/null 2>&1');
+echo "OK\n";
+?>
 ?>
