0bc16712a9f9ef18543a9d292a4f28610be98950
loic <lgevrey@oxya.com>
2015-05-21 23:17:33 +0200

    Bugfix

--- desktop/js/update.js
+++ desktop/js/update.js
@@ -36,30 +36,30 @@
 });
 
  $('#bt_reapplySpecifyUpdate').on('click',function(){
-     var level = "-1";
-     var mode = '';
-     if($('#cb_forceReapplyUpdate').value() == 1){
-        mode = 'force';
-    }
-    jeedom.update.doAll({
-        mode: mode,
-        level: level,
-        version : $('#sel_updateVersion').value(),
-        onlyThisVersion : ($('#cb_allFromThisUpdate').value() == 1) ? 'no':'yes',
-        error: function (error) {
-            $('#div_alert').showAlert({message: error.message, level: 'danger'});
-        },
-        success: function () {
-           $("#md_specifyUpdate").dialog('close');
-           getJeedomLog(1, 'update');
-       }
-   });
+   var level = "-1";
+   var mode = '';
+   if($('#cb_forceReapplyUpdate').value() == 1){
+    mode = 'force';
+}
+jeedom.update.doAll({
+    mode: mode,
+    level: level,
+    version : $('#sel_updateVersion').value(),
+    onlyThisVersion : ($('#cb_allFromThisUpdate').value() == 1) ? 'no':'yes',
+    error: function (error) {
+        $('#div_alert').showAlert({message: error.message, level: 'danger'});
+    },
+    success: function () {
+     $("#md_specifyUpdate").dialog('close');
+     getJeedomLog(1, 'update');
+ }
+});
 });
 
  $('#bt_allChangelog').on('click', function () {
-     $('#md_modal2').dialog({title: "{{Changelog}}"});
-     $("#md_modal2").load('index.php?v=d&modal=market.allChangelog').dialog('open');
- });
+   $('#md_modal2').dialog({title: "{{Changelog}}"});
+   $("#md_modal2").load('index.php?v=d&modal=market.allChangelog').dialog('open');
+});
 
  $('.bt_updateAll').on('click', function () {
   var level = $(this).attr('data-level');
@@ -188,17 +188,19 @@
             }
             var log = '';
             var regex = /<br\s*[\/]?>/gi;
-            for (var i in data.result.reverse()) {
-                log += data.result[i][2].replace(regex, "\n");
-                if ($.trim(data.result[i][2].replace(regex, "\n")) == '[END ' + _log.toUpperCase() + ' SUCCESS]') {
-                    printUpdate();
-                    $('#div_alert').showAlert({message: '{{L\'opération est réussie}}', level: 'success'});
-                    _autoUpdate = 0;
-                }
-                if ($.trim(data.result[i][2].replace(regex, "\n")) == '[END ' + _log.toUpperCase() + ' ERROR]') {
-                    printUpdate();
-                    $('#div_alert').showAlert({message: '{{L\'opération a échoué}}', level: 'danger'});
-                    _autoUpdate = 0;
+            if($.isArray(data.result)){
+                for (var i in data.result.reverse()) {
+                    log += data.result[i][2].replace(regex, "\n");
+                    if ($.trim(data.result[i][2].replace(regex, "\n")) == '[END ' + _log.toUpperCase() + ' SUCCESS]') {
+                        printUpdate();
+                        $('#div_alert').showAlert({message: '{{L\'opération est réussie}}', level: 'success'});
+                        _autoUpdate = 0;
+                    }
+                    if ($.trim(data.result[i][2].replace(regex, "\n")) == '[END ' + _log.toUpperCase() + ' ERROR]') {
+                        printUpdate();
+                        $('#div_alert').showAlert({message: '{{L\'opération a échoué}}', level: 'danger'});
+                        _autoUpdate = 0;
+                    }
                 }
             }
             $('#pre_' + _log + 'Info').text(log);
