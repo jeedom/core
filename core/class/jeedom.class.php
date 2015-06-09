12d92fe08cb75abc3492ebe82bed3145637a926a
loic <lgevrey@oxya.com>
2015-06-09 19:24:45 +0200

    Add ttyS0

--- core/class/jeedom.class.php
+++ core/class/jeedom.class.php
@@ -147,8 +147,8 @@ class jeedom {
 				if (file_exists('/dev/S2')) {
 					$usbMapping['Banana PI'] = '/dev/S2';
 				}
-				if (file_exists('/dev/S0')) {
-					$usbMapping['Cubiboard'] = '/dev/S0';
+				if (file_exists('/dev/ttyS0')) {
+					$usbMapping['Cubiboard'] = '/dev/ttyS0';
 				}
 				foreach (ls('/dev/', 'ttyACM*') as $value) {
 					$usbMapping['/dev/' . $value] = '/dev/' . $value;
