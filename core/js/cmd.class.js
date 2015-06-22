1f6d8b7df692c8534468ddfa7cc7566053b12544
Loic <loicgevrey@gmail.com>
2015-06-22 20:34:20 +0200

    Bugfix UI

--- core/js/cmd.class.js
+++ core/js/cmd.class.js
@@ -37,11 +37,11 @@ jeedom.cmd.execute = function(_params) {
             if (data.state != 'ok') {
                 if(data.code == -32005){
                     if ($.mobile) {
-                     var result = prompt("Veuillez indiquer le code ?", "")
-                     if(result != null){
-                         _params.codeAccess = result;
-                         jeedom.cmd.execute(_params);
-                     }else{
+                       var result = prompt("Veuillez indiquer le code ?", "")
+                       if(result != null){
+                           _params.codeAccess = result;
+                           jeedom.cmd.execute(_params);
+                       }else{
                         if ('function' != typeof(_params.error)) {
                             $('#div_alert').showAlert({
                                 message: data.result,
@@ -57,11 +57,11 @@ jeedom.cmd.execute = function(_params) {
                         return data;
                     }
                 }else{
-                 bootbox.prompt("Veuillez indiquer le code ?", function (result) {
+                   bootbox.prompt("Veuillez indiquer le code ?", function (result) {
                     if(result != null){
-                     _params.codeAccess = result;
-                     jeedom.cmd.execute(_params);
-                 }else{
+                       _params.codeAccess = result;
+                       jeedom.cmd.execute(_params);
+                   }else{
                     if ('function' != typeof(_params.error)) {
                         $('#div_alert').showAlert({
                             message: data.result,
@@ -78,11 +78,11 @@ jeedom.cmd.execute = function(_params) {
                 }
 
             });
-             }
-         }else if(data.code == -32006){
-             if ($.mobile) {
-                 var result = confirm("Etes vous sur de vouloir faire cette action ?")
-                 if(result){
+               }
+           }else if(data.code == -32006){
+               if ($.mobile) {
+                   var result = confirm("Etes vous sur de vouloir faire cette action ?")
+                   if(result){
                     _params.confirmAction = 1;
                     jeedom.cmd.execute(_params);
                 }else{
@@ -103,9 +103,9 @@ jeedom.cmd.execute = function(_params) {
             }else{
                 bootbox.confirm("Etes vous sur de vouloir faire cette action ?", function (result) {
                     if(result){
-                     _params.confirmAction = 1;
-                     jeedom.cmd.execute(_params);
-                 }else{
+                       _params.confirmAction = 1;
+                       jeedom.cmd.execute(_params);
+                   }else{
                     if ('function' != typeof(_params.error)) {
                         $('#div_alert').showAlert({
                             message: data.result,
@@ -479,70 +479,86 @@ jeedom.cmd.changeSubType = function(_cmd) {
                         el = el.parent();
                     }
                     if (subtype[i].visible) {
-                        el.show();
-                        el.removeClass('hide');
-                    } else {
-                        el.hide();
-                        el.addClass('hide');
-                    }
-                    if (isset(subtype[i].parentVisible)) {
-                        if (subtype[i].parentVisible) {
-                            el.parent().show();
-                            el.parent().removeClass('hide');
-                        } else {
-                            el.parent().hide();
-                            el.parent().addClass('hide');
-                        }
-                    }
+                       if(el.hasClass('bootstrapSwitch')){
+                           el.parent().parent().show();
+                           el.parent().parent().removeClass('hide');
+                       }
+                       el.show();
+                       el.removeClass('hide');
+                   } else {
+                    if(el.hasClass('bootstrapSwitch')){
+                     el.parent().parent().hide();
+                     el.parent().parent().addClass('hide');
+                 }
+                 el.hide();
+                 el.addClass('hide');
+             }
+             if (isset(subtype[i].parentVisible)) {
+                if (subtype[i].parentVisible) {
+                    el.parent().show();
+                    el.parent().removeClass('hide');
                 } else {
-                    for (var j in subtype[i]) {
-                        var el = _cmd.find('.cmdAttr[data-l1key=' + i + '][data-l2key=' + j + ']');
-                        if (el.attr('type') == 'checkbox' && el.parent().is('span')) {
-                            el = el.parent();
-                        }
-                        if (isset(subtype[i][j].visible)) {
-                            if (subtype[i][j].visible) {
-                                el.show();
-                                el.removeClass('hide');
-                            } else {
-                                el.hide();
-                                el.addClass('hide');
-                            }
-                        }
-                        if (isset(subtype[i][j].parentVisible)) {
-                            if (subtype[i][j].parentVisible) {
-                                el.parent().show();
-                                el.parent().removeClass('hide');
-                            } else {
-                                el.parent().hide();
-                                el.parent().addClass('hide');
-                            }
-                        }
-                    }
+                    el.parent().hide();
+                    el.parent().addClass('hide');
                 }
             }
-
-            if (_cmd.find('.cmdAttr[data-l1key=type]').value() == 'action') {
-             if (_cmd.find('.cmdAttr[data-l1key=subType]').value() == 'slider' || _cmd.find('.cmdAttr[data-l1key=subType]').value() == 'other' || _cmd.find('.cmdAttr[data-l1key=subType]').value() == 'color') {
-                _cmd.find('.cmdAttr[data-l1key=value]').show();
+        } else {
+            for (var j in subtype[i]) {
+                var el = _cmd.find('.cmdAttr[data-l1key=' + i + '][data-l2key=' + j + ']');
+                if (el.attr('type') == 'checkbox' && el.parent().is('span')) {
+                    el = el.parent();
+                }
+                if (isset(subtype[i][j].visible)) {
+                    if (subtype[i][j].visible) {
+                        if(el.hasClass('bootstrapSwitch')){
+                          el.parent().parent().show();
+                          el.parent().parent().removeClass('hide');
+                      }
+                      el.show();
+                      el.removeClass('hide');
+                  } else {
+                    if(el.hasClass('bootstrapSwitch')){
+                       el.parent().parent().hide();
+                       el.parent().parent().addClass('hide');
+                   }
+                   el.hide();
+                   el.addClass('hide');
+               }
+           }
+           if (isset(subtype[i][j].parentVisible)) {
+            if (subtype[i][j].parentVisible) {
+                el.parent().show();
+                el.parent().removeClass('hide');
+            } else {
+                el.parent().hide();
+                el.parent().addClass('hide');
             }
-            _cmd.find('.cmdAttr[data-l1key=configuration][data-l2key=updateCmdId]').show();
-            _cmd.find('.cmdAttr[data-l1key=configuration][data-l2key=updateCmdToValue]').show();
-            _cmd.find('.cmdAttr[data-l1key=configuration][data-l2key=returnStateValue]').hide();
-            _cmd.find('.cmdAttr[data-l1key=configuration][data-l2key=returnStateTime]').hide();
-        }else{
-         _cmd.find('.cmdAttr[data-l1key=configuration][data-l2key=returnStateValue]').show();
-         _cmd.find('.cmdAttr[data-l1key=configuration][data-l2key=returnStateTime]').show();
-         _cmd.find('.cmdAttr[data-l1key=value]').hide();
-         _cmd.find('.cmdAttr[data-l1key=configuration][data-l2key=updateCmdId]').hide();
-         _cmd.find('.cmdAttr[data-l1key=configuration][data-l2key=updateCmdToValue]').hide();
-     }
-     _cmd.find('.cmdAttr[data-l1key=eventOnly]').trigger('change');
-      _cmd.find('.cmdAttr[data-l1key=eventOnly]').trigger('switchChange.bootstrapSwitch');
-     modifyWithoutSave = false;
-     if ('function' == typeof(initExpertMode)) {
-        initExpertMode();
+        }
     }
+}
+}
+
+if (_cmd.find('.cmdAttr[data-l1key=type]').value() == 'action') {
+   if (_cmd.find('.cmdAttr[data-l1key=subType]').value() == 'slider' || _cmd.find('.cmdAttr[data-l1key=subType]').value() == 'other' || _cmd.find('.cmdAttr[data-l1key=subType]').value() == 'color') {
+    _cmd.find('.cmdAttr[data-l1key=value]').show();
+}
+_cmd.find('.cmdAttr[data-l1key=configuration][data-l2key=updateCmdId]').show();
+_cmd.find('.cmdAttr[data-l1key=configuration][data-l2key=updateCmdToValue]').show();
+_cmd.find('.cmdAttr[data-l1key=configuration][data-l2key=returnStateValue]').hide();
+_cmd.find('.cmdAttr[data-l1key=configuration][data-l2key=returnStateTime]').hide();
+}else{
+   _cmd.find('.cmdAttr[data-l1key=configuration][data-l2key=returnStateValue]').show();
+   _cmd.find('.cmdAttr[data-l1key=configuration][data-l2key=returnStateTime]').show();
+   _cmd.find('.cmdAttr[data-l1key=value]').hide();
+   _cmd.find('.cmdAttr[data-l1key=configuration][data-l2key=updateCmdId]').hide();
+   _cmd.find('.cmdAttr[data-l1key=configuration][data-l2key=updateCmdToValue]').hide();
+}
+_cmd.find('.cmdAttr[data-l1key=eventOnly]').trigger('change');
+_cmd.find('.cmdAttr[data-l1key=eventOnly]').trigger('switchChange.bootstrapSwitch');
+modifyWithoutSave = false;
+if ('function' == typeof(initExpertMode)) {
+    initExpertMode();
+}
 }
 });
 };
