423ec67ff3d540352d242bfe03231124dad2b867
Loic <loicgevrey@gmail.com>
2015-06-29 21:21:39 +0200

    Remove connect label

--- desktop/php/connection.php
+++ desktop/php/connection.php
@@ -16,7 +16,6 @@ foreach ($_GET AS $var => $value) {
 }
 ?>
             <form method="post" name="login" action="index.php?v=d<?php echo htmlspecialchars($getParams);?>" style="position : relative;top : -80px;" class="form-signin">
-                <h2 class="form-signin-heading">{{Connectez-vous}}</h2>
                 <input type="text" name="connect" id="connect" hidden value="1" style="display: none;"/>
                 <input class="input-block-level" type="text" name="login" id="login" placeholder="{{Nom d'utilisateur}}"/><br/>
                 <br/><input class="input-block-level" type="password" id="mdp" name="mdp" placeholder="{{Mot de passe}}"/><br/>
