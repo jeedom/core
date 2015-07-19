d0c947172a32894e183f7701d62ce77ae5889da8
Loic <loicgevrey@gmail.com>
2015-07-19 21:47:43 +0200

    Fix

--- install/update/1.199.0.php
+++ install/update/1.199.0.php
@@ -12,7 +12,7 @@ if ($hwname == 'RPI/RPI2') {
 	echo "OK\n";
 } else {
 	echo 'Ajout dépot nodejs v12...';
-	exec("sufo sed -i '/deb http:\/\/repo.gbps.io\/BSP:\/Cubox-i\/Debian_Jessie\/ .\//d' /etc/apt/sources.list");
+	exec("sudo sed -i '/deb http:\/\/repo.gbps.io\/BSP:\/Cubox-i\/Debian_Jessie\/ .\//d' /etc/apt/sources.list");
 	exec('curl -sL https://deb.nodesource.com/setup_0.12 | sudo bash -');
 	echo "OK\n";
 	echo 'Mise à jour des depots';
