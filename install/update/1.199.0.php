8540d3fc06c08899b969d8660f2ebb02ed967ee1
Loic <loicgevrey@gmail.com>
2015-07-19 21:45:07 +0200

    Add nodejs update

--- install/update/1.199.0.php
+++ install/update/1.199.0.php
@@ -1,19 +1,25 @@
 <?php
 $hwname = jeedom::getHardwareName();
-echo 'Ajout dépot nodejs v12...';
 if ($hwname == 'RPI/RPI2') {
-
+	echo 'Ajout dépot nodejs v12 pour RPI...';
+	exec('curl -sLS https://apt.adafruit.com/add | sudo bash');
+	echo "OK\n";
+	echo 'Mise à jour des depots';
+	exec('sudo apt-get update');
+	echo "OK\n";
+	echo 'Mise à jour nodejs';
+	exec('sudo apt-get install node');
+	echo "OK\n";
 } else {
+	echo 'Ajout dépot nodejs v12...';
 	exec("sufo sed -i '/deb http:\/\/repo.gbps.io\/BSP:\/Cubox-i\/Debian_Jessie\/ .\//d' /etc/apt/sources.list");
 	exec('curl -sL https://deb.nodesource.com/setup_0.12 | sudo bash -');
-
+	echo "OK\n";
+	echo 'Mise à jour des depots';
+	exec('sudo apt-get update');
+	echo "OK\n";
+	echo 'Mise à jour nodejs';
+	exec('sudo apt-get install nodejs');
+	echo "OK\n";
 }
-echo "OK\n";
-echo 'Mise à jour des depots';
-exec('sudo apt-get update');
-echo "OK\n";
-echo 'Mise à jour nodejs';
-exec('sudo apt-get install nodejs');
-echo "OK\n";
-?>
 ?>
