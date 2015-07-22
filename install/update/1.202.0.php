c83e8e8316369ca87c14bcf5c2f1c7368eb8a8ca
loic <lgevrey@oxya.com>
2015-07-22 18:42:44 +0200

    Fix sudo

--- install/update/1.202.0.php
+++ install/update/1.202.0.php
@@ -1,6 +1,6 @@
 <?php
 echo 'Mise à jour de socket.io et express (peut etre long > 30min)';
-echo shell_exec('cd ' . dirname(__FILE__) . '/../../core/nodeJS;rm -rf node_modules;sudo npm install socket.io;npm install express');
+echo shell_exec('cd ' . dirname(__FILE__) . '/../../core/nodeJS;rm -rf node_modules;sudo npm install socket.io;sudo npm install express');
 echo shell_exec('sudo service jeedom restart');
 echo "OK\n";
 ?>
