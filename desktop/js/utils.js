c7c6377f793a912af472e8d2decfa8534353f18a
Loic <loicgevrey@gmail.com>
2015-07-26 11:00:08 +0200

    UI improvement

--- desktop/js/utils.js
+++ desktop/js/utils.js
@@ -310,7 +310,7 @@ function initRowOverflow() {
     if ($(window).width() < 1180) {
         $('.row-overflow > div').css('height', 'auto').css('overflow-y', 'initial').css('overflow-x', 'initial');
     } else {
-        var hWindow = $(window).height() - $('header').height() - 10;
+        var hWindow = $(window).height() - $('header').height() - 45;
         $('.row-overflow > div').height(hWindow).css('overflow-y', 'auto').css('overflow-x', 'hidden');
     }
 }
