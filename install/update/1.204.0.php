bb2a0318320d9426de7dcb096931df197d0ea015
loic <lgevrey@oxya.com>
2015-08-12 11:48:33 +0200

    Fix update

--- install/update/1.204.0.php
+++ install/update/1.204.0.php
@@ -5,6 +5,6 @@ if (jeedom::getHardwareName() == 'Jeedomboard' && jeedom::isCapable('sudo')) {
 	echo "Update depot";
 	exec('sudo apt-get update');
 	echo "Install nodejs 0.10.38";
-	exec('sudo apt-get -y install nodejs=0.10.38~dfsg-1');
+	exec('sudo apt-get --reinstall install nodejs');
 }
 ?>
