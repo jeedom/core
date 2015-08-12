6bff57b6c9114aa78081f9af9b0e8459f6cc083e
loic <lgevrey@oxya.com>
2015-08-12 14:40:09 +0200

    Readd npm

--- install/update/1.204.0.php
+++ install/update/1.204.0.php
@@ -4,7 +4,8 @@ if (jeedom::getHardwareName() == 'Jeedomboard' && jeedom::isCapable('sudo')) {
 	exec('sudo rm /etc/apt/sources.list.d/nodesource.list');
 	echo "Update depot";
 	exec('sudo apt-get update');
-	echo "Install nodejs 0.10.38";
+	echo "Install nodejs 0.10.XX";
 	exec('sudo apt-get --reinstall install -y --force-yes nodejs=$(apt-cache madison nodejs | head -n 1 | cut -f 2 -d "|" | xargs)');
+	exec('sudo apt-get --reinstall install -y --force-yes npm');
 }
 ?>
