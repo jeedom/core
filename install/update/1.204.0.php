5c90832e7f644d8ce4d93595fb5eb989442103fb
loic <lgevrey@oxya.com>
2015-08-12 12:03:36 +0200

    Fix downgrade nodejs

--- install/update/1.204.0.php
+++ install/update/1.204.0.php
@@ -5,6 +5,6 @@ if (jeedom::getHardwareName() == 'Jeedomboard' && jeedom::isCapable('sudo')) {
 	echo "Update depot";
 	exec('sudo apt-get update');
 	echo "Install nodejs 0.10.38";
-	exec('sudo apt-get --reinstall install nodejs');
+	exec('sudo apt-get --reinstall install -y --force-yes nodejs=$(apt-cache madison nodejs | head -n 1 | cut -f 2 -d "|" | xargs)');
 }
 ?>
