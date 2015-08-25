332c7f723c160724a1e22bf0c5b06246313cc0bf
loic <lgevrey@oxya.com>
2015-08-25 16:24:55 +0200

    UI improvement

--- desktop/php/scenarioAssist.php
+++ desktop/php/scenarioAssist.php
@@ -78,37 +78,37 @@ if (is_array($scenarioListGroup)) {
 
 <div id="scenarioThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
 
-   <div class="scenarioListContainer">
-       <legend>{{Gestion}}</legend>
-       <div class="cursor" id="bt_addScenario2" style="background-color : #ffffff; height : 100px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
-         <center>
-            <i class="fa fa-plus-circle" style="font-size : 4em;color:#94ca02;"></i>
-        </center>
-        <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02"><center>{{Ajouter}}</center></span>
-    </div>
-    <?php if (config::byKey('enableScenario') == 0) {?>
-        <div class="cursor expertModeVisible" id="bt_changeAllScenarioState2" data-state="1" style="background-color : #ffffff; height : 100px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
-         <center>
-             <i class="fa fa-check" style="font-size : 4em;color:#5cb85c;"></i>
-         </center>
-         <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#5cb85c"><center>{{Activer scénarios}}</center></span>
-     </div>
-     <?php } else {?>
+ <div class="scenarioListContainer">
+     <legend>{{Gestion}}</legend>
+     <div class="cursor" id="bt_addScenario2" style="background-color : #ffffff; height : 100px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
+       <center>
+        <i class="fa fa-plus-circle" style="font-size : 4em;color:#94ca02;"></i>
+    </center>
+    <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02"><center>{{Ajouter}}</center></span>
+</div>
+<?php if (config::byKey('enableScenario') == 0) {?>
+    <div class="cursor expertModeVisible" id="bt_changeAllScenarioState2" data-state="1" style="background-color : #ffffff; height : 100px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
+       <center>
+           <i class="fa fa-check" style="font-size : 4em;color:#5cb85c;"></i>
+       </center>
+       <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#5cb85c"><center>{{Activer scénarios}}</center></span>
+   </div>
+   <?php } else {?>
    <div class="cursor expertModeVisible" id="bt_changeAllScenarioState2" data-state="0" style="background-color : #ffffff; height : 100px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
-         <center>
-             <i class="fa fa-times" style="font-size : 4em;color:#d9534f;"></i>
-         </center>
-         <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#d9534f"><center>{{Désactiver scénarios}}</center></span>
-     </div>
-     <?php }
+       <center>
+           <i class="fa fa-times" style="font-size : 4em;color:#d9534f;"></i>
+       </center>
+       <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#d9534f"><center>{{Désactiver scénarios}}</center></span>
+   </div>
+   <?php }
 ?>
 
-     <div class="cursor expertModeVisible" id="bt_displayScenarioVariable2" style="background-color : #ffffff; height : 100px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
-         <center>
-            <i class="fa fa-eye" style="font-size : 4em;color:#337ab7;"></i>
-        </center>
-        <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#337ab7"><center>{{Voir variables}}</center></span>
-    </div>
+   <div class="cursor expertModeVisible" id="bt_displayScenarioVariable2" style="background-color : #ffffff; height : 100px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
+       <center>
+        <i class="fa fa-eye" style="font-size : 4em;color:#337ab7;"></i>
+    </center>
+    <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#337ab7"><center>{{Voir variables}}</center></span>
+</div>
 </div>
 
 <legend>{{Mes scénarios}}</legend>
@@ -258,91 +258,93 @@ foreach ($scenarioListGroup as $group) {
                     </div>
                     <div class="form-group mode provoke trigger">
                         <label class="col-xs-3 control-label" >{{Par}}</label>
-                        <div class="col-xs-7">
+                        <div class="col-xs-9">
+                           <div class="input-group">
                             <input class="scenarioAttr form-control" data-l1key="trigger" disabled>
-                        </div>
-                        <div class="col-xs-2">
-                            <a class="btn btn-default cursor" id="bt_selectTrigger"><i class="fa fa-list-alt"></i></a>
-                        </div>
-                    </div>
-                    <hr/>
-                    <div class="form-group">
-                        <label class="col-xs-3 control-label" >{{Condition optionnelle}}</label>
-                        <div class="col-xs-2">
-                            <input type="checkbox" class="bootstrapSwitch" id="cb_conditionStart" />
+                            <span class="input-group-btn">
+                                <a class="btn btn-default cursor" id="bt_selectTrigger"><i class="fa fa-list-alt"></i></a>
+                            </span>
                         </div>
                     </div>
-                    <div class="form-group condition" style="display: none;">
-                        <label class="col-xs-3 control-label" >{{Si}}</label>
-                        <div class="col-xs-7">
-                            <input class="form-control" id="in_cmdCondition" disabled>
+                </div>
+                <hr/>
+                <div class="form-group">
+                    <label class="col-xs-3 control-label" >{{Condition optionnelle}}</label>
+                    <div class="col-xs-2">
+                        <input type="checkbox" class="bootstrapSwitch" id="cb_conditionStart" />
+                    </div>
+                </div>
+                <div class="form-group condition" style="display: none;">
+                    <label class="col-xs-3 control-label" >{{Si}}</label>
+                    <div class="col-xs-7">
+                        <input class="form-control" id="in_cmdCondition" disabled>
+                    </div>
+                    <div class="col-xs-2">
+                        <a class="btn btn-default cursor" id="bt_selectCondition"><i class="fa fa-list-alt"></i></a>
+                    </div>
+                </div>
+                <div class="form-group condition" style="display: none;">
+                    <label class="col-xs-3 control-label" >{{Est}}</label>
+                    <div class='conditionOptions' data-type="info" data-subtype="numeric" style="display : none;">
+                        <div class="col-xs-4">
+                            <select class="conditionAttr form-control" data-l1key="operator">
+                                <option value="==">{{égal}}</option>
+                                <option value=">">{{supérieur}}</option>
+                                <option value="<">{{inférieur}}</option>
+                                <option value="!=">{{différent}}</option>
+                            </select>
                         </div>
-                        <div class="col-xs-2">
-                            <a class="btn btn-default cursor" id="bt_selectCondition"><i class="fa fa-list-alt"></i></a>
+                        <div class="col-xs-5">
+                            <input type="number" class="conditionAttr form-control" data-l1key="operande" />
                         </div>
                     </div>
-                    <div class="form-group condition" style="display: none;">
-                        <label class="col-xs-3 control-label" >{{Est}}</label>
-                        <div class='conditionOptions' data-type="info" data-subtype="numeric" style="display : none;">
-                            <div class="col-xs-4">
-                                <select class="conditionAttr form-control" data-l1key="operator">
-                                    <option value="==">{{égal}}</option>
-                                    <option value=">">{{supérieur}}</option>
-                                    <option value="<">{{inférieur}}</option>
-                                    <option value="!=">{{différent}}</option>
-                                </select>
-                            </div>
-                            <div class="col-xs-5">
-                                <input type="number" class="conditionAttr form-control" data-l1key="operande" />
-                            </div>
+                    <div class='conditionOptions' data-type="info" data-subtype="string" style="display : none;">
+                        <div class="col-xs-4">
+                            <select class="conditionAttr form-control" data-l1key="operator">
+                                <option value="==">{{égal}}</option>
+                                <option value="!=">{{différent}}</option>
+                                <option value="~">{{contient}}</option>
+                                <option value="!~">{{ne contient pas}}</option>
+                            </select>
                         </div>
-                        <div class='conditionOptions' data-type="info" data-subtype="string" style="display : none;">
-                            <div class="col-xs-4">
-                                <select class="conditionAttr form-control" data-l1key="operator">
-                                    <option value="==">{{égal}}</option>
-                                    <option value="!=">{{différent}}</option>
-                                    <option value="~">{{contient}}</option>
-                                    <option value="!~">{{ne contient pas}}</option>
-                                </select>
-                            </div>
-                            <div class="col-xs-5">
-                                <input class="conditionAttr form-control" data-l1key="operande" />
-                            </div>
-                        </div>
-                        <div class='conditionOptions' data-type="info" data-subtype="binary" style="display : none;">
-                            <div class="col-xs-9">
-                                <input class="conditionAttr" data-l1key="operator" value="==" style="display : none;" />
-                                <select class="conditionAttr form-control" data-l1key="operande">
-                                    <option value="1">{{Ouvert}}</option>
-                                    <option value="0">{{Fermé}}</option>
-                                    <option value="1">{{Allumé}}</option>
-                                    <option value="0">{{Eteint}}</option>
-                                    <option value="1">{{Déclenché}}</option>
-                                    <option value="0">{{Au repos}}</option>
-                                </select>
-                            </div>
+                        <div class="col-xs-5">
+                            <input class="conditionAttr form-control" data-l1key="operande" />
                         </div>
                     </div>
-                </form>
-            </div>
-        </div>
-        <div class="col-sm-6">
-            <div class="well">
-                <form class="form-horizontal">
-                    <legend>3) Actions
-                        <a class="btn btn-xs pull-right btn-success" id="bt_addAction"><i class="fa fa-plus-circle"></i> Ajouter une action</a>
-                    </legend>
-                    <div id="div_actionList"></div>
-                </form>
-            </div>
+                    <div class='conditionOptions' data-type="info" data-subtype="binary" style="display : none;">
+                        <div class="col-xs-9">
+                            <input class="conditionAttr" data-l1key="operator" value="==" style="display : none;" />
+                            <select class="conditionAttr form-control" data-l1key="operande">
+                                <option value="1">{{Ouvert}}</option>
+                                <option value="0">{{Fermé}}</option>
+                                <option value="1">{{Allumé}}</option>
+                                <option value="0">{{Eteint}}</option>
+                                <option value="1">{{Déclenché}}</option>
+                                <option value="0">{{Au repos}}</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
+    <div class="col-sm-6">
+        <div class="well">
+            <form class="form-horizontal">
+                <legend>3) Actions
+                    <a class="btn btn-xs pull-right btn-success" id="bt_addAction"><i class="fa fa-plus-circle"></i> Ajouter une action</a>
+                </legend>
+                <div id="div_actionList"></div>
+            </form>
+        </div>
+    </div>
+</div>
 
-    <div class="form-actions">
-        <a class="btn btn-warning tooltips" id="bt_testScenario" title='{{Veuillez sauvegarder avant de tester. Ceci peut ne pas aboutir.}}'><i class="fa fa-gamepad"></i> Exécuter</a>
-        <a class="btn btn-danger" id="bt_delScenario"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
-        <a class="btn btn-success" id="bt_saveScenario"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
-    </div>
+<div class="form-actions">
+    <a class="btn btn-warning tooltips" id="bt_testScenario" title='{{Veuillez sauvegarder avant de tester. Ceci peut ne pas aboutir.}}'><i class="fa fa-gamepad"></i> Exécuter</a>
+    <a class="btn btn-danger" id="bt_delScenario"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
+    <a class="btn btn-success" id="bt_saveScenario"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
+</div>
 
 </div>
 </div>
