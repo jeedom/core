ce06f177db4a93b8bf3ff31555cba9e482aa2064
loic <lgevrey@oxya.com>
2015-08-28 16:36:25 +0200

    Fix update

--- install/update/1.208.0.php
+++ install/update/1.208.0.php
@@ -1,5 +1,5 @@
 <?php
 if (jeedom::isCapable('sudo')) {
-	exec("sed -i 's/expose_php = On/expose_php = Off/g' /etc/php5/fpm/php.ini");
+	exec("sudo sed -i 's/expose_php = On/expose_php = Off/g' /etc/php5/fpm/php.ini");
 }
 ?>
