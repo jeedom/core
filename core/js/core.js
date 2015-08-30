d4410bb6dee38a360626d44637f5aac3a96c8251
Loic <loicgevrey@gmail.com>
2015-08-30 17:44:04 +0200

    Try fix double columns

--- core/js/core.js
+++ core/js/core.js
@@ -141,9 +141,9 @@ function getDeviceType() {
     if (result.type == 'phone') {
         var ori = window.orientation;
         if (ori == 90 || ori == -90) {
-            result.bSize = (result.width / 3) - 20;
+            result.bSize = (result.width / 3) - 25;
         } else {
-            result.bSize = (result.width / 2) - 6;
+            result.bSize = (result.width / 2) - 11;
         }
     }
     return result;
