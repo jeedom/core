30a7d5a4f804d3b87035b6f4ceb2853f798aded4
loic <lgevrey@oxya.com>
2015-09-09 12:31:29 +0200

    Allow empty cmd

--- core/class/cmd.class.php
+++ core/class/cmd.class.php
@@ -1009,7 +1009,7 @@ class cmd {
 	}
 
 	public function event($_value, $_loop = 1) {
-		if ((trim($_value) === '' && $_value !== false) || $_loop > 4 || $this->getType() != 'info') {
+		if ($_loop > 4 || $this->getType() != 'info') {
 			return;
 		}
 		$value = $this->formatValue($_value);
