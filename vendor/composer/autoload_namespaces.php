0b4dbc3ccfc446d0a6dee9e14d5fa96397fdad18
loic <lgevrey@oxya.com>
2015-10-05 14:52:39 +0200

    Add monolog

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -8,6 +8,7 @@ $baseDir = dirname($vendorDir);
 return array(
     'SecurityLib' => array($vendorDir . '/ircmaxell/security-lib/lib'),
     'RandomLib' => array($vendorDir . '/ircmaxell/random-lib/lib'),
+    'Psr\\Log\\' => array($vendorDir . '/psr/log'),
     'Doctrine\\Common\\Cache\\' => array($vendorDir . '/doctrine/cache/lib'),
     'Cron' => array($vendorDir . '/mtdowling/cron-expression/src'),
 );
