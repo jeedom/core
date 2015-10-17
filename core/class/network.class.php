90eb495244c3fb5cc34d6b93d3d0c021064e9ad8
Loïc <loic@jeedom.com>
2015-10-17 15:53:23 +0200

    Fix for docker

--- core/class/network.class.php
+++ core/class/network.class.php
@@ -566,7 +566,7 @@ class network {
 		if (config::byKey('network::disableMangement') == 1) {
 			return;
 		}
-		if (!jeedom::isCapable('sudo')) {
+		if (!jeedom::isCapable('sudo') || jeedom::getHardwareName() == 'docker') {
 			return;
 		}
 		try {
