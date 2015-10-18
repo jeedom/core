4d64c75fd46f158ab16034a7fbc72f3e8e56fac9
Loïc <loic@jeedom.com>
2015-10-18 17:32:26 +0200

    Some fix

--- desktop/js/dashboard.js
+++ desktop/js/dashboard.js
@@ -195,7 +195,7 @@ function getObjectHtml(_object_id){
                 });
             });
 $('#div_ob'+_object_id+'.div_displayEquipement .eqLogic-widget').draggable('disable');
-}, 1);
+}, 10);
 }
 });
 }
