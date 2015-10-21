4b50deadfd86051c67bc14323b58d2a3597733e1
loic <lgevrey@oxya.com>
2015-10-21 10:10:30 +0200

    Try fix update

--- install/update/1.212.0.php
+++ install/update/1.212.0.php
@@ -1,8 +1,14 @@
 <?php
-$sql = 'SELECT *
+if (config::byKey('update1.212first', 'core', 0) == 0) {
+	config::save('update1.212first', 1);
+	jeedom::update();
+	die();
+} else {
+	$sql = 'SELECT *
         FROM cache';
-$caches = DB::Prepare($sql, array(), DB::FETCH_TYPE_ALL, PDO::FETCH_CLASS, 'cache');
-foreach ($caches as $cache) {
-	$cache->save();
+	$caches = DB::Prepare($sql, array(), DB::FETCH_TYPE_ALL, PDO::FETCH_CLASS, 'cache');
+	foreach ($caches as $cache) {
+		$cache->save();
+	}
 }
 ?>
