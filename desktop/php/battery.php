fc8bf2ad218859478cd4084c07126361d858b433
sarakha63 <sarakha_ludovic@yahoo.fr>
2015-10-25 01:38:38 +0200

    More work on battery (not yet finished)

--- desktop/php/battery.php
+++ desktop/php/battery.php
@@ -13,17 +13,17 @@ foreach (object::all() as $object) {
 				$color = '#f1c40f';
 			}
 			echo '<div class="eqLogic eqLogic-widget" style="min-width:80px;background-color:' . $color . '">';
-			echo '<center class="widget-name"><span style="font-size : 1.5em;">' . $eqLogic->getName() . '</span><br/><span style="font-size: 0.95em;position:relative;top:-5px;">' . $object->getName() . '</span></center>';
-			echo '<center><span style="font-size:2.2em;font-weight: bold;">' . $eqLogic->getConfiguration('batteryStatus', -2) . '</span><span>%</span></center>';
-			echo '<center>{{Le }}' . $eqLogic->getConfiguration('batteryStatusDatetime', __('inconnue', __FILE__)) . '</center>';
+			echo '<center class="widget-name"><span style="font-size : 1.5em;cursor:default;">' . $eqLogic->getName() . '</span><br/><span style="font-size: 0.95em;position:relative;top:-5px;cursor:default;">' . $object->getName() . '</span></center>';
+			echo '<center><span style="font-size:2.2em;font-weight: bold;cursor:default;">' . $eqLogic->getConfiguration('batteryStatus', -2) . '</span><span>%</span></center>';
+			echo '<center style="cursor:default;">{{Le }}' . $eqLogic->getConfiguration('batteryStatusDatetime', __('inconnue', __FILE__)) . '</center>';
 			if ($eqLogic->getConfiguration('battery_type', '') != '') {
-				echo '<center>' . $eqLogic->getConfiguration('battery_type', '') . '</center>';
+				echo '<span class="pull-right" style="font-size : 0.8em;margin-bottom: 3px;margin-right: 5px;cursor:default;" title="Piles">' . $eqLogic->getConfiguration('battery_type', '') . '</span>';
 			}
+			echo '<span class="pull-left" style="font-size : 0.8em;margin-bottom: 3px;margin-left: 5px;cursor:default;" title="Piles">' . ucfirst($eqLogic->getEqType_name()) . '</span>';
 			echo '</div>';
 		}
 	}
 }
-echo '</div>';
 echo '</div>';
 ?>
 <?php include_file('desktop', 'battery', 'js');?>
