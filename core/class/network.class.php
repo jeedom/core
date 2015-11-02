a5c4a52c444712d5d0edb12881f6297f1858aeed
loic <lgevrey@oxya.com>
2015-11-02 10:23:33 +0100

    Improve IP detection

--- core/class/network.class.php
+++ core/class/network.class.php
@@ -37,6 +37,8 @@ class network {
 	public static function getClientIp() {
 		if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
 			return $_SERVER['HTTP_X_FORWARDED_FOR'];
+		} elseif (isset($_SERVER['HTTP_X_REAL_IP'])) {
+			return $_SERVER['HTTP_X_REAL_IP'];
 		} elseif (isset($_SERVER['HTTP_CLIENT_IP'])) {
 			return $_SERVER['HTTP_CLIENT_IP'];
 		} elseif (isset($_SERVER['REMOTE_ADDR'])) {
