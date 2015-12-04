6b49062b29e094384fab461b5d318eef7ae50240
Loïc <loic@jeedom.com>
2015-12-04 22:42:11 +0100

    First try

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,4 @@
 <?php
 require_once dirname(__FILE__) . "/../core/php/core.inc.php";
-echo "\nBegin Jeedom test";
+echo "\nBegin Jeedom test\n";
 ?>
---
+++ tests/scenarioExpressionTest.php
@@ -0,0 +1,8 @@
+<?php
+class ScenarioExpressionTest extends \PHPUnit_Framework_TestCase {
+	public function testSlugify() {
+		$this->assertEquals('hello-world', 'Hello World');
+	}
+}
+
+?>
