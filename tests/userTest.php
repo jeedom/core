ba93300bb2f1c169ccbd66511df6886d9f94eb2c
Loïc <loic@jeedom.com>
2015-12-04 23:09:11 +0100

    Add some test

--- tests/userTest.php
+++ tests/userTest.php
@@ -1,14 +1,26 @@
 <?php
 class userTest extends \PHPUnit_Framework_TestCase {
-	public function testCalcul() {
+	public function testCreate() {
 		$user_array = array(
 			'login' => 'test',
 			'password' => 'test',
 		);
 		$user = new user();
 		utils::a2o($user, $user_array);
+		$user->setPassword(sha1($user_array['password']));
 		$user->save();
 		$this->assertEquals(2, $user->getId());
+		$this->assertEquals($user_array['login'], $user->getLogin());
+		$this->assertEquals(sha1($user_array['password']), $user->getPassword());
+		return $user;
+	}
+
+	/**
+	 * @depends testCreate
+	 */
+	public function testGet($_user) {
+		$user = user::byId($_user->getId());
+		$this->assertSame($user, $_user);
 	}
 }
 ?>
