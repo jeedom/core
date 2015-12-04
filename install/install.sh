dc6950926f8aaa396072afcb2da1b8daf65a92ff
Loïc <loic@jeedom.com>
2015-12-04 19:28:33 +0100

    Simplify installation

--- install/install.sh
+++ install/install.sh
@@ -13,12 +13,7 @@
 
 install_msg_en() {
     msg_installer_welcome="*      Welcome to the Jeedom installer/updater        *"
-    msg_usage1="Usage: $0 [<webserver_name>]"
-    msg_usage2="            webserver_name can be 'nginx' (default)"
     msg_nginx_config="*                  NGINX configuration                 *"
-    msg_question_install_jeedom="Are you sure you want to install Jeedom?"
-    msg_warning_install_jeedom="Warning: this will overwrite the default ${ws_upname} configuration if it exists!"
-    msg_warning_overwrite_jeedom="Warning: your existing Jeedom installation will be overwritten!"
     msg_yes="yes"
     msg_no="no"
     msg_yesno="yes/no: "
@@ -35,6 +30,7 @@ install_msg_en() {
     msg_install_jeedom="*                 Installing de Jeedom                 *"
     msg_update_jeedom="*                  Updating de Jeedom                  *"
     msg_setup_cron="*                   Setting up cron                    *"
+    msg_setup_cron="*                   Setting up nginx                   *"
     msg_post_install_actions="*             Post-installation actions                *"
     msg_post_update_actions="*               Post-update actions                    *"
     msg_install_complete="*                Installation complete                 *"
@@ -46,121 +42,11 @@ install_msg_en() {
     msg_php_already_optimized="PHP is already optimized, using : "
     msg_uptodate="is already installed and up-to-date"
     msg_needinstallupdate="needs to be installed or to be updated"
-    msg_nginx_ssl_config="*                 NGINX SSL configuration               *"
-}
-
-
-install_msg_fr() {
-    msg_installer_welcome="*Bienvenue dans l'assistant d'intallation/mise à jour de Jeedom*"
-    msg_usage1="Utilisation: $0 [<nom_du_webserver>]"
-    msg_usage2="             nom_du_webserver peut être 'nginx' (par défaut)"
-    msg_nginx_config="*                Configuration de NGINX                *"
-    msg_question_install_jeedom="Etes-vous sûr de vouloir installer Jeedom ?"
-    msg_warning_install_jeedom="Attention : ceci écrasera la configuration par défaut de ${ws_upname} si elle existe !"
-    msg_warning_overwrite_jeedom="Attention : votre installation existante de Jeedom va être écrasée !"
-    msg_yes="oui"
-    msg_no="non"
-    msg_yesno="oui / non : "
-    msg_cancel_install="Annulation de l'installation"
-    msg_answer_yesno="Répondez oui ou non"
-    msg_install_deps="*             Installation des dépendances             *"
-    msg_passwd_mysql="Quel mot de passe venez vous de taper (mot de passe root de la MySql) ?"
-    msg_confirm_passwd_mysql="Confirmez vous que le mot de passe est :"
-    msg_bad_passwd_mysql="Le mot de passe MySQL fourni est invalide !"
-    msg_setup_dirs_and_privs="* Création des répertoires et mise en place des droits *"
-    msg_copy_jeedom_files="*             Copie des fichiers de Jeedom             *"
-    msg_unable_to_download_file="Impossible de télécharger le fichier"
-    msg_config_db="*          Configuration de la base de données         *"
-    msg_install_jeedom="*                Installation de Jeedom                *"
-    msg_update_jeedom="*                Mise à jour de Jeedom                 *"
-    msg_setup_cron="*                Mise en place du cron                 *"
-    msg_post_install_actions="*             Action post installation                 *"
-    msg_post_update_actions="*              Action post mise à jour                 *"
-    msg_install_complete="*                Installation terminée                 *"
-    msg_update_complete="*                Mise à jour terminée                  *"
-    msg_or="ou"
-    msg_login_info1="Vous pouvez vous connecter sur Jeedom en allant sur :"
-    msg_login_info2="Vos identifiants sont :"
-    msg_php_version="PHP version trouvé : "
-    msg_php_already_optimized="PHP est déjà optimisé, utilisation de : "
-    msg_uptodate="est déjà installé et à jour"
-    msg_needinstallupdate="nécessite une installation ou une mise à jour"
-    msg_ask_install_nginx_ssl="Voules vous mettre en place un certification SSL auto signé"
-    msg_nginx_ssl_config="*                 NGINX SSL configuration               *"
-}
-
-
-install_msg_de() {
-    msg_installer_welcome="*      Willkommen beim Jeedom Installer / Updater        *"
-    msg_usage1="Einsatz: $0 [<Name_des_Webservers>]"
-    msg_usage2="            Webserver_Name kann 'nginx' (Standard) sein"
-    msg_nginx_config="*                  NGINX Konfiguration                 *"
-    msg_question_install_jeedom="Sind Sie sicher, dass Sie Jeedom installieren wollen?"
-    msg_warning_install_jeedom="Warnung: Diese überschreibt die Standard Konfiguration ${ws_upname}, falls vorhanden!"
-    msg_warning_overwrite_jeedom="Warnung: Ihr vorhandene Jeedom Installation wird überschrieben!"
-    msg_yes="ja"
-    msg_no="nein"
-    msg_yesno="ja/nein: "
-    msg_cancel_install="Abbruch der Installation"
-    msg_answer_yesno="Antwort ja oder nein"
-    msg_install_deps="*               Installations Abhängigkeiten            *"
-    msg_passwd_mysql="Welches Kennwort haben Sie gerade eingegeben (Root von MySQL)? "
-    msg_confirm_passwd_mysql="Bestätigen sie, dass das Passwort:"
-    msg_bad_passwd_mysql="Das MySQL-Passwort ist ungültig!"
-    msg_setup_dirs_and_privs="*      Das Erzeugen von Verzeichnissen und die Aufstellung von Rechten       *"
-    msg_copy_jeedom_files="*                Das Kopieren von Jeedom-Dateien              *"
-    msg_unable_to_download_file="Unmöglich die Datei herrunter zu laden"
-    msg_config_db="*               Konfiguration der Datenbank               *"
-    msg_install_jeedom="*                Installieren von Jeedom                *"
-    msg_update_jeedom="*                  Aktualisieren von Jeedom                  *"
-    msg_setup_cron="*                    Cron einrichten                   *"
-    msg_post_install_actions="*             Aktionen nach der Installation                *"
-    msg_post_update_actions="*               Aktionen nach Update                    *"
-    msg_install_complete="*                Installation abgeschlossen                *"
-    msg_update_complete="*                   Aktualisierung abgeschlossen                    *"
-    msg_or="or"
-    msg_login_info1="Sie können sich in Jeedom einzloggen, indem Sie auf:"
-    msg_login_info2="Ihre Anmeldedaten sind:"
-    msg_php_version="PHP Version gefunden: "
-    msg_php_already_optimized="PHP wird bereits optimiert, mit:  "
-    msg_uptodate="ist bereits installiert und auf dem neuesten Stand"
-    msg_needinstallupdate="Muss installiert oder aktualisiert werden!"
-    msg_ask_install_nginx_ssl="Möchten Sie SSL installieren, selbst signiertes Zertifikat"
-    msg_nginx_ssl_config="*                 NGINX SSL-Konfiguration               *"
 }
 
 ########################## Helper functions ############################
 
-
-setup_i18n() {
-    lang=${LANG:=en_US}
-    case ${lang} in
-        [Ff][Rr]*)
-            install_msg_fr
-        ;;
-        [Ee][Nn]*|*)
-            install_msg_en
-        ;;
-        [De][De]*|*)
-            install_msg_de
-        ;;
-    esac
-}
-
-
-usage_help() {
-    echo "${msg_usage1}"
-    echo "${msg_usage2}"
-    exit 1
-}
-
-
 configure_php() {
-    [ -z "`getent group dialout | grep www-data`" ] && adduser www-data dialout
-    GPIO_GROUP="`cat /etc/group | grep -e 'gpio'`"
-    if [ -z "${GPIO_GROUP}" ] ; then
-        [ -z "`getent group gpio | grep www-data`" ] && adduser www-data gpio
-    fi
     sed -i 's/max_execution_time = 30/max_execution_time = 300/g' /etc/php5/fpm/php.ini
     sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 1G/g' /etc/php5/fpm/php.ini
     sed -i 's/post_max_size = 8M/post_max_size = 1G/g' /etc/php5/fpm/php.ini
@@ -178,61 +64,17 @@ configure_nginx() {
         fi
     done
     service nginx stop
-    JEEDOM_ROOT="`cat /etc/nginx/sites-available/default | grep -e 'root /usr/share/nginx/www/jeedom;'`"
-    if [ -f '/etc/nginx/sites-available/defaults' ] ; then
-        rm /etc/nginx/sites-available/default
-    fi
     cp install/nginx_default /etc/nginx/sites-available/default
-    if [ ! -z "${JEEDOM_ROOT}" ] ; then
-        sed -i 's%root /usr/share/nginx/www;%root /usr/share/nginx/www/jeedom;%g' /etc/nginx/sites-available/default
-    fi
     if [ ! -f '/etc/nginx/sites-enabled/default' ] ; then
         ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
     fi
     service nginx restart
     configure_php
-    update-rc.d nginx defaults
-
-    JEEDOM_CRON="`crontab -l | grep -e 'jeeCron.php'`"
-
-    if [ -z "${JEEDOM_CRON}" ] ; then
-        croncmd="su --shell=/bin/bash - www-data -c '/usr/bin/php /usr/share/nginx/www/jeedom/core/php/jeeCron.php' >> /dev/null 2>&1"
-        cronjob="* * * * * $croncmd"
-        ( crontab -l | grep -v "$croncmd" ; echo "$cronjob" ) | crontab -
-    fi
-    for i in apache2 apache mongoose ; do
-        if [ -f "/etc/init.d/${i}" ] ; then
-            service ${i} stop
-            update-rc.d -f ${i} remove
-        fi
-    done          
-}
-
-is_version_greater_or_equal() {
-    # Compare two "X.Y.Z" formated versions
-    # Return 0 if $1 is lesser than $2
-    # Return 1 if $1 is greater than or equal to $2
-
-    newCMP="$1"
-    [ -z "$1" ] && newCMP="0.0.0"
-
-    # only compare the 2 first digits
-    for i in 1 2 3 ; do
-        REF="`echo $2 | cut -d. -f$i`"
-        CMP="`echo ${newCMP} | cut -d. -f$i`"
-        if [ $CMP -lt $REF ] ; then
-            # not greater or equal
-            return 0
-            break
-        fi
-    done
-    # greater or equal
-    return 1
+    update-rc.d nginx defaults         
 }
 
 install_dependency() {
     apt-get update
-    apt-get -y install
     apt-get -y install build-essential
     apt-get -y install curl
     apt-get -y install libarchive-dev
@@ -243,7 +85,6 @@ install_dependency() {
     apt-get -y install libtinyxml-dev
     apt-get -y install libxml2
     apt-get -y install make
-    apt-get -y install miniupnpc
     apt-get -y install mysql-client
     apt-get -y install mysql-common
     apt-get -y install mysql-server
@@ -263,39 +104,41 @@ install_dependency() {
     apt-get -y install python-serial
     apt-get -y install unzip
     apt-get -y install usb-modeswitch
-    apt-get -y install ffmpeg
-    apt-get -y install avconv
     apt-get -y install libudev1
     apt-get -y install ca-certificates
-    apt-get -y install htop
-
-    pecl install oauth
-    if [ $? -eq 0 ] ; then
-        for i in fpm cli ; do
-            PHP_OAUTH="`cat /etc/php5/${i}/php.ini | grep -e 'oauth.so'`"
-            if [ -z "${PHP_OAUTH}" ] ; then
-                echo "extension=oauth.so" >> /etc/php5/${i}/php.ini
-            fi
-        done
-    fi
-
+    apt-get install -y nginx-common nginx-full
     apt-get autoremove
 }
 
+install_mysql(){
+    MYSQL_HOST=localhost
+    MYSQL_PORT=3306
+    MYSQL_JEEDOM_USER=jeedom
+    MYSQL_JEEDOM_DBNAME=jeedom
+    echo "mysql-server mysql-server/root_password password root" | debconf-set-selections
+    echo "mysql-server mysql-server/root_password_again password root" | debconf-set-selections
+    apt-get install -y mysql-server mysql-client
 
-install_dependency_nginx() {
-    apt-get install -y nginx-common nginx-full
+    bdd_password=$(cat /dev/urandom | tr -cd 'a-f0-9' | head -c 15)
+    echo "DROP USER '${MYSQL_JEEDOM_USER}'@'%'" | mysql -uroot -proot
+    echo "CREATE USER '${MYSQL_JEEDOM_USER}'@'%' IDENTIFIED BY '${bdd_password}';" | mysql -uroot -proot
+    echo "DROP DATABASE IF EXISTS ${MYSQL_JEEDOM_DBNAME};" | mysql -uroot -proot
+    echo "CREATE DATABASE ${MYSQL_JEEDOM_DBNAME};" | mysql -uroot -proot
+    echo "GRANT ALL PRIVILEGES ON ${MYSQL_JEEDOM_DBNAME}.* TO '${MYSQL_JEEDOM_USER}'@'%';" | mysql -uroot -proot
+
+    cp core/config/common.config.sample.php core/config/common.config.php
+    sed -i -e "s/#PASSWORD#/${bdd_password}/g" core/config/common.config.php 
+    sed -i -e "s/#DBNAME#/${MYSQL_JEEDOM_DBNAME}/g" core/config/common.config.php 
+    sed -i -e "s/#USERNAME#/${MYSQL_JEEDOM_USER}/g" core/config/common.config.php 
+    sed -i -e "s/#PORT#/${MYSQL_PORT}/g" core/config/common.config.php 
+    sed -i -e "s/#HOST#/${MYSQL_HOST}/g" core/config/common.config.php 
+    chown www-data:www-data core/config/common.config.php
 }
-
 
 ##################### Main (script entry point) ########################
 
-
-webserver=${1-nginx}
-ws_upname="$(echo ${webserver} | tr 'a-z' 'A-Z')"
-
 # Select the right language, among available ones
-setup_i18n
+install_msg_en
 
 echo "********************************************************"
 echo "${msg_installer_welcome}"
@@ -308,25 +151,14 @@ if [ $(id -u) != 0 ] ; then
     exit 1
 fi
 
-# Check that the provided ${webserver} is supported [nginx]
-case ${webserver} in
-    nginx)
-        # Configuration
-        webserver_home="/usr/share/nginx/www"
-        croncmd="su --shell=/bin/bash - www-data -c 'nice -n 19 /usr/bin/php /usr/share/nginx/www/jeedom/core/php/jeeCron.php' >> /dev/null 2>&1"
-    ;;
-    *)
-        usage_help
-        exit 1
-    ;;
-esac
+webserver_home="/var/www"
+croncmd="su --shell=/bin/bash - www-data -c 'nice -n 19 /usr/bin/php ${webserver_home}/jeedom/core/php/jeeCron.php' >> /dev/null 2>&1"
 
 echo "********************************************************"
 echo "${msg_install_deps}"
 echo "********************************************************"
 
 install_dependency
-install_dependency_nginx
 
 echo "${msg_passwd_mysql}"
 while true ; do
@@ -374,7 +206,7 @@ echo "********************************************************"
 if [ -d "jeedom" ] ; then
     rm -rf jeedom
 fi
-BRANCH="stable"
+BRANCH="beta"
 if [ -z "$1" ]; then
     BRANCH="$1"
 fi
@@ -393,51 +225,43 @@ chown -R www-data:www-data "${webserver_home}"
 rm -rf jeedom.zip
 cd jeedom
 
-MYSQL_HOST=localhost
-MYSQL_PORT=3306
-MYSQL_JEEDOM_USER=jeedom
-MYSQL_JEEDOM_DBNAME=jeedom
-
 echo "********************************************************"
 echo "${msg_config_db}"
 echo "********************************************************"
-bdd_password=$(cat /dev/urandom | tr -cd 'a-f0-9' | head -c 15)
-echo "DROP USER '${MYSQL_JEEDOM_USER}'@'%'" | mysql -uroot -p"${MySQL_root}"
-echo "CREATE USER '${MYSQL_JEEDOM_USER}'@'%' IDENTIFIED BY '${bdd_password}';" | mysql -uroot -p"${MySQL_root}"
-echo "DROP DATABASE IF EXISTS ${MYSQL_JEEDOM_DBNAME};" | mysql -uroot -p"${MySQL_root}"
-echo "CREATE DATABASE ${MYSQL_JEEDOM_DBNAME};" | mysql -uroot -p"${MySQL_root}"
-echo "GRANT ALL PRIVILEGES ON ${MYSQL_JEEDOM_DBNAME}.* TO '${MYSQL_JEEDOM_USER}'@'%';" | mysql -uroot -p"${MySQL_root}"
+
+install_mysql
 
 echo "********************************************************"
 echo "${msg_install_jeedom}"
 echo "********************************************************"
-cp core/config/common.config.sample.php core/config/common.config.php
-sed -i -e "s/#PASSWORD#/${bdd_password}/g" core/config/common.config.php 
-sed -i -e "s/#DBNAME#/${MYSQL_JEEDOM_DBNAME}/g" core/config/common.config.php 
-sed -i -e "s/#USERNAME#/${MYSQL_JEEDOM_USER}/g" core/config/common.config.php 
-sed -i -e "s/#PORT#/${MYSQL_PORT}/g" core/config/common.config.php 
-sed -i -e "s/#HOST#/${MYSQL_HOST}/g" core/config/common.config.php 
-chown www-data:www-data core/config/common.config.php
+
 php install/install.php mode=force
 
 echo "********************************************************"
 echo "${msg_setup_cron}"
 echo "********************************************************"
 
-case ${webserver} in
-    nginx)
-        configure_nginx
-    ;;
-esac
+JEEDOM_CRON="`crontab -l | grep -e 'jeeCron.php'`"
+
+if [ -z "${JEEDOM_CRON}" ] ; then
+    croncmd="su --shell=/bin/bash - www-data -c '/usr/bin/php /var/www/jeedom/core/php/jeeCron.php' >> /dev/null 2>&1"
+    cronjob="* * * * * $croncmd"
+    ( crontab -l | grep -v "$croncmd" ; echo "$cronjob" ) | crontab -
+fi
+
+echo "www-data ALL=(ALL) NOPASSWD: ALL" | (EDITOR="tee -a" visudo)
+
+echo "********************************************************"
+echo "${msg_setup_nginx}"
+echo "********************************************************"
+
+configure_nginx
 
 echo "********************************************************"
 echo "${msg_post_install_actions}"
 echo "********************************************************"
-cp install/motd /etc
-chown root:root /etc/motd
-chmod 644 /etc/motd
-chown -R www-data:www-data /usr/share/nginx/www/jeedom
-chmod -R 775 /usr/share/nginx/www/jeedom
+chown -R www-data:www-data ${webserver_home}/jeedom
+chmod -R 775 ${webserver_home}/jeedom
 service php5-fpm restart
 
 echo "********************************************************"
