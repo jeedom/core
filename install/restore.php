0e3f25fe9d9017440bd674e28c943ce082e158b4
loic <lgevrey@oxya.com>
2015-12-08 16:11:04 +0100

    Add dep install on restore

--- install/restore.php
+++ install/restore.php
@@ -166,6 +166,14 @@ try {
 
 	foreach (plugin::listPlugin(true) as $plugin) {
 		$plugin_id = $plugin->getId();
+		if ($plugin->getHasDependency() == 1) {
+			if (function_exists($plugin_id . '::dependancy_info') && function_exists($plugin_id . '::dependancy_install')) {
+				$dependancy_info = $plugin_id::dependancy_info();
+				if ($dependancy_info['state'] == 'nok') {
+					$plugin_id::dependancy_install();
+				}
+			}
+		}
 		if (method_exists($plugin_id, 'restore')) {
 			echo 'Restauration specifique du plugin ' . $plugin_id . '...';
 			if (file_exists($tmp . '/plugin_backup/' . $plugin_id)) {
