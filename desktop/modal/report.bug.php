dcf5462c16b42f804d914b5581437135ae2557ca
Loïc <loic@jeedom.com>
2015-12-12 16:20:01 +0100

    Redirect user to faq on ticket + add link to true changelog

--- desktop/modal/report.bug.php
+++ desktop/modal/report.bug.php
@@ -11,61 +11,56 @@ if (config::byKey('market::apikey') == '' && config::byKey('market::username') =
 ?>
 <div id='div_alertReportBug'></div>
 <form class="form-horizontal" role="form" id="form_reportBug">
-    <div class='alert alert-info'>
-        {{Attention lors de l'envoi d'un rapport de bug, tous les logs sont automatiquement envoyés en même temps. Cependant ces logs ne seront accessibles que par vous-même et l'équipe de support.}}
-    </div>
     <div class='alert alert-warning'>
         {{Merci de vérifier avant toute ouverture de ticket :}}<br/>
         {{- que la question n'a pas déjà été posée sur le <a href='https://jeedom.fr/forum'>forum</a>}}<br/>
-        {{- que la question ne porte pas sur un plugin beta (sauf demande d'ouverture d'un ticket du développeur)}}<br/>
         {{- que la catégorie est bien sélectionnée pour que votre ticket soit traité dans les plus courts délais}}<br/>
-        {{- que la question porte sur un plugin market officiel}}<br/>
-        {{- que la réponse n'est pas déjà dans la <a href='https://jeedom.fr/doc'>documentation</a>}}<br/><br/>
+        {{- que la réponse n'est pas déjà dans la <a href='https://jeedom.fr/doc'>documentation</a>}}<br/>
+        {{- que la réponse n'est pas déjà dans la <a href='http://prd.jeedom.com/faq'>faq</a>}}<br/><br/>
         {{N'oubliez pas que poser la question sur le forum vous fournira généralement une réponse plus rapide que par ticket.}}
     </div>
     <div class="form-group">
         <label class="col-sm-2 control-label">{{Titre}}</label>
-        <div class="col-sm-9">
-            <input class="form-control input-sm ticketAttr" data-l1key="title"/>
+        <div class="col-sm-7">
+            <input class="form-control ticketAttr" data-l1key="title"/>
+        </div>
+        <div class="col-sm-2">
+            <a class="btn btn-default" id="bt_searchOnFaq"><i class="fa fa-search"></i> {{Chercher}}</a>
         </div>
     </div>
-    <div class="form-group">
-        <label class="col-sm-2 control-label">{{Type}}</label>
-        <div class="col-sm-2">
-            <select class="form-control input-sm ticketAttr" data-l1key="type">
-                <option value='question'>{{Question}}</option>
-                <option value='incident'>{{Incident}}</option>
-            </select>
-        </div>
-        <label class="col-sm-2 control-label">{{Catégorie}}</label>
-        <div class="col-sm-4">
-            <select class="form-control input-sm ticketAttr" data-l1key="category">
-                <option>{{Général}}</option>
-                <option>{{Scénario}}</option>
-                <option>{{Historique}}</option>
-                <?php
+    <div id="div_reportModalSendAction" style="display:none;">
+        <div class="form-group">
+            <label class="col-sm-2 control-label">{{Type}}</label>
+            <div class="col-sm-2">
+                <select class="form-control ticketAttr" data-l1key="type">
+                    <option value='question'>{{Question}}</option>
+                    <option value='incident'>{{Incident}}</option>
+                </select>
+            </div>
+            <label class="col-sm-2 control-label">{{Catégorie}}</label>
+            <div class="col-sm-4">
+                <select class="form-control ticketAttr" data-l1key="category">
+                    <option>{{Général}}</option>
+                    <option>{{Scénario}}</option>
+                    <option>{{Historique}}</option>
+                    <?php
 foreach (plugin::listPlugin(true) as $plugin) {
 	echo '<option>plugin::' . $plugin->getId() . '</option>';
 }
 ?>
-           </select>
+               </select>
+           </div>
        </div>
-   </div>
-   <div class="form-group">
-    <label class="col-sm-2 control-label">{{Donner un accès au support}}</label>
-    <div class="col-sm-9">
-       <input type="checkbox" class="ticketAttr bootstrapSwitch" data-l1key="allowRemoteAccess" />
-   </div>
-</div>
-<div class="form-group">
-    <label class="col-sm-2 control-label">{{Message}}</label>
-    <div class="col-sm-9">
-        <textarea class="form-control messageAttr input-sm" data-l1key="message" rows="4" ></textarea>
-        <input class="form-control ticketAttr" data-l1key="options" data-l2key="page" style="display: none;"/>
+       <div class="form-group">
+        <label class="col-sm-2 control-label">{{Message}}</label>
+        <div class="col-sm-9">
+            <textarea class="form-control messageAttr input-sm" data-l1key="message" rows="8" ></textarea>
+            <input class="form-control ticketAttr" data-l1key="options" data-l2key="page" style="display: none;"/>
+        </div>
     </div>
-</div>
-<div class="form-actions" style="height: 20px;">
-    <a class="btn btn-success pull-right" id="bt_sendBugReport" style="color:white;"><i class="fa fa-check-circle"></i> {{Envoyer}}</a>
+    <div class="form-actions" style="height: 20px;">
+        <a class="btn btn-success pull-right" id="bt_sendBugReport" style="color:white;"><i class="fa fa-check-circle"></i> {{Envoyer}}</a>
+    </div>
 </div>
 </form>
 
@@ -98,4 +93,9 @@ foreach (plugin::listPlugin(true) as $plugin) {
         }
     });
     });
+
+$('#bt_searchOnFaq').on('click',function(){
+    $('#div_reportModalSendAction').show();
+    window.open('http://prd.jeedom.com/faq/?submit=&action=search&search='+encodeURIComponent($('#form_reportBug .ticketAttr[data-l1key=title]').value()), '_blank');
+});
 </script>
