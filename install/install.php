c3eb16c0edbc6c0c4b295d0ac5245c956ef9cee8
loic <lgevrey@oxya.com>
2015-12-31 12:00:58 +0100

    Some fix

--- install/install.php
+++ install/install.php
@@ -27,8 +27,9 @@ if (php_sapi_name() != 'cli' || isset($_SERVER['REQUEST_METHOD']) || !isset($_SE
 set_time_limit(1800);
 echo "[START UPDATE]\n";
 
-if (shell_exec('(ps ax || ps w) | grep -i "install/install.php" | grep -v grep | wc -l') > 1) {
-	echo "Une mise a jour/installation est deja en cours. Vous devez attendre qu'elle soit finie avant d'en relancer une";
+if (shell_exec('(ps ax || ps w) | grep -i "install/install.php" | grep -v grep | grep -v sudo | wc -l') > 1) {
+	echo shell_exec('(ps ax || ps w) | grep -i "install/install.php" | grep -v grep | grep -v sudo');
+	echo "Une mise a jour/installation est deja en cours. Vous devez attendre qu'elle soit finie avant d'en relancer une\n";
 	echo "[END UPDATE]\n";
 	die();
 }
