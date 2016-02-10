966aa1d2766acda426056ce8383c514a5292d060
loic <lgevrey@oxya.com>
2016-02-10 16:22:33 +0100

    Fix expression evaluator

--- core/class/jeedom.class.php
+++ core/class/jeedom.class.php
@@ -497,7 +497,8 @@ class jeedom {
 
 	public static function evaluateExpression($_input) {
 		try {
-			$_input = scenarioExpression::setTags($_input);
+			$_scenario = null;
+			$_input = scenarioExpression::setTags($_input, $_scenario, true);
 			$result = evaluate($_input);
 			if (is_bool($result) || is_numeric($result)) {
 				return $result;
