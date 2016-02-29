cbf5731ce350db7eaa34a44f23086bd565c826d3
loic <lgevrey@oxya.com>
2016-02-29 09:52:01 +0100

    Bugfix js

--- desktop/js/utils.js
+++ desktop/js/utils.js
@@ -362,22 +362,22 @@ function showHelpModal(_name, _plugin) {
     if (init(_plugin) != '' && _plugin != undefined) {
         $('#div_helpWebsite').load('index.php?v=d&modal=help.website&page=doc_plugin_' + _plugin + '.php #primary', function () {
             if ($('#div_helpWebsite').find('.alert.alert-danger').length > 0 || $.trim($('#div_helpWebsite').text()) == '') {
-                $('a[href=#div_helpSpe]').click();
-                $('a[href=#div_helpWebsite]').hide();
+                $('a[href="#div_helpSpe"]').click();
+                $('a[href="#div_helpWebsite"]').hide();
             } else {
-                $('a[href=#div_helpWebsite]').show();
-                $('a[href=#div_helpWebsite]').click();
+                $('a[href="#div_helpWebsite"]').show();
+                $('a[href="#div_helpWebsite"]').click();
             }
         });
         $('#div_helpSpe').load('index.php?v=d&plugin=' + _plugin + '&modal=help.' + init(_name));
     } else {
         $('#div_helpWebsite').load('index.php?v=d&modal=help.website&page=doc_' + init(_name) + '.php #primary', function () {
             if ($('#div_helpWebsite').find('.alert.alert-danger').length > 0 || $.trim($('#div_helpWebsite').text()) == '') {
-                $('a[href=#div_helpSpe]').click();
-                $('a[href=#div_helpWebsite]').hide();
+                $('a[href="#div_helpSpe"]').click();
+                $('a[href="#div_helpWebsite"]').hide();
             } else {
-                $('a[href=#div_helpWebsite]').show();
-                $('a[href=#div_helpWebsite]').click();
+                $('a[href="#div_helpWebsite"]').show();
+                $('a[href="#div_helpWebsite"]').click();
             }
         });
         $('#div_helpSpe').load('index.php?v=d&modal=help.' + init(_name));
