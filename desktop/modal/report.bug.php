a0475518c69f73bc20c5acadee5923ff1cf18bf3
Loïc <loic@jeedom.com>
2016-03-06 11:17:41 +0100

    Improve support process

--- desktop/modal/report.bug.php
+++ desktop/modal/report.bug.php
@@ -11,10 +11,9 @@ if (config::byKey('market::apikey') == '' && config::byKey('market::username') =
 ?>
 <div id='div_alertReportBug'></div>
 <form class="form-horizontal" role="form" id="form_reportBug">
-    <div class='alert alert-info'>
-        {{Vous avez une idée ? Vous voulez la partager ? N'hésitez pas à nous la décrire <a href="https://jeedom.com/feedback" target="_blank">ici</a>}}
-    </div>
-    <div class='alert alert-warning'>
+    <div class="panel panel-success">
+     <div class="panel-heading"><h3 class="panel-title"><i class="fa fa-info"></i> {{Etape 1 : Information sur les tickets}}</h3></div>
+     <div class="panel-body">
         {{Merci de vérifier avant toute ouverture de ticket :}}<br/>
         {{- que la question n'a pas déjà été posée sur le <a href='https://jeedom.com/forum'>forum</a>}}<br/>
         {{- que la catégorie est bien sélectionnée pour que votre ticket soit traité dans les plus courts délais}}<br/>
@@ -22,39 +21,63 @@ if (config::byKey('market::apikey') == '' && config::byKey('market::username') =
         {{- que la réponse n'est pas déjà dans la <a href='https://jeedom.com/faq'>faq</a>}}<br/><br/>
         {{N'oubliez pas que poser la question sur le forum vous fournira généralement une réponse plus rapide que par ticket.}}
     </div>
+
+</div>
+
+<div class="panel panel-primary">
+ <div class="panel-heading"><h3 class="panel-title"><i class="fa fa-cogs"></i> {{Etape 2 : Catégorie et type de la demande}}</h3></div>
+ <div class="panel-body">
     <div class="form-group">
-        <label class="col-sm-2 control-label">{{Titre}}</label>
-        <div class="col-sm-7">
-            <input class="form-control ticketAttr" data-l1key="title"/>
+        <label class="col-sm-2 control-label">{{Type}}</label>
+        <div class="col-sm-2">
+            <select class="form-control ticketAttr" data-l1key="type">
+                <option value=''>{{Aucun}}</option>
+                <option value='question'>{{Question}}</option>
+                <option value='incident'>{{Incident}}</option>
+            </select>
         </div>
+        <label class="col-sm-2 control-label">{{Catégorie}}</label>
+        <div class="col-sm-4">
+            <select class="form-control ticketAttr" data-l1key="category">
+                <option value=''>{{Aucune}}</option>
+                <option data-pagehelp="core/<?php echo config::byKey('language', 'core', 'fr_FR'); ?>/doc-core-depannage.html">{{Général}}</option>
+                <option data-pagehelp="core/<?php echo config::byKey('language', 'core', 'fr_FR'); ?>/doc-core-scenario.html">{{Scénario}}</option>
+                <?php
+foreach (plugin::listPlugin(true) as $plugin) {
+	echo '<option value="plugin::' . $plugin->getId() . '" data-pagehelp="plugins/' . $plugin->getId() . '/' . config::byKey('language', 'core', 'fr_FR') . '/' . $plugin->getId() . '">Plugin ' . $plugin->getName() . '</option>';
+}
+?>
+           </select>
+       </div>
+   </div>
+
+</div>
+</div>
+
+<div id="div_reportModalSearchAction" class="panel panel-primary" style="display:none;">
+ <div class="panel-heading"><h3 class="panel-title"><i class="fa fa-search"></i> {{Etape 3 : Chercher dans la documentation}}</h3></div>
+ <div class="panel-body">
+    <div class="form-group">
+        <label class="col-sm-2 control-label">{{Rechercher}}</label>
         <div class="col-sm-2">
             <a class="btn btn-default" id="bt_searchOnFaq"><i class="fa fa-search"></i> {{Chercher}}</a>
         </div>
     </div>
-    <div id="div_reportModalSendAction" style="display:none;">
-        <div class="form-group">
-            <label class="col-sm-2 control-label">{{Type}}</label>
-            <div class="col-sm-2">
-                <select class="form-control ticketAttr" data-l1key="type">
-                    <option value='question'>{{Question}}</option>
-                    <option value='incident'>{{Incident}}</option>
-                </select>
-            </div>
-            <label class="col-sm-2 control-label">{{Catégorie}}</label>
-            <div class="col-sm-4">
-                <select class="form-control ticketAttr" data-l1key="category">
-                    <option>{{Général}}</option>
-                    <option>{{Scénario}}</option>
-                    <option>{{Historique}}</option>
-                    <?php
-foreach (plugin::listPlugin(true) as $plugin) {
-	echo '<option value="plugin::' . $plugin->getId() . '">Plugin ' . $plugin->getName() . '</option>';
-}
-?>
-               </select>
-           </div>
-       </div>
+</div>
+</div>
+
+<div class="panel panel-primary" id="div_reportModalSendAction" style="display:none;">
+    <div class="panel-heading"><h3 class="panel-title"><i class="fa fa-pencil"></i> {{Etape 4 : Demande de support}}</h3></div>
+    <div class="panel-body">
+
        <div class="form-group">
+        <label class="col-sm-2 control-label">{{Titre}}</label>
+        <div class="col-sm-7">
+            <input class="form-control ticketAttr" data-l1key="title"/>
+        </div>
+    </div>
+
+    <div class="form-group">
         <label class="col-sm-2 control-label">{{Message}}</label>
         <div class="col-sm-9">
             <textarea class="form-control messageAttr input-sm" data-l1key="message" rows="8" ></textarea>
@@ -64,6 +87,7 @@ foreach (plugin::listPlugin(true) as $plugin) {
     <div class="form-actions" style="height: 20px;">
         <a class="btn btn-success pull-right" id="bt_sendBugReport" style="color:white;"><i class="fa fa-check-circle"></i> {{Envoyer}}</a>
     </div>
+</div>
 </div>
 </form>
 
@@ -97,8 +121,17 @@ foreach (plugin::listPlugin(true) as $plugin) {
     });
     });
 
-$('#bt_searchOnFaq').on('click',function(){
-    $('#div_reportModalSendAction').show();
-    window.open('https://jeedom.com/faq/?submit=&action=search&search='+encodeURIComponent($('#form_reportBug .ticketAttr[data-l1key=title]').value()), '_blank');
-});
+    $('#bt_searchOnFaq').on('click',function(){
+        $('#div_reportModalSendAction').show();
+        window.open('https://jeedom.com/doc/documentation/'+$('.ticketAttr[data-l1key=category] option:selected').attr('data-pagehelp'), '_blank');
+    });
+
+    $('.ticketAttr[data-l1key=type],.ticketAttr[data-l1key=category]').on('change',function(){
+        if($('.ticketAttr[data-l1key=type]').value() != '' && $('.ticketAttr[data-l1key=category]').value() != ''){
+            $('#div_reportModalSearchAction').show();
+        }else{
+            $('#div_reportModalSearchAction').hide();
+        }
+        $('#div_reportModalSendAction').hide();
+    });
 </script>
