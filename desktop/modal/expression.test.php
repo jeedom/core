22f3a660ff13189649067e7ad9b3412ada5607c5
Loïc <loic@jeedom.com>
2016-03-06 11:21:01 +0100

    Typo

--- desktop/modal/expression.test.php
+++ desktop/modal/expression.test.php
@@ -50,7 +50,7 @@ if (!isConnect('admin')) {
 				$('#div_expressionTestResult').empty();
 				var html = '<div class="alert alert-info">';
 				if(data.correct == 'nok'){
-					html += '<strong>{{Attention je pense qu\'il y a un soucis car le résultat et le même que l\'expression}}</strong><br\>';
+					html += '<strong>{{Attention je pense qu\'il y a un soucis car le résultat est le même que l\'expression}}</strong><br\>';
 				}
 				html += '{{Je vais évaluer : }} <strong>'+data.evaluate+'</strong><br/>';
 				html += '{{Résultat : }} <strong>'+data.result+'</strong>';
