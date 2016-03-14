75003e5b1873f468d9f2bafadf76b57abdac23b6
loic <lgevrey@oxya.com>
2016-03-14 09:30:13 +0100

    Re add cache

--- mobile/js/equipment.js
+++ mobile/js/equipment.js
@@ -20,61 +20,65 @@ function initEquipment(_object_id) {
         }
     });
 
-if (isset(_object_id)) {
-    jeedom.object.toHtml({
-        id: _object_id,
-        version: 'mobile',
-        error: function (error) {
-            $('#div_alert').showAlert({message: error.message, level: 'danger'});
-        },
-        success: function (html) {
-            if(_object_id == 'all'){
-             jeedom.object.all({
-                error: function (error) {
-                    $('#div_alert').showAlert({message: error.message, level: 'danger'});
-                },
-                success: function (objects) {
-                   var div = '';
-                   for(var i in html){
-                    div += '<div class="div_displayEquipement">';
-                    div += '<legend>';
-                    for(var j in objects){
-                        if(objects[j].id == i){
-                         div += objects[j].name;
-                     }
-                 }
-                 div += '</legend>';
-                 div += html[i]
-                 div += '</div>';
+    if (isset(_object_id)) {
+        jeedom.object.toHtml({
+            id: _object_id,
+            version: 'mobile',
+            error: function (error) {
+                $('#div_alert').showAlert({message: error.message, level: 'danger'});
+            },
+            success: function (html) {
+                if(_object_id == 'all'){
+                   jeedom.object.all({
+                    error: function (error) {
+                        $('#div_alert').showAlert({message: error.message, level: 'danger'});
+                    },
+                    success: function (objects) {
+                     var div = '';
+                     for(var i in html){
+                        div += '<div class="div_displayEquipement">';
+                        div += '<legend>';
+                        for(var j in objects){
+                            if(objects[j].id == i){
+                               div += objects[j].name;
+                           }
+                       }
+                       div += '</legend>';
+                       div += html[i]
+                       div += '</div>';
+                   }
+                   try {
+                       $('#div_displayEquipement').empty().html(div).trigger('create');
+                   }catch(err) {
+                    console.log(err);
+                }
+                setTileSize('.eqLogic');
+                setTimeout(function () {
+                    $('.div_displayEquipement').packery({gutter : 4});
+                }, 10);
+            }
+        });  
+               }else{
+                 $('#div_displayEquipement').empty().html(html).trigger('create');
+                 setTileSize('.eqLogic');
+                 setTimeout(function () {
+                    $('#div_displayEquipement').packery({gutter : 4});
+                }, 10);
              }
-             $('#div_displayEquipement').empty().html(div).trigger('create');
-             setTileSize('.eqLogic');
-             setTimeout(function () {
-                $('.div_displayEquipement').packery({gutter : 4});
-            }, 10);
+
          }
-     });  
-         }else{
-           $('#div_displayEquipement').empty().html(html).trigger('create');
-           setTileSize('.eqLogic');
-           setTimeout(function () {
-            $('#div_displayEquipement').packery({gutter : 4});
-        }, 10);
+     });
+    } else {
+        $('#bottompanel').panel('open');
+    }
+
+    $(window).on("orientationchange", function (event) {
+        deviceInfo = getDeviceType();
+        setTileSize('.eqLogic');
+        if(_object_id == 'all'){
+            $('.div_displayEquipement').packery({gutter : 4});
+        }else{
+           $('#div_displayEquipement').packery({gutter : 4}); 
        }
-
-   }
-});
-} else {
-    $('#bottompanel').panel('open');
-}
-
-$(window).on("orientationchange", function (event) {
-    deviceInfo = getDeviceType();
-    setTileSize('.eqLogic');
-    if(_object_id == 'all'){
-        $('.div_displayEquipement').packery({gutter : 4});
-    }else{
-     $('#div_displayEquipement').packery({gutter : 4}); 
- }
-});
+   });
 }
