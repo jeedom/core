71ecd39893513ee7dd0d6ab77a527ac2df2e8e10
loic <lgevrey@oxya.com>
2016-03-14 09:29:57 +0100

    Add try catch on display widget

--- core/class/eqLogic.class.php
+++ core/class/eqLogic.class.php
@@ -448,7 +448,7 @@ class eqLogic {
 		}
 		$mc = cache::byKey('widgetHtml' . $_version . $this->getId());
 		if ($mc->getValue() != '') {
-			return preg_replace("/" . preg_quote(self::UIDDELIMITER) . "(.*?)" . preg_quote(self::UIDDELIMITER) . "/", self::UIDDELIMITER . mt_rand() . self::UIDDELIMITER, $mc->getValue());
+			//return preg_replace("/" . preg_quote(self::UIDDELIMITER) . "(.*?)" . preg_quote(self::UIDDELIMITER) . "/", self::UIDDELIMITER . mt_rand() . self::UIDDELIMITER, $mc->getValue());
 		}
 
 		$replace = array(
