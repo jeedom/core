af45b75c00be3e1c584ba93d61d312cf9b839e7d
loic <lgevrey@oxya.com>
2016-03-15 18:11:13 +0100

    Add icon on view edit

--- desktop/php/view_edit.php
+++ desktop/php/view_edit.php
@@ -27,7 +27,10 @@ if (!hasRight('viewedit')) {
                 <li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
                 <?php
 foreach (view::all() as $view) {
-	echo '<li class="cursor li_view" data-view_id="' . $view->getId() . '"><a>' . trim($view->getDisplay('icon')) . ' ' . $view->getName() . '</a></li>';
+	echo '<li class="cursor li_view" data-view_id="' . $view->getId() . '">';
+	echo '<i class="fa fa-arrows-v pull-left cursor"></i>';
+	echo '<a style="position:relative;left:15px;">' . trim($view->getDisplay('icon')) . ' ' . $view->getName() . '</a>';
+	echo '</li>';
 }
 ?>
            </ul>
