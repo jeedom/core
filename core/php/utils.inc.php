5b9168ff67db73e17ddc1ed8a7acda0407d44011
Loïc <loic@jeedom.com>
2016-03-20 17:40:45 +0100

    Improve client ip methode

--- core/php/utils.inc.php
+++ core/php/utils.inc.php
@@ -252,7 +252,9 @@ function convertDuration($time) {
 }
 
 function getClientIp() {
-	if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+	if (isset($_SERVER['HTTP_X_REAL_IP'])) {
+		return $_SERVER['HTTP_X_REAL_IP'];
+	} elseif (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
 		return $_SERVER['HTTP_X_FORWARDED_FOR'];
 	} elseif (isset($_SERVER['HTTP_CLIENT_IP'])) {
 		return $_SERVER['HTTP_CLIENT_IP'];
