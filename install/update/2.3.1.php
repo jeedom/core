cc46ab289294b3b6549b43320aecec218aeb5d50
Loïc <loic@jeedom.com>
2016-04-05 18:13:35 +0200

    Typo

--- install/update/2.3.1.php
+++ install/update/2.3.1.php
@@ -1,3 +1,3 @@
 <?php
-shell_exec('sudo rm /etc/apache2/conf-available/other-vhosts-access-log.conf;sudo rm /etc/apache2/conf-enabled/other-vhosts-access-log.conf');
+shell_exec('sudo rm /etc/apache2/conf-available/other-vhosts-access-log.conf > /dev/null;sudo rm /etc/apache2/conf-enabled/other-vhosts-access-log.conf > /dev/null');
 ?>
