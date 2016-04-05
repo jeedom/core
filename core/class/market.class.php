cefc4e843d2a303c8b51b67784dce01f88dd031a
Loïc <loic@jeedom.com>
2016-04-05 17:11:08 +0200

    Use more /tmp folder

--- core/class/market.class.php
+++ core/class/market.class.php
@@ -611,7 +611,7 @@ class market {
 			@chmod(dirname(__FILE__) . '/../../plugins', 0775);
 		}
 		log::add('update', 'alert', __('Début de la mise à jour de : ', __FILE__) . $this->getLogicalId() . "\n");
-		$tmp_dir = dirname(__FILE__) . '/../../tmp';
+		$tmp_dir = '/tmp';
 		$tmp = $tmp_dir . '/' . $this->getLogicalId() . '.zip';
 		if (file_exists($tmp)) {
 			unlink($tmp);
@@ -657,6 +657,7 @@ class market {
 						throw new Exception(__('Impossible d\'installer le plugin. Les fichiers n\'ont pas pu être décompressés : ', __FILE__) . substr($content, 255));
 					}
 					$zip->close();
+					unlink($tmp);
 					log::add('update', 'alert', __("OK\n", __FILE__));
 					log::add('update', 'alert', __('Installation de ', __FILE__) . $this->getLogicalId() . '...');
 					try {
@@ -703,7 +704,9 @@ class market {
 							$ErrMsg = "Erreur inconnue (Code $res)";
 							break;
 					}
-					throw new Exception(__('Impossible de décompresser le zip : ', __FILE__) . $tmp . __('. Erreur : ', __FILE__) . $ErrMsg . '. Avez vous acheté le plugin ?');
+					$content = file_get_contents($tmp);
+					unlink($tmp);
+					throw new Exception(__('Impossible de décompresser le zip : ', __FILE__) . $tmp . __('. Erreur : ', __FILE__) . $ErrMsg . '.' . $content);
 				}
 				break;
 			default:
