7196b4012f3e8b70d867439154500f7d6a9dc04e
Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
2016-04-21 23:08:48 +0200

    Centralise getJS et getCSS

--- core/php/getJS.php
+++ core/php/getJS.php
@@ -15,25 +15,4 @@
  * You should have received a copy of the GNU General Public License
  * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
  */
-require_once dirname(__FILE__) . "/core.inc.php";
-$file = dirname(__FILE__) . "/../../" . init('file');
-$pathinfo = pathinfo($file);
-if ($pathinfo['extension'] != 'js') {
-	die();
-}
-if (file_exists($file)) {
-	header('Content-Type: application/javascript');
-	$lastModified = filemtime($file);
-	$etagFile = (init('md5') == '') ? md5_file($file) : init('md5');
-	$ifModifiedSince = (isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) ? $_SERVER['HTTP_IF_MODIFIED_SINCE'] : false);
-	$etagHeader = (isset($_SERVER['HTTP_IF_NONE_MATCH']) ? trim($_SERVER['HTTP_IF_NONE_MATCH']) : false);
-	header("Last-Modified: " . gmdate("D, d M Y H:i:s", $lastModified) . " GMT");
-	header("Etag: $etagFile");
-	header('Cache-Control: public');
-	if (@strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']) == $lastModified || $etagHeader == $etagFile) {
-		header("HTTP/1.1 304 Not Modified");
-		exit;
-	}
-	echo translate::exec(file_get_contents($file), init('file'), true);
-	exit;
-}
+require_once 'getResource.php';
