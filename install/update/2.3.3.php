3fefe831595d8bebe8f00f3d929371adc1c3d425
Loïc <loic@jeedom.com>
2016-04-29 14:21:56 +0200

    Fix update branch

--- install/update/2.3.3.php
+++ install/update/2.3.3.php
@@ -3,6 +3,7 @@ try {
 	if (config::byKey('market::username') != '') {
 		config::save('market::enable', 1);
 	}
+	config::save('core::branch', config::byKey('market::branch'));
 } catch (Exception $exc) {
 	echo $exc->getMessage();
 }
