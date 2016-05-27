ab99aa00da5362b5f3536027b7651d324f447971
Loïc <loic@jeedom.com>
2016-05-27 11:27:24 +0200

    Typo

--- install/install.sh
+++ install/install.sh
@@ -18,7 +18,7 @@ fi
 apt_install() {
   apt-get -y install "$@"
   if [ $? -ne 0 ]; then
-    echo -e "${ROUGE}Could not install $@ - abort${NORMAL}"
+    echo "${ROUGE}Could not install $@ - abort${NORMAL}"
     exit 1
   fi
 }
@@ -26,65 +26,65 @@ apt_install() {
 mysql_sql() {
   echo "$@" | mysql -uroot -proot
   if [ $? -ne 0 ]; then
-    echo -e "C${ROUGE}ould not execute $@ into mysql - abort${NORMAL}"
+    echo "C${ROUGE}ould not execute $@ into mysql - abort${NORMAL}"
     exit 1
   fi
 }
 
 step_1_upgrade() {
 	echo "---------------------------------------------------------------------"
-	echo -e "${JAUNE}Start step_1_upgrade${NORMAL}"
+	echo "${JAUNE}Start step_1_upgrade${NORMAL}"
 	apt-get update
 	apt-get -y dist-upgrade
-	echo -e "${VERT}step_1_upgrade success${NORMAL}"
+	echo "${VERT}step_1_upgrade success${NORMAL}"
 }
 
 step_2_mainpackage() {
 	echo "---------------------------------------------------------------------"
-	echo -e "${JAUNE}Start step_2_mainpackage${NORMAL}"
+	echo "${JAUNE}Start step_2_mainpackage${NORMAL}"
 	apt_install ntp ca-certificates unzip curl sudo
-	echo -e "${VERT}step_2_mainpackage success${NORMAL}"
+	echo "${VERT}step_2_mainpackage success${NORMAL}"
 }
 
 step_3_mysql() {
 	echo "---------------------------------------------------------------------"
-	echo -e "${JAUNE}Start step_3_mysql${NORMAL}"
+	echo "${JAUNE}Start step_3_mysql${NORMAL}"
 	echo "mysql-server mysql-server/root_password password root" | debconf-set-selections
 	echo "mysql-server mysql-server/root_password_again password root" | debconf-set-selections
 	apt_install mysql-client mysql-common mysql-server
 	service mysql start
 	if [ $? -ne 0 ]; then
-    	echo -e "${ROUGE}Could not start database - abort${NORMAL}"
+    	echo "${ROUGE}Could not start database - abort${NORMAL}"
     	exit 1
   	fi
 	mysqladmin -u root password root
 	if [ $? -ne 0 ]; then
-    	echo -e "${ROUGE}Could not connect to database - abort${NORMAL}"
+    	echo "${ROUGE}Could not connect to database - abort${NORMAL}"
     	exit 1
   	fi
-	echo -e "${VERT}step_3_mysql success${NORMAL}"
+	echo "${VERT}step_3_mysql success${NORMAL}"
 }
 
 step_4_apache() {
 	echo "---------------------------------------------------------------------"
-	echo -e "${JAUNE}Start step_4_apache${NORMAL}"
+	echo "${JAUNE}Start step_4_apache${NORMAL}"
 	apt_install apache2 apache2-mpm-prefork apache2-utils libexpat1 ssl-cert
 	apt_install	libapache2-mod-php5 php5 php5-common php5-curl php5-dev php5-gd php-pear php5-json php5-memcached php5-mysql php5-cli
-	echo -e "${VERT}step_4_apache success${NORMAL}"
+	echo "${VERT}step_4_apache success${NORMAL}"
 }
 
 step_5_jeedom_download() {
 	echo "---------------------------------------------------------------------"
-	echo -e "${JAUNE}Start step_5_jeedom_download${NORMAL}"
+	echo "${JAUNE}Start step_5_jeedom_download${NORMAL}"
 	wget https://github.com/jeedom/core/archive/stable.zip -O /tmp/jeedom.zip
 	if [ $? -ne 0 ]; then
-		echo -e "${JAUNE}Could not download jeedom from github, use preadd version if exist${NORMAL}"
+		echo "${JAUNE}Could not download jeedom from github, use preadd version if exist${NORMAL}"
 		if [ -f /root/jeedom.zip ]; then
 			cp /root/jeedom.zip /tmp/jeedom.zip
 		fi
 	fi
-	if [ ! -e /tmp/jeedom.zip ]; then
-		echo -e "${ROUGE}Could not retrieve jeedom.zip package - abort${NORMAL}"
+	if [ ! /tmp/jeedom.zip ]; then
+		echo "${ROUGE}Could not retrieve jeedom.zip package - abort${NORMAL}"
     	exit 1
 	fi
 	mkdir -p /var/www/html
@@ -92,17 +92,17 @@ step_5_jeedom_download() {
 	rm -rf /root/core-*
 	unzip -q /tmp/jeedom.zip -d /root/
 	if [ $? -ne 0 ]; then
-    	echo -e "${ROUGE}Could not unzip archive - abort${NORMAL}"
+    	echo "${ROUGE}Could not unzip archive - abort${NORMAL}"
     	exit 1
   	fi
 	cp -R /root/core-*/* /var/www/html/
 	rm -rf /root/core-*
-	echo -e "${VERT}step_5_jeedom_download success${NORMAL}"
+	echo "${VERT}step_5_jeedom_download success${NORMAL}"
 }
 
 step_6_jeedom_customization() {
 	echo "---------------------------------------------------------------------"
-	echo -e "${JAUNE}Start step_6_jeedom_customization${NORMAL}"
+	echo "${JAUNE}Start step_6_jeedom_customization${NORMAL}"
 	cp /var/www/html/install/apache_security /etc/apache2/conf-available/security.conf
 	rm /etc/apache2/conf-enabled/security.conf
 	ln -s /etc/apache2/conf-available/security.conf /etc/apache2/conf-enabled/
@@ -110,15 +110,15 @@ step_6_jeedom_customization() {
 	rm /etc/apache2/conf-enabled/other-vhosts-access-log.conf
 	systemctl restart apache2
 	if [ $? -ne 0 ]; then
-    	echo -e "${ROUGE}Could not restart apache - abort${NORMAL}"
+    	echo "${ROUGE}Could not restart apache - abort${NORMAL}"
     	exit 1
   	fi
-	echo -e "${VERT}step_6_jeedom_customization success${NORMAL}"
+	echo "${VERT}step_6_jeedom_customization success${NORMAL}"
 }
 
 step_7_jeedom_installation() {
-	echo -e "---------------------------------------------------------------------"
-	echo -e "${JAUNE}Start step_7_jeedom_installation${NORMAL}"
+	echo "---------------------------------------------------------------------"
+	echo "${JAUNE}Start step_7_jeedom_installation${NORMAL}"
 	mysql_sql "DROP USER 'jeedom'@'%';"
 	mysql_sql "CREATE USER 'jeedom'@'%' IDENTIFIED BY 'jeedom';"
 	mysql_sql "DROP DATABASE IF EXISTS jeedom;"
@@ -134,42 +134,42 @@ step_7_jeedom_installation() {
 	chown -R www-data:www-data /var/www/html
 	php /var/www/html/install/install.php mode=force
 	if [ $? -ne 0 ]; then
-    	echo -e "${ROUGE}Could not install jeedom - abort${NORMAL}"
+    	echo "${ROUGE}Could not install jeedom - abort${NORMAL}"
     	exit 1
   	fi
-	echo -e "${VERT}step_7_jeedom_installation success${NORMAL}"
+	echo "${VERT}step_7_jeedom_installation success${NORMAL}"
 }
 
 step_8_jeedom_crontab() {
 	echo "---------------------------------------------------------------------"
-	echo -e "${JAUNE}Start step_8_jeedom_crontab${NORMAL}"
+	echo "${JAUNE}Start step_8_jeedom_crontab${NORMAL}"
 	echo "* * * * * su --shell=/bin/bash - www-data -c '/usr/bin/php /var/www/html/core/php/jeeCron.php' >> /dev/null" | crontab -
 	if [ $? -ne 0 ]; then
-    	echo -e "${ROUGE}Could not install jeedom cron - abort${NORMAL}"
+    	echo "${ROUGE}Could not install jeedom cron - abort${NORMAL}"
     	exit 1
   	fi
-	echo -e "${VERT}step_8_jeedom_crontab success${NORMAL}"
+	echo "${VERT}step_8_jeedom_crontab success${NORMAL}"
 }
 
 step_9_jeedom_sudo() {
 	echo "---------------------------------------------------------------------"
-	echo -e "${JAUNE}Start step_9_jeedom_sudo${NORMAL}"
+	echo "${JAUNE}Start step_9_jeedom_sudo${NORMAL}"
 	echo "www-data ALL=(ALL) NOPASSWD: ALL" | (EDITOR="tee -a" visudo)
 	if [ $? -ne 0 ]; then
     	echo "${ROUGE}Could not install make jeedom sudo - abort${NORMAL}"
     	exit 1
   	fi
-	echo -e "${VERT}step_9_jeedom_sudo success${NORMAL}"
+	echo "${VERT}step_9_jeedom_sudo success${NORMAL}"
 }
 
-echo -e "${JAUNE}Welcome to jeedom installer${NORMAL}"
+echo "${JAUNE}Welcome to jeedom installer${NORMAL}"
 STEP=0
 
 while getopts ":s:" opt; do
   case $opt in
     s) STEP="$OPTARG"
     ;;
-    \?) echo -e "${ROUGE}Invalid option -$OPTARG${NORMAL}" >&2
+    \?) echo "${ROUGE}Invalid option -$OPTARG${NORMAL}" >&2
     ;;
   esac
 done
@@ -204,6 +204,6 @@ case ${STEP} in
 	;;
    9) step_9_jeedom_sudo
 	;;
-   *) echo -e "${ROUGE}Sorry, I can not get a ${STEP} step for you!${NORMAL}"
+   *) echo "${ROUGE}Sorry, I can not get a ${STEP} step for you!${NORMAL}"
 	;;
 esac
