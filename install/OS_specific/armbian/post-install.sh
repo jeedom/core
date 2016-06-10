ab398fbf6d184becc51fb366c2bb75f6bdcf3c70
Loïc <loic@jeedom.com>
2016-06-10 19:23:05 +0200

    fix motd

--- install/OS_specific/armbian/post-install.sh
+++ install/OS_specific/armbian/post-install.sh
@@ -2,3 +2,4 @@
 rm /etc/update-motd.d/10-header
 rm /etc/update-motd.d/99-point-to-faq
 wget https://raw.githubusercontent.com/jeedom/core/beta/install/OS_specific/armbian/10-header -O /etc/update-motd.d/10-header
+chmod +x /etc/update-motd.d/10-header
