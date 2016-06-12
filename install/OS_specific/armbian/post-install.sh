afef8a0ecf50f8b0d8135c16ebd0353dba7c5fb1
Loïc <loic@jeedom.com>
2016-06-12 15:54:43 +0200

    fix rsyslog on armbian

--- install/OS_specific/armbian/post-install.sh
+++ install/OS_specific/armbian/post-install.sh
@@ -3,3 +3,5 @@ rm /etc/update-motd.d/10-header
 rm /etc/update-motd.d/99-point-to-faq
 wget https://raw.githubusercontent.com/jeedom/core/beta/install/OS_specific/armbian/10-header -O /etc/update-motd.d/10-header
 chmod +x /etc/update-motd.d/10-header
+sed -i '/# The named pipe \/dev\/xconsole/,$d' /etc/rsyslog.conf
+systemctl restart rsyslog.service
