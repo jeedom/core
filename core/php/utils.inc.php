bc22db79285450a954f7d6903298da4adabd2410
Loïc <loic@jeedom.com>
2016-06-16 15:39:01 +0200

    Close jeedom/issues#8

--- core/php/utils.inc.php
+++ core/php/utils.inc.php
@@ -608,21 +608,14 @@ function calculPath($_path) {
 
 function getDirectorySize($path) {
 	$totalsize = 0;
-	$totalcount = 0;
-	$dircount = 0;
 	if ($handle = opendir($path)) {
 		while (false !== ($file = readdir($handle))) {
 			$nextpath = $path . '/' . $file;
 			if ($file != '.' && $file != '..' && !is_link($nextpath)) {
 				if (is_dir($nextpath)) {
-					$dircount++;
-					$result = getDirectorySize($nextpath);
-					$totalsize += $result['size'];
-					$totalcount += $result['count'];
-					$dircount += $result['dircount'];
+					$totalsize += getDirectorySize($nextpath);
 				} elseif (is_file($nextpath)) {
 					$totalsize += filesize($nextpath);
-					$totalcount++;
 				}
 			}
 		}
