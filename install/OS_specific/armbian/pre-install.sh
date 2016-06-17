6b546c4d57f21126c66c177767ad60beff720699
Loïc <loic@jeedom.com>
2016-06-17 12:27:41 +0200

    bugfix

--- install/OS_specific/armbian/pre-install.sh
+++ install/OS_specific/armbian/pre-install.sh
@@ -6,6 +6,7 @@ systemctl stop serial-getty@ttymxc0.service
 systemctl stop serial-getty@ttyS0.service
 systemctl mask serial-getty@ttyS0.service
 sed -i 's/interactive/ondemand/g' /etc/default/cpufrequtils
+sed -i 's/odroidc2/jeedom/g' /etc/hosts
 apt-get update
 apt-get -y autoremove
 apt-get -y dist-upgrade
