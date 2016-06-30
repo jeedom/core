9b72a263611c4d7c3e72f95d25f6034b0eb7ea22
Loïc <loic@jeedom.com>
2016-06-30 19:02:01 +0200

    fix error log

--- install/install.sh
+++ install/install.sh
@@ -149,6 +149,11 @@ step_7_jeedom_customization() {
 	cp ${WEBSERVER_HOME}/install/apache_security /etc/apache2/conf-available/security.conf
 	rm /etc/apache2/conf-enabled/security.conf > /dev/null 2>&1
 	ln -s /etc/apache2/conf-available/security.conf /etc/apache2/conf-enabled/
+
+	cp ${WEBSERVER_HOME}/install/apache_default /etc/apache2/sites-available/000-default.conf
+	rm /etc/apache2/sites-enabled/000-default.conf > /dev/null 2>&1
+	ln -s /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-enabled/
+
 	rm /etc/apache2/conf-available/other-vhosts-access-log.conf > /dev/null 2>&1
 	rm /etc/apache2/conf-enabled/other-vhosts-access-log.conf > /dev/null 2>&1
 	for file in ` find / -name php.ini -type f`;do
