7ec5129797997edea34c38d118617a18f9c588bc
Loïc <loic@jeedom.com>
2016-07-07 21:05:54 +0200

    FIx warning

--- core/class/log.class.php
+++ core/class/log.class.php
@@ -155,8 +155,7 @@ class log {
 		if ($maxLineLog < self::DEFAULT_MAX_LINE) {
 			$maxLineLog = self::DEFAULT_MAX_LINE;
 		}
-		com_shell::execute('sudo chmod 777 ' . $_path . ' ;echo "$(sed \'s/\\n/\
-/g\' | tail -n ' . $maxLineLog . ' ' . $_path . ')" > ' . $_path);
+		com_shell::execute('sudo chmod 777 ' . $_path . ' ;tail -n ' . $maxLineLog . ' ' . $_path . ')" > ' . $_path);
 		@chown($_path, 'www-data');
 		@chgrp($_path, 'www-data');
 	}
