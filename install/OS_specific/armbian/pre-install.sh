067caeeb0ac302a40e918ce0d92217ce0cd85159
Loïc <loic@jeedom.com>
2016-07-11 16:24:34 +0200

    Update pre-install.sh

--- install/OS_specific/armbian/pre-install.sh
+++ install/OS_specific/armbian/pre-install.sh
@@ -17,5 +17,5 @@ echo jeedom > /etc/hostname
 (echo "Mjeedom96";echo "Mjeedom96";) | passwd root
 rm /root/.not_logged_in_yet
 rm -rf /root/.bashrc
-wget https://raw.githubusercontent.com/jeedom/core/beta/install/bashrc -O /root/.bashrc
-(echo "@reboot root [[ -f /var/www/html/core/config/common.config.php ]] && exit 0;cd /root;echo '<script>setTimeout(function(){ location.reload(); }, 1000);</script>Start installation of jeedom in 30s, please wait.' > /var/www/html/index.html;sleep 30;rm -rf /root/install.sh;wget https://raw.githubusercontent.com/jeedom/core/beta/install/install.sh -O /root/install.sh;chmod +x /root/install.sh;PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin;/root/install.sh -h 1 > /var/www/html/index.html 2>&1;"; crontab -l) | crontab -
+wget https://raw.githubusercontent.com/jeedom/core/stable/install/bashrc -O /root/.bashrc
+(echo "@reboot root [[ -f /var/www/html/core/config/common.config.php ]] && exit 0;cd /root;echo '<script>setTimeout(function(){ location.reload(); }, 1000);</script>Start installation of jeedom in 30s, please wait.' > /var/www/html/index.html;sleep 30;rm -rf /root/install.sh;wget https://raw.githubusercontent.com/jeedom/core/stable/install/install.sh -O /root/install.sh;chmod +x /root/install.sh;PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin;/root/install.sh -h 1 > /var/www/html/index.html 2>&1;"; crontab -l) | crontab -
