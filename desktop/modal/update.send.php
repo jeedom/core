c2881405561138eaa8ae5a8e5cc818ed4505ddb5
Loïc <loic@jeedom.com>
2016-07-20 18:41:15 +0200

    Bugfix

--- desktop/modal/update.send.php
+++ desktop/modal/update.send.php
@@ -2,7 +2,7 @@
 if (!isConnect('admin')) {
 	throw new Exception('{{401 - Accès non autorisé}}');
 }
-$repo = update::repoById(init('repo'));
+$repo = update::repoById(init('repo', 'market'));
 if ($repo['enable'] == 0) {
 	throw new Exception(__('Le repository est inactif : ', __FILE__) . init('repo'));
 }
