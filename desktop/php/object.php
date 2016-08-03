18ab6888a1d4845dd632013ebc10a721e3af5034
Loïc <loic@jeedom.com>
2016-08-03 16:37:19 +0200

    Add option

--- desktop/php/object.php
+++ desktop/php/object.php
@@ -158,6 +158,16 @@ foreach (jeedom::getConfiguration('object:summary') as $key => $value) {
 }
 echo '</tr>';
 echo '<tr>';
+echo '<tr>';
+echo '<td>';
+echo '{{Masquer en mobile}}';
+echo '</td>';
+foreach (jeedom::getConfiguration('object:summary') as $key => $value) {
+	echo '<td>';
+	echo '<input type="checkbox" class="objectAttr" data-l1key="configuration" data-l2key="summary::hide::mobile::' . $key . '" />';
+	echo '</td>';
+}
+echo '</tr>';
 ?>
       </table>
 	  <legend style="cursor:default;"><i class="fa fa-tachometer"></i>  {{Commandes}}</legend>
