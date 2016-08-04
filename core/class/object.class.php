d7e9f204d0e76caf361b3a15de32b7619ef46b80
Loïc <loic@jeedom.com>
2016-08-04 09:45:32 +0200

    ui improvement

--- core/class/object.class.php
+++ core/class/object.class.php
@@ -183,7 +183,7 @@ class object {
 			if (isset($def[$key]['iconOff']) && $result == 0) {
 				$icon = $def[$key]['iconOff'];
 			}
-			if ($result == 0 && $def[$key]['unit'] == ''){
+			if ($result == 0 && $def[$key]['unit'] == '') {
 				$result = '';
 			}
 			$return .= '<span style="margin-right:' . $margin . 'px;"><i class="' . $icon . '"></i> <span class="objectSummary' . $key . '">' . $result . '</span> ' . $def[$key]['unit'] . '</span> ';
@@ -347,7 +347,7 @@ class object {
 				if (isset($value['iconOff']) && $result == 0) {
 					$icon = $value['iconOff'];
 				}
-				if ($result == 0 && $value['unit'] == ''){
+				if ($result == 0 && $value['unit'] == '') {
 					$result = '';
 				}
 				$return .= '<span style="margin-right:5px;"><i class="' . $icon . '"></i> <span class="objectSummary' . $key . '">' . $result . '</span> ' . $value['unit'] . '</span>';
