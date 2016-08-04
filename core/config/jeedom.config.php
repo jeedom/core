b4d635e2a6ee2f5017e9abee3a8e02109d582d5a
Ludovic <ludovic@jeedom.com>
2016-08-04 13:05:30 +0200

    added luminosity

--- core/config/jeedom.config.php
+++ core/config/jeedom.config.php
@@ -29,6 +29,7 @@ $JEEDOM_INTERNAL_CONFIG = array(
 			'outlet' => array('name' => 'Prise', 'calcul' => 'sum', 'icon' => 'icon jeedom-prise', 'unit' => '', 'count' => 'binary'),
 			'temperature' => array('name' => 'Température', 'calcul' => 'avg', 'icon' => 'icon divers-thermometer31', 'unit' => '°C'),
 			'humidity' => array('name' => 'Humidité', 'calcul' => 'avg', 'icon' => 'fa fa-tint', 'unit' => '%'),
+			'luminosity' => array('name' => 'Luminosité', 'calcul' => 'avg', 'icon' => 'icon meteo-soleil', 'unit' => 'lx'),
 			'power' => array('name' => 'Puissance', 'calcul' => 'sum', 'icon' => 'fa fa-bolt', 'unit' => 'W'),
 		),
 	),
