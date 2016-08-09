9a2f17b4bcb7793da35d7915f3ffaca5d6bae5d9
Ludovic <ludovic@jeedom.com>
2016-08-09 18:59:51 +0200

    removed dummy log

--- core/php/downloadFile.php
+++ core/php/downloadFile.php
@@ -34,11 +34,9 @@ if (strpos($pathfile, '*') === false) {
 		throw new Exception(__('Fichier non trouvé : ', __FILE__) . $pathfile);
 	}
 } else if (is_dir(str_replace('*','',$pathfile))) {
-	log::add('camera','error','toto');
 	system('cd ' . dirname($pathfile) . ';tar cfz ' . '/tmp/archive.tar.gz * > /dev/null 2>&1');
 	$pathfile = '/tmp/archive.tar.gz';
 } else {
-	log::add('camera','error','tata');
 	$pattern = array_pop(explode('/', $pathfile));
 	system('cd ' . dirname($pathfile) . ';tar cfz ' . '/tmp/archive.tar.gz ' . $pattern .'> /dev/null 2>&1');
 	$pathfile = '/tmp/archive.tar.gz';
