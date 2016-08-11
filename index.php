82f98fdf6d71c07257ad14215a9f52ff42223517
Loïc <loic@jeedom.com>
2016-08-11 11:42:43 +0200

    improvement

--- index.php
+++ index.php
@@ -15,85 +15,89 @@
  * You should have received a copy of the GNU General Public License
  * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
  */
+try {
 
-if (!file_exists(dirname(__FILE__) . '/core/config/common.config.php')) {
-	header("location: install/setup.php");
-}
-
-if (!isset($_GET['v'])) {
-	$useragent = (isset($_SERVER["HTTP_USER_AGENT"])) ? $_SERVER["HTTP_USER_AGENT"] : 'none';
-	$getParams = (stristr($useragent, "Android") || strpos($useragent, "iPod") || strpos($useragent, "iPhone") || strpos($useragent, "Mobile") || strpos($useragent, "WebOS") || strpos($useragent, "mobile") || strpos($useragent, "hp-tablet"))
-	? 'm' : 'd';
-	foreach ($_GET AS $var => $value) {
-		$getParams .= '&' . $var . '=' . $value;
+	if (!file_exists(dirname(__FILE__) . '/core/config/common.config.php')) {
+		header("location: install/setup.php");
 	}
-	$url = 'index.php?v=' . trim($getParams, '&');
-	if (headers_sent()) {
-		echo '<script type="text/javascript">';
-		echo "window.location.href='$url';";
-		echo '</script>';
-	} else {
-		exit(header('Location: ' . $url));
+
+	if (!isset($_GET['v'])) {
+		$useragent = (isset($_SERVER["HTTP_USER_AGENT"])) ? $_SERVER["HTTP_USER_AGENT"] : 'none';
+		$getParams = (stristr($useragent, "Android") || strpos($useragent, "iPod") || strpos($useragent, "iPhone") || strpos($useragent, "Mobile") || strpos($useragent, "WebOS") || strpos($useragent, "mobile") || strpos($useragent, "hp-tablet"))
+		? 'm' : 'd';
+		foreach ($_GET AS $var => $value) {
+			$getParams .= '&' . $var . '=' . $value;
+		}
+		$url = 'index.php?v=' . trim($getParams, '&');
+		if (headers_sent()) {
+			echo '<script type="text/javascript">';
+			echo "window.location.href='$url';";
+			echo '</script>';
+		} else {
+			exit(header('Location: ' . $url));
+		}
+		die();
 	}
-	die();
-}
 
-require_once dirname(__FILE__) . "/core/php/core.inc.php";
+	require_once dirname(__FILE__) . "/core/php/core.inc.php";
 
-function include_authenticated_file($_folder, $_fn, $_type, $_plugin, $alert_folder) {
-	include_file('core', 'authentification', 'php');
-	try {
-		if (!isConnect()) {
-			throw new Exception('{{401 - Accès non autorisé}}');
+	function include_authenticated_file($_folder, $_fn, $_type, $_plugin, $alert_folder) {
+		include_file('core', 'authentification', 'php');
+		try {
+			if (!isConnect()) {
+				throw new Exception('{{401 - Accès non autorisé}}');
+			}
+			include_file($_folder, $_fn, $_type, $_plugin);
+		} catch (Exception $e) {
+			$_folder = 'desktop/modal';
+			if (init('plugin') != '') {
+				$_folder = 'plugins/' . init('plugin') . '/' . $_folder;
+			}
+			ob_end_clean(); //Clean pile after expetion (to prevent no-traduction)
+			echo '<div class="alert alert-danger div_alert">';
+			echo translate::exec(displayExeption($e), $alert_folder);
+			echo '</div>';
+		}
+	}
+	if (isset($_GET['v']) && $_GET['v'] == 'd') {
+		if (isset($_GET['modal'])) {
+			$alert_folder = 'desktop/modal/' . init('modal') . '.php';
+			if (init('plugin') != '') {
+				$alert_folder = 'plugins/' . init('plugin') . '/' . $alert_folder;
+			}
+			include_authenticated_file('desktop', init('modal'), 'modal', init('plugin'), $alert_folder);
+		} elseif (isset($_GET['configure'])) {
+			$alert_folder = 'plugin_info/configure.php';
+			if (init('plugin') != '') {
+				$alert_folder = 'plugins/' . init('plugin') . '/' . $alert_folder;
+			}
+			include_authenticated_file('plugin_info', 'configuration', 'configuration', init('plugin'), $alert_folder);
+		} elseif (isset($_GET['ajax']) && $_GET['ajax'] == 1) {
+			$alert_folder = 'desktop/php/' . init('modal') . '.php';
+			if (init('m') != '') {
+				$_folder = 'plugins/' . init('m') . '/' . $_folder;
+			}
+			include_authenticated_file('desktop', init('p'), 'php', init('m'), $alert_folder);
+		} else {
+			include_file('desktop', 'index', 'php');
+		}
+	} elseif (isset($_GET['v']) && $_GET['v'] == 'm') {
+		$_folder = 'mobile';
+		$_fn = 'index';
+		$_type = 'html';
+		$_plugin = '';
+		if (isset($_GET['modal'])) {
+			$_fn = init('modal');
+			$_type = 'modal';
+			$_plugin = init('plugin');
+		} elseif (isset($_GET['p']) && isset($_GET['ajax'])) {
+			$_fn = $_GET['p'];
+			$_plugin = isset($_GET['m']) ? $_GET['m'] : $_plugin;
 		}
 		include_file($_folder, $_fn, $_type, $_plugin);
-	} catch (Exception $e) {
-		$_folder = 'desktop/modal';
-		if (init('plugin') != '') {
-			$_folder = 'plugins/' . init('plugin') . '/' . $_folder;
-		}
-		ob_end_clean(); //Clean pile after expetion (to prevent no-traduction)
-		echo '<div class="alert alert-danger div_alert">';
-		echo translate::exec(displayExeption($e), $alert_folder);
-		echo '</div>';
-	}
-}
-if (isset($_GET['v']) && $_GET['v'] == 'd') {
-	if (isset($_GET['modal'])) {
-		$alert_folder = 'desktop/modal/' . init('modal') . '.php';
-		if (init('plugin') != '') {
-			$alert_folder = 'plugins/' . init('plugin') . '/' . $alert_folder;
-		}
-		include_authenticated_file('desktop', init('modal'), 'modal', init('plugin'), $alert_folder);
-	} elseif (isset($_GET['configure'])) {
-		$alert_folder = 'plugin_info/configure.php';
-		if (init('plugin') != '') {
-			$alert_folder = 'plugins/' . init('plugin') . '/' . $alert_folder;
-		}
-		include_authenticated_file('plugin_info', 'configuration', 'configuration', init('plugin'), $alert_folder);
-	} elseif (isset($_GET['ajax']) && $_GET['ajax'] == 1) {
-		$alert_folder = 'desktop/php/' . init('modal') . '.php';
-		if (init('m') != '') {
-			$_folder = 'plugins/' . init('m') . '/' . $_folder;
-		}
-		include_authenticated_file('desktop', init('p'), 'php', init('m'), $alert_folder);
 	} else {
-		include_file('desktop', 'index', 'php');
+		echo "Erreur: veuillez contacter l'administrateur";
 	}
-} elseif (isset($_GET['v']) && $_GET['v'] == 'm') {
-	$_folder = 'mobile';
-	$_fn = 'index';
-	$_type = 'html';
-	$_plugin = '';
-	if (isset($_GET['modal'])) {
-		$_fn = init('modal');
-		$_type = 'modal';
-		$_plugin = init('plugin');
-	} elseif (isset($_GET['p']) && isset($_GET['ajax'])) {
-		$_fn = $_GET['p'];
-		$_plugin = isset($_GET['m']) ? $_GET['m'] : $_plugin;
-	}
-	include_file($_folder, $_fn, $_type, $_plugin);
-} else {
-	echo "Erreur: veuillez contacter l'administrateur";
+} catch (Exception $e) {
+	echo $e->getMessage();
 }
