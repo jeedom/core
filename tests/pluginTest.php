0ac60282713a3de04a716987fff71151681f6178
Loïc <loic@jeedom.com>
2016-08-17 19:34:25 +0200

    fix test

--- tests/pluginTest.php
+++ tests/pluginTest.php
@@ -92,6 +92,7 @@ class pluginTest extends \PHPUnit_Framework_TestCase {
 	public function testCmdVirtualBinary($virtual) {
 		echo "\n" . __CLASS__ . '::' . __FUNCTION__ . ' : ';
 		$cmd = $virtual->getCmd(null, 'virtual_test_1');
+		$cmd->event(1);
 		$this->assertSame(1, $cmd->execCmd());
 		$cmd->event(0);
 		$this->assertSame(0, $cmd->execCmd());
