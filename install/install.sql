6a18f1cffcd3527c0a80de439dcb17c948686b8f
Loïc <loic@jeedom.com>
2016-09-21 13:05:57 +0200

    update sql install script

--- install/install.sql
+++ install/install.sql
@@ -24,7 +24,6 @@ CREATE TABLE IF NOT EXISTS `object` (
   `position` INT NULL,
   `configuration` TEXT NULL,
   `display` TEXT NULL,
-  `image` MEDIUMTEXT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `name_UNIQUE` (`name` ASC),
   INDEX `fk_object_object1_idx1` (`father_id` ASC),
