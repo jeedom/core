50ffa62213475a98a8920929714f74908126d47e
Loïc <loic@jeedom.com>
2016-09-24 10:57:08 +0200

    update rpi install

--- install/OS_specific/rpi/post-install.sh
+++ install/OS_specific/rpi/post-install.sh
@@ -1,13 +1,2 @@
 #!/bin/sh
-systemctl mask serial-getty@ttyAMA0.service
-systemctl stop serial-getty@ttyAMA0.service
-systemctl mask serial-getty@ttymxc0.service
-systemctl stop serial-getty@ttymxc0.service
-systemctl stop serial-getty@ttyS0.service
-systemctl mask serial-getty@ttyS0.service
-echo jeedom > /etc/hostname
-(echo "Mjeedom96";echo "Mjeedom96";) | passwd root
-rm /etc/motd
-wget https://raw.githubusercontent.com/jeedom/core/stable/install/motd -O /etc/motd
-rm -rf /root/.bashrc
-wget https://raw.githubusercontent.com/jeedom/core/stable/install/bashrc -O /root/.bashrc
+
