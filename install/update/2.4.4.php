2be1dcf02c061906b1f56c98b22fffebe5f0c074
Loïc <loic@jeedom.com>
2016-09-29 13:19:22 +0200

    Update 2.4.4.php

--- install/update/2.4.4.php
+++ install/update/2.4.4.php
@@ -5,4 +5,13 @@ try {
 } catch (Exception $exc) {
 	echo $exc->getMessage();
 }
+try {
+	foreach (plugin::listPlugin() as $plugin) {
+		if (file_exists(dirname(__FILE__) . '/../../plugins/' . $plugin->getId() . '/doc/fr_FR/.htaccess')) {
+			shell_exec('sudo rm -rf ' . dirname(__FILE__) . '/../../plugins/' . $plugin->getId() . '/doc/fr_FR/.htaccess');
+		}
+	}
+} catch (Exception $exc) {
+
+}
 ?>
