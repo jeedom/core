ce9bb6e512d4821f67ba244dd5e69eb8c1970379
sarakha63 <sarakha_ludovic@yahoo.fr>
2016-10-28 13:59:28 +0200

    Update index.php

--- desktop/php/index.php
+++ desktop/php/index.php
@@ -49,9 +49,9 @@ if (count($plugins_list) > 0) {
 				$plugin = $pluginList;
 				$title = ucfirst($plugin->getName()) . ' - Jeedom';
 			}
-			$plugin_menu .= '<li><a href="index.php?v=d&m=' . $pluginList->getId() . '&p=' . $pluginList->getIndex() . '"><img class="img-responsive" style="width : 20px;display:inline-block;" src="' . $pluginList->getPathImgIcon() . '" /> ' . $pluginList->getName() . ' </a></li>';
+			$plugin_menu .= '<li style="padding-right:10px"><a href="index.php?v=d&m=' . $pluginList->getId() . '&p=' . $pluginList->getIndex() . '"><img class="img-responsive" style="width : 20px;display:inline-block;" src="' . $pluginList->getPathImgIcon() . '" /> ' . $pluginList->getName() . '</a></li>';
 			if ($pluginList->getDisplay() != '' && config::bykey('displayDesktopPanel', $pluginList->getId(), 0) != 0) {
-				$panel_menu .= '<li><a href="index.php?v=d&m=' . $pluginList->getId() . '&p=' . $pluginList->getDisplay() . '"><img class="img-responsive" style="width : 20px;display:inline-block;" src="' . $pluginList->getPathImgIcon() . '" /> ' . $pluginList->getName() . ' </a></li>';
+				$panel_menu .= '<li style="padding-right:10px"><a href="index.php?v=d&m=' . $pluginList->getId() . '&p=' . $pluginList->getDisplay() . '"><img class="img-responsive" style="width : 20px;display:inline-block;" src="' . $pluginList->getPathImgIcon() . '" /> ' . $pluginList->getName() . '</a></li>';
 			}
 			if ($pluginList->getEventjs() == 1) {
 				$eventjs_plugin[] = $pluginList->getId();
