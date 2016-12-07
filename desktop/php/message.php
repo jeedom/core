23987b99c4fc5518444941f609d4962ae6b52714
Loïc <loic@jeedom.com>
2016-12-07 18:40:41 +0100

    Revert "Merge pull request #515 from Ethal/patch-6"
    This reverts commit 72df798b80e7c849928cd5a098d6ce4da09375f1, reversing
    changes made to 56a866f775640edd1b68024f3d5de1f7204f5671.

--- desktop/php/message.php
+++ desktop/php/message.php
@@ -34,7 +34,7 @@ foreach (message::listPlugin() as $plugin) {
         <?php
 foreach ($listMessage as $message) {
 	echo '<tr data-message_id="' . $message->getId() . '">';
-	echo '<td style = "text-align : center;" ><i class="fa fa-trash-o cursor removeMessage"></i></td>';
+	echo '<td><center><i class="fa fa-trash-o cursor removeMessage"></i></center></td>';
 	echo '<td class="datetime">' . $message->getDate() . '</td>';
 	echo '<td class="plugin">' . $message->getPlugin() . '</td>';
 	echo '<td class="message">' . html_entity_decode($message->getMessage()) . '</td>';
