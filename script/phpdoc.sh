5ec9986c816856aeb9d871d6c3586e54363fdc1e
Loïc <loic@jeedom.com>
2017-01-30 16:49:55 +0100

    bugfix

--- script/phpdoc.sh
+++ script/phpdoc.sh
@@ -13,13 +13,13 @@ if [ "$TRAVIS_REPO_SLUG" == "jeedom/core" ] && [ "$TRAVIS_PULL_REQUEST" == "fals
   mkdir -p /usr/jeedom
   cd /usr/jeedom
   git clone --branch=gh-pages https://github.com/jeedom/documentation.git
-  if [ ! -f /usr/jeedom/documenation/phpdoc ]; then
-    mkdir -p /usr/jeedom/documenation/phpdoc
+  if [ ! -f /usr/jeedom/documentation/phpdoc ]; then
+    mkdir -p /usr/jeedom/documentation/phpdoc
   fi
-  cd /usr/jeedom/documenation/phpdoc
-  rm -rf /usr/jeedom/documenation/phpdoc/*
-  cp -Rf $HOME/docs-latest/* /usr/jeedom/documenation/phpdoc/
-  cd /usr/jeedom/documenation/phpdoc
+  cd /usr/jeedom/documentation/phpdoc
+  rm -rf /usr/jeedom/documentation/phpdoc/*
+  cp -Rf $HOME/docs-latest/* /usr/jeedom/documentation/phpdoc/
+  cd /usr/jeedom/documentation
   git add -f .
   git commit -m "PHPDocumentor (Travis Build : $TRAVIS_BUILD_NUMBER  - Branch : $TRAVIS_BRANCH)"
   git push -fq origin gh-pages
