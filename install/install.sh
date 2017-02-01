e2ed4a70db896c120faa4577c26aa195f9937517
Loïc <loic@jeedom.com>
2017-02-01 11:53:00 +0100

    use dynamic path

--- install/install.sh
+++ install/install.sh
@@ -281,7 +281,7 @@ step_10_jeedom_post() {
 	echo "---------------------------------------------------------------------"
 	echo "${JAUNE}Start step_10_jeedom_post${NORMAL}"
 	if [ ! -f /etc/cron.d/jeedom ]; then
-		echo "* * * * * www-data /usr/bin/php /var/www/html/core/php/jeeCron.php >> /dev/null" > /etc/cron.d/jeedom
+		echo "* * * * * www-data /usr/bin/php ${WEBSERVER_HOME}/core/php/jeeCron.php >> /dev/null" > /etc/cron.d/jeedom
 		if [ $? -ne 0 ]; then
 	    	echo "${ROUGE}Could not install jeedom cron - abort${NORMAL}"
 	    	exit 1
