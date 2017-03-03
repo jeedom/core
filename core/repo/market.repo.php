791a4173712f7b243140df59b0d63ec3824e7799
Loïc <loic@jeedom.com>
2017-03-03 16:16:25 +0100

    try fix doc link for third plugin

--- core/repo/market.repo.php
+++ core/repo/market.repo.php
@@ -156,9 +156,14 @@ class repo_market {
 	}
 
 	public static function objectInfo($_update) {
+		$url = 'https://jeedom.github.io/documentation/plugins/' . $_update->getLogicalId() . '/' . config::byKey('language', 'core', 'fr_FR') . '/index.html';
+		$header = get_headers($url);
+		if (strpos($header[0], '200') === false) {
+			$url = 'https://jeedom.github.io/documentation/third_plugins/' . $_update->getLogicalId() . '/' . config::byKey('language', 'core', 'fr_FR') . '/index.html';
+		}
 		return array(
-			'doc' => 'https://jeedom.github.io/documentation/plugins/' . $_update->getLogicalId() . '/' . config::byKey('language', 'core', 'fr_FR') . '/index.html',
-			'changelog' => 'https://jeedom.github.io/documentation/plugins/' . $_update->getLogicalId() . '/' . config::byKey('language', 'core', 'fr_FR') . '/index.html#_changelog',
+			'doc' => $url,
+			'changelog' => $url . '#_changelog',
 			'display' => 'https://market.jeedom.fr/index.php?v=d&p=market&type=plugin&plugin_id=' . $_update->getLogicalId(),
 		);
 	}
