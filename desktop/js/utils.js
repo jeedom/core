982140e771588f21446cbf0994228590e5a9e939
Loïc <loic@jeedom.com>
2017-03-13 13:26:11 +0100

    prevent header

--- desktop/js/utils.js
+++ desktop/js/utils.js
@@ -38,7 +38,7 @@
 }
 
 $(function () {
-   toastr.options = {
+ toastr.options = {
     "closeButton": true,
     "debug": false,
     "positionClass": "toast-top-right",
@@ -250,9 +250,9 @@ if (isset(jeedom_langage)) {
     });
 
     $('#bt_showEventInRealTime').on('click',function(){
-       $('#md_modal').dialog({title: "{{Evénement en temps réel}}"});
-       $("#md_modal").load('index.php?v=d&modal=log.display&log=event').dialog('open');
-   });
+     $('#md_modal').dialog({title: "{{Evénement en temps réel}}"});
+     $("#md_modal").load('index.php?v=d&modal=log.display&log=event').dialog('open');
+ });
 
     $('#bt_gotoDashboard').on('click',function(){
         $('ul.dropdown-menu [data-toggle=dropdown]').parent().parent().parent().siblings().removeClass('open');
@@ -334,12 +334,13 @@ function initExpertMode() {
 
 function initReportMode() {
     if (getUrlVars('report') == 1) {
-        $('.reportModeVisible').show();
-        $('.reportModeHidden').hide();
-    } else {
-        $('.reportModeVisible').hide();
-        $('.reportModeHidden').show();
-    }
+       $('header').hide();
+       $('footer').hide();
+       $('#div_mainContainer').css('margin-top', '-50px');
+       $('#wrap').css('margin-bottom', '0px');
+       $('.reportModeVisible').show();
+       $('.reportModeHidden').hide();
+   } 
 }
 
 function initTableSorter() {
@@ -484,12 +485,12 @@ function positionEqLogic(_id,_preResize) {
         var eqLogic = $('.eqLogic-widget[data-eqlogic_id='+_id+']');
         eqLogic.css('margin','0px').css('padding','0px');
         if(init(_preResize,true)){
-         eqLogic.width(Math.floor(eqLogic.width() / 40) * 40);
-         eqLogic.height(Math.floor(eqLogic.height() / 80) * 80);
-     }
-     eqLogic.width(Math.ceil(eqLogic.width() / 40) * 40 + (Math.ceil(eqLogic.width() / 40)-1) * 2);
-     eqLogic.height(Math.ceil(eqLogic.height() / 80) * 80 + (Math.ceil(eqLogic.height() / 80)-1) * 2);
- }else{
+           eqLogic.width(Math.floor(eqLogic.width() / 40) * 40);
+           eqLogic.height(Math.floor(eqLogic.height() / 80) * 80);
+       }
+       eqLogic.width(Math.ceil(eqLogic.width() / 40) * 40 + (Math.ceil(eqLogic.width() / 40)-1) * 2);
+       eqLogic.height(Math.ceil(eqLogic.height() / 80) * 80 + (Math.ceil(eqLogic.height() / 80)-1) * 2);
+   }else{
     $('.eqLogic-widget:not(.jeedomAlreadyPosition)').css('margin','0px').css('padding','0px');
     $('.eqLogic-widget:not(.jeedomAlreadyPosition)').each(function () {
         $(this).width(Math.ceil($(this).width() / 40) * 40 + (Math.ceil($(this).width() / 40)-1) * 2);
