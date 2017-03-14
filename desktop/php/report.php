6b9d3352753c3deb772cb264fde3061c93895503
Loïc <loic@jeedom.com>
2017-03-14 11:29:14 +0100

    Bugfix

--- desktop/php/report.php
+++ desktop/php/report.php
@@ -52,66 +52,65 @@ foreach (plugin::listPlugin(true) as $plugin) {
 		continue;
 	}
 	$number = count(ls($report_path . '/plugin/' . $plugin->getId(), '*'));
-	echo '<li class="cursor li_reportType" data-type="plan" data-id="' . $plugin->getId() . '"><a>' . $plugin->getName() . ' (<span class="number">' . $number . '</span>)</a></li>';
+	echo '<li class="cursor li_reportType" data-type="plugin" data-id="' . $plugin->getId() . '"><a>' . $plugin->getName() . ' (<span class="number">' . $number . '</span>)</a></li>';
 }
 ?>
 			</ul>
 		</div>
 	</div>
-</div>
 
-<div class="col-lg-2 col-md-3 col-sm-3" style="margin-top : 4px;">
-	<div class="bs-sidebar" style="margin-top : 4px;">
-		<ul id="ul_report" class="nav nav-list bs-sidenav">
-			<li class="nav-header"><i class="fa fa-newspaper-o"></i> {{Rapport}}</li>
-			<li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
-		</ul>
+	<div class="col-lg-2 col-md-3 col-sm-3" style="margin-top : 4px;">
+		<div class="bs-sidebar" style="margin-top : 4px;">
+			<ul id="ul_report" class="nav nav-list bs-sidenav">
+				<li class="nav-header"><i class="fa fa-newspaper-o"></i> {{Rapport}}</li>
+				<li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
+			</ul>
+		</div>
 	</div>
-</div>
 
-<div class="col-lg-8 col-md-6 col-sm-6" style="margin-top : 4px;display:none;" id="div_reportForm">
-	<form class="form-horizontal">
-		<fieldset>
-			<legend>{{Général}}
-				<a class="btn btn-danger btn-xs pull-right" id="bt_removeAll"><i class="fa fa-trash"></i> {{Tout supprimer}}</a>
-				<a class="btn btn-warning btn-xs pull-right" id="bt_remove"><i class="fa fa-times"></i> {{Supprimer}}</a>
-				<a class="btn btn-success btn-xs pull-right" id="bt_download"><i class="fa fa-download"></i> {{Télécharger}}</a>
-			</legend>
-			<div class="form-group">
-				<label class="col-sm-2 col-xs-2 control-label">{{Nom}}</label>
-				<div class="col-sm-4 col-xs-4">
-					<input class="form-control reportAttr" type="text" data-l1key="filename" disabled />
+	<div class="col-lg-8 col-md-6 col-sm-6" style="margin-top : 4px;display:none;" id="div_reportForm">
+		<form class="form-horizontal">
+			<fieldset>
+				<legend>{{Général}}
+					<a class="btn btn-danger btn-xs pull-right" id="bt_removeAll"><i class="fa fa-trash"></i> {{Tout supprimer}}</a>
+					<a class="btn btn-warning btn-xs pull-right" id="bt_remove"><i class="fa fa-times"></i> {{Supprimer}}</a>
+					<a class="btn btn-success btn-xs pull-right" id="bt_download"><i class="fa fa-download"></i> {{Télécharger}}</a>
+				</legend>
+				<div class="form-group">
+					<label class="col-sm-2 col-xs-2 control-label">{{Nom}}</label>
+					<div class="col-sm-4 col-xs-4">
+						<input class="form-control reportAttr" type="text" data-l1key="filename" disabled />
+					</div>
 				</div>
-			</div>
-			<div class="form-group">
-				<label class="col-sm-2 col-xs-2 control-label">{{Chemin}}</label>
-				<div class="col-sm-8 col-xs-8">
-					<input class="form-control reportAttr" type="text" data-l1key="path" disabled />
+				<div class="form-group">
+					<label class="col-sm-2 col-xs-2 control-label">{{Chemin}}</label>
+					<div class="col-sm-8 col-xs-8">
+						<input class="form-control reportAttr" type="text" data-l1key="path" disabled />
+					</div>
 				</div>
-			</div>
-			<div class="form-group">
-				<label class="col-sm-2 col-xs-2 control-label">{{Type}}</label>
-				<div class="col-sm-4 col-xs-4">
-					<input class="form-control reportAttr" type="text" data-l1key="type" disabled />
+				<div class="form-group">
+					<label class="col-sm-2 col-xs-2 control-label">{{Type}}</label>
+					<div class="col-sm-4 col-xs-4">
+						<input class="form-control reportAttr" type="text" data-l1key="type" disabled />
+					</div>
 				</div>
-			</div>
-			<div class="form-group">
-				<label class="col-sm-2 col-xs-2 control-label">{{Id rapport}}</label>
-				<div class="col-sm-4 col-xs-4">
-					<input class="form-control reportAttr" type="text" data-l1key="id" disabled />
+				<div class="form-group">
+					<label class="col-sm-2 col-xs-2 control-label">{{Id rapport}}</label>
+					<div class="col-sm-4 col-xs-4">
+						<input class="form-control reportAttr" type="text" data-l1key="id" disabled />
+					</div>
 				</div>
-			</div>
-			<div class="form-group">
-				<label class="col-sm-2 col-xs-2 control-label">{{Extension}}</label>
-				<div class="col-sm-4 col-xs-4">
-					<input class="form-control reportAttr" type="text" data-l1key="extension" disabled />
+				<div class="form-group">
+					<label class="col-sm-2 col-xs-2 control-label">{{Extension}}</label>
+					<div class="col-sm-4 col-xs-4">
+						<input class="form-control reportAttr" type="text" data-l1key="extension" disabled />
+					</div>
 				</div>
-			</div>
 
-		</fieldset>
-	</form>
+			</fieldset>
+		</form>
 
-</div>
+	</div>
 
 </div>
 
