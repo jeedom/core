36e06261e66d7113fb9ad2e5593af40e9bdc104c
Loïc <loic@jeedom.com>
2017-03-20 14:45:51 +0100

    improve update

--- install/update/3.0.8.php
+++ install/update/3.0.8.php
@@ -1,8 +1,9 @@
 <?php
 echo 'Move cache and tmp jeedom to new folder (/tmp/jeedom). It can take some times....';
 jeedom::stop();
+cache::restore();
 shell_exec('sudo mkdir -p /tmp/jeedom');
-shell_exec('sudo mv /tmp/jeedom-cache /tmp/jeedom/cache');
+shell_exec('sudo rm -rf  /tmp/jeedom-cache');
 shell_exec('sudo touch /tmp/jeedom/started');
 shell_exec('sudo chmod 777 -R /tmp/jeedom');
 jeedom::start();
