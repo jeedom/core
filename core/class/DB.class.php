b93d33baad349b90d286366b5e6b526e3520f1e3
ColonelMoutarde <sanchezluc@gmail.com>
2017-04-21 14:48:53 +0200

    Boolean should be compared strictly

--- core/class/DB.class.php
+++ core/class/DB.class.php
@@ -80,15 +80,15 @@ class DB {
 		$stmt = self::getConnection()->prepare($_query);
 		$res = NULL;
 
-		if ($stmt != false && $stmt->execute($_params) != false) {
+		if ($stmt !== false && $stmt->execute($_params) !== false) {
 			if ($_fetchType == self::FETCH_TYPE_ROW) {
-				if ($_fetch_opt == NULL) {
+				if ($_fetch_opt === null) {
 					$res = $stmt->fetch($_fetch_param);
 				} else if ($_fetch_param == PDO::FETCH_CLASS) {
 					$res = $stmt->fetchObject($_fetch_opt);
 				}
 			} else {
-				if ($_fetch_opt == NULL) {
+				if ($_fetch_opt === null) {
 					$res = $stmt->fetchAll($_fetch_param);
 				} else {
 					$res = $stmt->fetchAll($_fetch_param, $_fetch_opt);
@@ -489,4 +489,3 @@ class DB {
 
 }
 
-?>
