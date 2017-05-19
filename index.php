b08ad1c5485f31f47a2f7d9d2c8fd654c5d3916e
Loïc <loic@jeedom.com>
2017-05-19 12:06:00 +0200

    bugfix

--- index.php
+++ index.php
@@ -40,44 +40,17 @@ try {
 	}
 
 	require_once dirname(__FILE__) . "/core/php/core.inc.php";
-
-	function include_authenticated_file($_folder, $_fn, $_type, $_plugin, $alert_folder) {
-		include_file('core', 'authentification', 'php');
-		try {
-			if (!isConnect()) {
-				throw new Exception('{{401 - Accès non autorisé}}');
-			}
-			include_file($_folder, $_fn, $_type, $_plugin);
-		} catch (Exception $e) {
-			$_folder = 'desktop/modal';
-			if (init('plugin') != '') {
-				$_folder = 'plugins/' . init('plugin') . '/' . $_folder;
-			}
-			ob_end_clean(); //Clean pile after expetion (to prevent no-traduction)
-			echo '<div class="alert alert-danger div_alert">';
-			echo translate::exec(displayExeption($e), $alert_folder);
-			echo '</div>';
-		}
-	}
 	if (isset($_GET['v']) && $_GET['v'] == 'd') {
 		if (isset($_GET['modal'])) {
-			$alert_folder = 'desktop/modal/' . init('modal') . '.php';
-			if (init('plugin') != '') {
-				$alert_folder = 'plugins/' . init('plugin') . '/' . $alert_folder;
-			}
-			include_authenticated_file('desktop', init('modal'), 'modal', init('plugin'), $alert_folder);
+			include_file('desktop', init('modal'), 'modal', init('plugin'));
 		} elseif (isset($_GET['configure'])) {
 			$alert_folder = 'plugin_info/configure.php';
 			if (init('plugin') != '') {
 				$alert_folder = 'plugins/' . init('plugin') . '/' . $alert_folder;
 			}
-			include_authenticated_file('plugin_info', 'configuration', 'configuration', init('plugin'), $alert_folder);
+			include_file('plugin_info', 'configuration', 'configuration', init('plugin'));
 		} elseif (isset($_GET['ajax']) && $_GET['ajax'] == 1) {
-			$alert_folder = 'desktop/php/' . init('modal') . '.php';
-			if (init('m') != '') {
-				$_folder = 'plugins/' . init('m') . '/' . $_folder;
-			}
-			include_authenticated_file('desktop', init('p'), 'php', init('m'), $alert_folder);
+			include_file('desktop', init('p'), 'php', init('m'));
 		} else {
 			include_file('desktop', 'index', 'php');
 		}
