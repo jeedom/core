e54cd5a413249718b845b996e1cfbbab13e4ccfa
sarakha63 <sarakha63@DESKTOP-D9IGIEV>
2017-06-23 17:08:17 +0200

    make it more visible

--- install/update/3.0.9.php
+++ install/update/3.0.9.php
@@ -6,8 +6,11 @@ if (config::byKey('update3.09first', 'core', 0) == 0) {
 	echo "OK\n";
 	echo 'Relance de la mise à jour (normal). A partir de là il faut encore attendre 5 minutes.';
 	echo 'La log peut etre illisible pour suivre l\'avancement allez dans log puis choississez update.';
+	echo 'Relance de la mise à jour (normal). A partir de là il faut encore attendre 5 minutes.';
 	echo 'La log peut etre illisible pour suivre l\'avancement allez dans log puis choississez update.';
+	echo 'Relance de la mise à jour (normal). A partir de là il faut encore attendre 5 minutes.';
 	echo 'La log peut etre illisible pour suivre l\'avancement allez dans log puis choississez update.';
+	echo 'Relance de la mise à jour (normal). A partir de là il faut encore attendre 5 minutes.';
 	echo 'La log peut etre illisible pour suivre l\'avancement allez dans log puis choississez update.';
 	sleep(20);
 	config::save('update3.09first', 1);
