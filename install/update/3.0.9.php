8055f72616e98304b6f218addd9e7dc0cfc78dde
sarakha63 <sarakha63@DESKTOP-D9IGIEV>
2017-06-23 17:01:46 +0200

    final version

--- install/update/3.0.9.php
+++ install/update/3.0.9.php
@@ -1,12 +1,16 @@
 <?php
-if (config::byKey('update1.309first', 'core', 0) == 0) {
+if (config::byKey('update3.09first', 'core', 0) == 0) {
 	echo 'Remove phpsysinfo and adminer';
 	shell_exec('sudo rm -rf ' . dirname(__FILE__) . '/../../../adminer*');
 	shell_exec('sudo rm -rf ' . dirname(__FILE__) . '/../../../sysinfo*');
 	echo "OK\n";
-	echo 'Relance de la mise à jour (normal). La log peut etre illisible pour suivre l\'avancement allez sur dans log puis choississez update';
+	echo 'Relance de la mise à jour (normal). A partir de là il faut encore attendre 5 minutes.';
+	echo 'La log peut etre illisible pour suivre l\'avancement allez dans log puis choississez update.';
+	echo 'La log peut etre illisible pour suivre l\'avancement allez dans log puis choississez update.';
+	echo 'La log peut etre illisible pour suivre l\'avancement allez dans log puis choississez update.';
+	echo 'La log peut etre illisible pour suivre l\'avancement allez dans log puis choississez update.';
 	sleep(20);
-	config::save('update1.309first', 1);
+	config::save('update3.09first', 1);
 	jeedom::update();
 	die();
 } else {
