88443b8911b42b6a10b76984ff0403462338dcf8
sarakha63 <sarakha63@DESKTOP-D9IGIEV>
2017-06-23 12:14:59 +0200

    avoid get sudo

--- install/update/3.0.9.php
+++ install/update/3.0.9.php
@@ -1,6 +1,6 @@
 <?php
 echo 'Remove phpsysinfo and adminer';
-shell_exec(system::getCmdSudo() . 'rm -rf ' . dirname(__FILE__) . '/../../../adminer*');
-shell_exec(system::getCmdSudo() . 'rm -rf ' . dirname(__FILE__) . '/../../../sysinfo*');
+shell_exec('sudo rm -rf ' . dirname(__FILE__) . '/../../../adminer*');
+shell_exec('sudo rm -rf ' . dirname(__FILE__) . '/../../../sysinfo*');
 echo "OK\n";
 ?>
