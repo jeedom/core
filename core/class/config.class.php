4d973b7230d5eae50c38f5c86f1d792b13418b31
ColonelMoutarde <sanchezluc@gmail.com>
2017-06-27 14:24:09 +0200

    fix

--- core/class/config.class.php
+++ core/class/config.class.php
@@ -46,11 +46,11 @@ class config {
         /**
 	 * Ajoute une clef à la config
          * @param string $_key
-         * @param string $_value
+         * @param string | object | array $_value
          * @param string $_plugin
          * @return boolean
          */
-	public static function save(string $_key,string $_value, string $_plugin = 'core') {
+	public static function save(string $_key, $_value, string $_plugin = 'core') {
 		if (is_object($_value) || is_array($_value)) {
 			$_value = json_encode($_value, JSON_UNESCAPED_UNICODE);
 		}
