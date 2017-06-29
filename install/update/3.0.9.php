3fad73efe0e8a9441490f1d65e1c51979d31e5c5
sarakha63 <sarakha63@DESKTOP-D9IGIEV>
2017-06-29 15:08:46 +0200

    update

--- install/update/3.0.9.php
+++ install/update/3.0.9.php
@@ -5,6 +5,8 @@ if (config::byKey('update3.09firstupd', 'core', 0) == 0) {
 		shell_exec('sudo rm -rf ' . dirname(__FILE__) . '/../../../adminer*');
 		shell_exec('sudo rm -rf ' . dirname(__FILE__) . '/../../../sysinfo*');
 		echo "OK\n";
+		jeedom::stop();
+		shell_exec('sudo rm -rf  /tmp/jeedom-cache');
 		shell_exec('sudo mv /home/jeedomtmp/jeedom* /tmp');
 		shell_exec('sudo rm -rf /home/jeedomtmp');
 		try {
@@ -15,7 +17,6 @@ if (config::byKey('update3.09firstupd', 'core', 0) == 0) {
 			echo "***ERREUR*** " . $ex->getMessage() . "\n";
 		}
 		echo 'Move cache and tmp jeedom to new folder (/tmp/jeedom). It can take some times....\n';
-		jeedom::stop();
 		sleep(5);
 		shell_exec('sudo mkdir -p /tmp/jeedom');
 		shell_exec('sudo rm -rf  /tmp/jeedom/cache;sudo mv /tmp/jeedom-cache /tmp/jeedom/cache');
