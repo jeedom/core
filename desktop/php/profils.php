12f0844067f1ece7e1aafa1bb70487c4b1225310
Loïc <loic@jeedom.com>
2017-07-18 11:33:39 +0200

    review profils page

--- desktop/php/profils.php
+++ desktop/php/profils.php
@@ -14,85 +14,83 @@ foreach (plugin::listPlugin() as $pluginList) {
 	}
 }
 ?>
-<legend><i class="fa fa-user"></i>  {{Profil}}</legend>
+<div style="margin-top: 5px;">
+<a class="btn btn-success pull-right" id="bt_saveProfils"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
+<ul class="nav nav-tabs" role="tablist">
+ <li role="presentation" class="active"><a href="#themetab" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-tint"></i> {{Thèmes}}</a></li>
+ <li role="presentation"><a href="#interfacetab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-laptop"></i> {{Interface}}</a></li>
+ <li role="presentation"><a href="#securitytab" aria-controls="profile" role="tab" data-toggle="tab"><i class="icon securite-key1"></i> {{Sécurité}}</a></li>
+ <li role="presentation"><a href="#notificationtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="icon securite-key1"></i> {{Notifications}}</a></li>
+</ul>
 
-<div class="panel-group" id="accordionConfiguration">
-  <input style="display: none;" class="userAttr form-control" data-l1key="id" />
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h3 class="panel-title">
-        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordionConfiguration" href="#config_themes">
-          <i class="fa fa-tint"></i>  {{Thèmes}}
-        </a>
-      </h3>
-    </div>
-    <div id="config_themes" class="panel-collapse collapse in">
-      <div class="panel-body">
-        <div class="pull-right img-responsive" id="div_imgThemeDesktop" style="height: 450px;"></div>
-        <form class="form-horizontal">
-          <fieldset>
-            <div class="form-group">
-              <label class="col-sm-3 control-label">{{Desktop}}</label>
-              <div class="col-sm-2">
-                <select class="userAttr form-control" data-l1key="options" data-l2key="bootstrap_theme">
-                  <option value="">Défaut</option>
-                  <?php
+<div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
+  <div role="tabpanel" class="tab-pane active" id="themetab">
+    <br/>
+    <div class="pull-right img-responsive" id="div_imgThemeDesktop" style="height: 450px;"></div>
+    <form class="form-horizontal">
+      <fieldset>
+        <div class="form-group">
+          <label class="col-sm-3 control-label">{{Desktop}}</label>
+          <div class="col-sm-2">
+            <select class="userAttr form-control" data-l1key="options" data-l2key="bootstrap_theme">
+              <option value="">Défaut</option>
+              <?php
 foreach (ls(dirname(__FILE__) . '/../../core/themes') as $dir) {
 	if (is_dir(dirname(__FILE__) . '/../../core/themes/' . $dir . '/desktop')) {
 		echo '<option value="' . trim($dir, '/') . '">' . ucfirst(str_replace('_', ' ', trim($dir, '/'))) . '</option>';
 	}
 }
 ?>
-              </select>
-            </div>
-          </div>
-          <div class="form-group">
-            <label class="col-sm-3 control-label">{{Mobile couleur}}</label>
-            <div class="col-sm-2">
-              <select class="userAttr form-control" data-l1key="options" data-l2key="mobile_theme_color">
-                <?php
+          </select>
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="col-sm-3 control-label">{{Mobile couleur}}</label>
+        <div class="col-sm-2">
+          <select class="userAttr form-control" data-l1key="options" data-l2key="mobile_theme_color">
+            <?php
 foreach (ls(dirname(__FILE__) . '/../../core/themes') as $dir) {
 	if (is_dir(dirname(__FILE__) . '/../../core/themes/' . $dir . '/mobile')) {
 		echo '<option value="' . trim($dir, '/') . '">' . ucfirst(str_replace('_', ' ', trim($dir, '/'))) . '</option>';
 	}
 }
 ?>
-            </select>
-          </div>
-        </div>
-        <div class="form-group">
-          <label class="col-sm-3 control-label">{{Graphique Desktop}}</label>
-          <div class="col-sm-2">
-            <select class="userAttr form-control" data-l1key="options" data-l2key="desktop_highcharts_theme">
-              <option value="">Défaut</option>
-              <option value="dark-blue">Dark-blue</option>
-              <option value="dark-green">Dark-green</option>
-              <option value="dark-unica">Dark-unica</option>
-              <option value="gray">Gray</option>
-              <option value="grid-light">Grid-light</option>
-              <option value="grid">Grid</option>
-              <option value="sand-signika">Sand-signika</option>
-              <option value="skies">Skies</option>
-            </select>
-          </div>
-        </div>
-        <div class="form-group">
-          <label class="col-sm-3 control-label">{{Graphique mobile}}</label>
-          <div class="col-sm-2">
-            <select class="userAttr form-control" data-l1key="options" data-l2key="mobile_highcharts_theme">
-              <option value="">Défaut</option>
-              <option value="dark-blue">Dark-blue</option>
-              <option value="dark-green">Dark-green</option>
-              <option value="dark-unica">Dark-unica</option>
-              <option value="gray">Gray</option>
-              <option value="grid-light">Grid-light</option>
-              <option value="grid">Grid</option>
-              <option value="sand-signika">Sand-signika</option>
-              <option value="skies">Skies</option>
-            </select>
-          </div>
-        </div>
-        <?php
+        </select>
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="col-sm-3 control-label">{{Graphique Desktop}}</label>
+      <div class="col-sm-2">
+        <select class="userAttr form-control" data-l1key="options" data-l2key="desktop_highcharts_theme">
+          <option value="">Défaut</option>
+          <option value="dark-blue">Dark-blue</option>
+          <option value="dark-green">Dark-green</option>
+          <option value="dark-unica">Dark-unica</option>
+          <option value="gray">Gray</option>
+          <option value="grid-light">Grid-light</option>
+          <option value="grid">Grid</option>
+          <option value="sand-signika">Sand-signika</option>
+          <option value="skies">Skies</option>
+        </select>
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="col-sm-3 control-label">{{Graphique mobile}}</label>
+      <div class="col-sm-2">
+        <select class="userAttr form-control" data-l1key="options" data-l2key="mobile_highcharts_theme">
+          <option value="">Défaut</option>
+          <option value="dark-blue">Dark-blue</option>
+          <option value="dark-green">Dark-green</option>
+          <option value="dark-unica">Dark-unica</option>
+          <option value="gray">Gray</option>
+          <option value="grid-light">Grid-light</option>
+          <option value="grid">Grid</option>
+          <option value="sand-signika">Sand-signika</option>
+          <option value="skies">Skies</option>
+        </select>
+      </div>
+    </div>
+    <?php
 foreach (jeedom::getConfiguration('eqLogic:displayType') as $key => $value) {
 	echo '<div class="form-group">';
 	echo '<label class="col-sm-3 control-label">{{Opacité par des widgets}} ' . $value['name'] . '</label>';
@@ -102,79 +100,58 @@ foreach (jeedom::getConfiguration('eqLogic:displayType') as $key => $value) {
 	echo '</div>';
 }
 ?>
-     </fieldset>
-   </form>
- </div>
-</div>
+ </fieldset>
+</form>
 </div>
 
-<div class="panel panel-default">
-  <div class="panel-heading">
-    <h3 class="panel-title">
-      <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordionConfiguration" href="#config_interface">
-        <i class="fa fa-laptop"></i>  {{Interface}}
-      </a>
-    </h3>
-  </div>
-  <div id="config_interface" class="panel-collapse collapse">
-    <div class="panel-body">
-      <form class="form-horizontal">
-        <fieldset>
-          <legend><i class="fa fa-home"></i>  {{Général}}</legend>
-          <div class="form-group">
-            <label class="col-sm-3 control-label">{{Afficher les menus}}</label>
-            <div class="col-sm-1">
-              <input type="checkbox" class="userAttr" data-l1key="options" data-l2key="doNotAutoHideMenu"/>
-            </div>
-          </div>
-        </fieldset>
-      </form>
-      <form class="form-horizontal">
-        <fieldset>
-          <legend><i class="fa fa-file-o"></i>  {{Page par défaut}}</legend>
-          <div class="form-group">
-            <label class="col-sm-3 control-label">{{Desktop}}</label>
-            <div class="col-sm-2">
-              <select class="userAttr form-control" data-l1key="options" data-l2key="homePage">
-                <?php
-foreach ($homePage as $key => $value) {
-	echo "<option value='$key'>$value</option>";
-}
-?>
-             </select>
-           </div>
-           <label class="col-sm-1 control-label">{{Mobile}}</label>
-           <div class="col-sm-2">
-            <select class="userAttr form-control" data-l1key="options" data-l2key="homePageMobile">
-              <option value="home">{{Accueil}}</option>
-              <?php
-foreach ($homePage as $key => $value) {
-	echo "<option value='$key'>$value</option>";
-}
-?>
-           </select>
-         </div>
-       </div>
-     </fieldset>
-   </form>
-   <form class="form-horizontal">
+<div role="tabpanel" class="tab-pane" id="interfacetab">
+<br/>
+  <form class="form-horizontal">
     <fieldset>
-     <legend><i class="fa fa-columns"></i>  {{Objet par défaut sur le dashboard}}</legend>
-     <div class="form-group">
-      <label class="col-sm-3 control-label">{{Desktop}}</label>
-      <div class="col-sm-2">
-        <select class="userAttr form-control" data-l1key="options" data-l2key="defaultDashboardObject">
+      <legend><i class="fa fa-home"></i>  {{Général}}</legend>
+      <div class="form-group">
+        <label class="col-sm-3 control-label">{{Afficher les menus}}</label>
+        <div class="col-sm-1">
+          <input type="checkbox" class="userAttr" data-l1key="options" data-l2key="doNotAutoHideMenu"/>
+        </div>
+      </div>
+    </fieldset>
+  </form>
+  <form class="form-horizontal">
+    <fieldset>
+      <legend><i class="fa fa-file-o"></i>  {{Page par défaut}}</legend>
+      <div class="form-group">
+        <label class="col-sm-3 control-label">{{Desktop}}</label>
+        <div class="col-sm-2">
+          <select class="userAttr form-control" data-l1key="options" data-l2key="homePage">
+            <?php
+foreach ($homePage as $key => $value) {
+	echo "<option value='$key'>$value</option>";
+}
+?>
+         </select>
+       </div>
+       <label class="col-sm-1 control-label">{{Mobile}}</label>
+       <div class="col-sm-2">
+        <select class="userAttr form-control" data-l1key="options" data-l2key="homePageMobile">
+          <option value="home">{{Accueil}}</option>
           <?php
-foreach (object::all() as $object) {
-	echo "<option value='" . $object->getId() . "'>" . $object->getName() . "</option>";
+foreach ($homePage as $key => $value) {
+	echo "<option value='$key'>$value</option>";
 }
 ?>
        </select>
      </div>
-     <label class="col-sm-1 control-label">{{Mobile}}</label>
-     <div class="col-sm-2">
-      <select class="userAttr form-control" data-l1key="options" data-l2key="defaultMobileObject">
-        <option value='all'>{{Tout}}</option>
+   </div>
+ </fieldset>
+</form>
+<form class="form-horizontal">
+  <fieldset>
+   <legend><i class="fa fa-columns"></i>  {{Objet par défaut sur le dashboard}}</legend>
+   <div class="form-group">
+    <label class="col-sm-3 control-label">{{Desktop}}</label>
+    <div class="col-sm-2">
+      <select class="userAttr form-control" data-l1key="options" data-l2key="defaultDashboardObject">
         <?php
 foreach (object::all() as $object) {
 	echo "<option value='" . $object->getId() . "'>" . $object->getName() . "</option>";
@@ -182,7 +159,18 @@ foreach (object::all() as $object) {
 ?>
      </select>
    </div>
+   <label class="col-sm-1 control-label">{{Mobile}}</label>
+   <div class="col-sm-2">
+    <select class="userAttr form-control" data-l1key="options" data-l2key="defaultMobileObject">
+      <option value='all'>{{Tout}}</option>
+      <?php
+foreach (object::all() as $object) {
+	echo "<option value='" . $object->getId() . "'>" . $object->getName() . "</option>";
+}
+?>
+   </select>
  </div>
+</div>
 </fieldset>
 </form>
 <form class="form-horizontal">
@@ -274,84 +262,74 @@ foreach (planHeader::all() as $plan) {
   </fieldset>
 </form>
 </div>
-</div>
-</div>
 
-<div class="panel panel-default">
-  <div class="panel-heading">
-    <h3 class="panel-title">
-      <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordionConfiguration" href="#config_other">
-        <i class="icon securite-key1"></i>  {{Sécurité & notifications}}
-      </a>
-    </h3>
-  </div>
-  <div id="config_other" class="panel-collapse collapse">
-    <div class="panel-body">
-      <form class="form-horizontal">
-        <fieldset>
-          <?php if (config::byKey('sso:allowRemoteUser') != 1) {
+<div role="tabpanel" class="tab-pane" id="securitytab">
+<br/>
+  <form class="form-horizontal">
+    <fieldset>
+      <?php if (config::byKey('sso:allowRemoteUser') != 1) {
 	?>
-           <div class="form-group">
-            <label class="col-lg-2 col-md-3 col-sm-4 col-xs-6 control-label">{{Authentification en 2 étapes}}</label>
-            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
-             <a class="btn btn-default" id="bt_configureTwoFactorAuthentification"><i class="fa fa-cogs"></i> {{Configurer}}</a>
-           </div>
-           <?php
+       <div class="form-group">
+        <label class="col-lg-2 col-md-3 col-sm-4 col-xs-6 control-label">{{Authentification en 2 étapes}}</label>
+        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
+         <a class="btn btn-default" id="bt_configureTwoFactorAuthentification"><i class="fa fa-cogs"></i> {{Configurer}}</a>
+       </div>
+       <?php
 if ($_SESSION['user']->getOptions('twoFactorAuthentification', 0) == 1) {
 		?>
-            <label class="col-lg-1 col-md-2 col-sm-2 col-xs-2 control-label">{{Actif}}</label>
-            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
-             <input type="checkbox" class="userAttr" data-l1key="options" data-l2key="twoFactorAuthentification" />
-           </div>
-           <?php }
+        <label class="col-lg-1 col-md-2 col-sm-2 col-xs-2 control-label">{{Actif}}</label>
+        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
+         <input type="checkbox" class="userAttr" data-l1key="options" data-l2key="twoFactorAuthentification" />
+       </div>
+       <?php }
 	?>
-         </div>
+     </div>
 
-         <div class="form-group">
-          <label class="col-lg-2 col-md-3 col-sm-4 col-xs-6 control-label">{{Mot de passe}}</label>
-          <div class="col-lg-3 col-md-4 col-sm-5 col-xs-6">
-            <input type="password" class="userAttr form-control" data-l1key="password" />
-          </div>
-        </div>
-        <div class="form-group">
-          <label class="col-lg-2 col-md-3 col-sm-4 col-xs-6 control-label">{{Retapez le mot de passe}}</label>
-          <div class="col-lg-3 col-md-4 col-sm-5 col-xs-6">
-            <input type="password" class="form-control" id="in_passwordCheck" />
-          </div>
-        </div>
-        <?php }
+     <div class="form-group">
+      <label class="col-lg-2 col-md-3 col-sm-4 col-xs-6 control-label">{{Mot de passe}}</label>
+      <div class="col-lg-3 col-md-4 col-sm-5 col-xs-6">
+        <input type="password" class="userAttr form-control" data-l1key="password" />
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="col-lg-2 col-md-3 col-sm-4 col-xs-6 control-label">{{Retapez le mot de passe}}</label>
+      <div class="col-lg-3 col-md-4 col-sm-5 col-xs-6">
+        <input type="password" class="form-control" id="in_passwordCheck" />
+      </div>
+    </div>
+    <?php }
 ?>
-        <div class="form-group expertModeVisible">
-          <label class="col-lg-2 col-md-3 col-sm-4 col-xs-6 control-label">{{Hash de l'utilisateur}}</label>
-          <div class="col-lg-3 col-md-4 col-sm-5 col-xs-6">
-            <span class="userAttr" data-l1key="hash"></span>
-          </div>
-          <div class="col-lg-2 col-md-3 col-sm-3">
-            <a class="btn btn-default form-control" id="bt_genUserKeyAPI"><i class="fa fa-refresh"></i> {{Regénérer}}</a>
-          </div>
-        </div>
-
-        <div class="form-group expertModeVisible">
-          <label class="col-lg-2 col-md-3 col-sm-4 col-xs-6 control-label">{{Commande de notification utilisateur}}</label>
-          <div class="col-lg-3 col-md-4 col-sm-5 col-xs-6">
-            <div class="input-group">
-              <input type="text" class="userAttr form-control" data-l1key="options" data-l2key="notification::cmd" />
-              <span class="input-group-btn">
-                <a class="btn btn-default cursor bt_selectWarnMeCmd" title="Rechercher une commande"><i class="fa fa-list-alt"></i></a>
-              </span>
-            </div>
-          </div>
-        </div>
-
-      </fieldset>
-    </form>
-  </div>
-</div>
+    <div class="form-group expertModeVisible">
+      <label class="col-lg-2 col-md-3 col-sm-4 col-xs-6 control-label">{{Hash de l'utilisateur}}</label>
+      <div class="col-lg-3 col-md-4 col-sm-5 col-xs-6">
+        <span class="userAttr" data-l1key="hash"></span>
+      </div>
+      <div class="col-lg-2 col-md-3 col-sm-3">
+        <a class="btn btn-default form-control" id="bt_genUserKeyAPI"><i class="fa fa-refresh"></i> {{Regénérer}}</a>
+      </div>
+    </div>
+  </fieldset>
+</form>
 </div>
 
+<div role="tabpanel" class="tab-pane" id="notificationtab">
 <br/>
-<div class="form-actions">
-  <a class="btn btn-success" id="bt_saveProfils"><i class="fa fa-check-circle icon-white"></i> {{Sauvegarder}}</a>
+  <form class="form-horizontal">
+    <fieldset>
+      <div class="form-group">
+        <label class="col-lg-2 col-md-3 col-sm-4 col-xs-6 control-label">{{Commande de notification utilisateur}}</label>
+        <div class="col-lg-3 col-md-4 col-sm-5 col-xs-6">
+          <div class="input-group">
+            <input type="text" class="userAttr form-control" data-l1key="options" data-l2key="notification::cmd" />
+            <span class="input-group-btn">
+              <a class="btn btn-default cursor bt_selectWarnMeCmd" title="Rechercher une commande"><i class="fa fa-list-alt"></i></a>
+            </span>
+          </div>
+        </div>
+      </div>
+    </fieldset>
+  </form>
+</div>
 </div>
 </div>
 <?php include_file("desktop", "profils", "js");?>
