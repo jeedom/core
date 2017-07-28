91d9bbbc63227f353af58159c45da33b34bd9ceb
Loïc <loic@jeedom.com>
2017-07-28 10:20:43 +0200

    bugfix

--- core/class/cmd.class.php
+++ core/class/cmd.class.php
@@ -1327,8 +1327,11 @@ class cmd {
 			return;
 		}
 		global $JEEDOM_INTERNAL_CONFIG;
+		$eqLogic = $this->getEqLogic();
+		$maxAlert = $eqLogic->getMaxCmdAlert();
+		$prevAlert = $eqLogic->getAlert();
 		$this->setCache('alertLevel', $_level);
-		if ($_level != 'none') {
+		if ($_level != 'none' && $prevAlert != $maxAlert) {
 			$message = __('Alert sur la commande ', __FILE__) . $this->getHumanName() . __(' niveau ', __FILE__) . $_level . __(' valeur : ', __FILE__) . $_value;
 			if ($this->getAlert($_level . 'during') != '' && $this->getAlert($_level . 'during') > 0) {
 				$message .= ' ' . __('pendant plus de ', __FILE__) . $this->getAlert($_level . 'during') . __(' minute(s)', __FILE__);
@@ -1351,9 +1354,7 @@ class cmd {
 				}
 			}
 		}
-		$eqLogic = $this->getEqLogic();
-		$maxAlert = $eqLogic->getMaxCmdAlert();
-		$prevAlert = $eqLogic->getAlert();
+
 		if ($prevAlert != $maxAlert) {
 			$status = array(
 				'warning' => 0,
