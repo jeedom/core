3d33c83b3a640b3d0e0c6ba7d04401c8802bfdbb
Loïc <loic@jeedom.com>
2017-08-07 15:33:12 +0200

    add filter on scenario summary

--- desktop/modal/scenario.summary.php
+++ desktop/modal/scenario.summary.php
@@ -7,19 +7,19 @@ if (!isConnect()) {
 <a class="btn btn-success btn-sm pull-right" id="bt_saveSummaryScenario"><i class="fa fa-check-circle"></i> {{Enregistrer}}</a>
 <a class="btn btn-default btn-sm pull-right" id="bt_refreshSummaryScenario"><i class="fa fa-refresh"></i> {{Rafraîchir}}</a>
 <br/><br/>
-<table class="table table-bordered table-condensed" id="table_scenarioSummary">
+<table class="table table-bordered table-condensed tablesorter" id="table_scenarioSummary">
 	<thead>
 		<tr>
 			<th>{{ID}}</th>
 			<th>{{Scénario}}</th>
 			<th>{{Statut}}</th>
 			<th>{{Dernier lancement}}</th>
-			<th>{{Actif}}</th>
-			<th>{{Visible}}</th>
-			<th>{{Multi lancement}}</th>
-			<th>{{Log}}</th>
-			<th>{{Mode synchrone}}</th>
-			<th>{{Actions}}</th>
+			<th data-sorter="false" data-filter="false">{{Actif}}</th>
+			<th data-sorter="false" data-filter="false">{{Visible}}</th>
+			<th data-sorter="false" data-filter="false">{{Multi lancement}}</th>
+			<th data-sorter="false" data-filter="false">{{Log}}</th>
+			<th data-sorter="false" data-filter="false">{{Mode synchrone}}</th>
+			<th data-sorter="false" data-filter="false">{{Actions}}</th>
 		</tr>
 	</thead>
 	<tbody>
@@ -28,6 +28,7 @@ if (!isConnect()) {
 </table>
 
 <script>
+	initTableSorter();
 	refreshScenarioSummary();
 
 	$('#bt_refreshSummaryScenario').off().on('click',function(){
@@ -103,6 +104,7 @@ if (!isConnect()) {
 					table.push(result);
 				}
 				$('#table_scenarioSummary tbody').append(table);
+				$("#table_scenarioSummary").trigger("update");
 
 				$('.bt_summarayViewLog').off().on('click',function(){
 					var tr = $(this).closest('tr');
