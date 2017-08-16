6b8b4d0d51ce539909f47dedc018b2be9761439a
Loïc <loic@jeedom.com>
2017-08-16 17:01:52 +0200

    typo

--- desktop/php/administration.php
+++ desktop/php/administration.php
@@ -347,7 +347,7 @@ echo $CONFIG['db']['password'];
 			<br/>
 			<form class="form-horizontal">
 				<fieldset>
-				<legend>{{LDAP}}</legend>
+					<legend>{{LDAP}}</legend>
 					<div class="form-group">
 						<label class="col-lg-2 col-md-3 col-sm-4 col-xs-6 control-label">{{Activer l'authentification LDAP}}</label>
 						<div class="col-sm-1">
@@ -1072,6 +1072,13 @@ echo '<span class="label label-primary" style="font-size:1em;"><span id="span_ca
 					<br/>
 					<form class="form-horizontal">
 						<fieldset>
+							<legend>{{Timeline}}</legend>
+							<div class="form-group">
+								<label class="col-lg-2 col-md-3 col-sm-4 col-xs-6 control-label">{{Nombre maximum d'évènements}}</label>
+								<div class="col-lg-3 col-md-4 col-sm-5 col-xs-6">
+									<input type="text" class="configKey form-control" data-l1key="timeline::maxevent"/>
+								</div>
+							</div>
 							<legend>{{Messages}}</legend>
 							<div class="form-group">
 								<label class="col-lg-2 col-md-3 col-sm-4 col-xs-6 control-label">{{Ajouter un message à chaque erreur dans les logs}}</label>
@@ -1120,13 +1127,6 @@ foreach ($JEEDOM_INTERNAL_CONFIG['alerts'] as $level => $value) {
 					</form>
 					<form class="form-horizontal">
 						<fieldset>
-						<legend>{{Timeline}}</legend>
-						<div class="form-group">
-								<label class="col-lg-2 col-md-3 col-sm-4 col-xs-6 control-label">{{Nombre maximum d'évènements}}</label>
-								<div class="col-lg-3 col-md-4 col-sm-5 col-xs-6">
-									<input type="text" class="configKey form-control" data-l1key="timeline::maxevent"/>
-								</div>
-							</div>
 							<legend>{{Log}}</legend>
 							<div class="form-group">
 								<label class="col-lg-2 col-md-3 col-sm-4 col-xs-6 control-label">{{Moteur de log}}</label>
