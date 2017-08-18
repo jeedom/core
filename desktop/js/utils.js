5bedef1b6a84a0d5828f505e47071a7f19d69408
Loïc <loic@jeedom.com>
2017-08-18 10:23:06 +0200

    improve ui

--- desktop/js/utils.js
+++ desktop/js/utils.js
@@ -178,7 +178,7 @@ if (isset(jeedom_langage)) {
         closeText: '',
         height: ((jQuery(window).height() - 100) < 700) ? jQuery(window).height() - 100 : 700,
         width: ((jQuery(window).width() - 100) < 900) ? (jQuery(window).width() - 100) : 900,
-        position: {my: 'center', at: 'center bottom-10px', of: window},
+        position: { my: "center bottom-10", at: "center bottom", of: window },
         open: function () {
             $("body").css({overflow: 'hidden'})
             $(this).closest( ".ui-dialog" ).find(":button").blur();
@@ -196,7 +196,7 @@ if (isset(jeedom_langage)) {
         closeText: '',
         height: (jQuery(window).height() - 100),
         width: ((jQuery(window).width() - 100) < 1500) ? (jQuery(window).width() - 50) : 1500,
-        position: {my: 'center', at: 'center bottom-10px', of: window},
+        position: { my: "center bottom-10", at: "center bottom", of: window },
         open: function () {
             $("body").css({overflow: 'hidden'});
             $(this).closest( ".ui-dialog" ).find(":button").blur();
@@ -246,17 +246,17 @@ if (isset(jeedom_langage)) {
     });
 
     $('#bt_getHelpPage').on('click',function(){
-     jeedom.getDocumentationUrl({
+       jeedom.getDocumentationUrl({
         plugin: $(this).attr('data-plugin'),
         page: $(this).attr('data-page'),
         error: function(error) {
             $('#div_alert').showAlert({message: error.message, level: 'danger'});
         },
         success: function(url) {
-         window.open(url,'_blank');
-     }
- });
- });
+           window.open(url,'_blank');
+       }
+   });
+   });
 
     /******************Gestion mode expert**********************/
 
@@ -326,9 +326,9 @@ if (isset(jeedom_langage)) {
     });
 
     $('#bt_showEventInRealTime').on('click',function(){
-     $('#md_modal').dialog({title: "{{Evénement en temps réel}}"});
-     $("#md_modal").load('index.php?v=d&modal=log.display&log=event').dialog('open');
- });
+       $('#md_modal').dialog({title: "{{Evénement en temps réel}}"});
+       $("#md_modal").load('index.php?v=d&modal=log.display&log=event').dialog('open');
+   });
 
     $('#bt_gotoDashboard').on('click',function(){
         $('ul.dropdown-menu [data-toggle=dropdown]').parent().parent().parent().siblings().removeClass('open');
@@ -351,8 +351,8 @@ if (isset(jeedom_langage)) {
     });
 
     $('body').on('click','.objectSummaryParent',function(){
-     loadPage('index.php?v=d&p=dashboard&summary='+$(this).data('summary')+'&object_id='+$(this).data('object_id'));
- });
+       loadPage('index.php?v=d&p=dashboard&summary='+$(this).data('summary')+'&object_id='+$(this).data('object_id'));
+   });
 
     initPage();
 });
@@ -396,10 +396,10 @@ function initRowOverflow() {
             hWindow -= 10;
         }
         if($('.row-overflow').attr('data-offset') != undefined){
-         hWindow -= $('.row-overflow').attr('data-offset');
-     }
-     $('.row-overflow > div').height(hWindow).css('overflow-y', 'auto').css('overflow-x', 'hidden').css('padding-top','5px');
- }
+           hWindow -= $('.row-overflow').attr('data-offset');
+       }
+       $('.row-overflow > div').height(hWindow).css('overflow-y', 'auto').css('overflow-x', 'hidden').css('padding-top','5px');
+   }
 }
 
 function initExpertMode() {
@@ -416,13 +416,13 @@ function initExpertMode() {
 
 function initReportMode() {
     if (getUrlVars('report') == 1) {
-       $('header').hide();
-       $('footer').hide();
-       $('#div_mainContainer').css('margin-top', '-50px');
-       $('#wrap').css('margin-bottom', '0px');
-       $('.reportModeVisible').show();
-       $('.reportModeHidden').hide();
-   } 
+     $('header').hide();
+     $('footer').hide();
+     $('#div_mainContainer').css('margin-top', '-50px');
+     $('#wrap').css('margin-bottom', '0px');
+     $('.reportModeVisible').show();
+     $('.reportModeHidden').hide();
+ } 
 }
 
 function initTableSorter() {
@@ -573,13 +573,13 @@ function positionEqLogic(_id,_preResize) {
             $(this).height('auto');
         }
         if(init(_preResize,true)){
-           eqLogic.width(Math.floor(eqLogic.width() / 40) * 40);
-           eqLogic.height(Math.floor(eqLogic.height() / 80) * 80);
-       }
-       eqLogic.width(Math.ceil(eqLogic.width() / 40) * 40 + (Math.ceil(eqLogic.width() / 40)-1) * 2);
-       eqLogic.height(Math.ceil(eqLogic.height() / 80) * 80 + (Math.ceil(eqLogic.height() / 80)-1) * 2);
-       eqLogic.trigger('resize');
-   }else{
+         eqLogic.width(Math.floor(eqLogic.width() / 40) * 40);
+         eqLogic.height(Math.floor(eqLogic.height() / 80) * 80);
+     }
+     eqLogic.width(Math.ceil(eqLogic.width() / 40) * 40 + (Math.ceil(eqLogic.width() / 40)-1) * 2);
+     eqLogic.height(Math.ceil(eqLogic.height() / 80) * 80 + (Math.ceil(eqLogic.height() / 80)-1) * 2);
+     eqLogic.trigger('resize');
+ }else{
     $('.eqLogic-widget:not(.jeedomAlreadyPosition)').css('margin','0px').css('padding','0px');
     $('.eqLogic-widget:not(.jeedomAlreadyPosition)').each(function () {
         if($(this).width() == 0){
