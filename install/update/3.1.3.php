7ff7caefb775672b79cd46ec6072459a12ef7ff2
Loïc <loic@jeedom.com>
2017-09-05 14:15:31 +0200

    fix watch dog

--- install/update/3.1.3.php
+++ install/update/3.1.3.php
@@ -2,8 +2,8 @@
 echo 'Add Jeedom watchdog';
 $cdir = dirname(__FILE__);
 if (!file_exists('/etc/cron.d/jeedom_watchdog')) {
-	file_put_contents('/tmp/jeedom_watchdog', '* * * * * root /usr/bin/php ' . $cdir . '/../core/php/watchdog.php >> /dev/null');
-	exec('sudo mv /tmp/jeedom_watchdog /etc/cron.d/jeedom_watchdog');
+	file_put_contents('/tmp/jeedom_watchdog', '* * * * * root /usr/bin/php ' . $cdir . '/../../core/php/watchdog.php >> /dev/null' . "\n");
+	exec('sudo mv /tmp/jeedom_watchdog /etc/cron.d/jeedom_watchdog;sudo chown root:root /etc/cron.d/jeedom_watchdog');
 }
 if (file_exists('/etc/cron.d/jeedom_watchdog')) {
 	echo " OK\n";
