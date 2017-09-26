b6be6596191dbe8811b4f60196cf980fd68e7c60
sarakha63 <sarakha_ludovic@yahoo.fr>
2017-09-26 16:41:59 +0200

    be more bootstrap compliant

--- desktop/php/history.php
+++ desktop/php/history.php
@@ -120,8 +120,6 @@ foreach (cmd::allHistoryCmd() as $cmd) {
 		</div>
 		<div role="tabpanel" class="tab-pane" id="timelinetab">
 			<br/>
-			<div class="row form-group">
-			<div class="col-sm-12">
 			<div class="btn-group" role="group" aria-label="...">
 				<a class="btn btn-sm btn-default bt_timelineZoom" data-zoom="h">H</a>
 				<a class="btn btn-sm btn-default bt_timelineZoom" data-zoom="d">J</a>
@@ -162,11 +160,7 @@ foreach (object::all() as $object) {
 				<option value="cmd">{{Commandes}}</option>
 				<option value="scenario">{{Scénarios}}</option>
 			</select>
-			</div>
-			</div>
-			<div class="row">
 			<div id="div_visualization"></div>
-			</div>
 
 		</div>
 	</div>
