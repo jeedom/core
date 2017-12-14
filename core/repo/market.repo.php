517de5ef60bff64af9610852d09feaad51912603
Loïc <loic@jeedom.com>
2017-12-14 08:24:03 +0100

    improve communication with market

--- core/repo/market.repo.php
+++ core/repo/market.repo.php
@@ -275,6 +275,9 @@ class repo_market {
 	/*     * ***********************CRON*************************** */
 
 	public static function cronHourly() {
+		if (strtotime(config::byKey('market::lastCommunication', 'core', 0)) > (strtotime('now') - 24 * 3600)) {
+			return;
+		}
 		sleep(rand(0, 900));
 		try {
 			self::test();
@@ -541,6 +544,7 @@ class repo_market {
 			if (isset($_result['register::hwkey_nok']) && $_result['register::hwkey_nok'] == 1) {
 				config::save('jeedom::installKey', '');
 			}
+			config::save('market::lastCommunication', date('Y-m-d H:i:s'));
 		}
 	}
 
