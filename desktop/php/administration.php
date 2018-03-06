b8d75693814c42d6aee3eff3877f7007df71a3af
COURVOISIER MICHAEL <courvoisier@netplus.ch>
2018-03-06 19:43:52 +0100

    add champ "complement"
    Ajout du champ complément pour permettre la compatibilité LDAP avec le serveur synology

--- desktop/php/administration.php
+++ desktop/php/administration.php
@@ -426,6 +426,12 @@ echo $CONFIG['db']['password'];
 								<input type="text" class="configKey form-control" data-l1key="ldap::usersearch" />
 							</div>
 						</div>
+                      	<div class="form-group">
+							<label class="col-lg-2 col-md-3 col-sm-4 col-xs-6 control-label">{{Complément}}</label>
+							<div class="col-lg-3 col-md-4 col-sm-5 col-xs-6">
+								<input type="text"  class="configKey form-control" data-l1key="ldap:complement" />
+							</div>
+						</div>
 						<div class="form-group">
 							<label class="col-lg-2 col-md-3 col-sm-4 col-xs-6 control-label">{{Filtre (optionnel)}}</label>
 							<div class="col-lg-3 col-md-4 col-sm-5 col-xs-6">
