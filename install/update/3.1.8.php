4c0572357d995c7942ca611e6441ffc8a70d3713
tmartinez <tmartinez69009@gmail.com>
2018-03-23 15:08:52 +0100

    bugfix repo

--- install/update/3.1.8.php
+++ install/update/3.1.8.php
@@ -5,9 +5,11 @@ foreach (interactDef::all() as $interactDef) {
 	$interactDef->save();
 }
 if (file_exists('/media/boot/multiboot/meson64_odroidc2.dtb.linux')) {
+	echo 'add jeedom repo gpg key';
+	exec('sudo wget -O - http://repo.jeedom.com/odroid/conf/jeedom.gpg.key | sudo apt-key add -');
 	echo 'Update Jeedom repository';
 	exec('sudo rm -rf /etc/apt/sources.list.d/jeedom.list');
-	exec('sudo apt-add-repository "deb http://repo.jeedom.com/odroid/ dists/stable/main/binary-arm64/"');
+	exec('sudo apt-add-repository "deb http://repo.jeedom.com/odroid/ stable main"');
 	echo " OK\n";
 	echo 'Update APT sources\n';
 	exec('sudo apt-get update');
