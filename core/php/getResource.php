ef8fbeadde4aa86eab6ca0e301db04884397658d
Loïc <loic@jeedom.com>
2018-04-17 17:32:02 +0200

    Revert "add gzip on css also"
    This reverts commit d5e88e8475a91d6a46a0df2eeaeba5598e0270c9.

--- core/php/getResource.php
+++ core/php/getResource.php
@@ -32,7 +32,7 @@ if (config::byKey('developperMode') == 0) {
 	if (file_exists($minFile)) {
 		$file = $minFile;
 	}
-	$minFile = $tempAssestsFolder . '/' . md5_file($file) . '.min.' . $pathinfo['extension'];
+	$minFile = $tempAssestsFolder . '/' . md5_file($file) . '.gzip.min.' . $pathinfo['extension'];
 	if (file_exists($minFile)) {
 		$file = $minFile;
 	}
@@ -86,15 +86,11 @@ if (file_exists($file)) {
 		echo $minifier->gzip($tempAssestsFolder . '/' . $etagFile . '.gzip.min.' . $pathinfo['extension']);
 	} elseif ($pathinfo['extension'] == 'css') {
 		if (strpos($pathinfo['filename'], '.min.') !== false || config::byKey('developperMode') == 1) {
-			if (strpos($pathinfo['filename'], '.gzip') !== false) {
-				header('Content-Encoding: gzip');
-			}
 			echo file_get_contents($file);
 			die();
 		}
 		$minifier = new Minify\CSS($file);
-		header('Content-Encoding: gzip');
-		echo $minifier->gzip($tempAssestsFolder . '/' . $etagFile . '.gzip.min.' . $pathinfo['extension']);
+		echo $minifier->minify($tempAssestsFolder . '/' . $etagFile . '.min.' . $pathinfo['extension']);
 	}
 	exit;
 }
