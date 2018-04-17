aee13ab28dd559baea2ab37b17924ddd2f93090e
Loïc <loic@jeedom.com>
2018-04-17 16:51:36 +0200

    bugfix

--- install/update/3.3.3.php
+++ install/update/3.3.3.php
@@ -1,5 +1,5 @@
 <?php
-shell_exec('sudo apt-get -y php7.0-mbstring');
-shell_exec('sudo apt-get -y php5-mbstring');
-shell_exec('sudo apt-get -y php-mbstring');
+shell_exec('sudo apt-get -y install php7.0-mbstring');
+shell_exec('sudo apt-get -y install php5-mbstring');
+shell_exec('sudo apt-get -y install php-mbstring');
 shell_exec('sudo systemctl restart apache2');
