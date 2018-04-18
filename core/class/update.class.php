ce548dd43116cc7572bb5fbecf2b13e7e232991a
murciano <y.murciano@systel-sa.com>
2018-04-18 13:53:54 +0200

    bugfix checkAllUpdate creating new core update

--- core/class/update.class.php
+++ core/class/update.class.php
@@ -65,7 +65,7 @@ class update {
 				}
 			}
 		}
-		if (!$findCore) {
+		if (!$findCore && ($_filter == '' || $_filter == 'core')) {
 			$update = new update();
 			$update->setType('core');
 			$update->setLogicalId('jeedom');
