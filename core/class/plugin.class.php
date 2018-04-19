e4ef19518372333e631696a737d2721f5e21c765
sarakha63 <sarakha_ludovic@yahoo.fr>
2018-04-19 15:13:22 +0200

    avoid issue if date is wrong when saved in cache

--- core/class/plugin.class.php
+++ core/class/plugin.class.php
@@ -589,7 +589,7 @@ class plugin {
 				if ($deamon_info['launchable'] == 'ok' && $deamon_info['state'] == 'nok' && method_exists($plugin_id, 'deamon_start')) {
 					$inprogress = cache::byKey('deamonStart' . $this->getId() . 'inprogress');
 					$info = $inprogress->getValue(array('state' => 0, 'datetime' => strtotime('now')));
-					if ($info['state'] == 1 && (strtotime('now') - 45) <= $info['datetime']) {
+					if ($info['state'] == 1 && abs(strtotime('now') - $info['datetime']) < 45) {
 						throw new Exception(__('Vous devez attendre au moins 45 secondes entre deux lancements du démon', __FILE__));
 					}
 					cache::set('deamonStart' . $this->getId() . 'inprogress', array('state' => 1, 'datetime' => strtotime('now')));
