13c2cc8c76d425c757179128b422cf47654d99b7
Loïc <loic@jeedom.com>
2018-04-26 14:29:26 +0200

    improve ui

--- desktop/php/update.php
+++ desktop/php/update.php
@@ -9,7 +9,7 @@ if (!isConnect('admin')) {
         <i class="fas fa-clock-o" style="cursor:default;"></i> <span style="cursor:default;">{{Dernière vérification : }}</span><span class="label label-info" id="span_lastUpdateCheck" style="margin-bottom: 5px;font-size:1em;cursor:default;"></span>
         <a class="btn btn-success pull-right" id="bt_saveUpdate" style="margin-top:5px;"><i class="far fa-check-circle"></i> {{Sauvegarder}}</a>
         <a class="btn btn-info pull-right" id="bt_checkAllUpdate" style="margin-top:5px;"><i class="fas fa-refresh"></i> {{Vérifier les mises à jour}}</a>
-            <a href="#" class="btn btn-default pull-right" id="bt_updateJeedom" style="margin-top:5px;"><i class="fas fa-check"></i> {{Mettre à jour}}</a>
+        <a href="#" class="btn btn-default pull-right" id="bt_updateJeedom" style="margin-top:5px;"><i class="fas fa-check"></i> {{Mettre à jour}}</a>
         <br/><br/>
 
         <ul class="nav nav-tabs" role="tablist">
@@ -59,45 +59,47 @@ if (!isConnect('admin')) {
 </div>
 
 <div id="md_specifyUpdate">
-   <form class="form-horizontal">
+ <form class="form-horizontal">
     <fieldset>
         <div class="alert alert-danger">{{Avant toute mise à jour, merci de consulter la note de version du core de Jeedom}} <a target="_blank" href="https://jeedom.github.io/core/fr_FR/noteVersion">{{ici}}<a></div>
-     <div class="form-group">
-         <div class="form-group">
-            <label class="col-xs-6 control-label">{{Pré-update (mettre à jour le script d'update avant)}}</label>
-            <div class="col-xs-4">
-                <input type="checkbox" class="updateOption" data-l1key="preUpdate" />
+           <div class="form-group">
+               <div class="form-group">
+                <label class="col-xs-6 control-label">{{Pré-update (mettre à jour le script d'update avant)}}</label>
+                <div class="col-xs-4">
+                    <input type="checkbox" class="updateOption" data-l1key="preUpdate" />
+                </div>
             </div>
-        </div>
-         <div class="form-group">
-            <label class="col-xs-6 control-label">{{Sauvegarder avant}}</label>
-            <div class="col-xs-4">
-                <input type="checkbox" class="updateOption" data-l1key="backup::before" checked />
+            <div class="form-group">
+                <label class="col-xs-6 control-label">{{Sauvegarder avant}}</label>
+                <div class="col-xs-4">
+                    <input type="checkbox" class="updateOption" data-l1key="backup::before" checked />
+                </div>
             </div>
-        </div>
-        <div class="form-group">
-            <label class="col-xs-6 control-label">{{Mettre à jour les plugins}}</label>
-            <div class="col-xs-4">
-                <input type="checkbox" class="updateOption" data-l1key="plugins" checked />
+            <div class="form-group">
+                <label class="col-xs-6 control-label">{{Mettre à jour les plugins}}</label>
+                <div class="col-xs-4">
+                    <input type="checkbox" class="updateOption" data-l1key="plugins" checked />
+                </div>
             </div>
-        </div>
-        <div class="form-group">
-            <label class="col-xs-6 control-label">{{Mettre à jour le core}}</label>
-            <div class="col-xs-4">
-                <input type="checkbox" class="updateOption" data-l1key="core" checked />
+            <div class="form-group">
+                <label class="col-xs-6 control-label">{{Mettre à jour le core}}</label>
+                <div class="col-xs-4">
+                    <input type="checkbox" class="updateOption" data-l1key="core" checked />
+                </div>
             </div>
-        </div>
-        <div class="form-group">
-            <label class="col-xs-6 control-label">{{Mode forcé}}</label>
-            <div class="col-xs-4">
-                <input type="checkbox" class="updateOption" data-l1key="force" />
+            <div class="form-group">
+                <label class="col-xs-6 control-label">{{Mode forcé}}</label>
+                <div class="col-xs-4">
+                    <input type="checkbox" class="updateOption" data-l1key="force" />
+                </div>
             </div>
-        </div>
-        <label class="col-xs-6 control-label">{{Mise à jour à réappliquer}}</label>
-        <div class="col-xs-5">
-            <select id="sel_updateVersion" class="form-control updateOption" data-l1key="update::reapply">
-                <option value="">{{Aucune}}</option>
-                <?php
+            <div class="alert alert-danger">{{L'option suivante n'est à modifier que sur demande du support sinon il faut ABSOLUMENT qu'elle soit sur "Aucune"}}</div>
+            <div class="form-group has-error">
+                <label class="col-xs-6 control-label ">{{Mise à jour à réappliquer}}</label>
+                <div class="col-xs-5">
+                    <select id="sel_updateVersion" class="form-control updateOption" data-l1key="update::reapply">
+                        <option value="">{{Aucune}}</option>
+                        <?php
 $updates = array();
 foreach (update::listCoreUpdate() as $udpate) {
 	$updates[str_replace(array('.php', '.sql'), '', $udpate)] = str_replace(array('.php', '.sql'), '', $udpate);
@@ -108,10 +110,11 @@ foreach ($updates as $value) {
 	echo '<option value="' . $value . '">' . $value . '</option>';
 }
 ?>
-           </select>
+                   </select>
+               </div>
+           </div>
        </div>
-   </div>
-</fieldset>
+   </fieldset>
 </form>
 <a class="btn btn-success pull-right" style="color:white;" id="bt_doUpdate"><i class="fas fa-check"></i> {{Mettre à jour}}</a>
 </div>
