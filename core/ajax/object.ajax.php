1cf0e2caf063b69964ee0de50e9d7699f949abdb
Loïc <loic@jeedom.com>
2018-05-08 19:48:32 +0200

    Update object.ajax.php

--- core/ajax/object.ajax.php
+++ core/ajax/object.ajax.php
@@ -56,7 +56,7 @@ try {
 		if (init('onlyHasEqLogic') != '') {
 			$return = array();
 			foreach ($objects as $object) {
-				if (count($object->getEqLogic(true, false, init('onlyHasEqLogic'), null, true)) == 0) {
+				if (count($object->getEqLogic(true, false, init('onlyHasEqLogic'), null, init('searchOnchild', true))) == 0) {
 					continue;
 				}
 				$return[] = $object;
