b391a5a1f9adca7ec970c39c2b554e13ce90b95f
Loïc <loic@jeedom.com>
2018-05-31 10:37:40 +0200

    Add oom counter on health

--- core/class/jeedom.class.php
+++ core/class/jeedom.class.php
@@ -212,6 +212,14 @@ class jeedom {
 			'comment' => '',
 		);
 
+		$value = shell_exec('sudo dmesg | grep oom | wc -l');
+		$return[] = array(
+			'name' => __('Mémoire suffisante', __FILE__),
+			'state' => ($value == 0),
+			'result' => $value,
+			'comment' => __('Nombre de processus tué par le noyaux pour manque de mémoire', __FILE__),
+		);
+
 		if ($values['SwapTotal'] != 0 && $values['SwapTotal'] !== null) {
 			$value = round(($values['SwapFree'] / $values['SwapTotal']) * 100);
 			$return[] = array(
