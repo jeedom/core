f0bd2830e7f75d17043e6638b64b6fa205d3d2cc
Loïc <loic@jeedom.com>
2018-06-04 17:49:20 +0200

    improvements

--- desktop/php/index.php
+++ desktop/php/index.php
@@ -1,5 +1,5 @@
 <?php
-if (init('rescue', 0) == 1 && !in_array(init('p'), array('custom', 'backup', 'cron', 'connection', 'log', 'database', 'editor', 'system'))) {
+if (init('rescue', 0) == 1 && !in_array(init('p'), array('custom', 'backup', 'cron', 'connection', 'log', 'database', 'editor', 'system', 'product_name', 'product_icon', 'product_image'))) {
 	$_GET['p'] = 'system';
 }
 include_file('core', 'authentification', 'php');
