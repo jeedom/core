cc69dfff5dda1b2741bd004405abc69de41c9589
Loïc <loic@jeedom.com>
2018-06-08 10:52:55 +0200

    bugfix

--- core/js/object.class.js
+++ core/js/object.class.js
@@ -123,6 +123,9 @@ jeedom.object.remove = function(_params) {
             if (isset(jeedom.object.cache.getEqLogic[_params.id])) {
                 delete jeedom.object.cache.getEqLogic[_params.id];
             }
+            if(isset(jeedom.object.cache.byId[_params.id])){
+                delete jeedom.object.cache.byId[_params.id];
+            }
             return data;
         }
     };
@@ -149,8 +152,11 @@ jeedom.object.save = function(_params) {
             if (isset(jeedom.object.cache.all)) {
                 delete jeedom.object.cache.all;
             }
-            if (isset(jeedom.object.cache.getEqLogic[_params.id])) {
-                delete jeedom.object.cache.getEqLogic[_params.id];
+            if (isset(jeedom.object.cache.getEqLogic[data.result.id])) {
+                delete jeedom.object.cache.getEqLogic[data.result.id];
+            }
+            if(isset(jeedom.object.cache.byId[data.result.id])){
+                delete jeedom.object.cache.byId[data.result.id];
             }
             return data;
         }
