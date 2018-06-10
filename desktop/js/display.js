0d87de445fc551fc8ab2c712e253e533a0b05478
Loïc <loic@jeedom.com>
2018-06-10 10:55:28 +0200

    improve ui

--- desktop/js/display.js
+++ desktop/js/display.js
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
  */
+
  $( ".eqLogicSortable" ).sortable({
   connectWith: ".eqLogicSortable",
   stop: function (event, ui) {
