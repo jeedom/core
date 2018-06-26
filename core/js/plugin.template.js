de9b5f8e1be73068b398652b549b22dca593ae06
Loïc <loic@jeedom.com>
2018-06-26 15:40:56 +0200

    Update plugin.template.js

--- core/js/plugin.template.js
+++ core/js/plugin.template.js
@@ -52,6 +52,14 @@
   });
 }
 
+var url = document.location.toString();
+if (url.match('#')) {
+    $('.nav-tabs a[href="#' + url.split('#')[1] + '"]').tab('show');
+} 
+$('.nav-tabs a').on('shown.bs.tab', function (e) {
+    window.location.hash = e.target.hash;
+})
+
 $('.eqLogicDisplayCard').on('click', function () {
     $('.li_eqLogic[data-eqLogic_id=' + $(this).attr('data-eqLogic_id') + ']').click();
 });
