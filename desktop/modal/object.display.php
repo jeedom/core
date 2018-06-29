4dd0d0ecb9b081f2afc12a3305aaa9a01dd6320a
Loïc <loic@jeedom.com>
2018-06-29 12:07:41 +0200

    Update object.display.php

--- desktop/modal/object.display.php
+++ desktop/modal/object.display.php
@@ -35,11 +35,41 @@ $array = utils::o2a($object);
 if (count($array) == 0) {
 	throw new Exception(__('L\'objet n\'a aucun élément : ', __FILE__) . print_r($array, true));
 }
+$otherInfo = array();
+
+if ($class == 'cron' && $array['class'] == 'scenario' && $array['function'] == 'doIn') {
+	$scenario = scenario::byId($array['option']['scenario_id']);
+	$scenarioElement = scenarioElement::byId($array['option']['scenarioElement_id']);
+	if (is_object($scenarioElement) && is_object($scenario)) {
+		$otherInfo['doIn'] = __('Scénario : ', __FILE__) . $scenario->getName() . "\n" . str_replace(array('"'), array("'"), $scenarioElement->export());
+	}
+}
 ?>
 
 <form class="form-horizontal">
-	<fieldset>
-		<?php
+  <fieldset>
+    <?php
+if (count($otherInfo) > 0) {
+	foreach ($otherInfo as $key => $value) {
+		echo '<div class="form-group">';
+		echo '<label class="col-sm-2 control-label">' . $key . '</label>';
+		echo '<div class="col-sm-10">';
+		if (is_array($value)) {
+			echo '<textarea class="form-control" rows="3" disabled>';
+			echo json_encode($value);
+			echo '</textarea>';
+		} else if (strpos($value, "\n")) {
+			echo '<pre disabled>';
+			echo $value;
+			echo '</pre>';
+		} else {
+			echo '<input class="form-control" disabled value="' . $value . '" />';
+		}
+		echo '</div>';
+		echo '</div>';
+	}
+	echo '<hr/>';
+}
 foreach ($array as $key => $value) {
 	echo '<div class="form-group">';
 	echo '<label class="col-sm-2 control-label">' . $key . '</label>';
@@ -48,6 +78,10 @@ foreach ($array as $key => $value) {
 		echo '<textarea class="form-control" rows="3" disabled>';
 		echo json_encode($value);
 		echo '</textarea>';
+	} else if (strpos($value, "\n")) {
+		echo '<pre disabled>';
+		echo $value;
+		echo '</pre>';
 	} else {
 		echo '<input class="form-control" disabled value="' . $value . '" />';
 	}
@@ -55,5 +89,5 @@ foreach ($array as $key => $value) {
 	echo '</div>';
 }
 ?>
-	</fieldset>
+</fieldset>
 </form>
