edd236da3f2ca89e687f91b28fa4d33831fcb680
Loïc <loic@jeedom.com>
2018-07-02 19:13:27 +0200

    Update cron.js

--- desktop/js/cron.js
+++ desktop/js/cron.js
@@ -131,7 +131,9 @@ function addCron(_cron) {
     var tr = '<tr id="' + init(_cron.id) + '">';
     tr += '<td class="option"><span class="cronAttr" data-l1key="id"></span></td>';
     tr += '<td>';
-    tr += '<a class="btn btn-default btn-xs display"><i class="fas fa-file"></i></a>';
+    if(init(_cron.id) != ''){
+         tr += '<a class="btn btn-default btn-xs display"><i class="fas fa-file"></i></a>';
+    }
     if(init(_cron.deamon) == 0){
         if (init(_cron.state) == 'run') {
             tr += ' <a class="btn btn-danger btn-xs stop" style="color : white;"><i class="fas fa-stop"></i></a>';
