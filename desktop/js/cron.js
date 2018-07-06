75377a02f0eefa241316e8e4dfe620710206de00
Loïc <loic@jeedom.com>
2018-07-06 10:39:42 +0200

    ui

--- desktop/js/cron.js
+++ desktop/js/cron.js
@@ -132,7 +132,7 @@ function addCron(_cron) {
     tr += '<td class="option"><span class="cronAttr" data-l1key="id"></span></td>';
     tr += '<td>';
     if(init(_cron.id) != ''){
-         tr += '<a class="btn btn-default btn-xs display"><i class="fas fa-file"></i></a>';
+         tr += '<a class="btn btn-default btn-xs display"><i class="fas fa-file"></i></a> ';
     }
     if(init(_cron.deamon) == 0){
         if (init(_cron.state) == 'run') {
