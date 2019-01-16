cbe012db75462ae42f225bda6063fa5870a8c3fa
Loïc <loic@jeedom.com>
2019-01-16 15:13:00 +0100

    bugfix

--- core/class/update.class.php
+++ core/class/update.class.php
@@ -480,7 +480,7 @@ class update {
 	* @return type
 	*/
 	public function checkUpdate() {
-		if ($this->getConfiguration('doNotUpdate') == 1) {
+		if ($this->getConfiguration('doNotUpdate') == 1 && $this->getType() != 'core') {
 			log::add('update', 'alert', __('Vérification des mises à jour, mise à jour et réinstallation désactivées sur ', __FILE__) . $this->getLogicalId());
 			return;
 		}
