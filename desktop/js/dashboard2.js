80e0221859c286a59c8c4f8dba66c64341cf9c20
Loïc <loic@jeedom.com>
2019-01-23 15:31:03 +0100

    test new grid system

---
+++ desktop/js/dashboard2.js
@@ -0,0 +1,220 @@
+
+/* This file is part of Jeedom.
+*
+* Jeedom is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Jeedom is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+$(function(){
+  setTimeout(function(){
+    if(typeof rootObjectId != 'undefined'){
+      jeedom.object.getImgPath({
+        id : rootObjectId,
+        success : function(_path){
+          $('.backgroundforJeedom').css('background-image','url("'+_path+'")');
+        }
+      });
+    }
+    
+  },1);
+});
+
+$('#div_pageContainer').on( 'click','.eqLogic-widget .history', function () {
+  $('#md_modal2').dialog({title: "Historique"});
+  $("#md_modal2").load('index.php?v=d&modal=cmd.history&id=' + $(this).data('cmd_id')).dialog('open');
+});
+
+$('#bt_displayScenario').on('click', function () {
+  if ($(this).attr('data-display') == 1) {
+    $('#div_displayScenario').hide();
+    if ($('#bt_displayObject').attr('data-display') == 1) {
+      $('#div_displayObject').removeClass('col-lg-8 col-lg-10 col-lg-12 col-md-8 col-md-10 col-md-12 col-sm-8 col-sm-10 col-sm-12').addClass('col-lg-10 col-md-9 col-sm-8');
+    } else {
+      $('#div_displayObject').removeClass('col-lg-8 col-lg-10 col-lg-12 col-lg-8 col-lg-10 col-lg-12 col-md-8 col-md-10 col-md-12 col-sm-8 col-sm-10 col-sm-12').addClass('col-lg-12 col-md-12 col-sm-12');
+    }
+    $('.div_displayEquipement').each(function () {
+      $(this).packery();
+    });
+    $(this).attr('data-display', 0);
+  } else {
+    $('#div_displayScenario').show();
+    if ($('#bt_displayObject').attr('data-display') == 1) {
+      $('#div_displayObject').removeClass('col-lg-8 col-lg-10 col-lg-12 col-lg-8 col-lg-10 col-lg-12 col-md-8 col-md-10 col-md-12 col-sm-8 col-sm-10 col-sm-12').addClass('col-lg-8 col-md-7 col-sm-5');
+    } else {
+      $('#div_displayObject').removeClass('col-lg-8 col-lg-10 col-lg-12 col-lg-8 col-lg-10 col-lg-12 col-md-8 col-md-10 col-md-12 col-sm-8 col-sm-10 col-sm-12').addClass('col-lg-10 col-md-9 col-sm-7');
+    }
+    $('.div_displayEquipement').packery();
+    $(this).attr('data-display', 1);
+  }
+});
+
+$('#bt_displayObject').on('click', function () {
+  if ($(this).attr('data-display') == 1) {
+    $('#div_displayObjectList').hide();
+    if ($('#bt_displayScenario').attr('data-display') == 1) {
+      $('#div_displayObject').removeClass('col-lg-8 col-lg-10 col-lg-12 col-lg-8 col-lg-10 col-lg-12 col-md-8 col-md-10 col-md-12 col-sm-8 col-sm-10 col-sm-12').addClass('col-lg-10 col-md-9 col-sm-7');
+    } else {
+      $('#div_displayObject').removeClass('col-lg-8 col-lg-10 col-lg-12 col-lg-8 col-lg-10 col-lg-12 col-md-8 col-md-10 col-md-12 col-sm-8 col-sm-10 col-sm-12').addClass('col-lg-12 col-md-12 col-sm-12');
+    }
+    $('.div_displayEquipement').each(function () {
+      $(this).packery();
+    });
+    $(this).attr('data-display', 0);
+  } else {
+    $('#div_displayObjectList').show();
+    if ($('#bt_displayScenario').attr('data-display') == 1) {
+      $('#div_displayObject').removeClass('col-lg-8 col-lg-10 col-lg-12 col-lg-8 col-lg-10 col-lg-12 col-md-8 col-md-10 col-md-12 col-sm-8 col-sm-10 col-sm-12').addClass('col-lg-8 col-md-7 col-sm-5');
+    } else {
+      $('#div_displayObject').removeClass('col-lg-8 col-lg-10 col-lg-12 col-lg-8 col-lg-10 col-lg-12 col-md-8 col-md-10 col-md-12 col-sm-8 col-sm-10 col-sm-12').addClass('col-lg-10 col-md-9 col-sm-8');
+    }
+    $('.div_displayEquipement').packery();
+    $(this).attr('data-display', 1);
+  }
+});
+
+function editWidgetMode(_mode,_save){
+  return;
+  if(!isset(_mode)){
+    if($('#bt_editDashboardWidgetOrder').attr('data-mode') != undefined && $('#bt_editDashboardWidgetOrder').attr('data-mode') == 1){
+      editWidgetMode(0,false);
+      editWidgetMode(1,false);
+    }
+    return;
+  }
+  if(_mode == 0){
+    if(!isset(_save) || _save){
+      saveWidgetDisplay({dashboard : 1});
+    }
+    if( $('.div_displayEquipement .eqLogic-widget.ui-resizable').length > 0){
+      $('.div_displayEquipement .eqLogic-widget.allowResize').resizable('destroy');
+    }
+    if( $('.div_displayEquipement .eqLogic-widget.ui-draggable').length > 0){
+      $('.div_displayEquipement .eqLogic-widget').draggable('disable');
+    }
+    $('.div_displayEquipement .eqLogic-widget').css('box-shadow','');
+  }else{
+    $('.div_displayEquipement .eqLogic-widget').css('box-shadow','0 0 4px rgba(147,204,1,.14), 0 10px 16px rgba(147,204,1,.30)');
+    $('.div_displayEquipement .eqLogic-widget').draggable('enable');
+    $( ".div_displayEquipement .eqLogic-widget.allowResize").resizable({
+      resize: function( event, ui ) {
+        positionEqLogic(ui.element.attr('data-eqlogic_id'),false);
+        ui.element.closest('.div_displayEquipement').packery();
+      },
+      stop: function( event, ui ) {
+        positionEqLogic(ui.element.attr('data-eqlogic_id'),false);
+        ui.element.closest('.div_displayEquipement').packery();
+      }
+    });
+  }
+  editWidgetCmdMode(_mode);
+}
+
+function getObjectHtml(_object_id){
+  jeedom.object.toHtml({
+    id: _object_id,
+    version: 'dashboard',
+    category : SEL_CATEGORY,
+    summary : SEL_SUMMARY,
+    tag : SEL_TAG,
+    error: function (error) {
+      $('#div_alert').showAlert({message: error.message, level: 'danger'});
+    },
+    success: function (html) {
+      if($.trim(html) == ''){
+        $('#div_ob'+_object_id).parent().remove();
+        return;
+      }
+      try {
+        $('#div_ob'+_object_id).empty().html(html).parent().show();
+      }catch(err) {
+        console.log(err);
+      }
+      setTimeout(function(){
+        //positionEqLogic();
+        $('#div_ob'+_object_id+'.div_displayEquipement').disableSelection();
+        $("input").click(function() { $(this).focus(); });
+        $("textarea").click(function() { $(this).focus(); });
+        $("select").click(function() { $(this).focus(); });
+        
+        $('#div_ob'+_object_id+'.div_displayEquipement').each(function(){
+          var i = 0;
+          var j = 0;
+          $(this).find( ".eqLogic-widget" ).each(function(){
+            $(this).wrap( '<div class="grid-stack-item" data-gs-x="'+i+'" data-gs-y="'+j+'" data-gs-width="2" data-gs-height="2"></div>');
+            i += 2;
+            if(i>=12){
+              j += 2;
+              i=0;
+            }
+          });
+          
+          $('.eqLogic-widget').css('height','auto')
+          $('.eqLogic-widget').css('width','auto')
+          $('.eqLogic-widget').addClass('grid-stack-item-content');
+          var options = {
+            verticalMargin: 10,
+            horizontalMargin: 10
+          };
+          $(this).gridstack(options);
+        });
+        
+      },10);
+    }
+  });
+}
+
+$('#bt_editDashboardWidgetOrder').on('click',function(){
+  if($(this).attr('data-mode') == 1){
+    $.hideAlert();
+    $(this).attr('data-mode',0);
+    editWidgetMode(0);
+    $(this).css('color','black');
+    $('.counterReorderJeedom').remove();
+    $('.div_displayEquipement').packery();
+  }else{
+    $('#div_alert').showAlert({message: "{{Vous êtes en mode édition vous pouvez déplacer les widgets, les redimensionner et changer l'ordre des commandes dans les widgets. N'oubliez pas de quitter le mode édition pour sauvegarder}}", level: 'info'});
+    $(this).attr('data-mode',1);
+    editWidgetMode(1);
+    $(this).css('color','rgb(46, 176, 75)');
+  }
+});
+
+
+$('.li_object').on('click',function(){
+  var object_id = $(this).find('a').attr('data-object_id');
+  if($('.div_object[data-object_id='+object_id+']').html() != undefined){
+    jeedom.object.getImgPath({
+      id : object_id,
+      success : function(_path){
+        $('.backgroundforJeedom').css('background-image','url("'+_path+'")');
+      }
+    });
+    $('.li_object').removeClass('active');
+    $(this).addClass('active');
+    displayChildObject(object_id,false);
+  }else{
+    loadPage($(this).find('a').attr('data-href'));
+  }
+});
+
+function displayChildObject(_object_id,_recursion){
+  if(_recursion === false){
+    $('.div_object').hide();
+  }
+  $('.div_object[data-object_id='+_object_id+']').show({effect : 'drop',queue : false});
+  $('.div_object[data-father_id='+_object_id+']').each(function(){
+    $(this).show({effect : 'drop',queue : false});
+    displayChildObject($(this).attr('data-object_id'),true);
+  });
+}
---
+++ desktop/php/dashboard2.php
@@ -0,0 +1,136 @@
+<?php
+if (!isConnect()) {
+	throw new Exception('{{401 - Accès non autorisé}}');
+}
+sendVarToJs('SEL_OBJECT_ID', init('object_id'));
+sendVarToJs('SEL_CATEGORY', init('category', 'all'));
+sendVarToJs('SEL_TAG', init('tag', 'all'));
+sendVarToJs('SEL_SUMMARY', init('summary'));
+if (init('object_id') == '') {
+	$object = jeeObject::byId($_SESSION['user']->getOptions('defaultDashboardObject'));
+} else {
+	$object = jeeObject::byId(init('object_id'));
+}
+if (!is_object($object)) {
+	$object = jeeObject::rootObject();
+}
+if (!is_object($object)) {
+	throw new Exception('{{Aucun objet racine trouvé. Pour en créer un, allez dans Outils -> Objets.<br/> Si vous ne savez pas quoi faire ou que c\'est la première fois que vous utilisez Jeedom, n\'hésitez pas à consulter cette <a href="https://jeedom.github.io/documentation/premiers-pas/fr_FR/index" target="_blank">page</a> et celle-là si vous avez un pack : <a href="https://jeedom.com/start" target="_blank">page</a>}}');
+}
+$allObject = jeeObject::buildTree(null, true);
+foreach ($allObject as $value) {
+	if ($value->getId() == $object->getId()) {
+		$child_object = $value->getChilds();
+	}
+}
+sendVarToJs('rootObjectId', $object->getId());
+include_file('desktop', 'dashboard', 'css');
+?>
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/gridstack.js/0.4.0/gridstack.min.css" />
+<script type="text/javascript" src='//cdn.jsdelivr.net/npm/lodash@4.17.11/lodash.min.js'></script>
+<script type="text/javascript" src='//cdnjs.cloudflare.com/ajax/libs/gridstack.js/0.4.0/gridstack.min.js'></script>
+<script type="text/javascript" src='//cdnjs.cloudflare.com/ajax/libs/gridstack.js/0.4.0/gridstack.jQueryUI.min.js'></script>
+<div class="row row-overflow">
+	<?php
+	if ($_SESSION['user']->getOptions('displayObjetByDefault') == 1) {
+		echo '<div class="col-lg-2 col-md-3 col-sm-4" id="div_displayObjectList">';
+	} else {
+		echo '<div class="col-lg-2 col-md-3 col-sm-4" style="display:none;" id="div_displayObjectList">';
+	}
+	?>
+	<div class="bs-sidebar">
+		<ul id="ul_object" class="nav nav-list bs-sidenav">
+			<li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
+			<?php
+			foreach ($allObject as $object_li) {
+				$margin = 5 * $object_li->getConfiguration('parentNumber');
+				if ($object_li->getId() == $object->getId()) {
+					echo '<li class="cursor li_object active" ><a data-object_id="' . $object_li->getId() . '" data-href="index.php?v=d&p=dashboard2&object_id=' . $object_li->getId() . '&category=' . init('category', 'all') . '" style="padding: 2px 0px;"><span style="position:relative;left:' . $margin . 'px;font-size:0.85em;">' . $object_li->getHumanName(true, true) . '</span><span style="font-size : 0.65em;float:right;position:relative;top:7px;">' . $object_li->getHtmlSummary() . '</span></a></li>';
+				} else {
+					echo '<li class="cursor li_object" ><a data-object_id="' . $object_li->getId() . '" data-href="index.php?v=d&p=dashboard2&object_id=' . $object_li->getId() . '&category=' . init('category', 'all') . '" style="padding: 2px 0px;"><span style="position:relative;left:' . $margin . 'px;font-size:0.85em;">' . $object_li->getHumanName(true, true) . '</span><span style="font-size : 0.65em;float:right;position:relative;top:7px;">' . $object_li->getHtmlSummary() . '</span></a></li>';
+				}
+			}
+			?>
+		</ul>
+	</div>
+</div>
+<?php
+if ($_SESSION['user']->getOptions('displayScenarioByDefault') == 1) {
+	if ($_SESSION['user']->getOptions('displayObjetByDefault') == 1) {
+		echo '<div class="col-lg-8 col-md-7 col-sm-5" id="div_displayObject">';
+	} else {
+		echo '<div class="col-lg-10 col-md-9 col-sm-7" id="div_displayObject">';
+	}
+} else {
+	if ($_SESSION['user']->getOptions('displayObjetByDefault') == 1) {
+		echo '<div class="col-lg-10 col-md-9 col-sm-8" id="div_displayObject">';
+	} else {
+		echo '<div class="col-lg-12 col-md-12 col-sm-12" id="div_displayObject">';
+	}
+}
+?>
+<i class='fa fa-cogs pull-right cursor' id='bt_displayScenario' data-display='<?php echo $_SESSION['user']->getOptions('displayScenarioByDefault') ?>' title="{{Afficher/Masquer les scénarios}}"></i>
+<?php if (init('category', 'all') == 'all') {?>
+	<i class="fas fa-pencil-alt pull-right cursor" id="bt_editDashboardWidgetOrder" data-mode="0" style="margin-right : 10px;"></i>
+<?php } ?>
+<i class='fa fa-sort-amount-desc pull-right cursor' id='bt_categorieHidden' title="{{Trier vos équipements}}"></i>
+<i class='fa fa-picture-o cursor pull-right' id='bt_displayObject' data-display='<?php echo $_SESSION['user']->getOptions('displayObjetByDefault') ?>' title="{{Afficher/Masquer les objets}}"></i>
+<?php
+include_file('desktop', 'dashboard2', 'js');
+include_file('3rdparty', 'jquery.isotope/isotope.pkgd.min', 'js');
+include_file('3rdparty', 'jquery.multi-column-select/multi-column-select', 'js');
+?>
+<div class="row" >
+	<?php
+	if (init('object_id') != '') {
+		echo '<div class="col-md-12">';
+	} else {
+		echo '<div class="col-md-' . $object->getDisplay('dashboard::size', 12) . '">';
+	}
+	echo '<div data-object_id="' . $object->getId() . '" data-father_id="' . $object->getFather_id() . '" class="div_object">';
+	echo '<legend style="margin-bottom : 0px;"><a class="div_object" style="text-decoration:none" href="index.php?v=d&p=object&id=' . $object->getId() . '">' . $object->getDisplay('icon') . ' ' . $object->getName() . '</a><span style="font-size : 0.6em;margin-left:10px;">' . $object->getHtmlSummary() . '</span> <i class="fas fa-compress pull-right cursor bt_editDashboardWidgetAutoResize" id="edit_object_' . $object->getId() . '" data-mode="0" style="margin-right : 10px; display: none;"></i> </legend>';
+	echo '<div class="div_displayEquipement grid-stack" id="div_ob' . $object->getId() . '" style="width: 100%;padding-top:3px;margin-bottom : 3px;">';
+	echo '<script>getObjectHtml(' . $object->getId() . ')</script>';
+	echo '</div>';
+	echo '</div>';
+	echo '</div>';
+	
+	foreach ($allObject as $value) {
+		if ($value->getId() != $object->getId()) {
+			continue;
+		}
+		foreach ($value->getChilds() as $child) {
+			if ($child->getConfiguration('hideOnDashboard', 0) == 1) {
+				continue;
+			}
+			echo '<div class="col-md-' . $child->getDisplay('dashboard::size', 12) . '">';
+			echo '<div data-object_id="' . $child->getId() . '" data-father_id="' . $child->getFather_id() . '" style="margin-bottom : 3px;" class="div_object">';
+			echo '<legend style="margin-bottom : 0px;"><a style="text-decoration:none" href="index.php?v=d&p=object&id=' . $child->getId() . '">' . $child->getDisplay('icon') . ' ' . $child->getName() . '</a><span style="font-size : 0.6em;margin-left:10px;">' . $child->getHtmlSummary() . '</span> <i class="fas fa-compress pull-right cursor bt_editDashboardWidgetAutoResize" id="edit_object_' . $child->getId() . '" data-mode="0" style="margin-right : 10px; display: none;"></i></legend>';
+			echo '<div class="div_displayEquipement grid-stack" id="div_ob' . $child->getId() . '" style="width: 100%;padding-top:3px;margin-bottom : 3px;">';
+			echo '<script>getObjectHtml(' . $child->getId() . ')</script>';
+			echo '</div>';
+			echo '</div>';
+			echo '</div>';
+		}
+	}
+	?>
+</div>
+</div>
+<?php
+if ($_SESSION['user']->getOptions('displayScenarioByDefault') == 1) {
+	echo '<div class="col-lg-2 col-md-2 col-sm-3" id="div_displayScenario">';
+} else {
+	echo '<div class="col-lg-2 col-md-2 col-sm-3" id="div_displayScenario" style="display:none;">';
+}
+?>
+<legend><i class="fas fa-history"></i> {{Scénarios}}</legend>
+<?php
+foreach (scenario::all() as $scenario) {
+	if ($scenario->getIsVisible() == 0) {
+		continue;
+	}
+	echo $scenario->toHtml('dashboard');
+}
+?>
+</div>
+</div>
