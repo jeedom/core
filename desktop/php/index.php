e18efb4403c3491f1a04fdc87ee60a2c0d1b6b59
Loïc <loic@jeedom.com>
2019-01-25 16:49:41 +0100

    bugfix

--- desktop/php/index.php
+++ desktop/php/index.php
@@ -386,7 +386,7 @@ if (init('rescue', 0) == 0) {
 												<?php if (isset($plugin) && is_object($plugin) && $plugin->getIssue() != '') { ?>
 													<a target="_blank" href="<?php echo $plugin->getIssue() ?>"><i class="fas fa-exclamation-circle" ></i> {{Rapport de bug}}</a>
 												<?php } else {?>
-													<a class="bt_reportBug cursor"><i class="fas fa-exclamation-circle" ></i> {{Demande de support}}</a>
+													<a class="bt_reportBug"><i class="fas fa-exclamation-circle" ></i> {{Demande de support}}</a>
 												<?php } ?>
 											</li>
 										<?php } ?>
