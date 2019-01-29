b6a18f1d150da0f8d90e6e6183c3e3a856c1d934
Loïc <loic@jeedom.com>
2019-01-29 14:21:39 +0100

    allow css update

--- core/ajax/jeedom.ajax.php
+++ core/ajax/jeedom.ajax.php
@@ -307,7 +307,7 @@ try {
 	if (init('action') == 'getFileContent') {
 		unautorizedInDemo();
 		$pathinfo = pathinfo(init('path'));
-		if (!in_array($pathinfo['extension'], array('php', 'js', 'json', 'sql', 'ini','html','py'))) {
+		if (!in_array($pathinfo['extension'], array('php', 'js', 'json', 'sql', 'ini','html','py','css'))) {
 			throw new Exception(__('Vous ne pouvez éditer ce type d\'extension : ' . $pathinfo['extension'], __FILE__));
 		}
 		ajax::success(file_get_contents(init('path')));
@@ -316,7 +316,7 @@ try {
 	if (init('action') == 'setFileContent') {
 		unautorizedInDemo();
 		$pathinfo = pathinfo(init('path'));
-		if (!in_array($pathinfo['extension'], array('php', 'js', 'json', 'sql', 'ini','html','py'))) {
+		if (!in_array($pathinfo['extension'], array('php', 'js', 'json', 'sql', 'ini','html','py','css'))) {
 			throw new Exception(__('Vous ne pouvez éditer ce type d\'extension : ' . $pathinfo['extension'], __FILE__));
 		}
 		ajax::success(file_put_contents(init('path'), init('content')));
@@ -325,7 +325,7 @@ try {
 	if (init('action') == 'deleteFile') {
 		unautorizedInDemo();
 		$pathinfo = pathinfo(init('path'));
-		if (!in_array($pathinfo['extension'], array('php', 'js', 'json', 'sql', 'ini'))) {
+		if (!in_array($pathinfo['extension'], array('php', 'js', 'json', 'sql', 'ini','css'))) {
 			throw new Exception(__('Vous ne pouvez éditer ce type d\'extension : ' . $pathinfo['extension'], __FILE__));
 		}
 		ajax::success(unlink(init('path')));
@@ -334,7 +334,7 @@ try {
 	if (init('action') == 'createFile') {
 		unautorizedInDemo();
 		$pathinfo = pathinfo(init('name'));
-		if (!in_array($pathinfo['extension'], array('php', 'js', 'json', 'sql', 'ini'))) {
+		if (!in_array($pathinfo['extension'], array('php', 'js', 'json', 'sql', 'ini','css'))) {
 			throw new Exception(__('Vous ne pouvez éditer ce type d\'extension : ' . $pathinfo['extension'], __FILE__));
 		}
 		touch(init('path') . init('name'));
