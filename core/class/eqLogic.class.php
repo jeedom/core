fc37defa6a6fbd8184296b0e33fa0441e0e8095b
Loïc <loic@jeedom.com>
2019-02-08 14:52:45 +0100

    re enable cache

--- core/class/eqLogic.class.php
+++ core/class/eqLogic.class.php
@@ -652,7 +652,7 @@ class eqLogic {
 		if (!$_noCache) {
 			$mc = cache::byKey('widgetHtml' . $this->getId() . $_version . $user_id);
 			if ($mc->getValue() != '') {
-				//return preg_replace("/" . preg_quote(self::UIDDELIMITER) . "(.*?)" . preg_quote(self::UIDDELIMITER) . "/", self::UIDDELIMITER . mt_rand() . self::UIDDELIMITER, $mc->getValue());
+				return preg_replace("/" . preg_quote(self::UIDDELIMITER) . "(.*?)" . preg_quote(self::UIDDELIMITER) . "/", self::UIDDELIMITER . mt_rand() . self::UIDDELIMITER, $mc->getValue());
 			}
 		}
 		$translate_category = '';
