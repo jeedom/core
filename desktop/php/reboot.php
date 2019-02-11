3bf10388141ad652c43653613903d90ef2852022
Rocket13011 <alexandre@jeedom.com>
2019-02-11 11:54:46 +0100

    Variabilisation + Translate

--- desktop/php/reboot.php
+++ desktop/php/reboot.php
@@ -17,13 +17,13 @@ if (!isConnect('admin')) {
 </style>
 
 <div id="contenu">
-	<h2 style="opacity:0.8;"><span class="glyphicon glyphicon-refresh"></span> Redémarrage</h2>
+	<h2 style="opacity:0.8;"><span class="glyphicon glyphicon-refresh"></span> {{Redémarrage}}</h2>
 	<div class="progress">
 		<div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar" id="progressbar_reboot" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%">
 			<span class="sr-only"></span>
 		</div>
 	</div>
-	<div id="div_reboot_jeedom_texte"><h6>Merci de patienter...</h6></div>
+	<div id="div_reboot_jeedom_texte"><h6>{{Merci de patienter...}}</h6></div>
 </div>
 <iframe src="" id="iframe_reboot" style="display:none"></iframe>
 
@@ -48,7 +48,7 @@ if (!isConnect('admin')) {
 	function page_rebootjs(rebooti){
 		refresh();
 		if(rebooti=='1'){
-			$('#div_reboot_jeedom_texte').empty().html('<h6>Jeedom est de nouveau opérationnel. Vous allez être redirigé vers votre dashboard.</h6>');
+			$('#div_reboot_jeedom_texte').empty().html('<h6><?php echo config::byKey("product_name"); ?> {{est de nouveau opérationnel. Vous allez être redirigé vers votre dashboard.}}</h6>');
 			$('#progressbar_reboot').addClass('progress-bar-success').removeClass('progress-bar-danger');
 			$('#progressbar_reboot').width('75%');
 			setTimeout("$('#progressbar_reboot').width('100%');", 3500);
@@ -57,13 +57,13 @@ if (!isConnect('admin')) {
 			testjeedom++;
 			if(testjeedom > '15'){
 				$('#progressbar_reboot').addClass('progress-bar-danger').removeClass('progress-bar-success');
-				$('#div_reboot_jeedom_texte').empty().html('<h6>Jeedom n\'a pas encore redémarré, nous continuons cependant de tester son retour.<br />N\'hésitez pas à la débrancher/rebrancher électriquement.</h6>');
+				$('#div_reboot_jeedom_texte').empty().html('<h6><?php echo config::byKey("product_name"); ?> {{n\'a pas encore redémarré, nous continuons cependant de tester son retour.}}<br />{{N\'hésitez pas à la débrancher/rebrancher électriquement.}}</h6>');
 			}
 		}
 	}
 
 	function reboot_jeedom(rebooti){
-		$('#div_reboot_jeedom_texte').empty().html('<h6>Merci de patienter...<br />Jeedom est en cours de redémarrage.</h6>');
+		$('#div_reboot_jeedom_texte').empty().html('<h6>{{Merci de patienter...}}<br /><?php echo config::byKey("product_name"); ?> {{est en cours de redémarrage.}}</h6>');
 		$('#progressbar_reboot').width('25%');
 		setInterval('page_rebootjs(rebooti)', 15000);
 	}
