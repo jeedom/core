c311092c633ecd92555041146f117242513f7646
Loïc <loic@jeedom.com>
2019-02-17 11:08:27 +0100

    Update history.class.php

--- core/class/history.class.php
+++ core/class/history.class.php
@@ -509,21 +509,15 @@ class history {
 						return -1;
 					}
 					$currentValue = $histories[0]->getValue();
-					$dateTo = date('Y-m-d H:i:s');
-					$duration = strtotime($dateTo) - strtotime($histories[0]->getDatetime());
 					for ($i = 0; $i < $c - 1; $i++) {
-						$history = $histories[$i];
-						$value = $history->getValue();
-						$date = $history->getDatetime();
 						$nextValue = $histories[$i + 1]->getValue();
 						if ($currentValue != $nextValue) {
-							return $duration;
-						}
-						if ($i > 0) {
-							$duration += strtotime($histories[$i - 1]->getDatetime()) - strtotime($date);
+							break;
 						}
 					}
-					return -1;
+					$dateTo = date('Y-m-d H:i:s');
+					$duration = strtotime($dateTo) - strtotime($histories[$i]->getDatetime());
+					return $duration;
 				}
 				
 				public static function lastStateDuration($_cmd_id, $_value = null) {
