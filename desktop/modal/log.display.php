a75bee6da56795e36401d54a0f83ed4e95cb3ec5
Loïc <loic@jeedom.com>
2019-02-19 09:49:41 +0100

    Update log.display.php

--- desktop/modal/log.display.php
+++ desktop/modal/log.display.php
@@ -1,19 +1,19 @@
 <?php
 /* This file is part of Jeedom.
- *
- * Jeedom is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Jeedom is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
- */
+*
+* Jeedom is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Jeedom is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 if (!isConnect('admin')) {
 	throw new Exception('401 Unauthorized');
@@ -25,30 +25,30 @@ sendVarToJS('log_default_search', init('search', ''));
 <a class="btn btn-warning pull-right" id="bt_logdisplayclearLog"><i class="fas fa-times"></i> {{Vider}}</a>
 <a class="btn btn-success pull-right" id="bt_logdisplaydownloadLog"><i class="fas fa-cloud-download-alt"></i> {{Télécharger}}</a>
 <a class="btn btn-warning pull-right" data-state="1" id="bt_eventLogStopStart"><i class="fas fa-pause"></i> {{Pause}}</a>
-<input class="form-control pull-right" id="in_eventLogSearch" style="width : 300px;" placeholder="{{Rechercher}}" />
+<input class="form-control pull-right" id="in_eventLogSearch" style="width : 200px;margin-left:5px;" placeholder="{{Rechercher}}" />
 <br/><br/><br/>
 <pre id='pre_eventlog' style='overflow: auto; height: calc(100% - 65px);with:90%;'></pre>
 <script>
-	jeedom.log.autoupdate({
+jeedom.log.autoupdate({
+	log : log_display_name,
+	default_search : log_default_search,
+	display : $('#pre_eventlog'),
+	search : $('#in_eventLogSearch'),
+	control : $('#bt_eventLogStopStart'),
+});
+
+$("#bt_logdisplayclearLog").on('click', function(event) {
+	jeedom.log.clear({
 		log : log_display_name,
-		default_search : log_default_search,
-		display : $('#pre_eventlog'),
-		search : $('#in_eventLogSearch'),
-		control : $('#bt_eventLogStopStart'),
 	});
+});
 
-	$("#bt_logdisplayclearLog").on('click', function(event) {
-		jeedom.log.clear({
-			log : log_display_name,
-		});
+$("#bt_logdisplayremoveLog").on('click', function(event) {
+	jeedom.log.remove({
+		log : log_display_name,
 	});
-
-	$("#bt_logdisplayremoveLog").on('click', function(event) {
-		jeedom.log.remove({
-			log : log_display_name,
-		});
-	});
-	$('#bt_logdisplaydownloadLog').click(function() {
-		window.open('core/php/downloadFile.php?pathfile=log/' + log_display_name, "_blank", null);
-	});
+});
+$('#bt_logdisplaydownloadLog').click(function() {
+	window.open('core/php/downloadFile.php?pathfile=log/' + log_display_name, "_blank", null);
+});
 </script>
