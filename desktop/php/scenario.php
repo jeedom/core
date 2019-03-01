5857bdf7f39b978b8c977027ef26f18392ff01cb
Loïc <loic@jeedom.com>
2019-03-01 10:20:13 +0100

    typo

--- desktop/php/scenario.php
+++ desktop/php/scenario.php
@@ -55,7 +55,7 @@ if (is_array($scenarioListGroup)) {
 				<span style="position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#337ab7"><center>{{Testeur d'expression}}</center></span>
 			</div>
 		</div>
-
+		
 		<legend><i class="icon jeedom-clap_cinema"></i>  {{Mes scénarios}}</legend>
 		<?php
 		if (count($totalScenario) == 0) {
@@ -128,7 +128,7 @@ if (is_array($scenarioListGroup)) {
 		}
 		?>
 	</div>
-
+	
 	<div id="div_editScenario" class="col-xs-12" style="display: none;" >
 		<div class="input-group pull-right" style="display:inline">
 			<span class="input-group-btn">
@@ -278,7 +278,7 @@ if (is_array($scenarioListGroup)) {
 							<div class="scheduleMode"></div>
 						</div>
 						<div class="provokeMode provokeDisplay" style="display: none;">
-
+							
 						</div>
 					</form>
 				</div>
@@ -289,7 +289,7 @@ if (is_array($scenarioListGroup)) {
 			<div id="div_scenarioElement" class="element" style="padding-bottom: 70px;"></div>
 		</div>
 	</div>
-
+	
 </div>
 </div>
 
@@ -337,31 +337,31 @@ if (is_array($scenarioListGroup)) {
 				<div class="alert alert-info addElementTypeDescription if">
 					Permet de faire des conditions dans votre scénario. Par exemple : Si mon détecteur d’ouverture de porte se déclenche Alors allumer la lumière.
 				</div>
-
+				
 				<div class="alert alert-info addElementTypeDescription action" style="display:none;">
 					Permet de lancer une action, sur un de vos modules, scénarios ou autre. Par exemple : Passer votre sirène sur ON.
 				</div>
-
+				
 				<div class="alert alert-info addElementTypeDescription for" style="display:none;">
 					Une boucle permet de réaliser une action de façon répétée un certain nombre de fois. Par exemple : Permet de répéter une action de 1 à X, c’est-à-dire X fois.
 				</div>
-
+				
 				<div class="alert alert-info addElementTypeDescription in" style="display:none;">
 					Permet de faire une action dans X min. Par exemple : Dans 5 min, éteindre la lumière.
 				</div>
-
+				
 				<div class="alert alert-info addElementTypeDescription at" style="display:none;">
 					A un temps précis, cet élément permet de lancer une action. Par exemple : A 9h30, ouvrir les volets.
 				</div>
-
+				
 				<div class="alert alert-info addElementTypeDescription code" style="display:none;">
 					Cet élément permet de rajouter dans votre scénario de la programmation à l’aide d’un code, PHP/Shell, etc.
 				</div>
-
+				
 				<div class="alert alert-info addElementTypeDescription comment" style="display:none;">
 					Permet de commenter votre scénario.
 				</div>
-
+				
 			</div>
 			<div class="modal-footer">
 				<a class="btn btn-danger" data-dismiss="modal"><i class="fas fa-minus-circle"></i> {{Annuler}}</a>
