b4cd99eabfe1080c70a153e2a24ba40f08b2283f
Loïc <loic@jeedom.com>
2019-03-03 12:09:12 +0100

    add box chadow on hover

---
+++ core/themes/core2019_Dark/desktop/core2019_Dark.js
@@ -0,0 +1,16 @@
+$(function () {
+  if(typeof theme_config != 'undefined'){
+    if(typeof theme_config['widget::shadow'] != 'undefined' && theme_config['widget::shadow'] == '1'){
+      var style = '<style>';
+      style += '.eqLogic-widget:hover,.scenario-widget:hover {';
+      style += 'box-shadow: 0 6px 22px rgba(0,0,0,0.25);';
+      style += 'z-index: 5;';
+      style += '}';
+      style += '.eqLogic-widget,.scenario-widget {';
+      style += 'transition: box-shadow 0.4s cubic-bezier(.25,.8,.25,1);';
+      style += '}';
+      style += '</style>';
+      $('html > head').append($(style));
+    }
+  }
+});
