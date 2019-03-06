ccfa2a5ff494568fb73edbf75d2acdeb4758ad96
KiboOst <KiboOst@users.noreply.github.com>
2019-03-06 13:34:14 +0100

    Update home.js

--- mobile/js/home.js
+++ mobile/js/home.js
@@ -10,8 +10,8 @@ function initHome() {
   $('#bottompanel_otherActionList').append('<a id="bt_switchTheme" class="ui-bottom-sheet-link ui-btn ui-btn-inline waves-effect waves-button"><i class="fas fa-sync-alt"></i> {{Basculer le thème}}</a>');
   $('#bottompanel_otherActionList').append('<a id="bt_forceReload" class="ui-bottom-sheet-link ui-btn ui-btn-inline waves-effect waves-button"><i class="fas fa-retweet"></i> {{Forcer mise à jour}}</a>');
   $('#bottompanel_otherActionList').append('<a href="#" id="bt_logout" class="ui-bottom-sheet-link ui-btn ui-btn-inline waves-effect waves-button"><i class="fas fa-sign-out-alt"></i> {{Déconnexion}}</a>');
-  
-  
+
+
   jeedom.object.all({
     error: function (error) {
       $('#div_alert').showAlert({message: error.message, level: 'danger'});
@@ -34,7 +34,7 @@ function initHome() {
       jeedom.object.summaryUpdate(summaries);
     }
   });
-  
+
   jeedom.view.all({
     error: function (error) {
       $('#div_alert').showAlert({message: error.message, level: 'danger'});
@@ -51,7 +51,7 @@ function initHome() {
       $('#bottompanel_viewList').empty().append(li);
     }
   });
-  
+
   jeedom.plan.allHeader({
     error: function (error) {
       $('#div_alert').showAlert({message: error.message, level: 'danger'});
@@ -68,7 +68,7 @@ function initHome() {
       $('#bottompanel_planList').empty().append(li);
     }
   });
-  
+
   jeedom.plan3d.allHeader({
     error: function (error) {
       $('#div_alert').showAlert({message: error.message, level: 'danger'});
@@ -85,8 +85,8 @@ function initHome() {
       $('#bottompanel_plan3dList').empty().append(li);
     }
   });
-  
-  
+
+
   if (plugins.length > 0) {
     var li = '';
     for (var i in plugins) {
@@ -109,7 +109,7 @@ function initHome() {
   } else {
     $('#bt_listPlugin').hide();
   }
-  
+
   $('#bt_logout').off('click').on('click', function () {
     $.ajax({
       type: "POST",
@@ -130,9 +130,14 @@ function initHome() {
       }
     });
   });
-  
+
   $('#bt_forceReload').off('click').on('click', function () {
     window.location.reload(true);
   })
+
+  jeedom.version({success : function(version) {
+    $('#homeVersion').html(version)
+  }})
+
   setTimeout(function(){$('#pagecontainer').css('padding-top','64px');; }, 100);
 }
