d80a78a76fbb9068bb49c82a6dea0277ed577013
Loïc <loic@jeedom.com>
2019-03-14 15:31:59 +0100

    Update index.php

--- desktop/php/index.php
+++ desktop/php/index.php
@@ -244,7 +244,7 @@ if (init('rescue', 0) == 0) {
 			<header class="navbar navbar-fixed-top navbar-default reportModeHidden" style="margin-bottom: 0px !important;">
 				<div class="container-fluid">
 					<div class="navbar-header">
-						<a class="navbar-brand hidden-xs" href="<?php echo $homeLink; ?>">
+						<a class="navbar-brand" href="<?php echo $homeLink; ?>">
 							<img src="<?php echo $configs['product_image'] ?>" height="30" style="position: relative; top:-5px;"/>
 						</a>
 						<button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".navbar-collapse">
