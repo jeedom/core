6f17f8d1d9c5a6465360187bf3acecf7a60a1cf4
Loïc <loic@jeedom.com>
2019-03-14 17:45:19 +0100

    Update scenarioExpression.class.php

--- core/class/scenarioExpression.class.php
+++ core/class/scenarioExpression.class.php
@@ -1501,7 +1501,7 @@ class scenarioExpression {
 						case 'eqAnalyse':
 						$url = network::getNetworkAccess('internal') . '/index.php?v=d&p=eqAnalyse&report=1';
 						$this->setLog($scenario, __('Génération du rapport ', __FILE__) . $url);
-						$cmd_parameters['files'] = array(report::generate($url,'other','analyze',$options['export_type'], $options));
+						$cmd_parameters['files'] = array(report::generate($url,'other','eqAnalyse',$options['export_type'], $options));
 						$cmd_parameters['title'] = __('[' . config::byKey('name') . '] Rapport équipement du ', __FILE__) . date('Y-m-d H:i:s');
 						$cmd_parameters['message'] = __('Veuillez trouver ci-joint le rapport équipement généré le ', __FILE__) . date('Y-m-d H:i:s');
 						break;
