eeb8c8dacf279d255fd8e1cb9c325ea05bc9f28e
Loïc <loic@jeedom.com>
2019-03-16 15:45:22 +0100

    Update jeedom.class.php

--- core/class/jeedom.class.php
+++ core/class/jeedom.class.php
@@ -396,10 +396,10 @@ class jeedom {
 		if (trim($_apikey) == '') {
 			return false;
 		}
+		if($_plugin != 'core' && self::apiAccess($_apikey)){
+			return true;
+		}
 		if ($_plugin != 'core' && $_plugin != 'proapi' && !self::apiModeResult(config::byKey('api::' . $_plugin . '::mode', 'core', 'enable'))) {
-			if($_plugin != 'core'){
-				return self::apiAccess($_apikey);
-			}
 			return false;
 		}
 		$apikey = self::getApiKey($_plugin);
@@ -415,9 +415,6 @@ class jeedom {
 			$_USER_GLOBAL = $user;
 			log::add('connection', 'info', __('Connexion par API de l\'utilisateur : ', __FILE__) . $user->getLogin());
 			return true;
-		}
-		if($_plugin != 'core'){
-			return self::apiAccess($_apikey);
 		}
 		return false;
 	}
