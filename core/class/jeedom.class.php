bbfc40a7aae83ee31585e56b40eeb1dd52d14d10
Loïc <loic@jeedom.com>
2019-03-16 15:43:14 +0100

    simplify

--- core/class/jeedom.class.php
+++ core/class/jeedom.class.php
@@ -429,10 +429,10 @@ class jeedom {
 		if (trim($_apikey) == '') {
 			return false;
 		}
+		if($_plugin != 'core' && self::apiAccess($_apikey)){
+			return true;
+		}
 		if ($_plugin != 'core' && $_plugin != 'proapi' && !self::apiModeResult(config::byKey('api::' . $_plugin . '::mode', 'core', 'enable'))) {
-			if($_plugin != 'core'){
-				return self::apiAccess($_apikey);
-			}
 			return false;
 		}
 		$apikey = self::getApiKey($_plugin);
@@ -448,9 +448,6 @@ class jeedom {
 			$_USER_GLOBAL = $user;
 			log::add('connection', 'info', __('Connexion par API de l\'utilisateur : ', __FILE__) . $user->getLogin());
 			return true;
-		}
-		if($_plugin != 'core'){
-			return self::apiAccess($_apikey);
 		}
 		return false;
 	}
