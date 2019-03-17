dfa4f12d063e7551d0f057c8b8b84bb534636c18
noodom <bruno.comptes@gmail.com>
2019-03-17 12:25:04 +0100

    ponctuations

--- core/class/plugin.class.php
+++ core/class/plugin.class.php
@@ -286,7 +286,7 @@ class plugin {
 	public static function cron() {
 		$cache = cache::byKey('plugin::cron::inprogress');
 		if ($cache->getValue(0) > 3) {
-			message::add('core', __('La tache plugin::cron n\'arrive pas à finir à cause du plugin : ', __FILE__) . cache::byKey('plugin::cron::last')->getValue() . __(' nous vous conseillons de désactiver le plugin et de contacter l\'auteur', __FILE__));
+			message::add('core', __('La tache plugin::cron n\'arrive pas à finir à cause du plugin : ', __FILE__) . cache::byKey('plugin::cron::last')->getValue() . __('. Nous vous conseillons de désactiver le plugin et de contacter l\'auteur', __FILE__));
 		}
 		cache::set('plugin::cron::inprogress', $cache->getValue(0) + 1);
 		foreach (self::listPlugin(true) as $plugin) {
@@ -311,7 +311,7 @@ class plugin {
 	public static function cron5() {
 		$cache = cache::byKey('plugin::cron5::inprogress');
 		if ($cache->getValue(0) > 3) {
-			message::add('core', __('La tache plugin::cron5 n\'arrive pas à finir à cause du plugin : ', __FILE__) . cache::byKey('plugin::cron5::last')->getValue() . __(' nous vous conseillons de désactiver le plugin et de contacter l\'auteur', __FILE__));
+			message::add('core', __('La tache plugin::cron5 n\'arrive pas à finir à cause du plugin : ', __FILE__) . cache::byKey('plugin::cron5::last')->getValue() . __('. Nous vous conseillons de désactiver le plugin et de contacter l\'auteur', __FILE__));
 		}
 		cache::set('plugin::cron5::inprogress', $cache->getValue(0) + 1);
 		foreach (self::listPlugin(true) as $plugin) {
@@ -336,7 +336,7 @@ class plugin {
 	public static function cron15() {
 		$cache = cache::byKey('plugin::cron15::inprogress');
 		if ($cache->getValue(0) > 3) {
-			message::add('core', __('La tache plugin::cron15 n\'arrive pas à finir à cause du plugin : ', __FILE__) . cache::byKey('plugin::cron15::last')->getValue() . __(' nous vous conseillons de désactiver le plugin et de contacter l\'auteur', __FILE__));
+			message::add('core', __('La tache plugin::cron15 n\'arrive pas à finir à cause du plugin : ', __FILE__) . cache::byKey('plugin::cron15::last')->getValue() . __(' . Nous vous conseillons de désactiver le plugin et de contacter l\'auteur', __FILE__));
 		}
 		cache::set('plugin::cron15::inprogress', $cache->getValue(0) + 1);
 		foreach (self::listPlugin(true) as $plugin) {
