703675df25c779dd528b242ac44f96a40627b7e7
Loïc <loic@jeedom.com>
2019-03-20 16:18:27 +0100

    add methode

--- core/js/eqLogic.class.js
+++ core/js/eqLogic.class.js
@@ -63,6 +63,45 @@ jeedom.eqLogic.save = function (_params) {
   $.ajax(paramsAJAX);
 }
 
+
+jeedom.eqLogic.byType = function (_params) {
+  var paramsRequired = ['type'];
+  var paramsSpecifics = {};
+  try {
+    jeedom.private.checkParamsRequired(_params || {}, paramsRequired);
+  } catch (e) {
+    (_params.error || paramsSpecifics.error || jeedom.private.default_params.error)(e);
+    return;
+  }
+  var params = $.extend({}, jeedom.private.default_params, paramsSpecifics, _params || {});
+  var paramsAJAX = jeedom.private.getParamsAJAX(params);
+  paramsAJAX.url = 'core/ajax/eqLogic.ajax.php';
+  paramsAJAX.data = {
+    action: 'listByType',
+    type: _params.type,
+  };
+  $.ajax(paramsAJAX);
+}
+
+jeedom.eqLogic.byObjectId = function (_params) {
+  var paramsRequired = ['object_id'];
+  var paramsSpecifics = {};
+  try {
+    jeedom.private.checkParamsRequired(_params || {}, paramsRequired);
+  } catch (e) {
+    (_params.error || paramsSpecifics.error || jeedom.private.default_params.error)(e);
+    return;
+  }
+  var params = $.extend({}, jeedom.private.default_params, paramsSpecifics, _params || {});
+  var paramsAJAX = jeedom.private.getParamsAJAX(params);
+  paramsAJAX.url = 'core/ajax/eqLogic.ajax.php';
+  paramsAJAX.data = {
+    action: 'listByObject',
+    object_id: _params.object_id,
+  };
+  $.ajax(paramsAJAX);
+}
+
 jeedom.eqLogic.simpleSave = function (_params) {
   var paramsRequired = ['eqLogic'];
   var paramsSpecifics = {};
