cdea5e8c634ba21b64645b6325e8773437fd5559
Loïc <loic@jeedom.com>
2019-03-20 15:58:26 +0100

    improve log

--- desktop/php/log.php
+++ desktop/php/log.php
@@ -25,30 +25,27 @@ natcasesort($list_logfile);
 				<li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
 				<?php
 				foreach ($list_logfile as $file) {
-					$hasErr = 0;
-					$flag = '<i class="fas fa-check"></i>';
-					if (shell_exec('grep -c -E "\[ERROR\]|\[error\]" ' . __DIR__ . '/../../log/' . $file) != 0) {
-						$flag = '<i class="fas fa-exclamation-triangle"></i>';
-					} else if (shell_exec('grep -c -E "\[WARNING\]" ' . __DIR__ . '/../../log/' . $file) != 0) {
-						$flag = '<i class="fa fa-exclamation-circle"></i>';
-					}
 					$fsize = filesize('log/' . $file);
-					if ( $fsize <= 1 )	{
+					
+					if ($fsize < 2){
 						$fsizelog = '';
+					}else if ($fsize < 1024){
+						$fsizelog = $fsize.' o';
+					}else if ( $fsize < 1048576){
+						$fsizelog = round($fsize / 1024,1) .' Ko';
 					}else{
-						if ( $fsize < 1024 ){
-							$fsizelog = '('.$fsize .' o)';
-						}else if ( $fsize < 1048576){
-							$fsizelog = '('.round($fsize / 1024,1) .' Ko)';
-						}else{
-							$fsizelog = '('.round($fsize / 1048576 ,1) .' Mo)';
-						}
+						$fsizelog = round($fsize / 1048576 ,1) .' Mo';
 					}
-					if ($file == $logfile) {
-						echo '<li class="cursor li_log active" data-log="' . $file . '" >' . $flag . '<a>' . $file . ' ' . $fsizelog . '</a></li>';
-					} else {
-						echo '<li class="cursor li_log" data-log="' . $file . '">' . $flag . '<a>' . $file . ' ' . $fsizelog . '</a></li>';
+					if($fsizelog != ''){
+						$fsizelog = ' ('.$fsizelog.')';
 					}
+					$flag = '<i class="fa fa-check" style="font-weight: bold;float:left;display:inline;margin-top:8px;color:green;"></i>';
+					if (shell_exec('grep -ci -E "\[:error\]|\[error\]" ' . __DIR__ . '/../../log/' . $file) != 0) {
+						$flag = '<i class="fa fa-exclamation-triangle" style="font-weight: bold;float:left;display:inline;margin-top:8px;color:red;"></i>';
+					} else if (shell_exec('grep -c -E "\[WARNING\]" ' . __DIR__ . '/../../log/' . $file) != 0) {
+						$flag = '<i class="fa fa-exclamation-circle" style="font-weight: bold;float:left;display:inline;margin-top:8px;color:orange;"></i>';
+					}
+					echo '<li class="cursor li_log ' .(($file == $logfile)?'active':'') .'" data-log="' . $file . '" >' . $flag . '<a>' . $file . $fsizelog . '</a></li>';
 				}
 				?>
 			</ul>
@@ -57,12 +54,12 @@ natcasesort($list_logfile);
 	<div class="col-lg-10 col-md-9 col-sm-8">
 		<div class="row">
 			<div class="col-lg-12">
-				<div class="input-group pull-right" style="display:inline-flex">
-					<input class="form-control roundedLeft input-sm" style="max-width:150px;" id="in_globalLogSearch" placeholder="{{Rechercher}}" />
-					<span class="input-group-btn">
-						<a class="btn btn-warning btn-sm" data-state="1" id="bt_globalLogStopStart"><i class="fas fa-pause"></i> {{Pause}}</a><a class="btn btn-success btn-sm" id="bt_downloadLog"><i class="fas fa-cloud-download-alt"></i> {{Télécharger}}</a><a class="btn btn-warning btn-sm" id="bt_clearLog"><i class="fas fa-times"></i> {{Vider}}</a><a class="btn btn-danger btn-sm" id="bt_removeLog"><i class="far fa-trash-alt"></i> {{Supprimer}}</a><a class="btn btn-danger btn-sm roundedRight" id="bt_removeAllLog"><i class="far fa-trash-alt"></i> {{Supprimer tous}}</a>
-					</span>
-				</div>
+				<a class="btn btn-danger pull-right" id="bt_removeAllLog"><i class="far fa-trash-alt"></i> {{Supprimer tous}}</a>
+				<a class="btn btn-danger pull-right" id="bt_removeLog"><i class="far fa-trash-alt"></i> {{Supprimer}}</a>
+				<a class="btn btn-warning pull-right" id="bt_clearLog"><i class="fas fa-times"></i> {{Vider}}</a>
+				<a class="btn btn-success pull-right" id="bt_downloadLog"><i class="fas fa-cloud-download-alt"></i> {{Télécharger}}</a>
+				<a class="btn btn-warning pull-right" data-state="1" id="bt_globalLogStopStart"><i class="fas fa-pause"></i> {{Pause}}</a>
+				<input style="max-width: 150px;" class="form-control pull-right" id="in_globalLogSearch" placeholder="{{Rechercher}}" />
 			</div>
 		</div>
 		<pre id='pre_globallog' style='overflow: auto; height: calc(100% - 50px);width:100%;margin-top: 5px;'></pre>
