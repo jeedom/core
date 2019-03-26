f2a564d4b9bce88ed93237e707a2a44967cd6245
KiboOst <KiboOst@users.noreply.github.com>
2019-03-26 13:48:00 +0100

    Update index.php

--- desktop/php/index.php
+++ desktop/php/index.php
@@ -164,10 +164,6 @@ if (init('rescue', 0) == 0) {
 	include_file('3rdparty', 'jquery.contextMenu/jquery.contextMenu.min', 'css');
 	include_file('3rdparty', 'jquery.contextMenu/jquery.contextMenu.min', 'js');
 	include_file('3rdparty', 'autosize/autosize.min', 'js');
-	if (!isConnect()) {
-		include_file('3rdparty', 'animate/animate', 'css');
-		include_file('3rdparty', 'animate/animate', 'js');
-	}
 	include_file('desktop', 'bootstrap', 'css');
 	include_file('desktop', 'desktop.main', 'css');
 
@@ -297,7 +293,7 @@ if (init('rescue', 0) == 0) {
 										<li><a href = "index.php?v=d&p=scenario"><i class = "fas fa-cogs"></i> {{Scénarios}}</a></li>
 										<li><a href="index.php?v=d&p=interact"><i class="far fa-comments"></i> {{Interactions}}</a></li>
 										<li><a href="index.php?v=d&p=widgets"><i class="fas fa-camera-retro"></i> {{Widgets}}</a></li>
-										<li><a href="#" id="bt_showNoteManager"><i class="fas fa-sticky-note"></i> {{Notes}}</a></li>
+										<li><a href="#" id="bt_showNoteManager"><i class="fas fa-sticky-note"></i> {{Notes}}</a></li>										
 									</ul>
 								</li>
 							<?php } ?>
