77b5230fd71cd79eb4087bd5d614738c2f8d4229
Loïc <loic@jeedom.com>
2019-04-12 18:35:59 +0200

    test

--- core/js/plugin.template.js
+++ core/js/plugin.template.js
@@ -23,7 +23,7 @@ if((!isset(userProfils.doNotAutoHideMenu) || userProfils.doNotAutoHideMenu != 1)
   $('#ul_eqLogic').closest('.bs-sidebar').parent().removeClass().addClass('col-xs-2');
   $('.eqLogicThumbnailDisplay').removeClass().addClass('eqLogicThumbnailDisplay col-xs-12');
   $('.eqLogic').removeClass('col-xs-10 col-lg-10 col-md-9 col-sm-8 col-lg-9 col-md-8 col-sm-7').addClass('eqLogic col-xs-12');
-
+  
   $('#ul_eqLogic').closest('.bs-sidebar').parent().on('mouseleave',function(){
     var timer = setTimeout(function(){
       $('#ul_eqLogic').closest('.bs-sidebar').parent().hide();
@@ -36,7 +36,7 @@ if((!isset(userProfils.doNotAutoHideMenu) || userProfils.doNotAutoHideMenu != 1)
   }).on("mouseenter", function(){
     clearTimeout($(this).data('timerMouseleave'));
   });
-
+  
   $('.bt_pluginTemplateShowSidebar').on('mouseenter',function(){
     var timer = setTimeout(function(){
       $('.eqLogicThumbnailDisplay').removeClass().addClass('eqLogicThumbnailDisplay col-xs-10');
@@ -77,12 +77,10 @@ $(function(){
         eqsGroups = Array.from(new Set(eqsGroups))
         eqsGroups.sort()
         eqsList = []
-        for(i=0; i<eqsGroups.length; i++)
-        {
+        for(i=0; i<eqsGroups.length; i++){
           group = eqsGroups[i]
           eqsList[group] = []
-          for(j=0; j<_eqs.length; j++)
-          {
+          for(j=0; j<_eqs.length; j++){
             eq = _eqs[j]
             humanName = eq.humanName
             humanCut = humanName.split(']')
@@ -105,9 +103,7 @@ $(function(){
           }
           contextmenuitems[group] = {'name':group, 'items':items}
         }
-
-        if (Object.entries(contextmenuitems).length > 0 && contextmenuitems.constructor === Object)
-        {
+        if (Object.entries(contextmenuitems).length > 0 && contextmenuitems.constructor === Object){
           $('.nav.nav-tabs').contextMenu({
             selector: 'li',
             autoHide: true,
@@ -118,9 +114,9 @@ $(function(){
               if (document.location.toString().match('#')) {
                 tab = '#' + document.location.toString().split('#')[1];
                 if (tab != '#') {
-                    tab = $('a[href="' + tab + '"]')
+                  tab = $('a[href="' + tab + '"]')
                 } else {
-                    tab = null
+                  tab = null
                 }
               }
               $('.eqLogicDisplayCard[data-eqLogic_id="' + key + '"]').click()
@@ -129,11 +125,12 @@ $(function(){
             items: contextmenuitems
           })
         }
-
+        
       }
     })
+  }catch(err) {
+    console.log(err)
   }
-  catch(err) { console.log(err)}
 })
 
 var url = document.location.toString();
@@ -155,11 +152,13 @@ $('.eqLogicAction[data-action=gotoPluginConf]').on('click', function () {
   $("#md_modal").load('index.php?v=d&p=plugin&ajax=1&id='+eqType).dialog('open');
 });
 
-$('.eqLogicAction[data-action=returnToThumbnailDisplay]').on('click', function () {
+$('.eqLogicAction[data-action=returnToThumbnailDisplay]').off('click').on('click', function (event) {
   $('.eqLogic').hide();
   $('.eqLogicThumbnailDisplay').show();
   $('.li_eqLogic').removeClass('active');
   $('.eqLogicThumbnailContainer').packery();
+  event.preventDefault();
+  return false;
 });
 
 
@@ -212,7 +211,7 @@ $(".li_eqLogic,.eqLogicDisplayCard").on('click', function () {
       $('body').delegate('.cmd .cmdAttr[data-l1key=type]', 'change', function () {
         jeedom.cmd.changeType($(this).closest('.cmd'));
       });
-
+      
       $('body').delegate('.cmd .cmdAttr[data-l1key=subType]', 'change', function () {
         jeedom.cmd.changeSubType($(this).closest('.cmd'));
       });
@@ -322,7 +321,7 @@ $('.eqLogicAttr[data-l1key=object_id]').on('change', function () {
 
 $('.eqLogicAction[data-action=remove]').on('click', function () {
   if ($('.eqLogicAttr[data-l1key=id]').value() != undefined) {
-
+    
     jeedom.eqLogic.getUseBeforeRemove({
       id: $('.eqLogicAttr[data-l1key=id]').value(),
       error: function (error) {
@@ -460,7 +459,7 @@ $('#div_pageContainer').on( 'click','.cmd .cmdAction[data-action=test]',function
   } else {
     $('#div_alert').showAlert({message: '{{Veuillez activer l\'équipement avant de tester une de ses commandes}}', level: 'warning'});
   }
-
+  
 });
 
 $('#div_pageContainer').on( 'dblclick','.cmd input,select,span,a', function (event) {
