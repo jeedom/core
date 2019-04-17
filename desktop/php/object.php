d0cca28f4846a38c219085cd83332e2c8ff9b7a4
Loïc <loic@jeedom.com>
2019-04-17 12:47:24 +0200

    Update object.php

--- desktop/php/object.php
+++ desktop/php/object.php
@@ -121,12 +121,6 @@ $allObject = jeeObject::buildTree(null, false);
 							</div>
 						</div>
 						<div class="form-group">
-							<label class="col-lg-2 col-md-3 col-sm-4 col-xs-6 control-label">{{Couleur du texte du résumé}}</label>
-							<div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
-								<input type="color" class="objectAttr form-control" data-l1key="display" data-l2key="desktop::summaryTextColor" />
-							</div>
-						</div>
-						<div class="form-group">
 							<label class="col-lg-2 col-md-3 col-sm-4 col-xs-6 control-label">{{Taille sur le dashboard (1 à 12)}}</label>
 							<div class="col-xs-1">
 								<input type="number" class="objectAttr form-control" data-l1key="display" data-l2key="dashboard::size" />
