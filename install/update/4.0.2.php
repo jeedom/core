251f5e7f086de2a4575fabc1cae47856de6d3336
Loïc <loic@jeedom.com>
2019-04-17 15:04:08 +0200

    fix and cleaning

--- install/update/4.0.2.php
+++ install/update/4.0.2.php
@@ -2,95 +2,30 @@
 require_once __DIR__ . '/../../core/php/core.inc.php';
 $replace = array(
   '<i class="fa' => '<i class="fas'
+  "<i class=\'fa" => "<i class=\'fas"
 );
 
-foreach (jeeObject::all() as $object) {
-  try {
-    utils::a2o($object,str_replace(array_keys($replace), $replace, utils::o2a($object)));
-    $object->save();
-  } catch (\Exception $e) {
-    
+function replaceAndSaveFA5($objects){
+  foreach ($objects as $object) {
+    try {
+      $json =  json_encode(utils::o2a($object));
+      $json = str_replace(array_keys($replace), $replace,$json);
+      utils::a2o($object,json_decode($json,true));
+      $object->save();
+    } catch (\Exception $e) {
+      
+    }
   }
 }
 
-foreach (eqLogic::all() as $object) {
-  try {
-    utils::a2o($object,str_replace(array_keys($replace), $replace, utils::o2a($object)));
-    $object->save();
-  } catch (\Exception $e) {
-    
-  }
-}
-
-foreach (cmd::all() as $object) {
-  try {
-    utils::a2o($object,str_replace(array_keys($replace), $replace, utils::o2a($object)));
-    $object->save();
-  } catch (\Exception $e) {
-    
-  }
-}
-
-foreach (scenario::all() as $object) {
-  try {
-    utils::a2o($object,str_replace(array_keys($replace), $replace, utils::o2a($object)));
-    $object->save();
-  } catch (\Exception $e) {
-    
-  }
-}
-
-foreach (scenarioExpression::all() as $object) {
-  try {
-    utils::a2o($object,str_replace(array_keys($replace), $replace, utils::o2a($object)));
-    $object->save();
-  } catch (\Exception $e) {
-    
-  }
-}
-
-foreach (viewZone::all() as $object) {
-  try {
-    utils::a2o($object,str_replace(array_keys($replace), $replace, utils::o2a($object)));
-    $object->save();
-  } catch (\Exception $e) {
-    
-  }
-}
-
-foreach (viewData::all() as $object) {
-  try {
-    utils::a2o($object,str_replace(array_keys($replace), $replace, utils::o2a($object)));
-    $object->save();
-  } catch (\Exception $e) {
-    
-  }
-}
-
-foreach (view::all() as $object) {
-  try {
-    utils::a2o($object,str_replace(array_keys($replace), $replace, utils::o2a($object)));
-    $object->save();
-  } catch (\Exception $e) {
-    
-  }
-}
-
-foreach (plan::all() as $object) {
-  try {
-    utils::a2o($object,str_replace(array_keys($replace), $replace, utils::o2a($object)));
-    $object->save();
-  } catch (\Exception $e) {
-    
-  }
-}
-
-foreach (planHeader::all() as $object) {
-  try {
-    utils::a2o($object,str_replace(array_keys($replace), $replace, utils::o2a($object)));
-    $object->save();
-  } catch (\Exception $e) {
-    
-  }
-}
+replaceAndSaveFA5(jeeObject::all());
+replaceAndSaveFA5(eqLogic::all());
+replaceAndSaveFA5(cmd::all());
+replaceAndSaveFA5(scenario::all();
+replaceAndSaveFA5(scenarioExpression::all());
+replaceAndSaveFA5(viewZone::all());
+replaceAndSaveFA5(viewData::all());
+replaceAndSaveFA5(view::all());
+replaceAndSaveFA5(plan::all());
+replaceAndSaveFA5(planHeader::all());
 ?>
