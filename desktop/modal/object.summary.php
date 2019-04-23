0561f235a7736e887611ba2e6fafb0aa28317f17
KiboOst <KiboOst@users.noreply.github.com>
2019-04-23 13:37:48 +0200

    Update object.summary.php

--- desktop/modal/object.summary.php
+++ desktop/modal/object.summary.php
@@ -12,7 +12,7 @@ if (!isConnect()) {
 			<th>{{Père}}</th>
 			<th data-sorter="false" data-filter="false">{{Visible}}</th>
 			<th data-sorter="false" data-filter="false">{{Masqué}}</th>
-			<th data-sorter="false" data-filter="false">{{Résumé Défini}} <sup title="Si grisé, alors il n'est pas remonté en résumé global">?</th>
+			<th data-sorter="false" data-filter="false">{{Résumé Défini}} <sup><i class="fas fa-question-circle tooltips" title="Si grisé, alors il n'est pas remonté en résumé global"></i></sup></th>
 			<th data-sorter="false" data-filter="false">{{Résumé Dashboard Masqué}}</th>
 			<th data-sorter="false" data-filter="false">{{Résumé Mobile Masqué}}</th>
 		</tr>
@@ -21,28 +21,28 @@ if (!isConnect()) {
 		<?php
 			$allObject = jeeObject::buildTree(null, false);
 			foreach ($allObject as $object) {
-				echo '<tr class="tr_object" data-object_id="' . $object->getId() . '"><td><span class="label label-info">' . $object->getId() . '</span></td>';
-				echo '<td>';
+				echo '<tr class="tr_object" data-object_id="' . $object->getId() . '"><td style="width:40px;"><span class="label label-info">' . $object->getId() . '</span></td>';
+				echo '<td style="width:50px;">';
 				for ($i = 0; $i < $object->getConfiguration('parentNumber'); $i++) {
 					echo '&nbsp;&nbsp;&nbsp;';
 				}
-				echo '<span style="font-size : 1.3em;">' . $object->getHumanName(true, true) . '</span>';
+				echo '<span>'.$object->getHumanName(true, true).'</span>';
 				echo '</td>';
 				$father = $object->getFather();
 				if ($father) {
-					echo '<td><span>' . $father->getHumanName(true, true) . '</span></td>';
+					echo '<td style="width:50px;"><span>' . $father->getHumanName(true, true) . '</span></td>';
 				} else {
 					echo '<td><span class="label label-info"></span></td>';
 				}
 				if ($object->getIsVisible()) {
-					echo '<td><span class="label label-success" title="{{Oui}}"><i class="fas fa-check"></i></span></td>';
+					echo '<td style="width:65px;"><span class="label label-success" title="{{Oui}}"><i class="fas fa-check"></i></span></td>';
 				} else {
-					echo '<td><span class="label label-danger" title="{{Non}}"><i class="fas fa-times"></i></span></td>';
+					echo '<td style="width:75px;"><span class="label label-danger" title="{{Non}}"><i class="fas fa-times"></i></span></td>';
 				}
 				if ($object->getConfiguration("hideOnDashboard", 0) == 1) {
-					echo '<td><span class="label label-success" title="{{Oui}}"><i class="fas fa-check"></i></span></td>';
+					echo '<td style="width:70px;"><span class="label label-success" title="{{Oui}}"><i class="fas fa-check"></i></span></td>';
 				} else {
-					echo '<td><span class="label label-danger" title="{{Non}}"><i class="fas fa-times"></i></span></td>';
+					echo '<td style="width:70px;"><span class="label label-danger" title="{{Non}}"><i class="fas fa-times"></i></span></td>';
 				}
 				echo '<td>';
 				foreach (config::byKey('object:summary') as $key => $value) {
