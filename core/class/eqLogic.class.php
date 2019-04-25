0281f00dcf7311679942feeeb81d5022946817fd
Loïc <loic@jeedom.com>
2019-04-25 11:10:50 +0200

    auto update battery change time

--- core/class/eqLogic.class.php
+++ core/class/eqLogic.class.php
@@ -1023,6 +1023,10 @@ class eqLogic {
 		if ($_pourcent < 0) {
 			$_pourcent = 0;
 		}
+		if($_pourcent > 90 && $_pourcent > ($this->getStatus('battery')*1.1) && $this->getStatus('battery',-1) >= 0){
+			$this->setConfiguration('batterytime',date('Y-m-d H:i:s'));
+			$this->save();
+		}
 		$warning_threshold = $this->getConfiguration('battery_warning_threshold', config::byKey('battery::warning'));
 		$danger_threshold = $this->getConfiguration('battery_danger_threshold', config::byKey('battery::danger'));
 		if ($_pourcent !== '' && $_pourcent < $danger_threshold) {
