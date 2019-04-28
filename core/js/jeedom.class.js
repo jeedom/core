3c91ded1aa28a5d4c5a0c4b81c32b3bd23448b31
Loïc <loic@jeedom.com>
2019-04-28 19:31:49 +0200

    Update jeedom.class.js

--- core/js/jeedom.class.js
+++ core/js/jeedom.class.js
@@ -681,3 +681,39 @@ jeedom.emptyRemoveHistory = function(_params) {
   };
   $.ajax(paramsAJAX);
 };
+
+jeedom.cleanFileSystemRight = function(_params) {
+  var paramsRequired = [];
+  var paramsSpecifics = {};
+  try {
+    jeedom.private.checkParamsRequired(_params || {}, paramsRequired);
+  } catch (e) {
+    (_params.error || paramsSpecifics.error || jeedom.private.default_params.error)(e);
+    return;
+  }
+  var params = $.extend({}, jeedom.private.default_params, paramsSpecifics, _params || {});
+  var paramsAJAX = jeedom.private.getParamsAJAX(params);
+  paramsAJAX.url = 'core/ajax/jeedom.ajax.php';
+  paramsAJAX.data = {
+    action: 'cleanFileSystemRight'
+  };
+  $.ajax(paramsAJAX);
+};
+
+jeedom.consistency = function(_params) {
+  var paramsRequired = [];
+  var paramsSpecifics = {};
+  try {
+    jeedom.private.checkParamsRequired(_params || {}, paramsRequired);
+  } catch (e) {
+    (_params.error || paramsSpecifics.error || jeedom.private.default_params.error)(e);
+    return;
+  }
+  var params = $.extend({}, jeedom.private.default_params, paramsSpecifics, _params || {});
+  var paramsAJAX = jeedom.private.getParamsAJAX(params);
+  paramsAJAX.url = 'core/ajax/jeedom.ajax.php';
+  paramsAJAX.data = {
+    action: 'consistency'
+  };
+  $.ajax(paramsAJAX);
+};
