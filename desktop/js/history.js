8c1cf79c04bbdd8afab239720819fed8a342ffef
KiboOst <KiboOst@users.noreply.github.com>
2019-04-30 09:04:35 +0200

    fix warning

--- desktop/js/history.js
+++ desktop/js/history.js
@@ -39,6 +39,9 @@ $('#bt_configureCalculHistory').on('click',function(){
 });
 
 $('#bt_clearGraph').on('click',function(){
+  if (jeedom.history.chart['div_graph'] === undefined) {
+    return
+  }
   while(jeedom.history.chart['div_graph'].chart.series.length > 0){
     jeedom.history.chart['div_graph'].chart.series[0].remove(true);
   }
