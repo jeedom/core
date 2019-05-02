43bc6c2418886553f2427ab5c975426a5193551d
Loïc <loic@jeedom.com>
2019-05-02 14:16:23 +0200

    poc gtb/gtc dashboard

---
+++ desktop/js/dashboardit.js
@@ -0,0 +1,62 @@
+/* This file is part of Jeedom.
+*
+* Jeedom is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Jeedom is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+$('#div_treeObject').off('click').on('select_node.jstree', function (node, selected) {
+  if (selected.node.a_attr['data-object_id'] != undefined) {
+    object_id = selected.node.a_attr['data-object_id'];
+    $('.div_displayEquipement').parent().empty().append('<legend>'+selected.node.a_attr['data-name']+'</legend><div class="div_displayEquipement"></div>');
+    getObjectHtml(object_id);
+  }
+});
+
+
+function getObjectHtml(_object_id){
+  jeedom.object.toHtml({
+    id: _object_id,
+    version: 'dashboard',
+    error: function (error) {
+      $('#div_alert').showAlert({message: error.message, level: 'danger'});
+    },
+    success: function (html) {
+      try {
+        $('.div_displayEquipement').html(html);
+      }catch(err) {
+        console.log(err);
+      }
+      setTimeout(function(){
+        positionEqLogic();
+        $('.div_displayEquipement').packery();
+        $('.div_displayEquipement').disableSelection();
+        $("input").click(function() { $(this).focus(); });
+        $("textarea").click(function() { $(this).focus(); });
+        $("select").click(function() { $(this).focus(); });
+      },10);
+    }
+  });
+}
+
+
+$("#div_treeObject").jstree({
+  "search" : {
+    show_only_matches : true,
+    show_only_matches_children : true,
+  },
+  "plugins" : [ "search" ]
+});
+
+$('#in_searchObject').keyup(function () {
+  $('#div_treeObject').jstree(true).search($('#in_searchObject').val());
+});
