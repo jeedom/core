bc84074e71ce05eb186ec4499ccc192f691512d9
Loïc <loic@jeedom.com>
2019-05-09 10:47:36 +0200

    improve modal management

--- desktop/js/utils.js
+++ desktop/js/utils.js
@@ -56,7 +56,7 @@ function loadPage(_url,_noPushHistory){
   try{
     $(".ui-dialog-content").dialog("close");
   }catch(e){}
-
+  
   if(_url.endsWith('#')){
     _url = _url.slice(0, -1);
   }
@@ -68,7 +68,7 @@ function loadPage(_url,_noPushHistory){
         window.history.pushState('','', _url)
       }
     } catch(e) {
-
+      
     }
   }
   if(isset(bootbox)){
@@ -117,7 +117,7 @@ $(function () {
     initRowOverflow();
   }
   $('body').attr('data-page',getUrlVars('p'));
-
+  
   $('body').off('jeedom_page_load').on('jeedom_page_load',function(){
     if (getUrlVars('saveSuccessFull') == 1) {
       $('#div_alert').showAlert({message: '{{Sauvegarde effectuée avec succès}}', level: 'success'});
@@ -128,7 +128,7 @@ $(function () {
       window.history.replaceState({}, document.title, window.location.href.split('&removeSuccessFull')[0]);
     }
   });
-
+  
   window.addEventListener('popstate', function (event){
     if(event.state === null){
       return;
@@ -136,7 +136,7 @@ $(function () {
     var url = window.location.href.split("index.php?");
     loadPage('index.php?'+url[1],true)
   });
-
+  
   $('body').on('click','a',function(e){
     if ($(window).width() < 768) {
       $('ul.dropdown-menu').css('display', '')
@@ -165,7 +165,7 @@ $(function () {
     e.preventDefault();
     e.stopPropagation();
   });
-
+  
   toastr.options = {
     "closeButton": true,
     "debug": false,
@@ -180,7 +180,7 @@ $(function () {
     "showMethod": "fadeIn",
     "hideMethod": "fadeOut"
   }
-
+  
   $('ul.dropdown-menu [data-toggle=dropdown]').on('click', function (event) {
     if ($(this).parent().hasClass('dropdown-submenu')) return
     event.preventDefault();
@@ -189,23 +189,23 @@ $(function () {
     $(this).parent().toggleClass('open');
     $('.dropdown-menu').dropdown('toggle');
   });
-
+  
   if (!navigator.userAgent.match(/Android/i)
-    && !navigator.userAgent.match(/webOS/i)
-    && !navigator.userAgent.match(/iPhone/i)
-    && !navigator.userAgent.match(/iPad/i)
-    && !navigator.userAgent.match(/iPod/i)
-    && !navigator.userAgent.match(/BlackBerry/i)
-    & !navigator.userAgent.match(/Windows Phone/i)
-  ) {
-    $('ul.dropdown-menu [data-toggle=dropdown]').on('mouseenter', function (event) {
-      if ($(window).width() < 768) return
-      event.preventDefault();
-      event.stopPropagation();
-      $(this).parent().siblings().removeClass('open');
-      $(this).parent().toggleClass('open');
-    });
-  }
+  && !navigator.userAgent.match(/webOS/i)
+  && !navigator.userAgent.match(/iPhone/i)
+  && !navigator.userAgent.match(/iPad/i)
+  && !navigator.userAgent.match(/iPod/i)
+  && !navigator.userAgent.match(/BlackBerry/i)
+  & !navigator.userAgent.match(/Windows Phone/i)
+) {
+  $('ul.dropdown-menu [data-toggle=dropdown]').on('mouseenter', function (event) {
+    if ($(window).width() < 768) return
+    event.preventDefault();
+    event.stopPropagation();
+    $(this).parent().siblings().removeClass('open');
+    $(this).parent().toggleClass('open');
+  });
+}
 
 $('li.dropdown-submenu a').on('click',function(event) {
   event.stopPropagation()
@@ -278,6 +278,10 @@ $("#md_reportBug").dialog({
   open: function () {
     $("body").css({overflow: 'hidden'})
     $(this).closest( ".ui-dialog" ).find(":button").blur();
+    $(this).dialog({
+      height: ((jQuery(window).height() - 100) < 700) ? jQuery(window).height() - 100 : 700,
+      width: ((jQuery(window).width() - 100) < 900) ? (jQuery(window).width() - 100) : 900,
+    });
   },
   beforeClose: function (event, ui) {
     $("body").css({overflow: 'inherit'})
@@ -296,6 +300,10 @@ $("#md_pageHelp").dialog({
   open: function () {
     $("body").css({overflow: 'hidden'});
     $(this).closest( ".ui-dialog" ).find(":button").blur();
+    $(this).dialog({
+      height: (jQuery(window).height() - 100),
+      width: ((jQuery(window).width() - 100) < 1500) ? (jQuery(window).width() - 50) : 1500,
+    });
   },
   beforeClose: function (event, ui) {
     $("body").css({overflow: 'inherit'});
@@ -311,8 +319,12 @@ $("#md_modal").dialog({
   width: ((jQuery(window).width() - 50) < 1500) ? (jQuery(window).width() - 50) : 1500,
   position: {my: 'center bottom-50', at: 'center bottom', of: window},
   open: function () {
-    $("body").css({overflow: 'hidden'});
-    $(this).closest( ".ui-dialog" ).find(":button").blur();
+    $('body').css({overflow: 'hidden'});
+    $(this).closest('.ui-dialog').find(':button').blur();
+    $(this).dialog({
+      height: (jQuery(window).height() - 125),
+      width: ((jQuery(window).width() - 50) < 1500) ? (jQuery(window).width() - 50) : 1500,
+    });
   },
   beforeClose: function (event, ui) {
     $("body").css({overflow: 'inherit'});
@@ -330,6 +342,10 @@ $("#md_modal2").dialog({
   open: function () {
     $("body").css({overflow: 'hidden'});
     $(this).closest( ".ui-dialog" ).find(":button").blur();
+    $(this).dialog({
+      height: (jQuery(window).height() - 125),
+      width: ((jQuery(window).width() - 150) < 1200) ? (jQuery(window).width() - 50) : 1200,
+    });
   },
   beforeClose: function (event, ui) {
     $("body").css({overflow: 'inherit'});
@@ -347,6 +363,10 @@ $("#md_modal3").dialog({
   open: function () {
     $("body").css({overflow: 'hidden'});
     $(this).closest( ".ui-dialog" ).find(":button").blur();
+    $(this).dialog({
+      height : (jQuery(window).height() - 125),
+      width : ((jQuery(window).width() - 250) < 1000) ? (jQuery(window).width() - 50) : 1000
+    });
   },
   beforeClose: function (event, ui) {
     $("body").css({overflow: 'inherit'});
@@ -566,7 +586,7 @@ function initTextArea(){
 }
 
 function initCheckBox(){
-
+  
 }
 
 function initPage(){
@@ -605,13 +625,13 @@ function dropDownsKeys() {
     prevDropDownValue = $(this).html()
     prevDropDownDatavalue = $(this).attr('value')
   })
-
+  
   $('.dropdown-toggle').keydown(function(event) {
     if (event.which == 13) {
       $('body').trigger('click')
       return false
     }
-
+    
     key = event.key
     if(key == 'Escape') {
       $(this).html(prevDropDownValue)
@@ -620,13 +640,13 @@ function dropDownsKeys() {
       $('body').trigger('click')
       return false
     }
-
+    
     selected = -1
     $(this).closest('.dropdown.open').find('ul li').each(function(index, li) {
       if ($(li).find('a').style('background-color') == 'var(--placeholder-color)' && selected < index) selected = index
       $(li).find('a').style('background-color', '')
     })
-
+    
     $(this).closest('.dropdown.open').find('ul li').each(function(index, li) {
       value = $(li).find('a').text().toLowerCase()
       //handle '(kiki) value':
@@ -640,13 +660,13 @@ function dropDownsKeys() {
         }
       }
       value = value.trim()
-
+      
       //handle 'value1, value2':
       values = null
       if (value.indexOf(',') > -1) {
         values = value.split(',')
       }
-
+      
       if (values) {
         for (i = 0; i < values.length; i++) {
           value = values[i].trim()
@@ -656,18 +676,18 @@ function dropDownsKeys() {
       } else {
         match = value.startsWith(key) && $(li).find('a').style('background-color') != 'var(--placeholder-color)'
       }
-
+      
       if (match && selected < index) {
         ul = $(this).closest('.dropdown').find('ul')
         $(li).find('a').style('background-color', 'var(--placeholder-color)', 'important')
         $(ul).scrollTop($(li).position().top - $(ul).find('li').first().position().top)
-
+        
         $(this).closest('.dropdown').find('button').html($(li).find('a').text() + '<span class="caret"></span>')
         $(this).closest('.dropdown').find('button').attr('value', $(li).find('a').attr('data-value'))
         $(this).closest('.dropdown').find('button').trigger('change')
         return false
       }
-
+      
     })
   })
 }
@@ -818,12 +838,12 @@ function notify(_title, _text, _class_name) {
 
 jQuery.fn.findAtDepth = function (selector, maxDepth) {
   var depths = [], i;
-
+  
   if (maxDepth > 0) {
     for (i = 1; i <= maxDepth; i++) {
       depths.push('> ' + new Array(i).join('* > ') + selector);
     }
-
+    
     selector = depths.join(', ');
   }
   return this.find(selector);
@@ -1069,14 +1089,14 @@ function editWidgetCmdMode(_mode){
       try{
         $('.eqLogic-widget.allowReorderCmd.eqLogic_layout_table table.tableCmd').sortable('destroy');
       }catch(e){
-
+        
       }
     }
     if( $('.eqLogic-widget.allowReorderCmd.eqLogic_layout_default.ui-sortable').length > 0){
       try{
         $('.eqLogic-widget.allowReorderCmd.eqLogic_layout_default').sortable('destroy');
       }catch(e){
-
+        
       }
     }
     if( $('.eqLogic-widget.ui-draggable').length > 0){
@@ -1235,7 +1255,7 @@ function editWidgetCmdMode(_mode){
       });
     }
   }
-
+  
   function hexToRgb(hex) {
     var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
     return result ? {
@@ -1244,12 +1264,12 @@ function editWidgetCmdMode(_mode){
       b: parseInt(result[3], 16)
     } : null;
   }
-
+  
   function componentToHex(c) {
     var hex = c.toString(16);
     return hex.length == 1 ? "0" + hex : hex;
   }
-
+  
   function rgbToHex(r, g, b) {
     if ($.type(r) === 'string' && !g) {
       r = r.trim()
@@ -1269,7 +1289,7 @@ function editWidgetCmdMode(_mode){
     }
     return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b)
   }
-
+  
   function addOrUpdateUrl(_param,_value){
     var url = new URL(window.location.href );
     var query_string = url.search;
@@ -1278,3 +1298,4 @@ function editWidgetCmdMode(_mode){
     url.search = search_params.toString();
     window.history.pushState('','', url.toString());
   }
+  
