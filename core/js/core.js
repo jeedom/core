cf4a09e51e3581c0926ba85c9195376dfd34088d
Loïc <loic@jeedom.com>
2019-05-10 11:26:02 +0200

    Update core.js

--- core/js/core.js
+++ core/js/core.js
@@ -123,9 +123,9 @@ function getDeviceType() {
   if (result.type == 'phone') {
     var ori = window.orientation;
     if (ori == 90 || ori == -90) {
-      result.bSize = (result.width / 3) - 21;
+      result.bSize = (result.width / 3) - 6;
     } else {
-      result.bSize = (result.width / 2) - 5.5;
+      result.bSize = (result.width / 2) - 6;
     }
   }
   return result;
