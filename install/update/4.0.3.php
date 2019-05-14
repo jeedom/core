cc375853712e20725f53c2726c08a5d4fbe68ff2
Loïc <loic@jeedom.com>
2019-05-14 19:13:55 +0200

    bugfix

--- install/update/4.0.3.php
+++ install/update/4.0.3.php
@@ -1,10 +1,10 @@
 <?php
 require_once __DIR__ . '/../../core/php/core.inc.php';
 $widget_dir = __DIR__.'/../../plugins/widget/core/template';
-if(file_exist($widget_dir)){
+if(file_exists($widget_dir)){
   echo "Copy widget of plugin widget to jeedom custom widget dir\n";
   $cib_dir = _DIR__.'/../../data/customTemplates/';
-  if(!file_exist($cib_dir)){
+  if(!file_exists($cib_dir)){
     mkdir($cib_dir);
   }
   exec('cp -R '.$widget_dir.'/* '.$cib_dir);
