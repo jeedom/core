85fdf174751ebf0a4e66582343fd8cadf9a555dc
Loïc <loic@jeedom.com>
2019-05-14 19:04:43 +0200

    typo

--- install/update/4.0.3.php
+++ install/update/4.0.3.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/../../core/php/core.inc.php';
 $widget_dir = __DIR__.'/../../plugins/widget/core/template';
 if(file_exist($widget_dir)){
-  echo "Copy widget of plugin widget to jeedom custom widget dir";
+  echo "Copy widget of plugin widget to jeedom custom widget dir\n";
   $cib_dir = _DIR__.'/../../data/customTemplates/';
   if(!file_exist($cib_dir)){
     mkdir($cib_dir);
