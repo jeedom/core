e3a1d46ff535fa06f78ac5990230238c7f6b1ecf
KiboOst <KiboOst@users.noreply.github.com>
2019-05-22 19:20:47 +0200

    api inputs

--- desktop/php/administration.php
+++ desktop/php/administration.php
@@ -322,7 +322,7 @@ user::isBan();
 						</label>
 						<div class="col-lg-5 col-md-5 col-sm-7 col-xs-12">
 							<div class="input-group">
-								<input class="span_apikey roundedLeft form-control" disabled value="<?php echo $configs['api']; ?>" />
+								<input class="span_apikey roundedLeft form-control" readonly value="<?php echo $configs['api']; ?>" />
 								<span class="input-group-btn">
 									<a class="btn btn-default form-control bt_regenerate_api roundedRight" data-plugin="core"><i class="fas fa-sync"></i></a>
 								</span>
@@ -336,7 +336,7 @@ user::isBan();
 						</label>
 						<div class="col-lg-5 col-md-5 col-sm-7 col-xs-12">
 							<div class="input-group">
-								<input class="span_apikey roundedLeft form-control" disabled value="<?php echo $configs['apipro']; ?>" />
+								<input class="span_apikey roundedLeft form-control" readonly value="<?php echo $configs['apipro']; ?>" />
 								<span class="input-group-btn">
 									<a class="btn btn-default form-control bt_regenerate_api roundedRight" data-plugin="pro"><i class="fas fa-sync"></i></a>
 								</span>
@@ -361,7 +361,7 @@ user::isBan();
 						</label>
 						<div class="col-lg-5 col-md-5 col-sm-7 col-xs-12">
 							<div class="input-group">
-								<input class="span_apikey roundedLeft form-control" disabled value="<?php echo $configs['apimarket']; ?>" />
+								<input class="span_apikey roundedLeft form-control" readonly value="<?php echo $configs['apimarket']; ?>" />
 								<span class="input-group-btn">
 									<a class="btn btn-default form-control bt_regenerate_api roundedRight" data-plugin="apimarket"><i class="fas fa-sync"></i></a>
 								</span>
@@ -393,7 +393,7 @@ user::isBan();
 							echo '</label>';
 							echo '<div class="col-lg-5 col-md-5 col-sm-7 col-xs-12">';
 							echo '<div class="input-group">';
-							echo '<input class="span_apikey roundedLeft form-control" disabled value="' . config::byKey('api', $plugin->getId()) . '" />';
+							echo '<input class="span_apikey roundedLeft form-control" readonly value="' . config::byKey('api', $plugin->getId()) . '" />';
 							echo '<span class="input-group-btn">';
 							echo '<a class="btn btn-default form-control bt_regenerate_api roundedRight" data-plugin="' . $plugin->getId() . '"><i class="fas fa-sync"></i></a>';
 							echo '</span>';
