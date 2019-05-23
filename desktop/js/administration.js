e76a636d854d0eededf1e7e96306da10e9636497
KiboOst <KiboOst@users.noreply.github.com>
2019-05-23 14:15:59 +0200

    Update administration.js

--- desktop/js/administration.js
+++ desktop/js/administration.js
@@ -24,14 +24,14 @@ if (_url.match('#') && _url.split('#')[1] != '' && $('.nav-tabs a[href="#' + _ur
 //searching
 $('#in_searchConfig').keyup(function () {
   var search = $(this).value()
-  
+
   //replace found els with random numbered span to place them back to right place. Avoid cloning els for better saving.
   $('span[searchId]').each(function() {
     el = $('#searchResult [searchId="' + $(this).attr('searchId') + '"]')
     el.removeAttr('searchId')
     $(this).replaceWith(el)
   })
-  
+
   $('#searchResult').empty()
   if(search == '') {
     $('.nav-tabs.nav-primary').show()
@@ -41,16 +41,16 @@ $('#in_searchConfig').keyup(function () {
   }
   if (search.length < 3) return
   search = search.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
-  
+
   $('.nav-tabs.nav-primary').hide()
   $('.tab-content').hide()
-  
+
   var prevTab = ''
   $('.form-group > .control-label').each(function() {
     var text = $(this).html().toLowerCase()
     text = text.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
     if (text.indexOf(search.toLowerCase()) >= 0){
-      
+
       //get element tab to create link to:
       var tabId = $(this).closest('div[role="tabpanel"]').attr('id')
       tabName = $('ul.nav-primary a[href="#' + tabId + '"]').html()
@@ -58,7 +58,7 @@ $('#in_searchConfig').keyup(function () {
         $('#searchResult').append('<a role="searchTabLink" href="#'+tabId+'">'+tabName+'</a>')
       }
       prevTab = tabId
-      
+
       el = $(this).closest('.form-group')
       searchId = Math.random()
       el.attr('searchId', searchId)
@@ -84,6 +84,10 @@ $('#bt_resetConfigSearch').on('click', function () {
 })
 
 $(function () {
+  if ($('input[data-l1key="theme_changeAccordingTime"]').prop('checked')) {
+    $('button[data-l1key="default_bootstrap_theme_night"]').prop('disabled', false)
+    $('button[data-l1key="mobile_theme_color_night"]').prop('disabled', false)
+  }
   setTimeout(function(){
     modifyWithoutSave = false;
   }, 1000);
@@ -93,13 +97,23 @@ $(function () {
 function initPickers() {
   $('input[data-l1key="theme_start_day_hour"]').datetimepicker({datepicker:false, format:'H:i', step:10})
   $('input[data-l1key="theme_end_day_hour"]').datetimepicker({datepicker:false, format:'H:i', step:10})
-  
+
   $('input[type="number"]').spinner({
     icons: { down: "ui-icon-triangle-1-s", up: "ui-icon-triangle-1-n" }
   });
 }
 $(function(){
   initPickers()
+})
+
+$('input[data-l1key="theme_changeAccordingTime"]').on('click', function (event) {
+  if ($(this).prop('checked')) {
+    $('button[data-l1key="default_bootstrap_theme_night"]').prop('disabled', false)
+    $('button[data-l1key="mobile_theme_color_night"]').prop('disabled', false)
+  } else {
+    $('button[data-l1key="default_bootstrap_theme_night"]').prop('disabled', true)
+    $('button[data-l1key="mobile_theme_color_night"]').prop('disabled', true)
+  }
 })
 
 $('#div_pageContainer').delegate('.configKey[data-l1key="market::allowDNS"],.configKey[data-l1key="network::disableMangement"]', 'change', function () {
@@ -328,7 +342,7 @@ $("#bt_testLdapConnection").on('click', function (event) {
       });
     }
   });
-  
+
   return false;
 });
 
@@ -652,7 +666,7 @@ function printConvertColor() {
         $('#div_alert').showAlert({message: data.result, level: 'danger'});
         return;
       }
-      
+
       $('#table_convertColor tbody').empty();
       for (var color in data.result) {
         addConvertColor(color, data.result[color]);
