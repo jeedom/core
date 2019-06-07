f98d747f830d8c0bfd124bc960db463c14f16e4a
KiboOst <KiboOst@users.noreply.github.com>
2019-06-07 19:50:17 +0200

    Update dashboard.js

--- desktop/js/dashboard.js
+++ desktop/js/dashboard.js
@@ -37,13 +37,13 @@ $('#in_searchWidget').off('keyup').on('keyup',function(){
     $('.div_displayEquipement').packery()
     return
   }
-  
+
   search = normTextLower(search)
   $('.eqLogic-widget').each(function() {
     var match = false
     text = normTextLower($(this).find('.widget-name').text())
     if (text.indexOf(search) >= 0) match = true
-    
+
     if ($(this).attr('data-tags') != undefined) {
       text = normTextLower($(this).attr('data-tags'))
       if (text.indexOf(search) >= 0) match = true
@@ -60,7 +60,7 @@ $('#in_searchWidget').off('keyup').on('keyup',function(){
       text = normTextLower($(this).attr('data-translate-category'))
       if (text.indexOf(search) >= 0) match = true
     }
-    
+
     if(match) {
       $(this).show()
     } else {
@@ -91,8 +91,7 @@ $('#bt_resetDashboardSearch').on('click', function () {
 })
 
 $('#div_pageContainer').on( 'click','.eqLogic-widget .history', function () {
-  $('#md_modal2').dialog({title: "Historique"})
-  .load('index.php?v=d&modal=cmd.history&id=' + $(this).data('cmd_id')).dialog('open');
+  $('#md_modal2').dialog({title: "Historique"}).load('index.php?v=d&modal=cmd.history&id=' + $(this).data('cmd_id')).dialog('open')
 });
 
 $('#bt_displayObject').on('click', function () {
@@ -130,7 +129,7 @@ function editWidgetMode(_mode,_save){
       $('.div_displayEquipement .eqLogic-widget').draggable('disable');
     }
     $('.div_displayEquipement .eqLogic-widget').removeClass('editingMode','');
-    
+
     if( $('.div_displayEquipement .scenario-widget.ui-resizable').length > 0){
       $('.div_displayEquipement .scenario-widget.allowResize').resizable('destroy');
     }
@@ -139,8 +138,7 @@ function editWidgetMode(_mode,_save){
     }
     $('.div_displayEquipement .scenario-widget').removeClass('editingMode','');
   }else{
-    $('.div_displayEquipement .eqLogic-widget').addClass('editingMode')
-    .draggable('enable');
+    $('.div_displayEquipement .eqLogic-widget').addClass('editingMode').draggable('enable');
     $('.div_displayEquipement .eqLogic-widget.allowResize').resizable({
       resize: function( event, ui ) {
         positionEqLogic(ui.element.attr('data-eqlogic_id'),false);
@@ -151,8 +149,7 @@ function editWidgetMode(_mode,_save){
         ui.element.closest('.div_displayEquipement').packery();
       }
     });
-    $('.div_displayEquipement .scenario-widget').addClass('editingMode')
-    .draggable('enable');
+    $('.div_displayEquipement .scenario-widget').addClass('editingMode').draggable('enable');
     $('.div_displayEquipement .scenario-widget.allowResize').resizable({
       resize: function( event, ui ) {
         positionEqLogic(ui.element.attr('data-scenario_id'),false,true);
@@ -183,16 +180,18 @@ function getObjectHtml(_object_id) {
       } catch(err) {
         console.log(err);
       }
+
       positionEqLogic();
-      $('#div_ob'+_object_id+'.div_displayEquipement').disableSelection();
+      var $divDisplayEq = $('#div_ob'+_object_id+'.div_displayEquipement')
+      $divDisplayEq.disableSelection();
       $("input").click(function() { $(this).focus(); });
       $("textarea").click(function() { $(this).focus(); });
       $("select").click(function() { $(this).focus(); });
-      
-      var container = $('#div_ob'+_object_id+'.div_displayEquipement').packery();
-      var packData = $('#div_ob'+_object_id+'.div_displayEquipement').data('packery');
+
+      var container = $divDisplayEq.packery();
+      var packData = $divDisplayEq.data('packery');
       if (isset(packData) && packData.items.length == 1) {
-        $('#div_ob'+_object_id+'.div_displayEquipement').packery('destroy').packery()
+        $divDisplayEq.packery('destroy').packery()
       }
       var itemElems =  container.find('.eqLogic-widget').draggable();
       container.packery('bindUIDraggableEvents',itemElems);
@@ -203,10 +202,12 @@ function getObjectHtml(_object_id) {
           $('.div_displayEquipement').packery();
         },1);
         var itemElems = container.packery('getItemElements');
+        var isEditing = false;
+        if($('#bt_editDashboardWidgetOrder').attr('data-mode') == 1) isEditing = true;
         $(itemElems).each( function( i, itemElem ) {
           $(itemElem).attr('data-order', i + 1 );
           value = i + 1;
-          if($('#bt_editDashboardWidgetOrder').attr('data-mode') == 1){
+          if (isEditing) {
             if ($(itemElem).find(".counterReorderJeedom").length) {
               $(itemElem).find(".counterReorderJeedom").text(value);
             } else {
@@ -224,7 +225,7 @@ function getObjectHtml(_object_id) {
 
 $('#bt_editDashboardWidgetOrder').on('click',function(){
   if($(this).attr('data-mode') == 1){
-    $('.tooltipstered').tooltipster('enable')
+  	$('.tooltipstered').tooltipster('enable')
     $.hideAlert();
     $(this).attr('data-mode',0);
     editWidgetMode(0);
@@ -233,7 +234,7 @@ $('#bt_editDashboardWidgetOrder').on('click',function(){
     $('.counterReorderJeedom').remove();
     $('.div_displayEquipement').packery();
   }else{
-    $('.tooltipstered').tooltipster('disable')
+  	$('.tooltipstered').tooltipster('disable')
     $('#div_alert').showAlert({message: "{{Vous êtes en mode édition vous pouvez déplacer les widgets, les redimensionner et changer l'ordre des commandes dans les widgets. N'oubliez pas de quitter le mode édition pour sauvegarder}}", level: 'info'});
     $(this).attr('data-mode',1);
     $('.bt_editDashboardWidgetAutoResize').show();
@@ -283,13 +284,11 @@ $('.li_object').on('click',function(){
 
 function displayChildObject(_object_id,_recursion){
   if(_recursion === false){
-    $('.div_object').addClass('hideByObjectSel')
-    .hide();
+    $('.div_object').addClass('hideByObjectSel').hide()
   }
   $('.div_object[data-object_id='+_object_id+']').show({effect : 'drop',queue : false})
-  .each(function(){
-    $(this).show({effect : 'drop',queue : false})
-    .find('.div_displayEquipement').packery();
-    displayChildObject($(this).attr('data-object_id'),true);
+  $('.div_object[data-father_id='+_object_id+']').each(function() {
+    $(this).show({effect : 'drop',queue : false}).find('.div_displayEquipement').packery()
+    displayChildObject($(this).attr('data-object_id'),true)
   });
 }
