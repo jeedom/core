4dcbbb4fdd7223210a401710d0cc87283444a21c
KiboOst <KiboOst@users.noreply.github.com>
2019-06-08 20:27:19 +0200

    Update scenario.js

--- mobile/js/scenario.js
+++ mobile/js/scenario.js
@@ -1,46 +1,47 @@
+$('body').attr('data-page', 'scenario')
+
 function initScenario() {
   jeedom.scenario.toHtml({
     id: 'all',
     version: 'mobile',
     error: function (error) {
-      $('#div_alert').showAlert({message: error.message, level: 'danger'});
+      $('#div_alert').showAlert({message: error.message, level: 'danger'})
     },
     success: function (htmls) {
-      $('#div_displayScenario').empty().html(htmls).trigger('create');
-      setTileSize('.scenario');
-      $('#div_displayScenario').packery({gutter : 0});
+      $('#div_displayScenario').empty().html(htmls).trigger('create')
+      setTileSize('.scenario')
+      $('#div_displayScenario').packery({gutter : 0})
     }
-  });
-  
-  $(window).on("resize", function (event) {
-    setTileSize('.scenario');
-    $('#div_displayScenario').packery({gutter : 0});
-  });
-  
-  $('#in_searchWidget').off('keyup').on('keyup',function(){
-    var search = $(this).value();
-    if(search == ''){
-      $('.scenario-widget').show();
-      $('#div_displayScenario').packery();
-      return;
-    }
-    $('.scenario-widget').each(function(){
-      var match = false;
-      if(match || $(this).find('.widget-name').text().toLowerCase().indexOf(search.toLowerCase()) >= 0){
-        match = true;
-      }
-      if(match){
-        $(this).show();
-      }else{
-        $(this).hide();
-      }
-    });
-    $('#div_displayScenario').packery();
-  });
-  
-  
-  $('#bt_eraseSearchInput').off('click').on('click',function(){
-    $('#in_searchWidget').val('');
-    $('#in_searchWidget').keyup();
   })
 }
+
+$(window).on("resize", function (event) {
+  setTileSize('.scenario')
+  $('#div_displayScenario').packery({gutter : 0})
+})
+
+$('#in_searchWidget').off('keyup').on('keyup',function(){
+  var search = $(this).value()
+  if(search == ''){
+    $('.scenario-widget').show()
+    $('#div_displayScenario').packery()
+    return
+  }
+  $('.scenario-widget').each(function(){
+    var match = false
+    if(match || $(this).find('.widget-name').text().toLowerCase().indexOf(search.toLowerCase()) >= 0){
+      match = true
+    }
+    if(match){
+      $(this).show()
+    }else{
+      $(this).hide()
+    }
+  })
+  $('#div_displayScenario').packery()
+})
+
+$('#bt_eraseSearchInput').off('click').on('click',function(){
+  $('#in_searchWidget').val('').keyup()
+})
+
