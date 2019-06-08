7f01f390ce886435335cd83fab99adc7d1a544c6
KiboOst <KiboOst@users.noreply.github.com>
2019-06-08 20:25:16 +0200

    Update eqAnalyse.js

--- mobile/js/eqAnalyse.js
+++ mobile/js/eqAnalyse.js
@@ -1,41 +1,41 @@
+$('body').attr('data-page', 'eqAnalyse')
+
 function initEqanalyse() {
 	jeedom.eqLogic.htmlAlert({
 		version : 'mobile',
 		error: function (error) {
-			$('#div_alert').showAlert({message: error.message, level: 'danger'});
+			$('#div_alert').showAlert({message: error.message, level: 'danger'})
 		},
 		success: function (eqLogics) {
-			div = '';
+			div = ''
 			for (var i in eqLogics) {
-				div += eqLogics[i].html;
+				div += eqLogics[i].html
 			}
-			$('#div_displayAlert').empty().html(div).trigger('create');
-			setTileSize('.eqLogic');
-			$('#div_displayAlert').packery({gutter : 0});
+			$('#div_displayAlert').empty().html(div).trigger('create')
+			setTileSize('.eqLogic')
+			$('#div_displayAlert').packery({gutter : 0})
 			setTimeout(function () {
-				$('#div_displayAlert').packery({gutter : 0});
-			}, 10);
+				$('#div_displayAlert').packery({gutter : 0})
+			}, 10)
 			jeedom.eqLogic.htmlBattery({
 				version : 'mobile',
 				error: function (error) {
-					$('#div_alert').showAlert({message: error.message, level: 'danger'});
+					$('#div_alert').showAlert({message: error.message, level: 'danger'})
 				},
 				success: function (eqLogics) {
-					div = '';
+					div = ''
 					for (var i in eqLogics) {
-						div += eqLogics[i].html;
+						div += eqLogics[i].html
 					}
-					$('#div_displayBattery').empty().html(div).trigger('create');
-					$('ul[data-role=nd2tabs]').tabs();
-					setTileSize('.eqLogic');
-					$('#div_displayBattery').packery({gutter : 0});
+					$('#div_displayBattery').empty().html(div).trigger('create')
+					$('ul[data-role=nd2tabs]').tabs()
+					setTileSize('.eqLogic')
+					$('#div_displayBattery').packery({gutter : 0})
 					setTimeout(function () {
-						$('#div_displayBattery').packery({gutter : 0});
-					}, 10);
+						$('#div_displayBattery').packery({gutter : 0})
+					}, 10)
 				}
-			});
+			})
 		}
-	});
-	
-	
+	})
 }
