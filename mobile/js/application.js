7a3fb29a59cec38aeeb1b2dbbd50b28f9e2ab169
Loïc <loic@jeedom.com>
2019-06-24 17:50:56 +0200

    add data-theme on body

--- mobile/js/application.js
+++ mobile/js/application.js
@@ -26,45 +26,47 @@ $(function () {
   nbActiveAjaxRequest = 0;
   utid = Date.now();
   $.mobile.orientationChangeEnabled = false;
-
+  
   $(window).on("resize", function (event) {
     deviceInfo = getDeviceType();
   });
-
+  
   initApplication();
-
+  
   $('body').delegate('.link', 'click', function () {
     modal(false);
     panel(false);
     page($(this).attr('data-page'), $(this).attr('data-title'), $(this).attr('data-option'), $(this).attr('data-plugin'));
   });
-
+  
   $('body').on('click','.objectSummaryParent',function(){
     modal(false);
     panel(false);
     page('equipment', '{{Résumé}}', $(this).data('object_id')+':'+$(this).data('summary'));
   });
-
+  
   $('body').on('taphold','.cmd[data-type=info]',function(){
     $('#bottompanel_mainoption').empty();
     $('#bottompanel_mainoption').append('<a class="link ui-bottom-sheet-link ui-btn ui-btn-inline waves-effect waves-button" data-page="history" data-title="{{Historique}}" data-option="'+$(this).data('cmd_id')+'"><i class="fas fa-chart-bar"></i> {{Historique}}</a>');
     $('#bottompanel_mainoption').append('<a class="ui-bottom-sheet-link ui-btn ui-btn-inline waves-effect waves-button" id="bt_warnmeCmd" data-cmd_id="'+$(this).data('cmd_id')+'"><i class="fas fa-bell"></i> {{Préviens moi}}</a>');
     $('#bottompanel_mainoption').panel('open');
   });
-
+  
   $('body').on('click','#bt_warnmeCmd',function(){
     page('warnme','{{Me prévenir si}}',{cmd_id : $(this).data('cmd_id')},null,true);
   });
-
+  
   $('body').on('click','#bt_switchTheme',function(){
+    $('body').attr('data-theme',userProfils.mobile_theme_color_night);
     var theme = 'core/themes/'+userProfils.mobile_theme_color_night+'/mobile/' + userProfils.mobile_theme_color_night + '.css';
     if($('#jQMnDColor').attr('href') == theme){
+      $('body').attr('data-theme',userProfils.mobile_theme_color);
       theme = 'core/themes/'+userProfils.mobile_theme_color+'/mobile/' + userProfils.mobile_theme_color + '.css';
     }
     $('#jQMnDColor').attr('href', theme);
     $('#jQMnDColor').attr('data-nochange',1);
   });
-
+  
   var webappCache = window.applicationCache;
   function updateCacheEvent(e) {
     if (webappCache.status == 3) {
@@ -94,7 +96,7 @@ $(function () {
     try{
       webappCache.update();
     } catch(e) {
-
+      
     }
   }
 });
@@ -164,21 +166,26 @@ function changeThemeAuto(_ambiantLight){
       if(sensor.illuminance < 200 && sensor.illuminance > 50){
         return;
       }
-      var theme = 'core/themes/'+jeedom.theme.mobile_theme_color+'/mobile/' + jeedom.theme.mobile_theme_color + '.css';
+      var theme = jeedom.theme.mobile_theme_color;
+      var themeCss = 'core/themes/'+jeedom.theme.mobile_theme_color+'/mobile/' + jeedom.theme.mobile_theme_color + '.css';
       if(sensor.illuminance < 50){
-        var theme = 'core/themes/'+jeedom.theme.mobile_theme_color_night+'/mobile/' + jeedom.theme.mobile_theme_color_night + '.css';
+        theme = jeedom.theme.mobile_theme_color_night;
+        themeCss = 'core/themes/'+jeedom.theme.mobile_theme_color_night+'/mobile/' + jeedom.theme.mobile_theme_color_night + '.css';
       }
-      if($('#jQMnDColor').attr('href') != theme){
+      if($('#jQMnDColor').attr('href') != themeCss){
         setTimeout(function(){
           if(sensor.illuminance < 100 && sensor.illuminance > 50){
             return;
           }
-          var theme = 'core/themes/'+jeedom.theme.mobile_theme_color+'/mobile/' + jeedom.theme.mobile_theme_color + '.css';
+          var theme = jeedom.theme.mobile_theme_color;
+          var themeCss = 'core/themes/'+jeedom.theme.mobile_theme_color+'/mobile/' + jeedom.theme.mobile_theme_color + '.css';
           if(sensor.illuminance < 50){
-            var theme = 'core/themes/'+jeedom.theme.mobile_theme_color_night+'/mobile/' + jeedom.theme.mobile_theme_color_night + '.css';
+            theme = jeedom.theme.mobile_theme_color_night;
+            themeCss = 'core/themes/'+jeedom.theme.mobile_theme_color_night+'/mobile/' + jeedom.theme.mobile_theme_color_night + '.css';
           }
-          if($('#jQMnDColor').attr('href') != theme){
-            $('#jQMnDColor').attr('href', theme);
+          if($('#jQMnDColor').attr('href') != themeCss){
+            $('body').attr('data-theme',theme);
+            $('#jQMnDColor').attr('href', themeCss);
           }
         }, 500);
       }
@@ -189,13 +196,16 @@ function changeThemeAuto(_ambiantLight){
       if($('#jQMnDColor').attr('data-nochange') == 1){
         return;
       }
-      var theme = 'core/themes/'+jeedom.theme.mobile_theme_color_night+'/mobile/' + jeedom.theme.mobile_theme_color_night + '.css';
+      var theme = jeedom.theme.mobile_theme_color_night;
+      var themeCss = 'core/themes/'+jeedom.theme.mobile_theme_color_night+'/mobile/' + jeedom.theme.mobile_theme_color_night + '.css';
       var currentTime = parseInt((new Date()).getHours()*100+ (new Date()).getMinutes());
       if(parseInt(jeedom.theme.theme_start_day_hour.replace(':','')) <  currentTime && parseInt(jeedom.theme.theme_end_day_hour.replace(':','')) >  currentTime){
-        var theme = 'core/themes/'+jeedom.theme.mobile_theme_color+'/mobile/' + jeedom.theme.mobile_theme_color + '.css';
+        theme = jeedom.theme.mobile_theme_color;
+        themeCss = 'core/themes/'+jeedom.theme.mobile_theme_color+'/mobile/' + jeedom.theme.mobile_theme_color + '.css';
       }
-      if($('#jQMnDColor').attr('href') != theme){
-        $('#jQMnDColor').attr('href', theme);
+      if($('#jQMnDColor').attr('href') != themeCss){
+        $('body').attr('data-theme',theme);
+        $('#jQMnDColor').attr('href', themeCss);
       }
     }, 60000);
   }
@@ -266,12 +276,13 @@ function initApplication(_reinit) {
         if(typeof jeedom.theme['interface::advance::coloredIcons'] != 'undefined' && jeedom.theme['interface::advance::coloredIcons'] == '1'){
           $('body').attr('data-coloredIcons',1);
         }
+        $('body').attr('data-theme',jeedom.theme.current_mobile_theme);
         $('#jQMnDColor').attr('href', 'core/themes/'+jeedom.theme.current_mobile_theme+'/mobile/' + jeedom.theme.current_mobile_theme + '.css');
         changeThemeAuto();
         if (isset(jeedom.theme.current_mobile_theme) && jeedom.theme.current_mobile_theme != '') {
           include.push( 'core/themes/'+jeedom.theme.current_mobile_theme+'/mobile/' + jeedom.theme.current_mobile_theme + '.js');
         }
-
+        
         var widget_shadow = true
         var useAdvance = 0
         if(typeof jeedom.theme != 'undefined'){
@@ -286,8 +297,6 @@ function initApplication(_reinit) {
           themePath = 'core/themes/' + jeedom.theme['mobile_theme_color'] + '/mobile'
           include.push( 'core/themes/'+jeedom.theme.current_mobile_theme+'/mobile/' + 'shadows.css');
         }
-
-
         if (isset(data.result.custom) && data.result.custom != null) {
           if (isset(data.result.custom.css) && data.result.custom.css) {
             include.push('mobile/custom/custom.css');
@@ -301,7 +310,7 @@ function initApplication(_reinit) {
             include.push('plugins/'+plugins[i].id+'/mobile/js/event.js');
           }
         }
-
+        
         $.get("core/php/icon.inc.php", function (data) {
           $("head").append(data);
           $.include(include, function () {
@@ -346,7 +355,7 @@ function page(_page, _title, _option, _plugin,_dialog) {
     $('#bottompanel_mainoption').panel('close');
     $('.ui-popup').popup('close');
   } catch (e) {
-
+    
   }
   if (isset(_title)) {
     if (!isset(_dialog) || !_dialog) {
