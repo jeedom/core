242c21ea262d42d1bc941b4db449b90f85c24da9
Loïc <loic@jeedom.com>
2019-06-25 09:17:01 +0200

    bugfix

--- desktop/modal/scenario.export.php
+++ desktop/modal/scenario.export.php
@@ -1,13 +1,13 @@
 <?php
 
 if (!isConnect()) {
-    throw new Exception('{{401 - Accès non autorisé}}');
+  throw new Exception('{{401 - Accès non autorisé}}');
 }
 
 $scenario = scenario::byId(init('scenario_id'));
 if (!is_object($scenario)) {
-    throw new Exception('{{Scénario introuvable}}');
+  throw new Exception('{{Scénario introuvable}}');
 }
 
-echo '<pre>' . $scenario->export() . '</pre>';
+echo '<textarea style="height:100%;width:100%" disabled>' . $scenario->export() . '</textarea>';
 ?>
