a808aa67414694db8fea936452a188087df2e136
Loïc <loic@jeedom.com>
2019-08-14 14:05:32 +0200

    add icon

--- desktop/modal/icon.selector.php
+++ desktop/modal/icon.selector.php
@@ -36,9 +36,9 @@ sendVarToJs('selectIcon', init('selectIcon', 0));
 }
 </style>
 <ul class="nav nav-tabs" role="tablist">
-	<li role="presentation" class="active"><a href="#icon" aria-controls="home" role="tab" data-toggle="tab">{{Icône}}</a></li>
+	<li role="presentation" class="active"><a href="#tabicon" aria-controls="home" role="tab" data-toggle="tab"><i class="fas fa-icons"></i> {{Icône}}</a></li>
 	<?php if(init('imgtab') == 1 || init('showimg') == 1){ ?>
-		<li role="presentation" ><a href="#img" aria-controls="home" role="tab" data-toggle="tab">{{Image}}</a></li>
+		<li role="presentation" ><a href="#tabimg" aria-controls="home" role="tab" data-toggle="tab"><i class="far fa-images"></i> {{Image}}</a></li>
 	<?php } ?>
 </ul>
 
@@ -59,10 +59,10 @@ sendVarToJs('selectIcon', init('selectIcon', 0));
 			<a id="bt_resetSearch" class="btn roundedRight" style="width:30px"><i class="fas fa-times"></i> </a>
 		</div>
 	</div>
-
-
+	
+	
 	<?php if(init('imgtab') == 1 || init('showimg') == 1){ ?>
-		<div role="tabpanel" class="tab-pane" id="img">
+		<div role="tabpanel" class="tab-pane" id="tabimg" style="width:calc(100% - 20px)">
 			<span class="btn btn-default btn-file pull-right">
 				<i class="fas fa-cloud-upload-alt"></i> {{Envoyer}}<input  id="bt_uploadImageIcon" type="file" name="file" style="display: inline-block;">
 			</span>
@@ -94,7 +94,7 @@ sendVarToJs('selectIcon', init('selectIcon', 0));
 					$('#mod_selectIcon').empty().load('index.php?v=d&modal=icon.selector&tabimg=1&showimg=1');
 				}
 			});
-
+			
 			$('.bt_removeImgIcon').on('click',function(){
 				var filename = $(this).attr('data-filename');
 				bootbox.confirm('{{Êtes-vous sûr de vouloir supprimer cette image}} <span style="font-weight: bold ;">' + filename + '</span> ?', function (result) {
@@ -114,8 +114,8 @@ sendVarToJs('selectIcon', init('selectIcon', 0));
 			</script>
 		</div>
 	<?php } ?>
-
-	<div role="tabpanel" class="tab-pane active" id="icon">
+	
+	<div role="tabpanel" class="tab-pane active" id="tabicon" style="width:calc(100% - 20px)">
 		<?php
 		$scanPaths = array('core/css/icon', 'data/fonts');
 		foreach ($scanPaths as $root) {
@@ -126,12 +126,12 @@ sendVarToJs('selectIcon', init('selectIcon', 0));
 				}
 				$fontfile = $root . $dir . 'fonts/' . substr($dir, 0, -1) . '.ttf';
 				if (!file_exists($fontfile)) continue;
-
+				
 				$css = file_get_contents($root . $dir . '/style.css');
 				$research = strtolower(str_replace('/', '', $dir));
 				preg_match_all("/\." . $research . "-(.*?):/", $css, $matches, PREG_SET_ORDER);
 				echo '<div class="iconCategory"><legend>{{' . str_replace('/', '', $dir) . '}}</legend>';
-
+				
 				$number = 1;
 				foreach ($matches as $match) {
 					if (isset($match[0])) {
@@ -425,7 +425,7 @@ $(function() {
 	//auto select actual icon:
 	if (selectIcon != "0") {
 		$(selectIcon).closest('.divIconSel').addClass('iconSelected')
-
+		
 		setTimeout(function() {
 			elem = $('div.divIconSel.iconSelected')
 			container = $('#mod_selectIcon > .tab-content')
@@ -433,6 +433,6 @@ $(function() {
 			container.animate({scrollTop: pos})
 		}, 250);
 	}
-
+	
 })
 </script>
