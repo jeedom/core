5bf5ff5a44fcf9b4e262bd26fe82a30b440df820
Loïc <loic@jeedom.com>
2019-09-13 11:35:04 +0200

    bugfix

--- desktop/js/plan.js
+++ desktop/js/plan.js
@@ -976,7 +976,7 @@ function displayObject(_plan,_html, _noRender) {
   _plan = init(_plan, {});
   _plan.position = init(_plan.position, {});
   _plan.css = init(_plan.css, {});
-  if (_plan.link_type == 'eqLogic' || _plan.link_type == 'scenario' || _plan.link_type == 'text' || _plan.link_type == 'image') {
+  if (_plan.link_type == 'eqLogic' || _plan.link_type == 'scenario' || _plan.link_type == 'text' || _plan.link_type == 'image' || _plan.link_type == 'zone') {
     $('.div_displayObject .'+_plan.link_type+'-widget[data-'+_plan.link_type+'_id=' + _plan.link_id + ']').remove();
   }else if (_plan.link_type == 'view' || _plan.link_type == 'plan') {
     $('.div_displayObject .'+_plan.link_type+'-link-widget[data-link_id=' + _plan.link_id + ']').remove();
