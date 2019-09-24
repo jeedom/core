3d903b39368d5ad0455e74a613490ac5084fe24c
Loïc <loic@jeedom.com>
2019-09-24 13:03:04 +0200

    test

--- core/js/jeedom.class.js
+++ core/js/jeedom.class.js
@@ -776,3 +776,16 @@ jeedom.cleanDatabase = function(_params) {
   };
   $.ajax(paramsAJAX);
 };
+
+jeedom.getThemeImgWidget(_light,_dark){
+  if(_light != '' && _dark == ''){
+    return _light;
+  }
+  if(_light == '' && _dark != ''){
+    return _dark;
+  }
+  if ($('body').attr('data-theme').endsWith('Light')) {
+    return _light;
+  }
+  return _dark;
+}
