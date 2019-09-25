76f6d27c233cee9a530d171c50c9297fdfb3cd73
Loïc <loic@jeedom.com>
2019-09-25 16:19:37 +0200

    improvement

--- core/class/cmd.class.php
+++ core/class/cmd.class.php
@@ -1174,8 +1174,11 @@ class cmd {
 		if(isset($widget_template[$this->getType()]) && isset($widget_template[$this->getType()][$this->getSubType()]) && isset($widget_template[$this->getType()][$this->getSubType()][$widget_name])){
 			$template_conf = $widget_template[$this->getType()][$this->getSubType()][$widget_name];
 			$template_name = 'cmd.' . $this->getType() . '.' . $this->getSubType() . '.' . $template_conf['template'];
-			if(isset($template_conf['replace'])){
+			if(isset($template_conf['replace']) && is_array($template_conf['replace']) && count($template_conf['replace']) > 0){
 				$replace = $template_conf['replace'];
+				foreach ($replace as &$value) {
+					$value = str_replace('#value#','"+_options.display_value+"',str_replace("'","\'",$value));
+				}
 			}else{
 				$replace = array();
 			}
@@ -1193,9 +1196,11 @@ class cmd {
 					if(!isset($test['state_dark'])){
 						$test['state_dark'] = '';
 					}
+					$test['state_light'] = str_replace('#value#','"+_options.display_value+"',str_replace("'","\'",$test['state_light']));
+					$test['state_dark'] = str_replace('#value#','"+_options.display_value+"',str_replace("'","\'",$test['state_dark']));
 					$test['operation'] = str_replace('#value#','_options.display_value',$test['operation']);
-					$replace['#test#'] .= 'if('. $test['operation'].'){cmd.attr("data-state",'.$i.');state=jeedom.widgets.getThemeImg(\''.str_replace("'","\'",$test['state_light']).'\',\''.str_replace("'","\'",$test['state_dark']).'\')}';
-					$replace['#change_theme#'] .= 'if(cmd.attr("data-state") == '.$i.'){state=jeedom.widgets.getThemeImg(\''.str_replace("'","\'",$test['state_light']).'\',\''.str_replace("'","\'",$test['state_dark']).'\')}';
+					$replace['#test#'] .= 'if('. $test['operation'].'){cmd.attr("data-state",'.$i.');state=jeedom.widgets.getThemeImg(\''.$test['state_light'].'\',\''.$test['state_dark'].'\')}';
+					$replace['#change_theme#'] .= 'if(cmd.attr("data-state") == '.$i.'){state=jeedom.widgets.getThemeImg(\''.$test['state_light'].'\',\''.$test['state_dark'].'\')}';
 					$i++;
 				}
 			}
