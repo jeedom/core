4c26f3f2bab7a101cb862431ee909b1b4a58f998
KiboOst <KiboOst@users.noreply.github.com>
2019-10-03 11:19:03 +0200

    better

--- desktop/js/history.js
+++ desktop/js/history.js
@@ -310,9 +310,9 @@ function displayTimeline(){
         tr += '<td>'
         if (data[i].group && data[i].plugins) {
           if (data[i].group == 'action') {
-            tr += data[i].type + '&#160&#160<i class="warning fas fa-terminal"></i><span style="display:none">action</span>'
+            tr += data[i].type + '&#160&#160<i class="warning fas fa-terminal"></i><span class="hidden">action</span>'
           } else {
-            tr += data[i].type + '&#160&#160<i class="info fas fa-info-circle"></i><span style="display:none">info</span>'
+            tr += data[i].type + '&#160&#160<i class="info fas fa-info-circle"></i><span class="hidden">info</span>'
           }
           tr += '&#160&#160' + data[i].plugins
         }
