4bea76d875212ea89a24f657c2f0c1104a795a43
Loïc <loic@jeedom.com>
2019-10-03 16:33:51 +0200

    fix

--- desktop/modal/first.use.php
+++ desktop/modal/first.use.php
@@ -4,52 +4,52 @@ if (!isConnect()) {
 }
 ?>
 <legend>
-    {{Mes premiers pas dans}} <?php echo config::byKey('product_name'); ?>
-    <a class='btn btn-default btn-xs pull-right' id='bt_doNotDisplayFirstUse'><i class="fas fa-eye-slash"></i> Ne plus afficher</a>
+	{{Mes premiers pas dans}} <?php echo config::byKey('product_name'); ?>
+	<a class='btn btn-default btn-xs pull-right' id='bt_doNotDisplayFirstUse'><i class="fas fa-eye-slash"></i> Ne plus afficher</a>
 </legend>
 <div id="div_alertFirstUse"></div>
 <center>
-    {{Bienvenue dans}} <?php echo config::byKey('product_name'); ?> {{, et merci d'avoir choisi cet outil pour votre habitat connecté. Voici 3 guides pour bien débuter avec}} <?php echo config::byKey('product_name'); ?> :
+	{{Bienvenue dans}} <?php echo config::byKey('product_name'); ?> {{, et merci d'avoir choisi cet outil pour votre habitat connecté. Voici 3 guides pour bien débuter avec}} <?php echo config::byKey('product_name'); ?> :
 </center>
 <br/><br/>
 
 <div class="row">
-    <div class="col-xs-4">
-       <center>
-        <a href="https://jeedom.com/start" target="_blank">
-            <i class="fas fa-picture-o" style="font-size:12em;"></i><br/>
-            {{Guide de démarrage}}
-        </a>
-    </center>
-</div>
-<div class="col-xs-4">
-   <center>
-    <a href="https://jeedom.github.io/documentation/premiers-pas/fr_FR/index" target="_blank">
-        <i class="fas fa-check-square" style="font-size:12em;"></i><br/>
-        {{Documentation de démarrage}}
-    </a>
-</center>
-</div>
-<div class="col-xs-4">
- <center>
-    <a href="https://jeedom.github.io/documentation" target="_blank">
-        <i class="fas fa-book" style="font-size:12em;"></i><br/>
-        {{Documentation}}
-    </a>
-</center>
-</div>
+	<div class="col-xs-4">
+		<center>
+			<a href="https://jeedom.com/start" target="_blank">
+				<i class="fas fa-image" style="font-size:12em;"></i><br/>
+				{{Guide de démarrage}}
+			</a>
+		</center>
+	</div>
+	<div class="col-xs-4">
+		<center>
+			<a href="https://jeedom.github.io/documentation/premiers-pas/fr_FR/index" target="_blank">
+				<i class="fas fa-check-square" style="font-size:12em;"></i><br/>
+				{{Documentation de démarrage}}
+			</a>
+		</center>
+	</div>
+	<div class="col-xs-4">
+		<center>
+			<a href="https://jeedom.github.io/documentation" target="_blank">
+				<i class="fas fa-book" style="font-size:12em;"></i><br/>
+				{{Documentation}}
+			</a>
+		</center>
+	</div>
 </div>
 
 <script>
-    $('#bt_doNotDisplayFirstUse').on('click', function () {
-        jeedom.config.save({
-            configuration: {'jeedom::firstUse': 0},
-            error: function (error) {
-                $('#div_alertFirstUse').showAlert({message: error.message, level: 'danger'});
-            },
-            success: function () {
-                $('#div_alertFirstUse').showAlert({message: '{{Sauvegarde réussie}}', level: 'success'});
-            }
-        });
+$('#bt_doNotDisplayFirstUse').on('click', function () {
+	jeedom.config.save({
+		configuration: {'jeedom::firstUse': 0},
+		error: function (error) {
+			$('#div_alertFirstUse').showAlert({message: error.message, level: 'danger'});
+		},
+		success: function () {
+			$('#div_alertFirstUse').showAlert({message: '{{Sauvegarde réussie}}', level: 'success'});
+		}
+	});
 });
 </script>
