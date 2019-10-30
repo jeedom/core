7785065b912b869e2780ab52f0466534c7782bcd
KiboOst <KiboOst@users.noreply.github.com>
2019-10-30 16:30:46 +0100

    Update auto check

--- desktop/js/update.js
+++ desktop/js/update.js
@@ -18,7 +18,34 @@ var hasUpdate = false;
 var hasUpdateOther = false;
 var progress = -2;
 var alertTimeout = null
-printUpdate();
+
+$(function() {
+  jeedom.config.load({
+    configuration: {"update::lastCheck":0},
+    error: function (error) {
+      $('#div_alert').showAlert({message: error.message, level: 'danger'})
+    },
+    success: function (data) {
+      var dbDate = data['update::lastCheck']
+      if (dbDate != 0) {
+        var dbDay = dbDate.split(' ')[0].split('-')
+        var dbTime = dbDate.split(' ')[1].split(':')
+        var dateChecked = new Date(dbDay[0],dbDay[1]-1,dbDay[2],dbTime[0],dbTime[1])
+        var dateNow = new Date()
+        var diffMs = dateNow - dateChecked
+        var diffMins = Math.round(((diffMs % 86400000) % 3600000) / 60000)
+        if (diffMins > 30) {
+          checkAllUpdate()
+        } else {
+          printUpdate()
+        }
+      } else {
+        printUpdate()
+      }
+    }
+  })
+})
+
 
 $("#md_specifyUpdate").dialog({
   closeText: '',
@@ -71,15 +98,7 @@ $('#bt_doUpdate').off('click').on('click', function () {
 
 $('#bt_checkAllUpdate').off('click').on('click', function () {
   if ($('a[href="#log"]').parent().hasClass('active')) $('a[href="#coreplugin"]').trigger('click')
-  $.hideAlert();
-  jeedom.update.checkAll({
-    error: function (error) {
-      $('#div_alert').showAlert({message: error.message, level: 'danger'});
-    },
-    success: function () {
-      printUpdate();
-    }
-  });
+  checkAllUpdate()
 });
 
 $('#table_update,#table_updateOther').delegate('.update', 'click', function () {
@@ -152,6 +171,18 @@ $(function () {
     $(this).tooltipster('open')
   })
 })
+
+function checkAllUpdate() {
+  $.hideAlert();
+  jeedom.update.checkAll({
+    error: function (error) {
+      $('#div_alert').showAlert({message: error.message, level: 'danger'});
+    },
+    success: function () {
+      printUpdate();
+    }
+  });
+}
 
 function getJeedomLog(_autoUpdate, _log) {
   $.ajax({
@@ -238,7 +269,7 @@ function printUpdate() {
       if (!hasUpdate && hasUpdateOther) $('li a[href="#other"]').trigger('click');
     }
   });
-  
+
   jeedom.config.load({
     configuration: {"update::lastCheck":0,"update::lastDateCore": 0},
     error: function (error) {
@@ -265,7 +296,7 @@ function addUpdate(_update) {
       if (!_update.configuration.hasOwnProperty('doNotUpdate') || _update.configuration.doNotUpdate == '0') hasUpdateOther = true;
     }
   }
-  
+
   var tr = '<tr data-id="' + init(_update.id) + '" data-logicalId="' + init(_update.logicalId) + '" data-type="' + init(_update.type) + '">';
   tr += '<td style="width:40px"><span class="updateAttr label ' + labelClass +'" data-l1key="status"></span>';
   tr += '</td>';
@@ -277,12 +308,12 @@ function addUpdate(_update) {
     if (_update.configuration.version.toLowerCase() != 'stable' && _update.configuration.version.toLowerCase() != 'beta') updClass = 'label-danger';
     tr += ' <span class="label ' + updClass + '">' + _update.configuration.version + '</span>';
   }
-  
+
   _localVersion = _update.localVersion
   if (_localVersion !== null && _localVersion.length > 19) _localVersion = _localVersion.substring(0,16) + '...'
   _remoteVersion = _update.remoteVersion
   if (_remoteVersion !== null && _remoteVersion.length > 19) _remoteVersion = _remoteVersion.substring(0,16) + '...'
-  
+
   tr += '</td>';
   tr += '<td style="width:160px;"><span class="label label-primary" data-l1key="localVersion">'+_localVersion+'</span></td>';
   tr += '<td style="width:160px;"><span class="label label-primary" data-l1key="remoteVersion">'+_remoteVersion+'</span></td>';
@@ -410,14 +441,14 @@ function createUpdateObserver() {
       }
     })
   })
-  
+
   var observerConfig = {
     attributes: true,
     childList: true,
     characterData: true,
     subtree: true
   }
-  
+
   var targetNode = document.getElementById('pre_updateInfo')
   _UpdateObserver_.observe(targetNode, observerConfig)
 }
@@ -428,7 +459,7 @@ function cleanUpdateLog() {
   if (prevUpdateText == currentUpdateText) return false
   lines = currentUpdateText.split("\n")
   l = lines.length
-  
+
   //update progress bar and clean text!
   linesRev = lines.slice().reverse()
   for(var i=0; i < l; i++) {
@@ -439,13 +470,13 @@ function cleanUpdateLog() {
       break
     }
   }
-  
+
   newLogText = ''
   for(var i=0; i < l; i++) {
     line = lines[i]
     if (line == '') continue
     if (line.startsWith('[PROGRESS]')) line = ''
-    
+
     //check ok at end of line:
     if (line.endsWith('OK')) {
       matches = line.match(/[. ]{1,}OK/g)
@@ -456,7 +487,7 @@ function cleanUpdateLog() {
         line = line.replace('OK', ' | OK')
       }
     }
-    
+
     //remove points ...
     matches = line.match(/[.]{2,}/g)
     if (matches) {
@@ -465,7 +496,7 @@ function cleanUpdateLog() {
       })
     }
     line = line.trim()
-    
+
     //check ok on next line, escaping progress inbetween:
     var offset = 1
     if (lines[i+1].startsWith('[PROGRESS]')) {
@@ -486,7 +517,7 @@ function cleanUpdateLog() {
       line += ' | OK'
       lines[i+offset] = ''
     }
-    
+
     if (line != '') {
       newLogText += line + '\n'
       $('#pre_updateInfo_clean').value(newLogText)
