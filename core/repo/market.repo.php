3fcbad7644ff580fb5c8f2b68dbaa60834160033
Loïc <loic@jeedom.com>
2019-11-10 10:39:31 +0100

    improvement

--- core/repo/market.repo.php
+++ core/repo/market.repo.php
@@ -516,10 +516,7 @@ class repo_market {
 		if(!file_exists('/etc/zabbix/zabbix_agentd.conf')){
 			return false;
 		}
-		if(exec('grep "jeedom.com" /etc/zabbix/zabbix_agentd.conf | wc -l') == 0){
-			return false;
-		}
-		if(exec('grep "zabbix.jeedom.com" /etc/zabbix/zabbix_agentd.conf | wc -l') > 0){
+		if(exec('grep "jeedom.com" /etc/zabbix/zabbix_agentd.conf | grep -v "zabbix.jeedom.com" | wc -l') == 0){
 			return false;
 		}
 		return (count(system::ps('zabbix')) > 0);
