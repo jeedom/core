ae7bba492c7c87f3993fc7b5be38ba0ab675acb2
Loïc <loic@jeedom.com>
2019-11-29 14:54:13 +0100

    improvements

--- desktop/js/dashboard.js
+++ desktop/js/dashboard.js
@@ -287,10 +287,12 @@ function getObjectHtml(_object_id) {
         console.log(err);
       }
       var $divDisplayEq = $('#div_ob'+_object_id+'.div_displayEquipement')
-      var count = $divDisplayEq.find('.scenario-widget:visible').length + $divDisplayEq.find('.eqLogic-widget:visible').length
-      if (count == 0) {
-        $('#div_ob'+_object_id).closest('.div_object').remove();
-        return;
+      if(SEL_SUMMARY != ''){
+        var count = $divDisplayEq.find('.scenario-widget:visible').length + $divDisplayEq.find('.eqLogic-widget:visible').length
+        if (count == 0) {
+          $('#div_ob'+_object_id).closest('.div_object').remove();
+          return;
+        }
       }
       positionEqLogic();
       
