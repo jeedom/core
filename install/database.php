74a7767a9becf39851c70e0946e9ea74371110a7
Emmanuel Bernaszuk <emmanuel.bernaszuk@kw12er.com>
2019-11-30 17:19:52 +0100

    Fix some database function calls

--- install/database.php
+++ install/database.php
@@ -19,14 +19,14 @@
 * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
 */
 
-require_once dirname(__DIR__).'/core/php/console.php';
-require_once __DIR__ . '/../core/config/common.config.php';
-require_once __DIR__ . '/../core/class/DB.class.php';
-echo "[START CHECK AND FIX DB]\n";
+require_once dirname(__DIR__) . '/core/php/console.php';
+require_once dirname(__DIR__) . '/core/config/common.config.php';
+require_once dirname(__DIR__) . '/core/class/DB.class.php';
+
+echo '[START CHECK AND FIX DB]'.PHP_EOL;
 try {
-  DB::compareAndFix(json_decode(file_get_contents(__DIR__.'/database.json'),true),'all',true);
-} catch (\Exception $e) {
-  echo "***ERREUR*** " . $ex->getMessage() . "\n";
+    DB::compareAndFix(json_decode(file_get_contents(__DIR__.'/database.json'),true),'all',true);
+} catch (\Exception $exception) {
+    echo '***ERREUR*** ' . $exception->getMessage() . "\n";
 }
-echo "[END CHECK AND FIX DB]\n";
-?>
+echo '[END CHECK AND FIX DB]'.PHP_EOL;
