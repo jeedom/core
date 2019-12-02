cc3e41034bf18f3b18de0ddace8d92c0df9c337f
lmanso <30408665+lmanso@users.noreply.github.com>
2019-12-02 14:39:57 +0100

    Custom error Echec lors du téléchargement

--- core/class/update.class.php
+++ core/class/update.class.php
@@ -309,9 +309,13 @@ class update {
 					if (!file_exists($tmp)) {
 						throw new Exception(__('Impossible de trouver le fichier zip : ', __FILE__) . $this->getConfiguration('path'));
 					}
-					if (filesize($tmp) < 100) {
+				if (filesize($tmp) < 100) {
+					if(jeedom::getHardwareName() == 'smart' && stristr(config::byKey('product_name'), 'Jeedom') == true){
 						throw new Exception(__('Echec lors du téléchargement du fichier. Veuillez réessayer plus tard (taille inférieure à 100 octets). Cela peut être dû à une absence de connexion au market (vérifiez dans la configuration de jeedom qu\'un test de connexion au market marche) ou lié à un manque de place, une version minimale requise non consistante avec votre version de Jeedom, un souci du plugin sur le market, etc.', __FILE__));
+					} else {
+						throw new Exception(__('Echec lors du téléchargement du fichier. Veuillez réessayer plus tard (taille inférieure à 100 octets). Cela peut être dû à une absence de connexion au market (vérifiez dans la configuration de' . jeedom::getHardwareName() . 'qu\'un test de connexion au market marche) ou lié à un manque de place, une version minimale requise non consistante avec votre version de' . jeedom::getHardwareName() . 'un souci du plugin sur le market, etc.', __FILE__));
 					}
+				}
 					$extension = strtolower(strrchr($tmp, '.'));
 					if (!in_array($extension, array('.zip'))) {
 						throw new Exception('Extension du fichier non valide (autorisé .zip) : ' . $extension);
