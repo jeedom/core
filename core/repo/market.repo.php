3390c743489467764709c4721914c074a446eaa2
Loïc <loic@jeedom.com>
2019-12-06 10:55:01 +0100

    Update market.repo.php

--- core/repo/market.repo.php
+++ core/repo/market.repo.php
@@ -30,7 +30,6 @@ class repo_market {
 		'backup' => true,
 		'hasConfiguration' => true,
 		'proxy' => true,
-		'sendPlugin' => true,
 		'hasStore' => true,
 		'test' => true,
 		'pullInstall' => true,
