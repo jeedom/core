6c537ae37c3003ce21a095c74ba9539b38c92325
Loïc <loic@jeedom.com>
2019-12-19 11:22:56 +0100

    Update user.js

--- desktop/js/user.js
+++ desktop/js/user.js
@@ -161,7 +161,7 @@
                 ligne += '</td>';
                 ligne += '<td>';
                 ligne += '<label><input type="checkbox" class="userAttr" data-l1key="enable" '+disable+' />{{Actif}}</label><br/>';
-                ligne += '<label><input type="checkbox" class="userAttr" data-l1key="options" data-l2key="localOnly" '+disable+' />{{Local}}</label><br/>';
+                ligne += '<label><input type="checkbox" class="userAttr" data-l1key="options" data-l2key="localOnly" '+disable+' />{{Local}}</label>';
                 if(data[i].profils == 'admin'){
                     ligne += '<br/><label><input type="checkbox" class="userAttr" data-l1key="options" data-l2key="doNotRotateHash" '+disable+' />{{Ne pas faire de rotation clef api}}</label>';
                 }
