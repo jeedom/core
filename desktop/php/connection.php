4e459644aed18bcd307e3570451cedfd504aafa7
lmanso <30408665+lmanso@users.noreply.github.com>
2019-12-23 17:15:12 +0100

    Custom btn connect

--- desktop/php/connection.php
+++ desktop/php/connection.php
@@ -94,6 +94,10 @@ if(config::byKey('product_connection_BG')){
 		echo "<style>";
 		echo "body { background:".config::byKey('product_connection_color')." !important;}";
 		echo "</style>";
+	}elseif(config::byKey('product_btn_login_color')){
+	echo "<style>";
+	echo "#bt_login_validate { background:".config::byKey('product_connection_color')." !important; border-color:".config::byKey('product_connection_color')." !important; }";
+	echo "</style>";
 	}
 	if(stristr(config::byKey('product_name'), 'Jeedom') == false){
 		echo "<style>";
