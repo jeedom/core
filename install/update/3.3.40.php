6c83b81b3e8a14102dbec753f7cb9a174c3ec5f1
Loïc <loic@jeedom.com>
2019-12-26 20:09:18 +0100

    Update 3.3.40.php

--- install/update/3.3.40.php
+++ install/update/3.3.40.php
@@ -9,3 +9,16 @@ foreach (user::all() as $user) {
   $user->setOptions('hashGenerated',date('Y-m-d H:i:s'));
   $user->save();
 }
+
+$sql = "SELECT concat('ALTER TABLE ', TABLE_NAME, ' DROP FOREIGN KEY ', CONSTRAINT_NAME, ';')
+FROM information_schema.key_column_usage
+WHERE CONSTRAINT_SCHEMA = 'jeedom'
+AND REFERENCED_TABLE_NAME IS NOT NULL;";
+$result = DB::prepare($sql,array(),DB::FETCH_TYPE_ALL);
+foreach ($result as $value) {
+  try {
+    DB::prepare(array_values($value)[0],array(),DB::FETCH_TYPE_ALL);
+  } catch (\Exception $e) {
+    
+  }
+}
