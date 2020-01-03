2bba3c5e16b7d9dd1790ee896f56a96e7a9e717a
Loïc <loic@jeedom.com>
2020-01-03 17:29:39 +0100

    Update jeedom.class.php

--- core/class/jeedom.class.php
+++ core/class/jeedom.class.php
@@ -1375,6 +1375,9 @@ class jeedom {
 	}
 	
 	public static function forceSyncHour() {
+		if(config::byKey('disallow_ntp') == 1){
+			return;
+		}
 		shell_exec(system::getCmdSudo() . 'service ntp stop;' . system::getCmdSudo() . 'ntpdate -s ' . config::byKey('ntp::optionalServer', 'core', '0.debian.pool.ntp.org') . ';' . system::getCmdSudo() . 'service ntp start');
 	}
 	
