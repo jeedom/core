c51ea18810b55b21193c52c5039a85a807382270
Loïc <loic@jeedom.com>
2020-01-11 12:13:15 +0100

    Update scenario.ajax.php

--- core/ajax/scenario.ajax.php
+++ core/ajax/scenario.ajax.php
@@ -86,7 +86,7 @@ try {
 	if (init('action') == 'testExpression') {
 		$return = array();
 		$scenario = null;
-		$return['evaluate'] = scenarioExpression::setTags(jeedom::fromHumanReadable(init('expression')), $scenario, true);
+		$return['evaluate'] = scenarioExpression::setTags(jeedom::fromHumanReadable(init('expression')), $scenario);
 		$return['result'] = evaluate($return['evaluate']);
 		$return['correct'] = 'ok';
 		if (trim($return['result']) == trim($return['evaluate'])) {
