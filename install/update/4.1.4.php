2efd7e436245df46be339c60aad44fc136ae53ef
Loïc <loic@jeedom.com>
2020-01-19 11:35:13 +0100

    Update 4.1.4.php

--- install/update/4.1.4.php
+++ install/update/4.1.4.php
@@ -1,4 +1,6 @@
 <?php
 require_once __DIR__ . '/../../core/php/core.inc.php';
-$cmd = "sudo sed -i 's#deb http://repo.jeedom.com/odroid/ dists/stable/main/binary-arm64/##g' /etc/apt/sources.list ";
-exec($cmd);
+if (file_exists('/media/boot/multiboot/meson64_odroidc2.dtb.linux')) {
+  $cmd = "sudo sed -i 's#deb http://repo.jeedom.com/odroid/ dists/stable/main/binary-arm64/##g' /etc/apt/sources.list ";
+  exec($cmd);
+}
