943a08ce4381c1ee33063fb8eb2938c9a21fd4c1
Loïc <loic@jeedom.com>
2020-02-17 18:12:57 +0100

    Update plugin.class.php

--- core/class/plugin.class.php
+++ core/class/plugin.class.php
@@ -243,14 +243,6 @@ class plugin {
 		return array();
 	}
 	
-	public static function saveTranslation($_plugin, $_language, $_translation) {
-		$dir = __DIR__ . '/../../plugins/' . $_plugin . '/core/i18n';
-		if (!file_exists($dir)) {
-			mkdir($dir, 0775, true);
-		}
-		file_put_contents($dir . '/' . $_language . '.json', json_encode($_translation, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
-	}
-	
 	public static function orderPlugin($a, $b) {
 		$al = strtolower($a->name);
 		$bl = strtolower($b->name);
