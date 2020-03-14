c64af05b4a80f4a6df1bc352d25b0fc0b632c67a
Nebz <28622481+NebzHB@users.noreply.github.com>
2020-03-14 10:48:16 +0100

    getBluetoothMapping only have 1 arg

--- core/class/jeedom.class.php
+++ core/class/jeedom.class.php
@@ -606,7 +606,7 @@ class jeedom {
 			if (isset($bluetoothMapping[$_name])) {
 				return $bluetoothMapping[$_name];
 			}
-			$bluetoothMapping = self::getBluetoothMapping('', true);
+			$bluetoothMapping = self::getBluetoothMapping('');
 			if (isset($bluetoothMapping[$_name])) {
 				return $bluetoothMapping[$_name];
 			}
