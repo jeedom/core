439f760d1b01b2e88a54eacc66cb6066fb6ed267
Loïc <loic@jeedom.com>
2020-03-19 17:39:24 +0100

    test with float button

--- mobile/js/application.js
+++ mobile/js/application.js
@@ -46,6 +46,7 @@ $(function() {
     $('div[data-role=header]').remove();
     $('#searchContainer').css('top',0);
     $('#bt_eraseSearchInput').css('top',0);
+    $('#pagecontainer').append('<a href="#bottompanel" id="bt_bottompanel" class="ui-btn ui-btn-inline ui-btn-fab ui-btn-raised clr-primary waves-effect waves-button waves-effect waves-button" style="position:fixed;bottom:10px;right:10px;"><i class="fas fa-bars" style="position:relative;top:-3px"></i></a>')
   }
   
   initApplication()
