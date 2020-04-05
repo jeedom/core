2079f939fe855b29fa1081f8bc0c4f014f1bec43
Loïc <loic@jeedom.com>
2020-04-05 16:59:44 +0200

    fix

--- core/api/tts.php
+++ core/api/tts.php
@@ -85,7 +85,7 @@ try {
 		shell_exec($cmd);
 	}else if($engine == 'pico'){
 		$volume = '-af "volume=' . init('volume', '6') . 'dB"';
-		$lang = init('lang',config::byKey('language'));
+		$lang = str_replace('_','-',init('lang',config::byKey('language')));
 		$avconv = 'avconv';
 		if(!com_shell::commandExists('avconv')){
 			$avconv = 'ffmpeg';
