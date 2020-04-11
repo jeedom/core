66bcade92557e18a5f8947d31ab4118e52de6f7f
Loïc <loic@jeedom.com>
2020-04-11 10:40:58 +0200

    revert

--- install/update/4.1.8.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__ . '/../../core/php/core.inc.php';
-exec("(ps ax || ps w) | grep -ie 'tunnel-linux-".system::getArch()."' | grep -v grep | awk '{print $1}' | xargs sudo kill -9 > /dev/null 2>&1");
-if(file_exists(__DIR__.'/../../script/tunnel')){
-  rrmdir(__DIR__.'/../../script/tunnel');
-}
-?>
