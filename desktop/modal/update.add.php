c16b28ec2d81515c47ca93cfd39ef549edc92ec9
Loïc <loic@jeedom.com>
2020-04-14 16:52:53 +0200

    Update update.add.php

--- desktop/modal/update.add.php
+++ desktop/modal/update.add.php
@@ -6,7 +6,7 @@ $repos = update::listRepo();
 ?>
  <div style="display: none;" id="div_repoAddAlert"></div>
  <legend>{{Source}}</legend>
- <div class="alert alert-danger">{{Attention, il n’y a pas d’assistance de l’équipe }}<?php echo config::byKey('product_name'); ?>{{ sur les plugins installés depuis une autre source que le Market }}<?php echo config::byKey('product_name'); ?>{{.De plus, l’installation d’un plugin depuis une autre source que le Market }}<?php echo config::byKey('product_name'); ?>{{ peut dans certains cas entrainer la perte globale d’assistance par l’équipe }}<?php echo config::byKey('product_name'); ?>.</div>
+ <div class="alert alert-danger">{{Attention, il n’y a pas d’assistance de l’équipe }}<?php echo config::byKey('product_name'); ?>{{ sur les plugins installés depuis une autre source que le Market }}<?php echo config::byKey('product_name'); ?>{{.De plus, l’installation d’un plugin depuis une autre source que le Market }}<?php echo config::byKey('product_name'); ?>{{ entraine la perte globale d’assistance par l’équipe }}<?php echo config::byKey('product_name'); ?>.</div>
  <form class="form-horizontal">
  	<fieldset>
  		<div class="form-group">
