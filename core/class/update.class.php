c11f7491eb0954d51fb1c6cbe9c986bf846db76c
Loïc <loic@jeedom.com>
2020-05-05 11:59:47 +0200

    typo

--- core/class/update.class.php
+++ core/class/update.class.php
@@ -426,15 +426,13 @@ class update {
 			@chmod(__DIR__ . '/../../plugins', 0775);
 		}
 		$cibDir = __DIR__ . '/../../plugins/' . $this->getLogicalId();
-		echo "Remove useless files...\n";
+		log::add('update', 'alert',  __('Supression des fichiers inutiles...', __FILE__));
 		foreach (array('3rdparty','desktop','mobile','core','docs','install','script','vendor') as $folder) {
-			echo 'Cleaning '.$folder."\n";
 			shell_exec('find '.$cibDir. '/'.$folder.'/* -mtime +7 -type f ! -iname "custom.*" ! -iname "common.config.php" -delete');
 		}
 		log::add('update', 'alert', __('Début de la mise à jour de : ', __FILE__) . $this->getLogicalId() . "\n");
 		switch ($this->getType()) {
 			case 'plugin':
-			
 			if (!file_exists($cibDir) && !mkdir($cibDir, 0775, true)) {
 				throw new Exception(__('Impossible de créer le dossier  : ' . $cibDir . '. Problème de droits ?', __FILE__));
 			}
