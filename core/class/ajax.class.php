b8666da707e7ed3cbd94126063f4a5bdbf0dac73
Loïc <loic@jeedom.com>
2020-05-10 10:05:44 +0200

    typo

--- core/class/ajax.class.php
+++ core/class/ajax.class.php
@@ -1,38 +1,38 @@
 <?php
 
 /* This file is part of Jeedom.
- *
- * Jeedom is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Jeedom is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
- */
+*
+* Jeedom is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Jeedom is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 /* * ***************************Includes********************************* */
 require_once __DIR__ . '/../../core/php/core.inc.php';
 
 class ajax {
 	/*     * *************************Attributs****************************** */
-
+	
 	/*     * *********************Methode static ************************* */
-
+	
 	public static function init($_checkToken = true) {
 		if (!headers_sent()) {
 			header('Content-Type: application/json');
 		}
 		if ($_checkToken && init('jeedom_token') != self::getToken()) {
-			self::error(__('Token d\'accès invalide', __FILE__));
+			self::error(__('Votre session a expiré, veuillez vous reconnecter', __FILE__));
 		}
 	}
-
+	
 	public static function getToken() {
 		if (session_status() == PHP_SESSION_NONE) {
 			@session_start();
@@ -45,17 +45,17 @@ class ajax {
 		}
 		return $_SESSION['jeedom_token'];
 	}
-
+	
 	public static function success($_data = '') {
 		echo self::getResponse($_data);
 		die();
 	}
-
+	
 	public static function error($_data = '', $_errorCode = 0) {
 		echo self::getResponse($_data, $_errorCode);
 		die();
 	}
-
+	
 	public static function getResponse($_data = '', $_errorCode = null) {
 		$isError = !(null === $_errorCode);
 		$return = array(
