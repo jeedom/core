86d48a5cc5d8b387d345eead16d5ac1cfd407f3e
Loïc <loic@jeedom.com>
2020-05-21 10:15:20 +0200

    Update first.use.php

--- desktop/modal/first.use.php
+++ desktop/modal/first.use.php
@@ -13,32 +13,50 @@ if (!isConnect()) {
 </center>
 <br/><br/>
 
-<div class="row">
-    <div class="col-xs-4">
-       <center>
-        <a href="https://jeedom.com/start" target="_blank">
-            <i class="fas fa-picture-o" style="font-size:12em;"></i><br/>
-            {{Guide de démarrage}}
-        </a>
-    </center>
-</div>
-<div class="col-xs-4">
-   <center>
-    <a href="https://jeedom.github.io/documentation/premiers-pas/fr_FR/index" target="_blank">
-        <i class="fas fa-check-square" style="font-size:12em;"></i><br/>
-        {{Documentation de démarrage}}
-    </a>
-</center>
-</div>
-<div class="col-xs-4">
- <center>
-    <a href="https://jeedom.github.io/documentation" target="_blank">
-        <i class="fas fa-book" style="font-size:12em;"></i><br/>
-        {{Documentation}}
-    </a>
-</center>
-</div>
-</div>
+	<div style="position: relative; transform: translateY(-50%); top: calc(50% - 120px);">
+		<div class="row">
+			<div class="col-xs-3">
+				<center>
+					<a href="https://start.jeedom.com/" target="_blank">
+						<i class="fas fa-image" style="font-size:40px;"></i><br/>
+						{{Guide de démarrage}}
+					</a>
+				</center>
+			</div>
+			<div class="col-xs-3">
+				<center>
+					<a href="https://doc.jeedom.com/fr_FR/concept/" target="_blank">
+						<i class="fas fa-cogs" style="font-size:40px;"></i><br/>
+						{{Concept}}
+					</a>
+				</center>
+			</div>
+			<div class="col-xs-3">
+				<center>
+					<a href="https://doc.jeedom.com/fr_FR/premiers-pas/" target="_blank">
+						<i class="fas fa-check-square" style="font-size:40px;"></i><br/>
+						{{Documentation de démarrage}}
+					</a>
+				</center>
+			</div>
+			<div class="col-xs-3">
+				<center>
+					<a href="https://doc.jeedom.com/" target="_blank">
+						<i class="fas fa-book" style="font-size:40px;"></i><br/>
+						{{Documentation}}
+					</a>
+				</center>
+			</div>
+		</div>
+
+		<br><br><br>
+		<center>
+			<a class="badge cursor" href="https://www.jeedom.com" target="_blank">Site</a> |
+			<a class="badge cursor" href="https://blog.jeedom.com/" target="_blank">Blog</a> |
+			<a class="badge cursor" href="https://community.jeedom.com/" target="_blank">Community</a> |
+			<a class="badge cursor" href="https://market.jeedom.com/" target="_blank">Market</a>
+		</center>
+	</div>
 
 <script>
     $('#bt_doNotDisplayFirstUse').on('click', function () {
