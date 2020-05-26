73ad3bfef96c1d15700581e0026ee2fd91e97cc4
Loïc <loic@jeedom.com>
2020-05-26 09:19:01 +0200

    Update plan.ajax.php

--- core/ajax/plan.ajax.php
+++ core/ajax/plan.ajax.php
@@ -24,7 +24,7 @@ try {
 		throw new Exception(__('401 - Accès non autorisé', __FILE__));
 	}
 	
-	ajax::init();
+	ajax::init(array('uploadImagePlan','uploadImage'));
 	
 	if (init('action') == 'save') {
 		if (!isConnect('admin')) {
