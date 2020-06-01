513aa0f97d048bf6e9bb867b240f4f075f1926fd
KiboOst <KiboOst@users.noreply.github.com>
2020-06-01 12:30:10 +0200

    dashboard js optimize

--- desktop/js/dashboard.js
+++ desktop/js/dashboard.js
@@ -38,17 +38,17 @@ $('body').off('mouseenter').off('mouseleave')
 })
 
 
-$(function(){
+$(function() {
   setTimeout(function() {
-    if(typeof rootObjectId != 'undefined'){
+    if (typeof rootObjectId != 'undefined') {
       jeedom.object.getImgPath({
         id : rootObjectId,
-        success : function(_path){
-          setBackgroundImg(_path);
+        success : function(_path) {
+          setBackgroundImg(_path)
         }
-      });
+      })
     }
-  },1);
+  }, 1)
 
   //autoResize new created tiles:
   setTimeout(function() {
@@ -120,7 +120,7 @@ $('#in_searchWidget').off('keyup').on('keyup',function() {
     } else {
       $(this).hide()
     }
-  });
+  })
   $('.div_displayEquipement').packery()
   $('.div_displayEquipement').each(function() {
     var count = $(this).find('.scenario-widget:visible').length + $(this).find('.eqLogic-widget:visible').length
@@ -220,7 +220,7 @@ function filterByCategory() {
 
 
 $('#div_pageContainer').off('click','.eqLogic-widget .history').on('click','.eqLogic-widget .history', function (event) {
-  if(isEditing) return false
+  if (isEditing) return false
   event.stopImmediatePropagation()
   event.stopPropagation()
   if (event.ctrlKey) {
@@ -238,29 +238,29 @@ $('#div_pageContainer').off('click','.eqLogic-widget .history').on('click','.eqL
 $('#bt_displayObject').on('click', function () {
   if (isEditing) return
   if ($(this).attr('data-display') == 1) {
-    $('#div_displayObjectList').hide();
-    $('#div_displayObject').removeClass('col-lg-8 col-lg-10 col-lg-12 col-lg-8 col-lg-10 col-lg-12 col-md-8 col-md-9 col-md-10 col-md-12 col-sm-8 col-sm-10 col-sm-12').addClass('col-lg-12 col-md-12 col-sm-12');
+    $('#div_displayObjectList').hide()
+    $('#div_displayObject').removeClass('col-lg-8 col-lg-10 col-lg-12 col-lg-8 col-lg-10 col-lg-12 col-md-8 col-md-9 col-md-10 col-md-12 col-sm-8 col-sm-10 col-sm-12').addClass('col-lg-12 col-md-12 col-sm-12')
     $('.div_displayEquipement').each(function () {
-      $(this).packery();
-    });
-    $(this).attr('data-display', 0);
+      $(this).packery()
+    })
+    $(this).attr('data-display', 0)
   } else {
-    $('#div_displayObjectList').show();
-    $('#div_displayObject').removeClass('col-lg-8 col-lg-10 col-lg-12 col-lg-8 col-lg-10 col-lg-12 col-md-8 col-md-10 col-md-12 col-sm-8 col-sm-10 col-sm-12').addClass('col-lg-10 col-md-9 col-sm-8');
-    $('.div_displayEquipement').packery();
-    $(this).attr('data-display', 1);
+    $('#div_displayObjectList').show()
+    $('#div_displayObject').removeClass('col-lg-8 col-lg-10 col-lg-12 col-lg-8 col-lg-10 col-lg-12 col-md-8 col-md-10 col-md-12 col-sm-8 col-sm-10 col-sm-12').addClass('col-lg-10 col-md-9 col-sm-8')
+    $('.div_displayEquipement').packery()
+    $(this).attr('data-display', 1)
   }
-});
+})
 
 var _draggingId = false
 var _orders = {}
-function editWidgetMode(_mode,_save){
+function editWidgetMode(_mode,_save) {
   if (!isset(_mode)) {
-    if($('#bt_editDashboardWidgetOrder').attr('data-mode') != undefined && $('#bt_editDashboardWidgetOrder').attr('data-mode') == 1){
-      editWidgetMode(0,false);
-      editWidgetMode(1,false);
+    if ($('#bt_editDashboardWidgetOrder').attr('data-mode') != undefined && $('#bt_editDashboardWidgetOrder').attr('data-mode') == 1) {
+      editWidgetMode(0,false)
+      editWidgetMode(1,false)
     }
-    return;
+    return
   }
   var divEquipements = $('.div_displayEquipement')
   if (_mode == 0) {
@@ -313,33 +313,33 @@ function editWidgetMode(_mode,_save){
     //set resizables:
     divEquipements.find('.eqLogic-widget.allowResize').resizable({
       resize: function( event, ui ) {
-        positionEqLogic(ui.element.attr('data-eqlogic_id'),false);
-        ui.element.closest('.div_displayEquipement').packery();
+        positionEqLogic(ui.element.attr('data-eqlogic_id'), false)
+        ui.element.closest('.div_displayEquipement').packery()
       },
       stop: function( event, ui ) {
-        positionEqLogic(ui.element.attr('data-eqlogic_id'),false);
-        ui.element.closest('.div_displayEquipement').packery();
+        positionEqLogic(ui.element.attr('data-eqlogic_id'), false)
+        ui.element.closest('.div_displayEquipement').packery()
       }
     })
     divEquipements.find('.scenario-widget.allowResize').resizable({
       resize: function( event, ui ) {
-        positionEqLogic(ui.element.attr('data-scenario_id'),false,true);
-        ui.element.closest('.div_displayEquipement').packery();
+        positionEqLogic(ui.element.attr('data-scenario_id'), false, true)
+        ui.element.closest('.div_displayEquipement').packery()
       },
       stop: function( event, ui ) {
-        positionEqLogic(ui.element.attr('data-scenario_id'),false,true);
-        ui.element.closest('.div_displayEquipement').packery();
+        positionEqLogic(ui.element.attr('data-scenario_id'), false, true)
+        ui.element.closest('.div_displayEquipement').packery()
       }
     })
 
     $('#div_displayObject .row').css('margin-top', '27px')
-    $('#dashTopBar').css({"position":"fixed","top":"55px","z-index":"5000","width":"calc(100% - "+($('body').width() - $('#dashTopBar').width())+'px)'});
+    $('#dashTopBar').css({"position":"fixed","top":"55px","z-index":"5000","width":"calc(100% - "+($('body').width() - $('#dashTopBar').width())+'px)'})
     $('#in_searchWidget').style("background-color", "var(--al-info-color)", "important")
     .style("color", "var(--linkHoverLight-color)", "important")
     .val("{{Vous êtes en mode édition vous pouvez déplacer les widgets, les redimensionner et changer l'ordre des commandes dans les widgets. N'oubliez pas de quitter le mode édition pour sauvegarder}}")
     .prop('readonly', true)
   }
-  editWidgetCmdMode(_mode);
+  editWidgetCmdMode(_mode)
 }
 
 function getObjectHtml(_object_id) {
@@ -350,47 +350,45 @@ function getObjectHtml(_object_id) {
     category : SEL_CATEGORY,
     summary : SEL_SUMMARY,
     tag : SEL_TAG,
-    error: function (error) {
-      $('#div_alert').showAlert({message: error.message, level: 'danger'});
+    error: function(error) {
+      $('#div_alert').showAlert({message: error.message, level: 'danger'})
     },
-    success: function (html) {
+    success: function(html) {
       try {
-        $('#div_ob'+_object_id).empty().html(html).parent().show();
+        $('#div_ob'+_object_id).empty().html(html).parent().show()
       } catch(err) {
-        console.log(err);
+        console.log(err)
       }
       var $divDisplayEq = $('#div_ob'+_object_id+'.div_displayEquipement')
-      if(SEL_SUMMARY != ''){
+      if (SEL_SUMMARY != '') {
         var count = $divDisplayEq.find('.scenario-widget:visible').length + $divDisplayEq.find('.eqLogic-widget:visible').length
         if (count == 0) {
-          $('#div_ob'+_object_id).closest('.div_object').remove();
-          return;
+          $('#div_ob'+_object_id).closest('.div_object').remove()
+          return
         }
       }
-      positionEqLogic();
+      positionEqLogic()
 
-      $divDisplayEq.disableSelection();
-      $("input").click(function() { $(this).focus(); });
-      $("textarea").click(function() { $(this).focus(); });
-      $("select").click(function() { $(this).focus(); });
+      $divDisplayEq.disableSelection()
+      $('input', 'textarea', 'select').click(function() { $(this).focus() })
 
-      var container = $divDisplayEq.packery();
-      var packData = $divDisplayEq.data('packery');
+      var container = $divDisplayEq.packery()
+      var packData = $divDisplayEq.data('packery')
       if (isset(packData) && packData.items.length == 1) {
         $divDisplayEq.packery('destroy').packery()
       }
-      var itemElems =  container.find('.eqLogic-widget').draggable();
-      container.packery('bindUIDraggableEvents',itemElems);
-      var itemElems =  container.find('.scenario-widget').draggable();
-      container.packery('bindUIDraggableEvents',itemElems);
+      var itemElems =  container.find('.eqLogic-widget').draggable()
+      container.packery('bindUIDraggableEvents',itemElems)
+      var itemElems =  container.find('.scenario-widget').draggable()
+      container.packery('bindUIDraggableEvents',itemElems)
 
       function orderItems() {
-        var itemElems = container.packery('getItemElements');
+        var itemElems = container.packery('getItemElements')
         var isEditing = ($('#bt_editDashboardWidgetOrder').attr('data-mode') == 1) ? true : false
 
         var _draggingOrder = _orders[_draggingId]
         var _newOrders = {}
-        $(itemElems).each( function( i, itemElem ) {
+        $(itemElems).each( function(i, itemElem ) {
           _newOrders[$(this).attr('data-editId')] = i + 1
         })
         var _draggingNewOrder = _newOrders[_draggingId]
@@ -416,11 +414,10 @@ function getObjectHtml(_object_id) {
         }
 
         //reload from dom positions:
-        $('.div_displayEquipement').packery('reloadItems')
-        $('.div_displayEquipement').packery()
+        $('.div_displayEquipement').packery('reloadItems').packery()
 
-        itemElems = container.packery('getItemElements');
-        $(itemElems).each( function( i, itemElem ) {
+        itemElems = container.packery('getItemElements')
+        $(itemElems).each( function(i, itemElem ) {
           $(itemElem).attr('data-order', i + 1 )
           var value = i + 1
           if (isEditing) {
@@ -437,12 +434,11 @@ function getObjectHtml(_object_id) {
       $(itemElems).each( function( i, itemElem ) {
         $(itemElem).attr('data-order', i + 1 )
       })
-      container.on('dragItemPositioned',orderItems);
+      container.on('dragItemPositioned',orderItems)
 
-      $('#div_ob'+_object_id+'.div_displayEquipement .eqLogic-widget').draggable('disable');
-      $('#div_ob'+_object_id+'.div_displayEquipement .scenario-widget').draggable('disable');
+      $('#div_ob'+_object_id+'.div_displayEquipement').find('.eqLogic-widget', '.scenario-widget').draggable('disable')
     }
-  });
+  })
 }
 
 $('#bt_editDashboardWidgetOrder').on('click',function() {
@@ -478,37 +474,38 @@ $('#bt_editDashboardWidgetOrder').on('click',function() {
         $(this).height(maxHeight)
       })
       objectContainer.packery()
-    });
+    })
     editWidgetMode(1)
   }
-});
+})
 
-$('.li_object').on('click',function(){
-  $('.div_object').removeClass('hideByObjectSel');
-  var object_id = $(this).find('a').attr('data-object_id');
+$('.li_object').on('click',function() {
+  $('.div_object').removeClass('hideByObjectSel')
+  var object_id = $(this).find('a').attr('data-object_id')
   if ($('.div_object[data-object_id='+object_id+']').html() != undefined) {
     jeedom.object.getImgPath({
       id : object_id,
       success : function(_path){
-        setBackgroundImg(_path);
+        setBackgroundImg(_path)
       }
-    });
-    $('.li_object').removeClass('active');
-    $(this).addClass('active');
-    displayChildObject(object_id,false);
-    addOrUpdateUrl('object_id',object_id);
+    })
+    $('.li_object').removeClass('active')
+    $(this).addClass('active')
+    displayChildObject(object_id,false)
+    addOrUpdateUrl('object_id',object_id)
   } else {
-    loadPage($(this).find('a').attr('data-href'));
+    loadPage($(this).find('a').attr('data-href'))
   }
-});
+})
 
-function displayChildObject(_object_id,_recursion){
-  if(_recursion === false){
+function displayChildObject(_object_id, _recursion) {
+  if (_recursion === false) {
     $('.div_object').addClass('hideByObjectSel').hide()
   }
-  $('.div_object[data-object_id='+_object_id+']').show({effect : 'drop',queue : false})
-  $('.div_object[data-father_id='+_object_id+']').each(function() {
-    $(this).show({effect : 'drop',queue : false}).find('.div_displayEquipement').packery()
-    displayChildObject($(this).attr('data-object_id'),true)
-  });
+  $('.div_object[data-object_id='+_object_id+']')
+    .show({effect : 'drop',queue : false})
+    .each(function() {
+      $(this).show({effect : 'drop',queue : false}).find('.div_displayEquipement').packery()
+      displayChildObject($(this).attr('data-object_id'),true)
+    })
 }
