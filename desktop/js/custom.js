9bf79f25afbf5ffdb12f3e3e916581d787c5286e
KiboOst <KiboOst@users.noreply.github.com>
2020-06-09 09:40:02 +0200

    js cleaning and fix

--- desktop/js/custom.js
+++ desktop/js/custom.js
@@ -16,53 +16,52 @@
 
 "use strict"
 
-
-var editorDesktopJS = null;
-var editorDesktopCSS = null;
-var editorMobileJS = null;
-var editorMobileCSS = null;
+var editorDesktopJS = null
+var editorDesktopCSS = null
+var editorMobileJS = null
+var editorMobileCSS = null
 var contentHeight = 0
 
 jeedom.config.load({
     configuration: $('#div_spanAlertMessage').getValues('.configKey:not(.noSet)')[0],
-    error: function (error) {
-        $('#div_alert').showAlert({message: error.message, level: 'danger'});
+    error: function(error) {
+        $('#div_alert').showAlert({message: error.message, level: 'danger'})
     },
-    success: function (data) {
-        $('#div_spanAlertMessage').setValues(data, '.configKey');
-        modifyWithoutSave = false;
+    success: function(data) {
+        $('#div_spanAlertMessage').setValues(data, '.configKey')
+        modifyWithoutSave = false
     }
-});
+})
 
-setTimeout(function () {
+setTimeout(function() {
     editorDesktopJS = CodeMirror.fromTextArea(document.getElementById("ta_jsDesktopContent"), {
         lineNumbers: true,
         mode: "text/javascript",
         matchBrackets: true,
         viewportMargin: Infinity
-    });
+    })
     editorDesktopCSS = CodeMirror.fromTextArea(document.getElementById("ta_cssDesktopContent"), {
         lineNumbers: true,
         mode: "text/css",
         matchBrackets: true,
         viewportMargin: Infinity
-    });
-    var hmenu = $('header.navbar').height();
-    var hcontainer = $('#div_pageContainer').height();
-    var hwin = $(window).height();
-    contentHeight = hwin - (hmenu + hcontainer) + 300;
-    editorDesktopJS.setSize(null, contentHeight);
-    editorDesktopCSS.setSize(null, contentHeight);
-}, 1);
+    })
+    var hmenu = $('header.navbar').height()
+    var hcontainer = $('#div_pageContainer').height()
+    var hwin = $(window).height()
+    contentHeight = hwin - (hmenu + hcontainer) + 300
+    editorDesktopJS.setSize(null, contentHeight)
+    editorDesktopCSS.setSize(null, contentHeight)
+}, 1)
 
- $('a[data-toggle="tab"][href="#mobile"]').on('shown.bs.tab', function (e) {
+ $('a[data-toggle="tab"][href="#mobile"]').on('shown.bs.tab', function() {
     if (editorMobileCSS == null) {
         editorMobileCSS = CodeMirror.fromTextArea(document.getElementById("ta_cssMobileContent"), {
             lineNumbers: true,
             mode: "text/css",
             matchBrackets: true,
             viewportMargin: Infinity
-        });
+        })
     }
     if (editorMobileJS == null) {
         editorMobileJS = CodeMirror.fromTextArea(document.getElementById("ta_jsMobileContent"), {
@@ -70,51 +69,50 @@ setTimeout(function () {
             mode: "text/javascript",
             matchBrackets: true,
             viewportMargin: Infinity
-        });
+        })
     }
-   editorMobileCSS.setSize(null, contentHeight);
-   editorMobileJS.setSize(null, contentHeight);
-});
+   editorMobileCSS.setSize(null, contentHeight)
+   editorMobileJS.setSize(null, contentHeight)
+})
 
- $('.saveCustom').on('click', function () {
-    var version = $(this).attr('data-version');
-    var type = $(this).attr('data-type');
-    var content = '';
-    var editor = null;
+ $('.saveCustom').on('click', function() {
+    var version = $(this).attr('data-version')
+    var type = $(this).attr('data-type')
+    var content = ''
+    var editor = null
     if (version == 'desktop' && type == 'js') {
-        editor = editorDesktopJS;
+        editor = editorDesktopJS
     }
     if (version == 'desktop' && type == 'css') {
-        editor = editorDesktopCSS;
+        editor = editorDesktopCSS
     }
     if (version == 'mobile' && type == 'js') {
-        editor = editorMobileJS;
+        editor = editorMobileJS
     }
     if (version == 'mobile' && type == 'css') {
-        editor = editorMobileCSS;
+        editor = editorMobileCSS
     }
     if (editor != null) {
-        content = editor.getValue();
+        content = editor.getValue()
     }
 
     jeedom.config.save({
         configuration: $('#div_spanAlertMessage').getValues('.configKey')[0],
-        error: function (error) {
-            $('#div_alert').showAlert({message: error.message, level: 'danger'});
+        error: function(error) {
+            $('#div_alert').showAlert({message: error.message, level: 'danger'})
         },
-        success: function () {
+        success: function() {
           jeedom.saveCustum({
             version: version,
             type: type,
             content: content,
-            error: function (error) {
-                $('#div_alert').showAlert({message: error.message, level: 'danger'});
+            error: function(error) {
+                $('#div_alert').showAlert({message: error.message, level: 'danger'})
             },
-            success: function (data) {
-                $('#div_alert').showAlert({message: 'Sauvegarde réussie', level: 'success'});
+            success: function(data) {
+                $('#div_alert').showAlert({message: 'Sauvegarde réussie', level: 'success'})
             }
-        });
+        })
       }
-  });
-});
-
+  })
+})
