783f27411d6a2c772625402f8f6be8b68d86a944
KiboOst <KiboOst@users.noreply.github.com>
2020-06-13 00:21:28 +0200

    Remove debug

--- desktop/js/connection.js
+++ desktop/js/connection.js
@@ -56,7 +56,6 @@ $('#bt_login_validate').on('click', function() {
         $(".veen .login-btn button").removeClass('active')
         $(this).addClass('active')
       } else {
-        console.log('log ok')
         $('.veen').animateCss('bounceOut', function() {
           $('.veen').hide()
           if (isset(deepUrl) && deepUrl.includes('index.php?v=d')) {
