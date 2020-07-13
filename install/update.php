b0dd43ba93f35ccf3e091f32a1f5013539cde9ad
Loïc <loic@jeedom.com>
2020-07-13 17:05:33 +0200

    Update update.php

--- install/update.php
+++ install/update.php
@@ -217,7 +217,6 @@ try {
 				echo "Remove temporary files...";
 				rrmdir($tmp_dir);
 				try {
-					shell_exec('rm -rf ' . __DIR__ . '/../tests');
 					shell_exec('rm -rf ' . __DIR__ . '/../.travis.yml');
 					shell_exec('rm -rf ' . __DIR__ . '/../phpunit.xml.dist');
 				} catch (Exception $e) {
