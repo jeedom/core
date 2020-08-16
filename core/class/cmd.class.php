ebeb6fcd274f7c57de0c9cce9faeef9d72a8f939
Naboleo <32004297+Naboleo@users.noreply.github.com>
2020-08-16 20:16:28 +0200

    Prise en charge de la conversions d'unités si préfixées par *
    exemple		$convertions=array( '*W' =>	array(1000,'W','KW','MW'),
    							'*Hz' =>	array(100,'Hz','KHz','MHz','GHz'),
    							'*io' =>	array(1024,'io','Kio','Mio','Gio','Tio'),
    							'*o' =>	array(1000,'o','Ko','Mo','Go','To'));

--- core/class/cmd.class.php
+++ core/class/cmd.class.php
@@ -1241,6 +1241,33 @@ class cmd {
 		return $template;
 	}
 
+	public static function autoValueArray($_value, $_decimal=99, $_unit = '', $_space = False){
+		$_unit=str_replace ("\"","",$_unit);
+		$_unit=str_replace ("\'","",$_unit);
+		$convertions=array( '*W' =>		array(1000,'W','KW','MW'),
+							'*Hz' =>	array(100,'Hz','KHz','MHz','GHz'),
+							'*io' =>	array(1024,'io','Kio','Mio','Gio','Tio'),
+							'*o' =>		array(1000,'o','Ko','Mo','Go','To'));
+		if(array_key_exists($_unit,$convertions)){
+			$mod=$convertions[$_unit][0];
+			$prefix = array_slice($convertions[$_unit],1);
+			$myval = self::autoValueFormat($_value, $mod, count($prefix)-2);
+			return array(round($myval[0],$_decimal),($_space ? ' ' : '') . $prefix[$myval[1]]);
+		 }else{
+			return array(round($_value,$_decimal),$_unit);
+		 }
+	}
+
+	private static function autoValueFormat($_value, $_mod = 1000, $_maxdiv = 10){
+		$val=floatval($_value);
+		$div=0;
+		while($val > $_mod && $div <$_maxdiv){
+			$val=floatval($val/$_mod);
+			$div++;
+		}
+		return array($val,$div);
+	}
+
 	public function toHtml($_version = 'dashboard', $_options = '') {
 		$_version = jeedom::versionAlias($_version);
 		$html = '';
@@ -1317,6 +1344,17 @@ class cmd {
 					}
 					if ($this->getSubType() == 'numeric' && trim($replace['#state#']) === '') {
 						$replace['#state#'] = 0;
+					}
+					if ($this->getSubType() == 'numeric' && trim($replace['#unite#']) != ''){
+						if ($this->getConfiguration('historizeRound') !== '' && is_numeric($this->getConfiguration('historizeRound')) && $this->getConfiguration('historizeRound') >= 0) {
+							$round=$this->getConfiguration('historizeRound');
+						}else{
+							$round=99;
+						}
+						
+						$valueInfo=self::autoValueArray($replace['#state#'],$round,$replace['#unite#']);
+						$replace['#state#']=$valueInfo[0];
+						$replace['#unite#']=$valueInfo[1];
 					}
 				}
 				if (method_exists($this, 'formatValueWidget')) {
