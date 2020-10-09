eab93a75e94debbb01e02f842958bd724fd7ac3a
Loïc <loic@jeedom.com>
2020-10-09 13:43:22 +0200

    add encrypt/decrypt function

--- core/class/jeedom.class.php
+++ core/class/jeedom.class.php
@@ -23,6 +23,7 @@ class jeedom {
 	/*     * *************************Attributs****************************** */
 	
 	private static $jeedomConfiguration;
+	private static $jeedom_encryption = null;
 	
 	/*     * ***********************Methode static*************************** */
 	
@@ -1395,6 +1396,36 @@ class jeedom {
 				com_shell::execute($cmd);
 			}
 			return $return;
+		}
+		
+		/*     * ******************Encrypt/decrypt*************************** */
+		public static function getEncryptionPassword(){
+			if(self::$jeedom_encryption == null){
+				if(!file_exists(__DIR__.'/../../data/jeedom_encryption.key')){
+					file_put_contents(config::genKey(),__DIR__.'/../../data/jeedom_encryption.key');
+				}
+				self::$jeedom_encryption = file_get_contents(__DIR__.'/../../data/jeedom_encryption.key');
+			}
+			return self::$jeedom_encryption;
+		}
+		
+		public static function encrypt($plaintext, $password = null) {
+			if($password == null){
+				$password = self::getEncryptionPassword();
+			}
+			$iv = openssl_random_pseudo_bytes(16);
+			$ciphertext = openssl_encrypt($plaintext, "AES-256-CBC", hash('sha256', $password, true), OPENSSL_RAW_DATA, $iv);
+			$hmac = hash_hmac('sha256', $ciphertext.$iv, hash('sha256', $password, true), true);
+			return base64_encode($iv.$hmac.$ciphertext);
+		}
+		
+		public static function decrypt($ciphertext, $password = null) {
+			if($password == null){
+				$password = self::getEncryptionPassword();
+			}
+			$ciphertext = base64_decode($ciphertext);
+			if (!hash_equals(hash_hmac('sha256', substr($ciphertext, 48).substr($ciphertext, 0, 16), hash('sha256', $password, true), true), substr($ciphertext, 16, 32))) return null;
+			return openssl_decrypt(substr($ciphertext, 48), "AES-256-CBC", hash('sha256', $password, true), OPENSSL_RAW_DATA, substr($ciphertext, 0, 16));
 		}
 		
 		/*     * ******************hardware management*************************** */
