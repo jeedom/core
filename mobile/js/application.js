a68f5b7f81cef1cb46f80555abd6751798f8e39b
KiboOst <KiboOst@users.noreply.github.com>
2020-11-11 15:34:21 +0100

    test mobile fix

--- mobile/js/application.js
+++ mobile/js/application.js
@@ -14,6 +14,10 @@ $(document).ajaxStop(function() {
   }
 })
 
+window.addEventListener('error', function(event) {
+  $.hideLoading()
+})
+
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
     navigator.serviceWorker.register('sw.js').then(function(registration) {
@@ -29,14 +33,14 @@ var PANEL_SCROLL = 0
 var APP_MODE = false
 $(function() {
   utid = Date.now()
-  
+
   $(window).on('orientationchange', function(event) {
     //wait to get new width:
     window.setTimeout(function() {
       $('body').trigger('orientationChanged', [event.orientation])
     }, 200)
   })
-  
+
   if (getUrlVars('app_mode') == 1) {
     APP_MODE = true
     $('.backgroundforJeedom').height('100%').css('top','0')
@@ -46,9 +50,9 @@ $(function() {
     $('#bt_eraseSearchInput').css('top',0)
     $('#pagecontainer').append('<a href="#bottompanel" id="bt_bottompanel" class="ui-btn ui-btn-inline ui-btn-fab ui-btn-raised clr-primary waves-effect waves-button waves-effect waves-button" style="position:fixed;bottom:10px;right:10px;"><i class="fas fa-bars" style="position:relative;top:-3px"></i></a>')
   }
-  
+
   initApplication()
-  
+
   $('body').on({
     'click': function(event) {
       modal(false)
@@ -56,27 +60,27 @@ $(function() {
       page($(this).attr('data-page'), $(this).attr('data-title'), $(this).attr('data-option'), $(this).attr('data-plugin'))
     }
   }, '.link')
-  
+
   $('body').on('click','.objectSummaryParent',function() {
     modal(false)
     panel(false)
     page('equipment', '{{Résumé}}', $(this).data('object_id')+':'+$(this).data('summary'))
   })
-  
+
   $('body').on('click','.cmd[data-type=info],.cmd .history[data-type=info]',function(event) {
     var mainOpt = $('#bottompanel_mainoption')
     mainOpt.empty()
     mainOpt.append('<a class="link ui-bottom-sheet-link ui-btn ui-btn-inline waves-effect waves-button" data-page="history" data-title="{{Historique}}" data-option="'+$(this).data('cmd_id')+'"><i class="fas fa-chart-bar"></i> {{Historique}}</a>')
     mainOpt.append('<a class="ui-bottom-sheet-link ui-btn ui-btn-inline waves-effect waves-button" id="bt_warnmeCmd" data-cmd_id="'+$(this).data('cmd_id')+'"><i class="fas fa-bell"></i> {{Préviens moi}}</a>')
-    
+
     mainOpt.panel('open')
     $(document).scrollTop(PANEL_SCROLL)
   })
-  
+
   $('body').on('click','#bt_warnmeCmd', function() {
     page('warnme','{{Me prévenir si}}',{cmd_id : $(this).data('cmd_id')}, null, true)
   })
-  
+
   $('body').on('click','#bt_switchTheme', function() {
     switchTheme(jeedom.theme)
     $('#bottompanel_otherActionList').panel('close')
@@ -90,7 +94,7 @@ $(window).on('popstate', function(event) {
   if ($('.ui-popup-container:not(.ui-popup-hidden)').length > 0) return
   event.preventDefault()
   if (PAGE_HISTORY.length <= 1) return
-  
+
   PAGE_HISTORY.pop()
   var history_page = PAGE_HISTORY.pop()
   if (!history_page || !history_page.page) {
@@ -152,7 +156,7 @@ function switchTheme(themeConfig) {
   var theme = 'core/themes/' + themeConfig.mobile_theme_color_night + '/mobile/' + themeConfig.mobile_theme_color_night + '.css'
   var themeShadows = 'core/themes/' + themeConfig.mobile_theme_color_night + '/mobile/shadows.css'
   var themeCook = 'alternate'
-  
+
   if ($('#jQMnDColor').attr('href') == theme) {
     $('body').attr('data-theme', themeConfig.mobile_theme_color)
     theme = 'core/themes/' + themeConfig.mobile_theme_color + '/mobile/' + themeConfig.mobile_theme_color + '.css'
@@ -163,14 +167,14 @@ function switchTheme(themeConfig) {
     $('#jQMnDColor').attr('href', theme).attr('data-nochange',1)
     $('body').attr('data-theme', themeConfig.mobile_theme_color_night)
   }
-  
+
   var now = new Date()
   var time = now.getTime()
   //+8hours in milliseconds:
   var expireTime = time + (8 * 3600 * 1000)
   now.setTime(expireTime)
   document.cookie = "currentThemeMobile=" + themeCook + "; expires=" + now.toGMTString() +"; path=/"
-  
+
   if ($("#shadows_theme_css").length > 0) $('#shadows_theme_css').attr('href', themeShadows)
   setBackgroundImage(BACKGROUND_IMG)
   triggerThemechange()
@@ -198,7 +202,7 @@ function changeThemeAuto(_ambiantLight){
   if (typeof jeedom.theme == 'undefined') return
   if (typeof jeedom.theme.mobile_theme_color_night == 'undefined' || typeof jeedom.theme.mobile_theme_color == 'undefined') return
   if (jeedom.theme.mobile_theme_color == jeedom.theme.mobile_theme_color_night) return
-  
+
   if (jeedom.theme.mobile_theme_useAmbientLight == "1" && 'AmbientLightSensor' in window) {
     const sensor = new AmbientLightSensor()
     sensor.onreading = () => {
@@ -244,7 +248,7 @@ function changeThemeAuto(_ambiantLight){
 
 function checkThemechange() {
   if ($('#jQMnDColor').attr('data-nochange') == 1) return
-  
+
   var theme = jeedom.theme.mobile_theme_color_night
   var themeCss = 'core/themes/'+jeedom.theme.mobile_theme_color_night+'/mobile/' + jeedom.theme.mobile_theme_color_night + '.css'
   var currentTime = parseInt((new Date()).getHours()*100+ (new Date()).getMinutes())
@@ -361,7 +365,7 @@ function initApplication(_reinit) {
         if (typeof jeedom.theme['interface::advance::coloredIcons'] != 'undefined' && jeedom.theme['interface::advance::coloredIcons'] == '1') {
           $('body').attr('data-coloredIcons',1)
         }
-        
+
         //set theme
         var widget_shadow = true
         var useAdvance = 0
@@ -390,13 +394,13 @@ function initApplication(_reinit) {
           $('#jQMnDColor').attr('href', themeCSS).attr('data-nochange',1)
         }
         $('#jQMnDColor').attr('href', themeCSS)
-        
+
         changeThemeAuto()
         checkThemechange()
         if (widget_shadow) {
           insertHeader("stylesheet", themeShadowCSS, null, null, 'shadows_theme_css', 'text/css')
         }
-        
+
         //custom:
         if (isset(data.result.custom) && data.result.custom != null) {
           if (isset(data.result.custom.css) && data.result.custom.css) {
@@ -406,20 +410,20 @@ function initApplication(_reinit) {
             include.push('mobile/custom/custom.js')
           }
         }
-        
+
         triggerThemechange()
         for(var i in plugins){
           if (plugins[i].eventjs == 1) {
             include.push('plugins/'+plugins[i].id+'/mobile/js/event.js')
           }
         }
-        
+
         $.get("core/php/icon.inc.php", function (data) {
           $("head").append(data)
           $.include(include, function() {
             deviceInfo = getDeviceType()
             jeedom.object.summaryUpdate([{object_id:'global'}])
-            
+
             if (APP_MODE) {
               page('home', 'Accueil')
             } else {
@@ -453,7 +457,7 @@ function initApplication(_reinit) {
                 page('home', '{{Accueil}}')
               }
             }
-            
+
             if (APP_MODE) {
               $('#pagecontainer').css('padding-top',0)
             } else {
@@ -473,7 +477,7 @@ function page(_page, _title, _option, _plugin, _dialog) {
     _option = _page[2]
     _page = _page[0]
   }
-  
+
   //handle browser history:
   if (PAGE_HISTORY[PAGE_HISTORY.length - 1]) {
     PAGE_HISTORY[PAGE_HISTORY.length - 1].scroll = $(document).scrollTop()
@@ -481,7 +485,7 @@ function page(_page, _title, _option, _plugin, _dialog) {
   if (!isset(_dialog) || !_dialog) {
     PAGE_HISTORY.push({page : _page, title : _title,option : _option, plugin : _plugin})
   }
-  
+
   $('#searchContainer').hide()
   setBackgroundImage('')
   $.showLoading()
@@ -490,7 +494,7 @@ function page(_page, _title, _option, _plugin, _dialog) {
     $('#bottompanel_mainoption').panel('close')
     $('.ui-popup').popup('close')
   } catch (e) {
-    
+
   }
   if (isset(_title)) {
     if (!isset(_dialog) || !_dialog) {
@@ -533,8 +537,8 @@ function page(_page, _title, _option, _plugin, _dialog) {
   if (init(_plugin) != '') {
     page += '&m=' + _plugin
   }
-  
-  
+
+
   if (isset(_dialog) && _dialog) {
     $('#popupDialog .content').load(page, function() {
       var functionName = ''
@@ -573,7 +577,7 @@ function page(_page, _title, _option, _plugin, _dialog) {
       } else {
         functionName = 'init' + _page.charAt(0).toUpperCase() + _page.substring(1).toLowerCase()
       }
-      
+
       if ('function' == typeof (window[functionName])) {
         if (init(_option) != '') {
           window[functionName](_option)
