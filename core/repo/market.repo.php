3d55ce07e9d7e1192c9b9e08cf9921a967fa2a79
Loïc <loic@jeedom.com>
2020-11-18 13:20:02 +0100

    Update market.repo.php

--- core/repo/market.repo.php
+++ core/repo/market.repo.php
@@ -367,6 +367,7 @@ class repo_market {
 	public static function cron5() {
 		try {
 			if(config::byKey('service::monitoring::enable')){
+				sleep(rand(0,60));
 				$data = array(
 					'health' => jeedom::health(),
 					'name' => config::byKey('name'),
