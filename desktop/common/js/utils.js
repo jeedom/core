c15cf28dcf721bf9576bc234a990a470b3470208
KiboOst <KiboOst@users.noreply.github.com>
2021-01-19 09:58:21 +0100

    summary click keep synthesis context

--- desktop/common/js/utils.js
+++ desktop/common/js/utils.js
@@ -653,9 +653,14 @@ function setJeedomGlobalUI() {
     $('#md_modal').dialog({title: "{{Erreur Javascript}}"}).load('index.php?v=d&modal=js.error').dialog('open')
   })
 
-  $('body').on('click', '.objectSummaryParent',function() {
+  $('body').on('click', '.objectSummaryParent', function() {
     if ($('body').attr('data-page') == "overview" && $(this).parents('.objectSummaryglobal').length == 0) return false
-    loadPage('index.php?v=d&p=dashboard&summary='+$(this).data('summary')+'&object_id='+$(this).data('object_id'))
+
+    var url = 'index.php?v=d&p=dashboard&summary=' + $(this).data('summary') + '&object_id=' + $(this).data('object_id')
+    if (window.location.href.includes('&btover=1')) {
+      url += '&btover=1'
+    }
+    loadPage(url)
   })
 
   $('body').off('click','.jeeHelper[data-helper=cron]').on('click','.jeeHelper[data-helper=cron]',function() {
