dac028114c858a3410a509f854b9b1e7f518c3e6
Loïc <loic@jeedom.com>
2021-04-06 14:24:01 +0200

    Update update.php

--- install/update.php
+++ install/update.php
@@ -217,7 +217,7 @@ try {
 				echo "Remove useless files...\n";
 				foreach (array('3rdparty','desktop','mobile','core','docs','install','script','vendor') as $folder) {
 					echo 'Cleaning '.$folder."\n";
-					shell_exec('find /var/www/html/'.$folder.'/* -mtime +7 -type f ! -iname "custom.*" ! -iname "common.config.php" -delete');
+					shell_exec('find '.__DIR__.'/../'.$folder.'/* -mtime +7 -type f ! -iname "custom.*" ! -iname "common.config.php" -delete');
 				}
 				config::save('update::lastDateCore', date('Y-m-d H:i:s'));
 			} catch (Exception $e) {
