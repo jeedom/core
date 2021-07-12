afdf2caa014e9e9aca7afd191d6fc5dcd87714eb
Loïc <loic@jeedom.com>
2021-07-12 15:38:39 +0200

    typo

--- desktop/php/dashboard.php
+++ desktop/php/dashboard.php
@@ -1,6 +1,6 @@
 <?php
 if (!isConnect()) {
-	throw new Exception(__('401 - Accès non autorisé',__FILE__));
+	throw new Exception(__('401 - Accès non autorisé', __FILE__));
 }
 
 sendVarToJS([
@@ -32,11 +32,11 @@ if ($DisplayByObject && !is_object($object)) {
 	$object = jeeObject::rootObject();
 	if (!is_object($object)) {
 		$alert = '{{Aucun objet racine trouvé. Pour en créer un, allez dans Outils -> Objets}}.<br/>';
-		if (config::byKey('doc::base_url', 'core') != ''){
-			$alert .= '{{Documentation}} : <a href="'.config::byKey('doc::base_url', 'core').'/fr_FR/concept/" class="cursor label alert-info" target="_blank">{{Concepts}}</a>';
-			$alert .= ' | <a href="'.config::byKey('doc::base_url', 'core').'/fr_FR/premiers-pas/" class="cursor label alert-info" target="_blank">{{Premiers pas}}</a>';
+		if (config::byKey('doc::base_url', 'core') != '') {
+			$alert .= '{{Documentation}} : <a href="' . config::byKey('doc::base_url', 'core') . '/fr_FR/concept/" class="cursor label alert-info" target="_blank">{{Concepts}}</a>';
+			$alert .= ' | <a href="' . config::byKey('doc::base_url', 'core') . '/fr_FR/premiers-pas/" class="cursor label alert-info" target="_blank">{{Premiers pas}}</a>';
 		}
-		echo '<div class="alert alert-warning">'.$alert.'</div>';
+		echo '<div class="alert alert-warning">' . $alert . '</div>';
 		return;
 	}
 }
@@ -75,7 +75,7 @@ foreach ($objectTree as $_object) {
 	<div id="dashTopBar" class="input-group">
 		<div class="input-group-btn">
 			<?php
-			if (init('btover', 0) == 0) {?>
+			if (init('btover', 0) == 0) { ?>
 				<a id="bt_overview" class="btn" data-state="0"><i class="icon jeedomapp-fleche-bas-line"></i></a>
 			<?php } else { ?>
 				<a id="bt_backOverview" href="index.php?v=d&p=overview" class="btn roundedLeft" title="{{Retour à la Synthèse}}"><i class="fas fa-arrow-circle-left"></i>
@@ -96,21 +96,21 @@ foreach ($objectTree as $_object) {
 				<li class="divider"></li>
 				<?php
 				foreach ((jeedom::getConfiguration('eqLogic:category')) as $key => $value) {
-					if ($key=='default') $key = '';
-					echo '<li><a><input checked type="checkbox" class="catFilterKey" data-key="'.$value['name'].'"/>&nbsp;<i class="'.$value['icon'].'"></i> '.$value['name'].'</a></li>';
+					if ($key == 'default') $key = '';
+					echo '<li><a><input checked type="checkbox" class="catFilterKey" data-key="' . $value['name'] . '"/>&nbsp;<i class="' . $value['icon'] . '"></i> ' . $value['name'] . '</a></li>';
 				}
 				?>
-				<li><a><input checked type="checkbox" class="catFilterKey" data-key="scenario"/>&nbsp;<i class="fas fa-cogs"></i> {{Scénario}}</a></li>
+				<li><a><input checked type="checkbox" class="catFilterKey" data-key="scenario" />&nbsp;<i class="fas fa-cogs"></i> {{Scénario}}</a></li>
 			</ul>
 		</div>
 		<?php
-		if (init('category', 'all') == 'all') {?>
+		if (init('category', 'all') == 'all') { ?>
 			<div class="input-group-btn">
 				<a id="bt_editDashboardWidgetOrder" data-mode="0" class="btn enabled roundedRight" title="{{Édition du Dashboard}}"><i class="fas fa-pencil-alt"></i></a>
 			</div>
 		<?php } ?>
 	</div>
-	
+
 	<?php
 	//display previews:
 	if (init('btover', 0) != 0) { //overview
@@ -122,40 +122,40 @@ foreach ($objectTree as $_object) {
 			if ($backUrl == '') {
 				$backUrl = 'core/img/background/jeedom_abstract_04_light.jpg';
 			}
-			$div .= '<div class="objectPreview cursor shadowed fullCorner" style="background:url('.$backUrl.')" data-object_id="'.$_object->getId().'">';
+			$div .= '<div class="objectPreview cursor shadowed fullCorner" style="background:url(' . $backUrl . ')" data-object_id="' . $_object->getId() . '">';
 			$div .= '<div class="topPreview topCorner nocursor">';
-			$div .= '<span class="name cursor">' . $_object->getDisplay('icon') .' '.$_object->getName() . '</span>';
+			$div .= '<span class="name cursor">' . $_object->getDisplay('icon') . ' ' . $_object->getName() . '</span>';
 			$div .= '</div>';
 			$div .= '</div>';
-			
+
 			$divSummaries .= $summaryCache[$_object->getId()];
 		}
-		$div .= $divSummaries.'</div></div>';
+		$div .= $divSummaries . '</div></div>';
 		echo $div;
 	} else { //dashboard
 		$div = '<div id="dashOverviewPrev" class="dashboard" style="display:none;">';
 		foreach ($objectTree as $_object) {
 			$margin = 8 * $_object->getConfiguration('parentNumber');
 			$dataHref = 'index.php?v=d&p=dashboard&object_id=' . $_object->getId();
-			$div .= '<div class="cursor li_object"><a data-object_id="' . $_object->getId() . '" data-href="'.$dataHref.'">';
-			$div .= '<span style="position:relative;left:' . $margin . 'px;">'.$_object->getHumanName(true, true) . '</a></span>';
-			
+			$div .= '<div class="cursor li_object"><a data-object_id="' . $_object->getId() . '" data-href="' . $dataHref . '">';
+			$div .= '<span style="position:relative;left:' . $margin . 'px;">' . $_object->getHumanName(true, true) . '</a></span>';
+
 			$div .= $summaryCache[$_object->getId()];
 			$div .= '</div>';
 		}
 		$div .= '</div>';
 		echo $div;
 	}
-	
+
 	include_file('desktop', 'dashboard', 'js');
-	
-	function formatJeedomObjectDiv($object, $toSummary=false) {
+
+	function formatJeedomObjectDiv($object, $toSummary = false) {
 		global $summaryCache;
 		$objectId =  $object->getId();
 		$divClass = 'div_object';
 		if ($toSummary) $divClass .= ' hidden';
 		$div =  '<div class="col-md-12">';
-		$div .= '<div data-object_id="' . $objectId . '" data-father_id="' . $object->getFather_id() . '" class="'.$divClass.'">';
+		$div .= '<div data-object_id="' . $objectId . '" data-father_id="' . $object->getFather_id() . '" class="' . $divClass . '">';
 		$div .= '<legend><span class="objectDashLegend fullCorner">';
 		if (init('childs', 1) == 0) {
 			$div .= '<a href="index.php?v=d&p=dashboard&object_id=' . $objectId . '&childs=0&btover=1"><i class="icon jeedomapp-fleche-haut-line"></i></a>';
@@ -169,7 +169,7 @@ foreach ($objectTree as $_object) {
 		</span>
 		</legend>';
 		$div .= '<div class="div_displayEquipement" id="div_ob' . $objectId . '">';
-		
+
 		if ($toSummary) {
 			$div .= '<script>getObjectHtmlFromSummary(' . $objectId . ')</script>';
 		} else {
@@ -179,7 +179,7 @@ foreach ($objectTree as $_object) {
 		echo $div;
 	}
 	?>
-	<div class="row" >
+	<div class="row">
 		<?php
 		if ($DisplayByObject) {
 			//show root object and all its childs:
@@ -201,7 +201,7 @@ foreach ($objectTree as $_object) {
 				formatJeedomObjectDiv($object, true);
 			}
 		}
-		
+
 		?>
 	</div>
 </div>
