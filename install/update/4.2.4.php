e3dbc9ab3fa4e18cabe3a51bf2ae8e157fa663a6
Loïc <loic@jeedom.com>
2021-10-07 15:56:09 +0200

    typo

--- install/update/4.2.4.php
+++ install/update/4.2.4.php
@@ -1,6 +1,9 @@
+<?php
+require_once __DIR__ . '/../../core/php/core.inc.php';
+
 if (file_exists('/media/boot/multiboot/meson64_odroidc2.dtb.linux')) {
-echo 'Update Jeedom repository';
-shell_exec('sudo cp /etc/ca-certificates.conf /etc/ca-certificates.conf.orig');
-shell_exec('sudo sed -i s%mozilla/DST_Root_CA_X3.crt%!mozilla/DST_Root_CA_X3.crt%g /etc/ca-certificates.conf');
-shell_exec('sudo update-ca-certificates');
+    echo 'Update Jeedom repository';
+    shell_exec('sudo cp /etc/ca-certificates.conf /etc/ca-certificates.conf.orig');
+    shell_exec('sudo sed -i s%mozilla/DST_Root_CA_X3.crt%!mozilla/DST_Root_CA_X3.crt%g /etc/ca-certificates.conf');
+    shell_exec('sudo update-ca-certificates');
 }
