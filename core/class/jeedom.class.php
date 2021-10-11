51b2127c7c8a16cde13d80199f4d4310c7a5473e
Loïc <loic@jeedom.com>
2021-10-11 17:40:46 +0200

    add check on api ley length

--- core/class/jeedom.class.php
+++ core/class/jeedom.class.php
@@ -511,7 +511,7 @@ class jeedom {
 	}
 
 	public static function apiAccess($_apikey = '', $_plugin = 'core') {
-		if (trim($_apikey) == '') {
+		if (trim($_apikey) == '' || strlen($_apikey) < 16) {
 			return false;
 		}
 		$user = user::byHash($_apikey);
