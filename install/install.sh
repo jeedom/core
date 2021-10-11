4c56024de3de67a4ad468c230fb3959f89f1eb9f
Loïc <loic@jeedom.com>
2021-10-11 14:43:09 +0200

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -220,6 +220,8 @@ step_8_jeedom_customization() {
   rm /etc/apache2/conf-available/other-vhosts-access-log.conf > /dev/null 2>&1
   rm /etc/apache2/conf-enabled/other-vhosts-access-log.conf > /dev/null 2>&1
   
+  echo '' > /etc/apache2/mods-available/alias.conf
+  
   mkdir /etc/systemd/system/apache2.service.d
   echo "[Service]" > /etc/systemd/system/apache2.service.d/override.conf
   echo "PrivateTmp=no" >> /etc/systemd/system/apache2.service.d/override.conf
