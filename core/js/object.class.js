90faaa570d42a0ecf70823fcc4185df8edae2722
KiboOst <kiboost@free.fr>
2021-10-14 21:59:13 +0200

    ajax object getUISelectList

--- core/js/object.class.js
+++ core/js/object.class.js
@@ -260,6 +260,27 @@ jeedom.object.setOrder = function(_params) {
   $.ajax(paramsAJAX);
 }
 
+jeedom.object.getUISelectList = function(_params) {
+  var paramsRequired = [];
+  var paramsSpecifics = {};
+  try {
+    jeedom.private.checkParamsRequired(_params || {}, paramsRequired);
+  } catch (e) {
+    (_params.error || paramsSpecifics.error || jeedom.private.default_params.error)(e);
+    return;
+  }
+  var params = $.extend({}, jeedom.private.default_params, paramsSpecifics, _params || {});
+  var paramsAJAX = jeedom.private.getParamsAJAX(params);
+  paramsAJAX.url = 'core/ajax/object.ajax.php';
+  paramsAJAX.data = {
+    action: 'getUISelectList',
+    objects: _params.none
+  };
+  $.ajax(paramsAJAX);
+}
+
+
+
 jeedom.object.summaryUpdate = function(_params) {
   var objects = {};
   var sends = {};
