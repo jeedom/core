0105c34b7db39a060d05728f335aed839f909dc0
KiboOst <kiboost@free.fr>
2021-11-27 13:01:12 +0100

    custom in rescue

--- desktop/php/index.php
+++ desktop/php/index.php
@@ -552,10 +552,11 @@ function setTheme() {
 							<li><a href="index.php?v=d&p=system&rescue=1"><i class="fas fa-terminal"></i> {{Système}}</a></li>
 							<li><a href="index.php?v=d&p=database&rescue=1"><i class="fas fa-database"></i> {{Database}}</a></li>
 							<li><a href="index.php?v=d&p=editor&rescue=1"><i class="fas fa-indent"></i> {{Editeur}}</a></li>
-							<li><a href="index.php?v=d&p=custom&rescue=1"><i class="fas fa-pen-square"></i> {{Personnalisation}}</a></li>
+							<li><a href="index.php?v=d&p=editor&type=custom&rescue=1"><i class="fas fa-pen-square"></i> {{Personnalisation}}</a></li>
 							<li><a href="index.php?v=d&p=backup&rescue=1"><i class="far fa-save"></i> {{Sauvegarde}}</a></li>
 							<li><a href="index.php?v=d&p=cron&rescue=1"><i class="fas fa-tasks"></i> {{Moteur de tâches}}</a></li>
 							<li><a href="index.php?v=d&p=log&rescue=1"><i class="far fa-file"></i> {{Log}}</a></li>
+							<li><a class="disabled">Jeedom v<?php echo jeedom::version(); ?></a></li>
 						</ul>
 					</nav>
 				</div>
