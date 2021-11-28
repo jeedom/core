338b4fc0ceda96814d56afe5ac9019ec5b8ea141
KiboOst <kiboost@free.fr>
2021-11-28 10:09:35 +0100

    Minor optim

--- core/class/translate.class.php
+++ core/class/translate.class.php
@@ -22,11 +22,17 @@ require_once __DIR__ . '/../php/core.inc.php';
 /*
 //DEBUG ONLY
 require_once  'utils.class.php';
+require_once  'jeeObject.class.php';
+require_once  'eqLogic.class.php';
+require_once  'cmd.class.php';
+require_once  'scenario.class.php';
 require_once  'scenarioExpression.class.php';
 require_once  'log.class.php';
 require_once  'message.class.php';
+require_once  'cache.class.php';
+require_once  'event.class.php';
 
-//log::add('debug_translate', 'error', 'loadTranslation: '.json_encode($test));
+//log::add('debug_translate', 'error', $_content);
 */
 
 class translate {
@@ -130,11 +136,11 @@ class translate {
 				$replace['{{' . $text . '}}'] = $text;
 			}
 			if (isset($translate[$_name][$text]) && $translate[$_name][$text] != '') {
-				$replace['{{' . $text . '}}'] = ltrim($translate[$_name][$text],'##');
-			} else if (strpos($text,"'") !== false && isset($translate[$_name][str_replace("'","\'",$text)]) && $translate[$_name][str_replace("'","\'",$text)] != '') {
-				$replace["{{" . $text . "}}"] = ltrim($translate[$_name][str_replace("'","\'",$text)],'##');
+				$replace['{{' . $text . '}}'] = ltrim($translate[$_name][$text], '##');
+			} else if (strpos($text, "'") !== false && isset($translate[$_name][str_replace("'", "\'", $text)]) && $translate[$_name][str_replace("'", "\'", $text)] != '') {
+				$replace["{{" . $text . "}}"] = ltrim($translate[$_name][str_replace("'","\'",$text)], '##');
 			}
-			if (!isset($replace['{{' . $text . '}}']) && isset($translate['common']) && isset($translate['common'][$text])) {
+			if (!isset($replace['{{' . $text . '}}']) && isset($translate['common'][$text])) {
 				$replace['{{' . $text . '}}'] = $translate['common'][$text];
 			}
 			if (!isset($replace['{{' . $text . '}}'])) {
