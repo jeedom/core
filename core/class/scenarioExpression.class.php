4bec8f56ae12761f7796561818beeb8ed03b49c4
Loïc <loic@jeedom.com>
2021-11-29 16:12:09 +0100

    same

--- core/class/scenarioExpression.class.php
+++ core/class/scenarioExpression.class.php
@@ -791,14 +791,20 @@ class scenarioExpression {
 		$cmds = cmd::byGenericTypeObjectId($_genericType, str_replace(array('#object', '#'), '', $_object), 'info');
 		if (count($cmds) > 0) {
 			global $JEEDOM_INTERNAL_CONFIG;
+			$calcul = null;
 			if (isset($JEEDOM_INTERNAL_CONFIG['generic_type'][$_genericType]['calcul'])) {
+				$calcul = $JEEDOM_INTERNAL_CONFIG['generic_type'][$_genericType]['calcul'];
+			} elseif (count($JEEDOM_INTERNAL_CONFIG['generic_type'][$_genericType]['subtype']) == 1 && $JEEDOM_INTERNAL_CONFIG['generic_type'][$_genericType]['subtype'][0] == 'string') {
+				$calcul = 'text';
+			}
+			if ($calcul != null) {
 				$values = array();
 				foreach ($cmds as $cmd) {
 					if (is_numeric($cmd->execCmd())) {
 						$values[] = $cmd->execCmd();
 					}
 				}
-				return round(jeedom::calculStat($JEEDOM_INTERNAL_CONFIG['generic_type'][$_genericType]['calcul'], $values), 1);
+				return round(jeedom::calculStat($calcul, $values), 1);
 			}
 			$result = 0;
 			foreach ($cmds as $cmd) {
