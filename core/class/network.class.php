8e67a20685f9a968985d4889e5637588665c718a
Loïc <loic@jeedom.com>
2022-01-25 14:11:52 +0100

    same

--- core/class/network.class.php
+++ core/class/network.class.php
@@ -480,12 +480,15 @@ class network {
 	}
 
 	public static function cron5() {
-		if (config::byKey('dns::token') != '' && config::byKey('market::allowDNS') == 1 && !network::test('external')) {
-			sleep(30);
+		if (config::byKey('dns::token') != '' && config::byKey('market::allowDNS') == 1) {
+			sleep(rand(0, 15));
 			if (!network::test('external')) {
-				log::add('network', 'error', __('Accès externe non ok, redémarrage du dns Jeedom', __FILE__));
-				self::dns_stop();
-				self::dns_start();
+				sleep(rand(20, 60));
+				if (!network::test('external')) {
+					log::add('network', 'error', __('Accès externe non ok, redémarrage du dns Jeedom', __FILE__));
+					self::dns_stop();
+					self::dns_start();
+				}
 			}
 		}
 		if (config::byKey('network::disableMangement') == 1) {
