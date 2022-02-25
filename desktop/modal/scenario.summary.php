c901a22c665025bd0c3c0d570f54e755f2bea870
KiboOst <kiboost@free.fr>
2022-02-25 14:35:27 +0100

    modal sc summary 4.3

--- desktop/modal/scenario.summary.php
+++ desktop/modal/scenario.summary.php
@@ -50,16 +50,182 @@ if (!isConnect()) {
 </table>
 
 <script>
-var $tableScSummary = $('#table_scenarioSummary')
+if (!jeeFrontEnd.md_scenarioSummary) {
+  jeeFrontEnd.md_scenarioSummary = {
+    init: function() {
+      this.$tableScSummary = $('#table_scenarioSummary')
+      this.refreshScenarioSummary()
+      this.$tableScSummary[0].config.widgetOptions.resizable_widths = ['40px', '', '70px', '170px', '62px', '80px', '70px', '70px', '90px', '155px', '85px']
+      this.$tableScSummary.trigger('applyWidgets')
+        .trigger('resizableReset')
+        .trigger('sorton', [[[3,1]]])
+    },
+    synchModaleToPage: function() {
+      $('#table_scenarioSummary tbody .scenario').each(function() {
+        var scId = $(this).attr('data-id')
+        var scPage = $('#scenarioThumbnailDisplay div.scenarioDisplayCard[data-scenario_id="'+scId+'"]')
+        if ($(this).find('input[data-l1key="isActive"]').is(':checked')) {
+          scPage.removeClass('inactive')
+        } else {
+          scPage.addClass('inactive')
+        }
+      })
+    },
+    refreshScenarioSummary: function() {
+      $.clearDivContent('tbody_scenarioSummary')
+      self = this
+      jeedom.scenario.allOrderedByGroupObjectName({
+        nocache: true,
+        asTag: true,
+        error: function (error) {
+          $('#div_alertScenarioSummary').showAlert({message: error.message, level: 'danger'})
+        },
+        success : function(data) {
+          var table = []
+          for(var i in data){
+            var tr = '<tr class="scenario" data-id="' + init(data[i].id) + '">'
+            tr += '<td>'
+            tr += '<span class="label label-info scenarioAttr" data-l1key="id"></span>'
+            tr += '</td>'
+            tr += '<td>'
+            tr += '<span class="scenarioAttr cursor bt_summaryGotoScenario" data-l1key="humanName"></span>'
+            tr += '</td>'
+            tr += '<td>'
+            switch (data[i].state) {
+              case 'error' :
+              tr += '<span class="label label-warning">{{Erreur}}</span>'
+              break
+              case 'on' :
+              tr += '<span class="label label-info">{{Actif}}</span>'
+              break
+              case 'in progress' :
+              tr += '<span class="label label-success">{{En cours}}</span>'
+              break
+              case 'stop' :
+              tr += '<span class="label label-danger">{{Arrêté}}</span>'
+              break
+            }
+            tr += '</td>'
+            tr += '<td>'
+            tr += '<span class="scenarioAttr" data-l1key="lastLaunch"></span>'
+            tr += '</td>'
+            tr += '<td class="center">'
+            tr += '<input type="checkbox" class="scenarioAttr" data-label-text="{{Actif}}" data-l1key="isActive">'
+            tr += '</td>'
+            tr += '<td class="center">'
+            tr += '<input type="checkbox" class="scenarioAttr" data-label-text="{{Visible}}" data-l1key="isVisible">'
+            tr += '</td>'
+            tr += '<td class="center">'
+            tr += '<input type="checkbox" class="scenarioAttr" data-l1key="configuration" data-l2key="allowMultiInstance">'
+            tr += '</td>'
+            tr += '<td class="center">'
+            tr += '<input type="checkbox" class="scenarioAttr" data-l1key="configuration" data-l2key="syncmode">'
+            tr += '</td>'
+            tr += '<td>'
+            tr += '<select class="scenarioAttr form-control input-sm" data-l1key="configuration" data-l2key="logmode">'
+            tr += '<option value="default">{{Défaut}}</option>'
+            tr += '<option value="none">{{Aucun}}</option>'
+            tr += '<option value="realtime">{{Temps réel}}</option>'
+            tr += '</select>'
+            tr += '</td>'
+            tr += '<td>'
+            tr += '<input type="checkbox" class="scenarioAttr" data-l1key="configuration" data-l2key="timeline::enable"> '
+            tr += ' <input class="scenarioAttr input-sm form-control" data-l1key="configuration" data-l2key="timeline::folder" style="width:80%;display:inline-block" placeholer="{{Dossier}}"/>';
+            tr += '</td>'
+            tr += '<td>'
+            tr += '<a class="btn btn-default tooltips btn-xs bt_summaryViewLog" title="{{Voir les logs}}"><i class="far fa-file"></i></a> '
+            if (data[i].state == 'in_progress') {
+              tr += '<a class="btn btn-danger tooltips btn-xs bt_summaryStopScenario" title="{{Exécuter}}"><i class="fas fa-stop"></i></a> '
+            } else {
+              tr += '<a class="btn btn-success tooltips btn-xs bt_summaryLaunchScenario" title="{{Exécuter}}"><i class="fas fa-play"></i></a> '
+            }
+            tr += '<a class="btn btn-danger tooltips btn-xs bt_summaryRemoveScenario" title="{{Supprimer ce scénario}}"><i class="far fa-trash-alt"></i></a> '
+            tr += '</td>'
+            tr += '</tr>'
+            var result = $(tr)
+            result.setValues(data[i], '.scenarioAttr')
+            table.push(result)
+          }
+
+          self.$tableScSummary.find('tbody').append(table)
+          self.$tableScSummary.trigger("update")
+
+          jeedom.timeline.autocompleteFolder()
+
+          $('#table_scenarioSummary .bt_summaryRemoveScenario').on('click', function(event) {
+            $.hideAlert()
+            var id = $(this).closest('tr').attr('data-id')
+            var name = $(this).closest('tr').find('span[data-l1key="humanName"]').text()
+            bootbox.confirm('{{Êtes-vous sûr de vouloir supprimer le scénario}} <span style="font-weight: bold ;">' + name + '</span> ?', function(result) {
+              if (result) {
+                jeedom.scenario.remove({
+                  id: id,
+                  error: function(error) {
+                    $('#div_alertScenarioSummary').showAlert({message: error.message, level: 'danger'})
+                  },
+                  success: function() {
+                    $('#table_scenarioSummary tr[data-id="'+id+'"]').remove()
+                    $('.scenarioDisplayCard[data-scenario_id="'+id+'"]').remove()
+                  }
+                })
+              }
+            })
+            return false
+          })
+
+          $('.bt_summaryViewLog').off().on('click', function() {
+            var tr = $(this).closest('tr')
+            $('#md_modal2').dialog({title: "{{Log d'exécution du scénario}}"}).load('index.php?v=d&modal=scenario.log.execution&scenario_id=' + tr.attr('data-id')).dialog('open')
+          })
+
+          $('.bt_summaryStopScenario').off().on('click', function() {
+            var tr = $(this).closest('tr')
+            jeedom.scenario.changeState({
+              id: tr.attr('data-id'),
+              state: 'stop',
+              error: function (error) {
+                $('#div_alertScenarioSummary').showAlert({message: error.message, level: 'danger'})
+              },
+              success:function(){
+                self.refreshScenarioSummary()
+              }
+            })
+          })
+
+          $('.bt_summaryLaunchScenario').off().on('click', function() {
+            var tr = $(this).closest('tr')
+            jeedom.scenario.changeState({
+              id: tr.attr('data-id'),
+              state: 'start',
+              error: function (error) {
+                $('#div_alertScenarioSummary').showAlert({message: error.message, level: 'danger'})
+              },
+              success:function(){
+                self.refreshScenarioSummary()
+              }
+            })
+          })
+
+          $('.bt_summaryGotoScenario').off().on('click', function() {
+            var tr = $(this).closest('tr')
+            window.location.href = 'index.php?v=d&p=scenario&id='+tr.attr('data-id')
+          })
+
+          setTimeout(function() {
+            self.$tableScSummary.closest('.ui-dialog').resize()
+          }, 500)
+        }
+      })
+    },
+  }
+}
+
+
 jeedomUtils.initTableSorter()
-refreshScenarioSummary()
-$tableScSummary[0].config.widgetOptions.resizable_widths = ['40px', '', '70px', '170px', '62px', '80px', '70px', '70px', '90px', '155px', '85px']
-$tableScSummary.trigger('applyWidgets')
-  .trigger('resizableReset')
-  .trigger('sorton', [[[1,0]]])
+jeeFrontEnd.md_scenarioSummary.init()
 
 $('#bt_refreshSummaryScenario').off().on('click', function() {
-  refreshScenarioSummary()
+  jeeFrontEnd.md_scenarioSummary.refreshScenarioSummary()
 })
 
 $('#bt_saveSummaryScenario').off().on('click', function() {
@@ -70,167 +236,9 @@ $('#bt_saveSummaryScenario').off().on('click', function() {
       $('#div_alertScenarioSummary').showAlert({message: error.message, level: 'danger'})
     },
     success : function(data) {
-      synchModaleToPage()
-      refreshScenarioSummary()
+      jeeFrontEnd.md_scenarioSummary.synchModaleToPage()
+      jeeFrontEnd.md_scenarioSummary.refreshScenarioSummary()
     }
   })
 })
-
-function synchModaleToPage() {
-  $('#table_scenarioSummary tbody .scenario').each(function() {
-    var scId = $(this).attr('data-id')
-    var scPage = $('#scenarioThumbnailDisplay div.scenarioDisplayCard[data-scenario_id="'+scId+'"]')
-    if ($(this).find('input[data-l1key="isActive"]').is(':checked')) {
-      scPage.removeClass('inactive')
-    } else {
-      scPage.addClass('inactive')
-    }
-  })
-}
-
-function refreshScenarioSummary() {
-  $.clearDivContent('tbody_scenarioSummary')
-  jeedom.scenario.allOrderedByGroupObjectName({
-    nocache: true,
-    asTag: true,
-    error: function (error) {
-      $('#div_alertScenarioSummary').showAlert({message: error.message, level: 'danger'})
-    },
-    success : function(data) {
-      var table = []
-      for(var i in data){
-        var tr = '<tr class="scenario" data-id="' + init(data[i].id) + '">'
-        tr += '<td>'
-        tr += '<span class="label label-info scenarioAttr" data-l1key="id"></span>'
-        tr += '</td>'
-        tr += '<td>'
-        tr += '<span class="scenarioAttr cursor bt_summaryGotoScenario" data-l1key="humanName"></span>'
-        tr += '</td>'
-        tr += '<td>'
-        switch (data[i].state) {
-          case 'error' :
-          tr += '<span class="label label-warning">{{Erreur}}</span>'
-          break
-          case 'on' :
-          tr += '<span class="label label-info">{{Actif}}</span>'
-          break
-          case 'in progress' :
-          tr += '<span class="label label-success">{{En cours}}</span>'
-          break
-          case 'stop' :
-          tr += '<span class="label label-danger">{{Arrêté}}</span>'
-          break
-        }
-        tr += '</td>'
-        tr += '<td>'
-        tr += '<span class="scenarioAttr" data-l1key="lastLaunch"></span>'
-        tr += '</td>'
-        tr += '<td class="center">'
-        tr += '<input type="checkbox" class="scenarioAttr" data-label-text="{{Actif}}" data-l1key="isActive">'
-        tr += '</td>'
-        tr += '<td class="center">'
-        tr += '<input type="checkbox" class="scenarioAttr" data-label-text="{{Visible}}" data-l1key="isVisible">'
-        tr += '</td>'
-        tr += '<td class="center">'
-        tr += '<input type="checkbox" class="scenarioAttr" data-l1key="configuration" data-l2key="allowMultiInstance">'
-        tr += '</td>'
-        tr += '<td class="center">'
-        tr += '<input type="checkbox" class="scenarioAttr" data-l1key="configuration" data-l2key="syncmode">'
-        tr += '</td>'
-        tr += '<td>'
-        tr += '<select class="scenarioAttr form-control input-sm" data-l1key="configuration" data-l2key="logmode">'
-        tr += '<option value="default">{{Défaut}}</option>'
-        tr += '<option value="none">{{Aucun}}</option>'
-        tr += '<option value="realtime">{{Temps réel}}</option>'
-        tr += '</select>'
-        tr += '</td>'
-        tr += '<td>'
-        tr += '<input type="checkbox" class="scenarioAttr" data-l1key="configuration" data-l2key="timeline::enable"> '
-        tr += ' <input class="scenarioAttr input-sm form-control" data-l1key="configuration" data-l2key="timeline::folder" style="width:80%;display:inline-block" placeholer="{{Dossier}}"/>';
-        tr += '</td>'
-        tr += '<td>'
-        tr += '<a class="btn btn-default tooltips btn-xs bt_summaryViewLog" title="{{Voir les logs}}"><i class="far fa-file"></i></a> '
-        if (data[i].state == 'in_progress') {
-          tr += '<a class="btn btn-danger tooltips btn-xs bt_summaryStopScenario" title="{{Exécuter}}"><i class="fas fa-stop"></i></a> '
-        } else {
-          tr += '<a class="btn btn-success tooltips btn-xs bt_summaryLaunchScenario" title="{{Exécuter}}"><i class="fas fa-play"></i></a> '
-        }
-        tr += '<a class="btn btn-danger tooltips btn-xs bt_summaryRemoveScenario" title="{{Supprimer ce scénario}}"><i class="far fa-trash-alt"></i></a> '
-        tr += '</td>'
-        tr += '</tr>'
-        var result = $(tr)
-        result.setValues(data[i], '.scenarioAttr')
-        table.push(result)
-      }
-
-      $tableScSummary.find('tbody').append(table)
-      $tableScSummary.trigger("update")
-
-      jeedom.timeline.autocompleteFolder()
-
-      $('#table_scenarioSummary .bt_summaryRemoveScenario').on('click', function(event) {
-        $.hideAlert()
-        var id = $(this).closest('tr').attr('data-id')
-        var name = $(this).closest('tr').find('span[data-l1key="humanName"]').text()
-        bootbox.confirm('{{Êtes-vous sûr de vouloir supprimer le scénario}} <span style="font-weight: bold ;">' + name + '</span> ?', function(result) {
-          if (result) {
-            jeedom.scenario.remove({
-              id: id,
-              error: function(error) {
-                $('#div_alertScenarioSummary').showAlert({message: error.message, level: 'danger'})
-              },
-              success: function() {
-                $('#table_scenarioSummary tr[data-id="'+id+'"]').remove()
-                $('.scenarioDisplayCard[data-scenario_id="'+id+'"]').remove()
-              }
-            })
-          }
-        })
-        return false
-      })
-
-      $('.bt_summaryViewLog').off().on('click', function() {
-        var tr = $(this).closest('tr')
-        $('#md_modal2').dialog({title: "{{Log d'exécution du scénario}}"}).load('index.php?v=d&modal=scenario.log.execution&scenario_id=' + tr.attr('data-id')).dialog('open')
-      })
-
-      $('.bt_summaryStopScenario').off().on('click', function() {
-        var tr = $(this).closest('tr')
-        jeedom.scenario.changeState({
-          id: tr.attr('data-id'),
-          state: 'stop',
-          error: function (error) {
-            $('#div_alertScenarioSummary').showAlert({message: error.message, level: 'danger'})
-          },
-          success:function(){
-            refreshScenarioSummary()
-          }
-        })
-      })
-
-      $('.bt_summaryLaunchScenario').off().on('click', function() {
-        var tr = $(this).closest('tr')
-        jeedom.scenario.changeState({
-          id: tr.attr('data-id'),
-          state: 'start',
-          error: function (error) {
-            $('#div_alertScenarioSummary').showAlert({message: error.message, level: 'danger'})
-          },
-          success:function(){
-            refreshScenarioSummary()
-          }
-        })
-      })
-
-      $('.bt_summaryGotoScenario').off().on('click', function() {
-        var tr = $(this).closest('tr')
-        window.location.href = 'index.php?v=d&p=scenario&id='+tr.attr('data-id')
-      })
-
-      setTimeout(function() {
-        $tableScSummary.closest('.ui-dialog').resize()
-      }, 500)
-    }
-  })
-}
 </script>
