6f554e5e4df1e4aeeb4cc23a29c72ccd09204940
KwiZeR15 <kwizer15@gmail.com>
2022-09-10 11:25:49 +0200

    Update composer

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -17,10 +17,10 @@ class ComposerStaticInit5a0c414efab2298f121e5cbed8bbcba9
         'e69f7f6ee287b969198c3c9d6777bd38' => __DIR__ . '/..' . '/symfony/polyfill-intl-normalizer/bootstrap.php',
         '023d27dca8066ef29e6739335ea73bad' => __DIR__ . '/..' . '/symfony/polyfill-php70/bootstrap.php',
         '25072dd6e2470089de65ae7bf11d3109' => __DIR__ . '/..' . '/symfony/polyfill-php72/bootstrap.php',
-        'f598d06aa772fa33d905e87be6398fb1' => __DIR__ . '/..' . '/symfony/polyfill-intl-idn/bootstrap.php',
         'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
         'a0edc8309cc5e1d60e3047b5df6b7052' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/functions_include.php',
         'ebdb698ed4152ae445614b69b5e4bb6a' => __DIR__ . '/..' . '/sabre/http/lib/functions.php',
+        'f598d06aa772fa33d905e87be6398fb1' => __DIR__ . '/..' . '/symfony/polyfill-intl-idn/bootstrap.php',
         'bd9634f2d41831496de0d3dfe4c94881' => __DIR__ . '/..' . '/symfony/polyfill-php56/bootstrap.php',
         '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
     );
@@ -70,10 +70,6 @@ class ComposerStaticInit5a0c414efab2298f121e5cbed8bbcba9
             'League\\OAuth2\\Client\\' => 21,
             'League\\Flysystem\\WebDAV\\' => 24,
             'League\\Flysystem\\' => 17,
-        ),
-        'I' => 
-        array (
-            'InfluxDB\\' => 9,
         ),
         'G' => 
         array (
@@ -224,10 +220,6 @@ class ComposerStaticInit5a0c414efab2298f121e5cbed8bbcba9
         'League\\Flysystem\\' => 
         array (
             0 => __DIR__ . '/..' . '/league/flysystem/src',
-        ),
-        'InfluxDB\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/influxdb/influxdb-php/src/InfluxDB',
         ),
         'GuzzleHttp\\Psr7\\' => 
         array (
