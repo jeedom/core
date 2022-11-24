371c070836c14f4b5c688922181e2ea7a357dab9
KiboOst <kiboost@free.fr>
2022-11-24 19:10:18 +0100

    domUtils for widgets

--- core/dom/dom.utils.js
+++ core/dom/dom.utils.js
@@ -377,6 +377,50 @@ NodeList.prototype.jeeValue = function(_value) {
   }
 }
 
+
+/* DOM utils namespace
+*/
+var domUtils = {
+  __description: 'DOM related Jeedom functions.'
+}
+
+domUtils.issetWidgetOptParam = function(_def, _param) {
+  if (_def != '#' + _param + '#') return true
+  return false
+}
+
+domUtils.createWidgetSlider = function(_options) {
+  var createOptions = {
+    start: [_options.state],
+    connect: [true, false],
+    step: _options.step,
+    range: {
+      'min': _options.min,
+      'max': _options.max
+    },
+    tooltips: _options.tooltips
+  }
+
+  if (isset(_options.format) && _options.format == true) {
+    createOptions.format = {
+      from: Number,
+      to: function(value) {
+        var dec = _options.step.toString().includes('.') ? (_options.step.toString().length - 1) - _options.step.toString().indexOf('.') : 0
+        return ((Math.round(value * (100 / _options.step)) / (100 / _options.step)).toFixed(dec) + ' ' + _options.unite).trim()
+      }
+    }
+  }
+
+  if (isset(_options.vertical) && _options.vertical == true) {
+    createOptions.orientation = 'vertical'
+    createOptions.direction = 'rtl'
+  }
+
+  return noUiSlider.create(_options.sliderDiv, createOptions)
+}
+
+
+
 //Global functions
 function isElement_jQuery(_element) {
   return (_element instanceof jQuery && _element.length)
