9d0eb35f89f3d1dae40291dd314943c532a2cb16
KiboOst <kiboost@free.fr>
2022-12-05 09:23:51 +0100

    better!

--- core/js/eqLogic.class.js
+++ core/js/eqLogic.class.js
@@ -573,6 +573,7 @@ jeedom.eqLogic.drawGraphInfo = function(_cmdId) {
           fillOpacity: 0.25,
           enableMouseTracking: false,
           animation: false,
+          pointWidth: 2,
           marker: {
             enabled: false
           },
