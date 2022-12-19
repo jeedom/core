c5fc0a5cb9546bf42cefbdf254573d80518e20b1
Loïc <loic@jeedom.com>
2022-12-19 15:09:05 +0100

    begin review select cmd modal

--- desktop/modal/cmd.human.insert.php
+++ desktop/modal/cmd.human.insert.php
@@ -19,130 +19,129 @@ if (!isConnect()) {
   throw new Exception('{{401 - Accès non autorisé}}');
 }
 ?>
-<table class="table table-condensed" id="table_mod_insertCmdValue_valueEqLogicToMessage">
-  <thead>
-    <tr>
-      <th style="width: 150px;">{{Objet}}</th>
-      <th style="width: 150px;">{{Equipement}}</th>
-      <th id="thCmd" style="width: 150px;">{{Commande}}</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td class="mod_insertCmdValue_object">
-        <select class='form-control'>
-          <?php echo jeeObject::getUISelectList(); ?>
-        </select>
-      </td>
-      <td class="mod_insertCmdValue_eqLogic"></td>
-      <td class="mod_insertCmdValue_cmd"></td>
-    </tr>
-  </tbody>
-</table>
+<form class="form-inline" id="mod_insertCmdValue_valueEqLogicToMessage">
+  <div class="form-group mod_insertCmdValue_object">
+    <select class='form-control'>
+      <?php echo jeeObject::getUISelectList(); ?>
+    </select>
+  </div>
+  <div class="form-group mod_insertCmdValue_eqLogic">
+
+  </div>
+  <div class="form-group mod_insertCmdValue_cmd">
+
+  </div>
+</form>
 
 <script>
-function mod_insertCmd() {}
+  function mod_insertCmd() {}
 
-mod_insertCmd.options = {}
-mod_insertCmd.options.cmd = {}
-mod_insertCmd.options.eqLogic = {}
-mod_insertCmd.options.object = {}
+  mod_insertCmd.options = {}
+  mod_insertCmd.options.cmd = {}
+  mod_insertCmd.options.eqLogic = {}
+  mod_insertCmd.options.object = {}
 
-mod_insertCmd.setOptions = function(_options) {
-  mod_insertCmd.options = _options
-  var _selectObject = document.getElementById('table_mod_insertCmdValue_valueEqLogicToMessage').querySelector('td.mod_insertCmdValue_object select')
-  if (!isset(mod_insertCmd.options.cmd)) {
-    mod_insertCmd.options.cmd = {}
-  }
-  if (!isset(mod_insertCmd.options.eqLogic)) {
-    mod_insertCmd.options.eqLogic = {}
-  }
-  if (!isset(mod_insertCmd.options.object)) {
-    mod_insertCmd.options.object = {}
-  }
-  if (isset(mod_insertCmd.options.object.id)) {
-    document.getElementById('table_mod_insertCmdValue_valueEqLogicToMessage').querySelector('td.mod_insertCmdValue_object select').value = mod_insertCmd.options.object.id
-  }
-  if (isset(mod_insertCmd.options.cmd.type)) {
-    if (mod_insertCmd.options.cmd.type == "info") $('#thCmd').text("{{Commande info}}")
-    if (mod_insertCmd.options.cmd.type == "action") $('#thCmd').text("{{Commande action}}")
-  }
-
-  mod_insertCmd.changeObjectCmd(_selectObject, mod_insertCmd.options)
-  $('#table_mod_insertCmdValue_valueEqLogicToMessage').on({
-    'change': function(event) {
-      mod_insertCmd.changeObjectCmd(_selectObject, mod_insertCmd.options)
+  mod_insertCmd.setOptions = function(_options) {
+    mod_insertCmd.options = _options
+    var _selectObject = document.getElementById('mod_insertCmdValue_valueEqLogicToMessage').querySelector('div.mod_insertCmdValue_object select')
+    if (!isset(mod_insertCmd.options.cmd)) {
+      mod_insertCmd.options.cmd = {}
     }
-  }, 'td.mod_insertCmdValue_object select')
-}
+    if (!isset(mod_insertCmd.options.eqLogic)) {
+      mod_insertCmd.options.eqLogic = {}
+    }
+    if (!isset(mod_insertCmd.options.object)) {
+      mod_insertCmd.options.object = {}
+    }
+    if (isset(mod_insertCmd.options.object.id)) {
+      document.getElementById('mod_insertCmdValue_valueEqLogicToMessage').querySelector('div.mod_insertCmdValue_object select').value = mod_insertCmd.options.object.id
+    }
+    if (isset(mod_insertCmd.options.cmd.type)) {
+      if (mod_insertCmd.options.cmd.type == "info") $('#thCmd').text("{{Commande info}}")
+      if (mod_insertCmd.options.cmd.type == "action") $('#thCmd').text("{{Commande action}}")
+    }
 
-mod_insertCmd.getValue = function() {
-  var object_name = $('#table_mod_insertCmdValue_valueEqLogicToMessage tbody tr:first .mod_insertCmdValue_object select option:selected').html().replace(/&nbsp;/g, '')
-  var equipement_name = $('#table_mod_insertCmdValue_valueEqLogicToMessage tbody tr:first .mod_insertCmdValue_eqLogic select option:selected').html()
-  var cmd_name = $('#table_mod_insertCmdValue_valueEqLogicToMessage tbody tr:first .mod_insertCmdValue_cmd select option:selected').html()
-  if (cmd_name == undefined) {
-    return ''
+    mod_insertCmd.changeObjectCmd(_selectObject, mod_insertCmd.options)
+    $('#mod_insertCmdValue_valueEqLogicToMessage').on({
+      'change': function(event) {
+        mod_insertCmd.changeObjectCmd(_selectObject, mod_insertCmd.options)
+      }
+    }, '.mod_insertCmdValue_object select')
   }
-  return '#[' + object_name + '][' + equipement_name + '][' + cmd_name + ']#'
-}
 
-mod_insertCmd.getCmdId = function() {
-  return document.querySelector('#table_mod_insertCmdValue_valueEqLogicToMessage tbody tr').querySelector('.mod_insertCmdValue_cmd select').value
-}
+  mod_insertCmd.getValue = function() {
+    var object_name = $('#mod_insertCmdValue_valueEqLogicToMessage .mod_insertCmdValue_object select option:selected').html().replace(/&nbsp;/g, '')
+    var equipement_name = $('#mod_insertCmdValue_valueEqLogicToMessage .mod_insertCmdValue_eqLogic select option:selected').html()
+    var cmd_name = $('#mod_insertCmdValue_valueEqLogicToMessage .mod_insertCmdValue_cmd select option:selected').html()
+    if (cmd_name == undefined) {
+      return ''
+    }
+    return '#[' + object_name + '][' + equipement_name + '][' + cmd_name + ']#'
+  }
 
-mod_insertCmd.getType = function() {
-  return document.querySelector('#table_mod_insertCmdValue_valueEqLogicToMessage tbody tr').querySelector('.mod_insertCmdValue_cmd select').selectedOptions[0].getAttribute('data-type')
-}
+  mod_insertCmd.getCmdId = function() {
+    return document.querySelector('#mod_insertCmdValue_valueEqLogicToMessage').querySelector('div.mod_insertCmdValue_cmd select').value
+  }
 
-mod_insertCmd.getSubType = function() {
-  return document.querySelector('#table_mod_insertCmdValue_valueEqLogicToMessage tbody tr').querySelector('.mod_insertCmdValue_cmd select').selectedOptions[0].getAttribute('data-subType')
-}
+  mod_insertCmd.getType = function() {
+    return document.querySelector('#mod_insertCmdValue_valueEqLogicToMessage').querySelector('div.mod_insertCmdValue_cmd select').selectedOptions[0].getAttribute('data-type')
+  }
 
-mod_insertCmd.changeObjectCmd = function(_select, _options) {
-  jeedom.object.getEqLogic({
-    id: (_select.jeeValue() == '' ? -1 : _select.jeeValue()),
-    orderByName : true,
-    onlyHasCmds : _options.cmd,
-    error: function(error) {
-      jeedomUtils.showAlert({message: error.message, level: 'danger'})
-    },
-    success: function(eqLogics) {
-      _select.closest('tr').querySelector('.mod_insertCmdValue_eqLogic').empty()
-      var selectEqLogic = '<select class="form-control">'
-      for (var i in eqLogics) {
-        if (init(mod_insertCmd.options.eqLogic.eqType_name, 'all') == 'all' || eqLogics[i].eqType_name == mod_insertCmd.options.eqLogic.eqType_name) {
-          selectEqLogic += '<option value="' + eqLogics[i].id + '">' + eqLogics[i].name + '</option>'
+  mod_insertCmd.getSubType = function() {
+    return document.querySelector('#mod_insertCmdValue_valueEqLogicToMessage').querySelector('div.mod_insertCmdValue_cmd select').selectedOptions[0].getAttribute('data-subType')
+  }
+
+  mod_insertCmd.changeObjectCmd = function(_select, _options) {
+    jeedom.object.getEqLogic({
+      id: (_select.jeeValue() == '' ? -1 : _select.jeeValue()),
+      orderByName: true,
+      onlyHasCmds: _options.cmd,
+      error: function(error) {
+        jeedomUtils.showAlert({
+          message: error.message,
+          level: 'danger'
+        })
+      },
+      success: function(eqLogics) {
+        _select.closest('form').querySelector('.mod_insertCmdValue_eqLogic').empty()
+        let selectEqLogic = '<select class="form-control">'
+        for (let i in eqLogics) {
+          if (init(mod_insertCmd.options.eqLogic.eqType_name, 'all') == 'all' || eqLogics[i].eqType_name == mod_insertCmd.options.eqLogic.eqType_name) {
+            selectEqLogic += '<option value="' + eqLogics[i].id + '">' + eqLogics[i].name + '</option>'
+          }
         }
+        selectEqLogic += '</select>'
+        _select.closest('form').querySelector('.mod_insertCmdValue_eqLogic').insertAdjacentHTML('beforeend', selectEqLogic)
+        _select.closest('form').querySelector('.mod_insertCmdValue_eqLogic select').addEventListener('change', function() {
+          mod_insertCmd.changeEqLogic(this, mod_insertCmd.options)
+        })
+        if (isset(mod_insertCmd.options.object.id)) {
+          _select.closest('form').querySelector('.mod_insertCmdValue_eqLogic select').jeeValue(mod_insertCmd.options.eqLogic.id)
+        }
+        mod_insertCmd.changeEqLogic(_select.closest('form').querySelector('.mod_insertCmdValue_eqLogic select'), mod_insertCmd.options)
       }
-      selectEqLogic += '</select>'
-      _select.closest('tr').querySelector('.mod_insertCmdValue_eqLogic').insertAdjacentHTML('beforeend', selectEqLogic)
-      _select.closest('tr').querySelector('.mod_insertCmdValue_eqLogic select').addEventListener('change', function() {
-        mod_insertCmd.changeEqLogic(this, mod_insertCmd.options)
-      })
-      if (isset(mod_insertCmd.options.object.id)) {
-        _select.closest('tr').querySelector('.mod_insertCmdValue_eqLogic select').jeeValue(mod_insertCmd.options.eqLogic.id)
-      }
-      mod_insertCmd.changeEqLogic(_select.closest('tr').querySelector('.mod_insertCmdValue_eqLogic select'), mod_insertCmd.options)
-    }
-  })
-}
+    })
+  }
 
-mod_insertCmd.changeEqLogic = function(_select) {
-  jeedom.eqLogic.buildSelectCmd({
-    id: _select.jeeValue(),
-    filter: mod_insertCmd.options.cmd,
-    error: function(error) {
-      jeedomUtils.showAlert({message: error.message, level: 'danger'})
-    },
-    success: function(html) {
-      try { //No tr on object without equipment
-        _select.closest('tr').querySelector('.mod_insertCmdValue_cmd').empty()
-        var selectCmd = '<select class="form-control">'
-        selectCmd += html
-        selectCmd += '</select>'
-        _select.closest('tr').querySelector('.mod_insertCmdValue_cmd').insertAdjacentHTML('beforeend', selectCmd)
-      } catch (error) { }
-    }
-  })
-}
+  mod_insertCmd.changeEqLogic = function(_select) {
+    jeedom.eqLogic.buildSelectCmd({
+      id: _select.jeeValue(),
+      filter: mod_insertCmd.options.cmd,
+      error: function(error) {
+        jeedomUtils.showAlert({
+          message: error.message,
+          level: 'danger'
+        })
+      },
+      success: function(html) {
+        try { //No tr on object without equipment
+          _select.closest('form').querySelector('.mod_insertCmdValue_cmd').empty()
+          var selectCmd = '<select class="form-control">'
+          selectCmd += html
+          selectCmd += '</select>'
+          _select.closest('form').querySelector('.mod_insertCmdValue_cmd').insertAdjacentHTML('beforeend', selectCmd)
+        } catch (error) {}
+      }
+    })
+  }
 </script>
