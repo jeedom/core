d82cb70450fbca0f6523e7de755b7acae3e4153d
pifou25 <nicolas_gras@hotmail.com>
2023-01-14 13:52:09 +0100

    Fix Docker init variables
    Add build args, env variables, volumes and expose default port 80
    The current image jeedom:beta is not beta but the V4-stable, this commit fix the bug.

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,18 @@ FROM debian:11
 
 MAINTAINER info@jeedom.com
 
+ARG WEBSERVER_HOME=/var/www/html
+ENV WEBSERVER_HOME=${WEBSERVER_HOME}
+ARG VERSION=V4-stable
+ENV VERSION=${VERSION}
+
+WORKDIR ${WEBSERVER_HOME}
+VOLUME ${WEBSERVER_HOME}
+VOLUME /var/lib/mysql
+
 COPY install/install.sh /tmp/
-RUN sh /tmp/install.sh
+RUN sh /tmp/install.sh -v ${VERSION} -w ${WEBSERVER_HOME}
 
-
+EXPOSE 80
 COPY install/OS_specific/Docker/init.sh /root/
 CMD ["sh", "/root/init.sh"]
--- install/OS_specific/Docker/init.sh
+++ install/OS_specific/Docker/init.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 echo 'Start init'
 
-if [ -f /var/www/html/core/config/common.config.php ]; then
+# $WEBSERVER_HOME and $VERSION env variables comes from Dockerfile
+
+if [ -f ${WEBSERVER_HOME}/core/config/common.config.php ]; then
 	echo 'Jeedom is already install'
 	JEEDOM_INSTALL=1
 else
 	echo 'Start jeedom installation'
 	JEEDOM_INSTALL=0
 	rm -rf /root/install.sh
-	wget https://raw.githubusercontent.com/jeedom/core/alpha/install/install.sh -O /root/install.sh
+	wget https://raw.githubusercontent.com/jeedom/core/${VERSION}/install/install.sh -O /root/install.sh
 	chmod +x /root/install.sh
-	/root/install.sh -s 6
+	/root/install.sh -s 6 -v ${VERSION} -w ${WEBSERVER_HOME}
 	if [ $(which mysqld | wc -l) -ne 0 ]; then
 		chown -R mysql:mysql /var/lib/mysql
 		mysql_install_db --user=mysql --basedir=/usr/ --ldata=/var/lib/mysql/
@@ -21,14 +23,14 @@ else
 		echo  "DROP DATABASE IF EXISTS jeedom;" | mysql
 		echo  "CREATE DATABASE jeedom;" | mysql
 		echo  "GRANT ALL PRIVILEGES ON jeedom.* TO 'jeedom'@'localhost';" | mysql
-		cp /var/www/html/core/config/common.config.sample.php /var/www/html/core/config/common.config.php
-		sed -i "s/#PASSWORD#/${MYSQL_JEEDOM_PASSWD}/g" /var/www/html/core/config/common.config.php
-		sed -i "s/#DBNAME#/jeedom/g" /var/www/html/core/config/common.config.php
-		sed -i "s/#USERNAME#/jeedom/g" /var/www/html/core/config/common.config.php
-		sed -i "s/#PORT#/3306/g" /var/www/html/core/config/common.config.php
-		sed -i "s/#HOST#/localhost/g" /var/www/html/core/config/common.config.php
-		/root/install.sh -s 10mariadb
-		/root/install.sh -s 11
+		cp ${WEBSERVER_HOME}/core/config/common.config.sample.php ${WEBSERVER_HOME}/core/config/common.config.php
+		sed -i "s/#PASSWORD#/${MYSQL_JEEDOM_PASSWD}/g" ${WEBSERVER_HOME}/core/config/common.config.php
+		sed -i "s/#DBNAME#/jeedom/g" ${WEBSERVER_HOME}/core/config/common.config.php
+		sed -i "s/#USERNAME#/jeedom/g" ${WEBSERVER_HOME}/core/config/common.config.php
+		sed -i "s/#PORT#/3306/g" ${WEBSERVER_HOME}/core/config/common.config.php
+		sed -i "s/#HOST#/localhost/g" ${WEBSERVER_HOME}/core/config/common.config.php
+		/root/install.sh -s 10 -v ${VERSION} -w ${WEBSERVER_HOME}
+		/root/install.sh -s 11 -v ${VERSION} -w ${WEBSERVER_HOME}
 	fi
 fi
 
@@ -48,19 +50,19 @@ fi
 if [ ${JEEDOM_INSTALL} -eq 0 ] && [ ! -z "${RESTOREBACKUP}" ] && [ "${RESTOREBACKUP}" != 'NO' ]; then
 	echo 'Need restore backup '${RESTOREBACKUP}
 	wget ${RESTOREBACKUP} -O /tmp/backup.tar.gz
-	php /var/www/html/install/restore.php backup=/tmp/backup.tar.gz
+	php ${WEBSERVER_HOME}/install/restore.php backup=/tmp/backup.tar.gz
 	rm /tmp/backup.tar.gz
 	if [ ! -z "${UPDATEJEEDOM}" ] && [ "${UPDATEJEEDOM}" != 'NO' ]; then
 		echo 'Need update jeedom'
-		php /var/www/html/install/update.php
+		php ${WEBSERVER_HOME}/install/update.php
 	fi
 fi
 
 echo 'All init complete'
 chmod 777 /dev/tty*
 chmod 777 -R /tmp
-chmod 755 -R /var/www/html
-chown -R www-data:www-data /var/www/html
+chmod 755 -R ${WEBSERVER_HOME}
+chown -R www-data:www-data ${WEBSERVER_HOME}
 
 echo 'Start apache2'
 service apache2 start
