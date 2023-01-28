e386ab803c13d4ae51f9053bbf6250ad79ca91a0
KiboOst <kiboost@free.fr>
2023-01-28 11:28:55 +0100

    :recycle: clean some $

--- core/js/jeedom.class.js
+++ core/js/jeedom.class.js
@@ -86,7 +86,7 @@ jeedom.changes = function() {
           if (['scenario::update', 'ui::update', 'jeedom::gotoplan', 'jeedom::alert', 'jeedom::alertPopup', 'jeedom::coloredIcons', 'message::refreshMessageNumber', 'update::refreshUpdateNumber', 'notify', 'checkThemechange', 'changeTheme'].includes(data.result[i].name)) {
             document.body.dispatchEvent(new CustomEvent(data.result[i].name, { detail: data.result[i].option }))
           } else {
-            $('body').trigger(data.result[i].name, data.result[i].option)
+            if (typeof jQuery === 'function') $('body').trigger(data.result[i].name, data.result[i].option)
           }
         } else {
           document.body.dispatchEvent(new CustomEvent(data.result[i].name))
@@ -179,7 +179,7 @@ jeedom.init = function() {
 
   document.body.addEventListener('ui::update', function(_event) {
     if (isset(_event.detail.page) && _event.detail.page != '') {
-      if ($.mobile) {
+      if ( typeof jQuery === 'function' && $.mobile) {
         if (!PAGE_HISTORY || PAGE_HISTORY.length == 0 || !PAGE_HISTORY[PAGE_HISTORY.length - 1].page || PAGE_HISTORY[PAGE_HISTORY.length - 1].page != _event.detail.page) {
           return
         }
@@ -205,7 +205,7 @@ jeedom.init = function() {
   document.body.addEventListener('jeedom::alert', function(_event) {
     if (!isset(_event.detail.message) || _event.detail.message.trim() == '') {
       if (isset(_event.detail.page) && _event.detail.page != '') {
-        if (getUrlVars('p') == _event.detail.page || ($.mobile && isset(CURRENT_PAGE) && CURRENT_PAGE == _event.detail.page)) {
+        if (getUrlVars('p') == _event.detail.page || (typeof jQuery === 'function' && $.mobile && isset(CURRENT_PAGE) && CURRENT_PAGE == _event.detail.page)) {
           jeedomUtils.hideAlert()
         }
       } else {
@@ -220,7 +220,7 @@ jeedom.init = function() {
         if (_event.detail.ttl) {
           options.ttl = _event.detail.ttl
         }
-        if (getUrlVars('p') == _event.detail.page || ($.mobile && isset(CURRENT_PAGE) && CURRENT_PAGE == _event.detail.page)) {
+        if (getUrlVars('p') == _event.detail.page || (typeof jQuery === 'function' && $.mobile && isset(CURRENT_PAGE) && CURRENT_PAGE == _event.detail.page)) {
           jeedomUtils.showAlert(options)
         }
       } else {
@@ -250,7 +250,7 @@ jeedom.init = function() {
   })
 
   document.body.addEventListener('checkThemechange', function(_event) {
-    if ($.mobile) {
+    if (typeof jQuery === 'function' && $.mobile) {
       document.getElementById('jQMnDColor').setAttribute('data-nochange', 0)
     } else {
       document.getElementById('jeedom_theme_currentcss').setAttribute('data-nochange', 0)
