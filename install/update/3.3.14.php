0459af6d7c2166964279ab97e07aed83ef669798
KiboOst <kiboost@free.fr>
2023-02-05 16:10:32 +0100

    :fire: remove update scripts v3

--- install/update/3.3.14.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/../../core/php/core.inc.php';
-if (exec('which at | wc -l') == 0) {
-  exec('sudo apt-get install -y at');
-}
--- install/update/3.3.2.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE eqLogic ADD tags VARCHAR(255) NULL;
-CREATE INDEX `tags` ON eqLogic (`tags` ASC);
