43be05ebfdbfa2a5feaf464117d2f0e3e1593cba
Pifou25 <nicolas_gras@hotmail.com>
2023-02-28 23:21:34 +0100

    override the Dockerhub repository name

--- install/OS_specific/Docker/init_workflow.sh
+++ install/OS_specific/Docker/init_workflow.sh
@@ -3,15 +3,17 @@
 # this script is used by CI/CD Github Workflow
 JEEDOM_VERSION=$(cat core/config/version)
 JEEDOM_SHORT_VERSION="$(echo "$JEEDOM_VERSION" | awk -F. '{print $1"."$2}')"
+# Docker hub repository may be overriden
+REPO=${DOCKER_HUB_REPO:-"jeedom"}
 
 if [[ "${GITHUB_REF_NAME}" == "V4-Stable" ]]; then
-  JEEDOM_TAGS="${DOCKER_HUB_USERNAME}/jeedom:latest,${DOCKER_HUB_USERNAME}/jeedom:$JEEDOM_SHORT_VERSION";
+  JEEDOM_TAGS="${REPO}/jeedom:latest,${REPO}/jeedom:$JEEDOM_SHORT_VERSION";
   GITHUB_BRANCH=${GITHUB_REF_NAME};
 elif [[ "${GITHUB_REF_NAME}" == "beta" ]]; then
-  JEEDOM_TAGS="${DOCKER_HUB_USERNAME}/jeedom:beta,${DOCKER_HUB_USERNAME}/jeedom:$JEEDOM_SHORT_VERSION";
+  JEEDOM_TAGS="${REPO}/jeedom:beta,${REPO}/jeedom:$JEEDOM_SHORT_VERSION";
   GITHUB_BRANCH=${GITHUB_REF_NAME};
 else
-  JEEDOM_TAGS="${DOCKER_HUB_USERNAME}/jeedom:$JEEDOM_SHORT_VERSION";
+  JEEDOM_TAGS="${REPO}/jeedom:$JEEDOM_SHORT_VERSION";
   GITHUB_BRANCH=alpha;
 fi
 
@@ -21,6 +23,7 @@ echo "JEEDOM_VERSION=$JEEDOM_VERSION" >> $GITHUB_ENV
 echo "JEEDOM_SHORT_VERSION=$JEEDOM_SHORT_VERSION" >> $GITHUB_ENV
 echo "JEEDOM_TAGS=$JEEDOM_TAGS" >> $GITHUB_ENV
 echo "GITHUB_BRANCH=$GITHUB_BRANCH" >> $GITHUB_ENV
+echo "JEEDOM_REPO=$REPO" >> $GITHUB_ENV
 
 # GITHUB_STEP_SUMMARY is the workflow summary
 # https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#adding-a-job-summary
