9a9ac8ce437511e36ce8e9d0b62ca024abe341d7
Sekiro-kost <julien@jeedom.com>
2023-04-17 15:52:01 +0200

    new message::removebyId method

--- core/api/jeeApi.php
+++ core/api/jeeApi.php
@@ -1014,6 +1014,20 @@ try {
 		message::removeAll();
 		$jsonrpc->makeSuccess('ok');
 	}
+	
+	if ($jsonrpc->getMethod() == 'message::removebyId') {
+       		 if (is_object($_USER_GLOBAL) && !in_array($_USER_GLOBAL->getProfils(), array('admin'))) {
+          		  throw new Exception(__('Vous n\'avez pas les droits de faire cette action', __FILE__), -32701);
+                 }
+                  $message = message::byId($params['messageId']);
+                  if(is_object($message)){
+                       $message->remove();
+		       $jsonrpc->makeSuccess('ok');
+                   }else{
+			 throw new Exception(__('Impossible de trouver le message :', __FILE__) . ' ' . secureXSS($params['messageId']));
+		  }
+        
+    	}
 
 	if ($jsonrpc->getMethod() == 'message::all') {
 		if (is_object($_USER_GLOBAL) && !in_array($_USER_GLOBAL->getProfils(), array('admin', 'user'))) {
