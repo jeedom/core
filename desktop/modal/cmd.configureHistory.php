5037b6c7283b48d6797ab5c7245c4e48db3a8866
Loïc <loic@jeedom.com>
2023-06-21 10:52:03 +0200

    bugfix

--- desktop/modal/cmd.configureHistory.php
+++ desktop/modal/cmd.configureHistory.php
@@ -222,9 +222,7 @@ if (!jeeFrontEnd.md_cmdConfigureHistory) {
         },
         success: function(data) {
           if (jeeFrontEnd.md_cmdConfigureHistory.vDataTable) {
-            jeeFrontEnd.md_cmdConfigureHistory.vDataTable.refresh()
             jeeFrontEnd.md_cmdConfigureHistory.resetChanges()
-            jeeFrontEnd.md_cmdConfigureHistory.init();
           }
           jeedomUtils.showAlert({
             attachTo: jeeDialog.get('#md_cmdConfigureHistory', 'dialog'),
