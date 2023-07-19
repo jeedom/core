e3ee57aada2985510ce23875bddaf7450f619172
Loïc <loic@jeedom.com>
2023-07-19 17:21:19 +0200

    Update network.class.php

--- core/class/network.class.php
+++ core/class/network.class.php
@@ -339,7 +339,9 @@ class network {
 			$openvpn->setLogicalId('dnsjeedom');
 			$openvpn->setEqType_name('openvpn');
 			$openvpn->setConfiguration('dev', 'tun');
-			$openvpn->setConfiguration('proto', 'udp');
+			if(strpos(config::byKey('dns::protocol'),config::byKey('dns::preferProtocol')) !== false){
+				$openvpn->setConfiguration('proto', config::byKey('dns::preferProtocol'));
+			}
 			if (config::byKey('dns::vpnurl') != '') {
 				$openvpn->setConfiguration('remote_host', config::byKey('dns::vpnurl'));
 			} else {
