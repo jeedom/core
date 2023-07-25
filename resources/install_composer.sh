d0abe57b0b21f36d19b04cf01c172db0076113ca
Loïc <loic@jeedom.com>
2023-07-25 10:04:16 +0200

    Create install_composer.sh

---
+++ resources/install_composer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Begin installation of composer"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === 'e21205b207c3ff031906575712edab6f13eb0b361f2085f1f1237b7126d785e826a450292b6cfd1d64d92e6563bbde02') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+sudo rm /usr/local/bin/composer
+sudo mv composer.phar /usr/local/bin/composer
+echo "End installation of composer"
