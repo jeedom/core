3dd6e22026138883e72af78879e82f7327d61c84
David <79108364+Phpvarious@users.noreply.github.com>
2023-10-03 21:34:58 +0200

    Update jeedom.class.js

--- core/js/jeedom.class.js
+++ core/js/jeedom.class.js
@@ -449,6 +449,24 @@ jeedom.getConfiguration = function(_params) {
   domUtils.ajax(paramsAJAX)
 }
 
+jeedom.getInfoApplication = function(_params) {
+  var paramsRequired = []
+  var paramsSpecifics = {}
+  try {
+    jeedom.private.checkParamsRequired(_params || {}, paramsRequired)
+  } catch (e) {
+    (_params.error || paramsSpecifics.error || jeedom.private.default_params.error)(e)
+    return
+  }
+  var params = domUtils.extend({}, jeedom.private.default_params, paramsSpecifics, _params || {})
+  var paramsAJAX = jeedom.private.getParamsAJAX(params)
+  paramsAJAX.url = 'core/ajax/jeedom.ajax.php'
+  paramsAJAX.data = {
+    action: 'getInfoApplication',
+  }
+  domUtils.ajax(paramsAJAX)
+}
+
 jeedom.haltSystem = function(_params) {
   var paramsRequired = []
   var paramsSpecifics = {}
