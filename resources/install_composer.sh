7714a7958655483ed5ed79de96e456f4738f0734
Loïc <1536036+zoic21@users.noreply.github.com>
2023-12-28 10:14:53 +0100

    Update install_composer.sh

--- resources/install_composer.sh
+++ resources/install_composer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Begin installation of composer"
+wget https://getcomposer.org/installer -O composer-setup.php
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === 'e21205b207c3ff031906575712edab6f13eb0b361f2085f1f1237b7126d785e826a450292b6cfd1d64d92e6563bbde02') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
 sudo rm /usr/local/bin/composer
