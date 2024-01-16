28f50efeac93106d168534ab1ea83b51875762c6
Salvialf <jeuxalf@live.fr>
2024-01-16 08:51:14 +0100

    remove duplicated function

--- desktop/js/update.js
+++ desktop/js/update.js
@@ -275,7 +275,7 @@ if (!jeeFrontEnd.update) {
           }
         }
       } else {
-        tr += '<a class="btn btn-xs bt_changelogCore" target="_blank"><i class="fas fa-book"></i><span class="hidden-1280"> {{Changelog}}</span></a> '
+        tr += '<a class="btn btn-xs" id="bt_changelogCore"><i class="fas fa-book"></i><span class="hidden-1280"> {{Changelog}}</span></a> '
       }
       if (_update.type != 'core') {
         if (_update.status == 'UPDATE') {
@@ -556,19 +556,7 @@ if (!jeeFrontEnd.update) {
           })
 
           contentEl.querySelector('.bt_changelogCore').addEventListener('click', function(event) {
-            jeedom.getDocumentationUrl({
-              page: 'changelog',
-              theme: document.body.getAttribute('data-theme'),
-              error: function(error) {
-                jeedomUtils.showAlert({
-                  message: error.message,
-                  level: 'danger'
-                })
-              },
-              success: function(url) {
-                window.open(url, '_blank')
-              }
-            })
+            document.getElementById('bt_changelogCore').triggerEvent('click')
           })
         },
         onShown: function() {
@@ -637,7 +625,7 @@ document.getElementById('div_pageContainer').addEventListener('click', function(
     return
   }
 
-  if (_target = event.target.closest('.bt_changelogCore')) {
+  if (_target = event.target.closest('#bt_changelogCore')) {
     jeedom.getDocumentationUrl({
       page: 'changelog',
       theme: document.body.getAttribute('data-theme'),
