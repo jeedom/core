90884928e5a41a4f0c971cbc7efb835bbd12a7de
Loïc <1536036+zoic21@users.noreply.github.com>
2024-02-19 20:35:36 +0100

    Update system.class.php

--- core/class/system.class.php
+++ core/class/system.class.php
@@ -586,8 +586,16 @@ class system {
 					case 'apt':
 						if ($_foreground) {
 							echo shell_exec(self::getCmdSudo() . " apt update 2>&1");
+							echo shell_exec(self::getCmdSudo() . " rm /var/lib/apt/lists/lock 2>&1");
+							echo shell_exec(self::getCmdSudo() . " rm /var/cache/apt/archives/lock 2>&1");
+							echo shell_exec(self::getCmdSudo() . " rm /var/lib/dpkg/lock* 2>&1");
+							echo shell_exec(self::getCmdSudo() . " sudo dpkg --configure -a --force-confdef 2>&1");
 						} else {
 							$cmd .= self::getCmdSudo() . " apt update\n";
+							$cmd .= self::getCmdSudo() . " rm /var/lib/apt/lists/lock\n";
+							$cmd .= self::getCmdSudo() . " rm /var/cache/apt/archives/lock\n";
+							$cmd .= self::getCmdSudo() . " rm /var/lib/dpkg/lock*\n";
+							$cmd .= self::getCmdSudo() . " sudo dpkg --configure -a --force-confdef\n";
 							$count++;
 							$cmd .= 'echo ' . $count . ' > ' . $progress_file . "\n";
 						}
