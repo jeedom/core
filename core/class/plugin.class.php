a9c8d699fcaeab9072eb19d3360bdaf5e291bcfb
sarakha63 <sarakha_ludovic@yahoo.fr>
2024-02-23 11:48:30 +0100

    use additionnalDepCheck if exists

--- core/class/plugin.class.php
+++ core/class/plugin.class.php
@@ -692,6 +692,12 @@ class plugin {
 			}
 			$return['last_launch'] = config::byKey('lastDependancyInstallTime', $this->getId(), __('Inconnue', __FILE__));
 			$return['auto'] = config::byKey('dependancyAutoMode', $this->getId(), 1);
+			if (!method_exists($plugin_id, 'additionnalDependancyCheck')){
+				$additionnal = $plugin_id::additionnalDependancyCheck();
+				if (isset($additionnal['state'])) {
+					$return['state'] = $additionnal['state'];
+				}
+			}
 			if ($return['state'] == 'ok') {
 				cache::set('dependancy' . $this->getID(), $return);
 			}
