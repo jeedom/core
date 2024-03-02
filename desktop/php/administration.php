e88903b01b08b88f4c826096e9810a05dd34f89d
Salvialf <jeuxalf@live.fr>
2024-03-02 20:47:28 +0100

    save space

--- desktop/php/administration.php
+++ desktop/php/administration.php
@@ -119,8 +119,8 @@ $productName = config::byKey('product_name');
 								<sup><i class="fas fa-question-circle" tooltip="{{Identifie votre}} <?php echo $productName; ?> {{sur le market}}"></i></sup>
 							</label>
 							<div class="col-md-6 col-xs-8">
-								<span class="label label-info"><?php echo jeedom::getHardwareKey() ?></span>
-								<a class="btn btn-sm btn-default pull-right" id="bt_resetHwKey" tooltip="{{Remise à zéro}}"><i class=" fas fa-undo-alt"></i></a>
+								<span class="label label-info" style="width:calc(100% - 40px);"><?php echo jeedom::getHardwareKey() ?></span>
+								<a class=" btn btn-sm btn-default pull-right" id="bt_resetHwKey" tooltip="{{Remise à zéro}}"><i class=" fas fa-undo-alt"></i></a>
 							</div>
 						</div>
 						<hr class="hrPrimary">
@@ -355,23 +355,21 @@ $productName = config::byKey('product_name');
 							<div class="col-md-6 col-xs-8">
 								<div class="input-group">
 									<span class="input-group-addon roundedLeft">{{Principal}}</span>
-									<select class="form-control configKey roundedRight" data-l1key="jeedom_theme_main" data-reload="1">
+									<select class="form-control configKey" data-l1key="jeedom_theme_main" data-reload="1">
 										<?php
 										foreach ((ls(__DIR__ . '/../../core/themes')) as $dir) {
 											if (is_dir(__DIR__ . '/../../core/themes/' . $dir . '/desktop')) {
-												echo '<option value="' . trim($dir, '/') . '">' . ucfirst(str_replace('_', ' ', trim($dir, '/'))) . '</option>';
+												echo '<option value="' . trim($dir, '/') . '">' . ucfirst(str_replace('core2019_', ' ', trim($dir, '/'))) . '</option>';
 											}
 										}
 										?>
 									</select>
-								</div>
-								<div class="input-group">
-									<span class="input-group-addon roundedLeft">{{Alternatif}}</span>
+									<span class="input-group-addon">{{Alternatif}}</span>
 									<select class="form-control configKey roundedRight" data-l1key="jeedom_theme_alternate" data-reload="1">
 										<?php
 										foreach ((ls(__DIR__ . '/../../core/themes')) as $dir) {
 											if (is_dir(__DIR__ . '/../../core/themes/' . $dir . '/desktop')) {
-												echo '<option value="' . trim($dir, '/') . '">' . ucfirst(str_replace('_', ' ', trim($dir, '/'))) . '</option>';
+												echo '<option value="' . trim($dir, '/') . '">' . ucfirst(str_replace('core2019_', ' ', trim($dir, '/'))) . '</option>';
 											}
 										}
 										?>
@@ -384,23 +382,21 @@ $productName = config::byKey('product_name');
 							<div class="col-md-6 col-xs-8">
 								<div class="input-group">
 									<span class="input-group-addon roundedLeft">{{Principal}}</span>
-									<select class="form-control configKey roundedRight" data-l1key="mobile_theme_color">
+									<select class="form-control configKey" data-l1key="mobile_theme_color">
 										<?php
 										foreach ((ls(__DIR__ . '/../../core/themes')) as $dir) {
 											if (is_dir(__DIR__ . '/../../core/themes/' . $dir . '/mobile')) {
-												echo '<option value="' . trim($dir, '/') . '">' . ucfirst(str_replace('_', ' ', trim($dir, '/'))) . '</option>';
+												echo '<option value="' . trim($dir, '/') . '">' . ucfirst(str_replace('core2019_', ' ', trim($dir, '/'))) . '</option>';
 											}
 										}
 										?>
 									</select>
-								</div>
-								<div class="input-group">
-									<span class="input-group-addon roundedLeft">{{Alternatif}}</span>
+									<span class="input-group-addon">{{Alternatif}}</span>
 									<select class="form-control configKey roundedRight" data-l1key="mobile_theme_color_night">
 										<?php
 										foreach ((ls(__DIR__ . '/../../core/themes')) as $dir) {
 											if (is_dir(__DIR__ . '/../../core/themes/' . $dir . '/mobile')) {
-												echo '<option value="' . trim($dir, '/') . '">' . ucfirst(str_replace('_', ' ', trim($dir, '/'))) . '</option>';
+												echo '<option value="' . trim($dir, '/') . '">' . ucfirst(str_replace('core2019_', ' ', trim($dir, '/'))) . '</option>';
 											}
 										}
 										?>
@@ -462,24 +458,20 @@ $productName = config::byKey('product_name');
 						</div>
 						<hr class="hrPrimary">
 						<div class="form-group">
-							<label class="col-xs-4 control-label">{{Notifications}}</label>
+							<label class="col-xs-4 control-label">{{Notifications}}
+								<sup><i class="fas fa-question-circle" tooltip="{{Position et durée d'affichage des notifications en secondes (0 = infini)}}"></i></sup>
+							</label>
 							<div class="col-md-6 col-xs-8">
 								<div class="input-group">
-									<span class="input-group-addon roundedLeft">{{Position}}
-										<sup><i class="fas fa-question-circle" tooltip="{{Position d'affichage des notifications}}"></i></sup>
-									</span>
-									<select class="form-control configKey roundedRight" data-l1key="interface::toast::position" data-reload="1">
+									<span class="input-group-addon roundedLeft">{{Position}}</span>
+									<select class="form-control configKey" data-l1key="interface::toast::position" data-reload="1">
 										<option value="toast-top-left">{{En haut à gauche}}</option>
 										<option value="toast-top-center">{{En haut au centre}}</option>
 										<option value="toast-top-right">{{En haut à droite}}</option>
 										<option value="toast-bottom-right">{{En bas à droite}}</option>
 										<option value="toast-bottom-left">{{En bas à gauche}}</option>
 									</select>
-								</div>
-								<div class="input-group">
-									<span class="input-group-addon roundedLeft">{{Durée}}
-										<sup><i class="fas fa-question-circle" tooltip="{{Durée d'affichage des notifications en secondes (0 = infini)}}"></i></sup>
-									</span>
+									<span class="input-group-addon">{{Durée}}</span>
 									<input type="number" min="0" max="30" step="1" class="configKey form-control ispin roundedRight" data-l1key="interface::toast::duration" data-reload="1">
 								</div>
 							</div>
@@ -573,19 +565,19 @@ $productName = config::byKey('product_name');
 							<div class="col-md-6 col-xs-8">
 								<div class="input-group">
 									<span class="input-group-addon roundedLeft">{{Actions humaines}}
-										<sup><i class="fas fa-question-circle" tooltip="{{Poids des actions humaines dans la réorganisation des tuiles}}"></i></sup>
+										<sup><i class="fas fa-question-circle" tooltip="{{Poids des actions humaines dans la réorganisation automatique des tuiles}}"></i></sup>
 									</span>
 									<input type="number" min="0" step="1" max="10" class="configKey form-control ispin roundedRight" data-l1key="autoreorder::weight_human_action">
 								</div>
 								<div class="input-group">
 									<span class="input-group-addon roundedLeft">{{Actions automatiques}}
-										<sup><i class="fas fa-question-circle" tooltip="{{Poids des actions automatiques dans la réorganisation des tuiles}}"></i></sup>
+										<sup><i class="fas fa-question-circle" tooltip="{{Poids des actions automatiques dans la réorganisation automatique des tuiles}}"></i></sup>
 									</span>
 									<input type="number" min="0" step="1" max="10" class="configKey form-control ispin roundedRight" data-l1key="autoreorder::weight_automation_action">
 								</div>
 								<div class="input-group">
 									<span class="input-group-addon roundedLeft">{{Consultation historique}}
-										<sup><i class="fas fa-question-circle" tooltip="{{Poids des actions de consultation de l'historique dans la réorganisation des tuiles}}"></i></sup>
+										<sup><i class="fas fa-question-circle" tooltip="{{Poids des actions de consultation de l'historique dans la réorganisation automatique des tuiles}}"></i></sup>
 									</span>
 									<input type="number" min="0" step="1" max="10" class="configKey form-control ispin roundedRight" data-l1key="autoreorder::weight_history">
 								</div>
@@ -648,10 +640,8 @@ $productName = config::byKey('product_name');
 							<div class="col-md-6 col-xs-8">
 								<div class="input-group">
 									<span class="input-group-addon roundedLeft">{{Thème Light}}</span>
-									<input type="number" min="0.1" max="1" step="0.05" class="configKey form-control ispin roundedRight" data-l1key="interface::background::opacitylight" data-reload="1">
-								</div>
-								<div class="input-group">
-									<span class="input-group-addon roundedLeft">{{Thème Dark}}</span>
+									<input type="number" min="0.1" max="1" step="0.05" class="configKey form-control ispin" data-l1key="interface::background::opacitylight" data-reload="1">
+									<span class="input-group-addon">{{Thème Dark}}</span>
 									<input type="number" min="0.1" max="1" step="0.05" class="configKey form-control ispin roundedRight" data-l1key="interface::background::opacitydark" data-reload="1">
 								</div>
 							</div>
