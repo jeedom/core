518c5709b2959ce84cc5b9a88e7b0fb864805078
Loïc <1536036+zoic21@users.noreply.github.com>
2024-06-12 20:52:56 +0200

    continue #2687

--- desktop/modal/cmd.configureHistory.php
+++ desktop/modal/cmd.configureHistory.php
@@ -47,6 +47,7 @@ sendVarToJs('jeephp2js.md_cmdConfigureHistory_numCmds', count($cmds));
           </select> {{Lissage}}</th>
         <th data-filter="false" data-type="select-custom">
         <select id="purgeSelectAll" class="input-xs">
+            <option value="">{{Default}}</option>
             <option value="-1 day">{{1 jour}}</option>
             <option value="-7 days">{{7 jours}}</option>
             <option value="-1 month">{{1 mois}}</option>
@@ -55,7 +56,6 @@ sendVarToJs('jeephp2js.md_cmdConfigureHistory_numCmds', count($cmds));
             <option value="-1 year">{{1 an}}</option>
             <option value="-2 years">{{2 ans}}</option>
             <option value="-3 years">{{3 ans}}</option>
-            <option value="">{{Default}}</option>
             <option value="never">{{Jamais}}</option>
           </select> {{Purge}}</th>
           <th data-filter="false" data-type="select-custom">
@@ -143,16 +143,16 @@ sendVarToJs('jeephp2js.md_cmdConfigureHistory_numCmds', count($cmds));
         if ($cmd->getType() == 'info') {
           $confHistoryPurge = $cmd->getConfiguration('historyPurge');
           $tr .= '<select class="form-control cmdAttr input-xs" data-l1key="configuration" data-l2key="historyPurge" ' .  (($right != 'x') ? 'disabled' : '') .  '>';
-          $tr .= '<option data-sorton="0" value="-1 day" ' . (($confHistoryPurge == '-1 day') ? 'selected' : '') . '>{{1 jour}}</option>';
-          $tr .= '<option data-sorton="1" value="-7 days" ' . (($confHistoryPurge == '-7 days') ? 'selected' : '') . '>{{7 jours}}</option>';
-          $tr .= '<option data-sorton="2" value="-1 month" ' . (($confHistoryPurge == '-1 month') ? 'selected' : '') . '>{{1 mois}}</option>';
-          $tr .= '<option data-sorton="3" value="-3 month" ' . (($confHistoryPurge == '-3 month') ? 'selected' : '') . '>{{3 mois}}</option>';
-          $tr .= '<option data-sorton="4" value="-6 month" ' . (($confHistoryPurge == '-6 month') ? 'selected' : '') . '>{{6 mois}}</option>';
-          $tr .= '<option data-sorton="5" value="-1 year" ' . (($confHistoryPurge == '-1 year') ? 'selected' : '') . '>{{1 an}}</option>';
-          $tr .= '<option data-sorton="6" value="-2 years" ' . (($confHistoryPurge == '-2 years') ? 'selected' : '') . '>{{2 ans}}</option>';
-          $tr .= '<option data-sorton="7" value="-3 years" ' . (($confHistoryPurge == '-3 years') ? 'selected' : '') . '>{{3 ans}}</option>';
-          $tr .= '<option data-sorton="8" value="" ' . (($confHistoryPurge == '') ? 'selected' : '') . '>{{Default}}</option>';
-          $tr .= '<option data-sorton="9" value="" ' . (($confHistoryPurge == 'never') ? 'selected' : '') . '>{{Jamais}}</option>';
+          $tr .= '<option data-sorton="0" value="" ' . (($confHistoryPurge == '') ? 'selected' : '') . '>{{Default}}</option>';
+          $tr .= '<option data-sorton="1" value="-1 day" ' . (($confHistoryPurge == '-1 day') ? 'selected' : '') . '>{{1 jour}}</option>';
+          $tr .= '<option data-sorton="2" value="-7 days" ' . (($confHistoryPurge == '-7 days') ? 'selected' : '') . '>{{7 jours}}</option>';
+          $tr .= '<option data-sorton="3" value="-1 month" ' . (($confHistoryPurge == '-1 month') ? 'selected' : '') . '>{{1 mois}}</option>';
+          $tr .= '<option data-sorton="4" value="-3 month" ' . (($confHistoryPurge == '-3 month') ? 'selected' : '') . '>{{3 mois}}</option>';
+          $tr .= '<option data-sorton="5" value="-6 month" ' . (($confHistoryPurge == '-6 month') ? 'selected' : '') . '>{{6 mois}}</option>';
+          $tr .= '<option data-sorton="6" value="-1 year" ' . (($confHistoryPurge == '-1 year') ? 'selected' : '') . '>{{1 an}}</option>';
+          $tr .= '<option data-sorton="7" value="-2 years" ' . (($confHistoryPurge == '-2 years') ? 'selected' : '') . '>{{2 ans}}</option>';
+          $tr .= '<option data-sorton="8" value="-3 years" ' . (($confHistoryPurge == '-3 years') ? 'selected' : '') . '>{{3 ans}}</option>';
+          $tr .= '<option data-sorton="9" value="never" ' . (($confHistoryPurge == 'never') ? 'selected' : '') . '>{{Jamais}}</option>';
           $tr .= '</select>';
         }
         $tr .= '</td>';
