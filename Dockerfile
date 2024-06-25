bf4eefd1a69ba1404ca74d19768feee1637472dd
Pifou25 <nicolas_gras@hotmail.com>
2024-06-25 22:36:11 +0000

    feat: build docker image with current branch source code
    fix: remove sshd configuration for fail2ban
    feat: init.sh with initialisation flag file
    fix: dont start any service during docker image build step i.e. install.sh script exclude starting services
    feat: add workflow to build image, run and test with sick.php script

--- Dockerfile
+++ Dockerfile
@@ -26,8 +26,36 @@ VOLUME ${WEBSERVER_HOME}
 VOLUME /var/lib/mysql
 
 COPY install/install.sh /tmp/
-RUN sh /tmp/install.sh -v ${VERSION} -w ${WEBSERVER_HOME} -d ${DATABASE}
+# install step by step : step_1_upgrade
+RUN sh /tmp/install.sh -s 1 -v ${VERSION} -w ${WEBSERVER_HOME} -d ${DATABASE} -i docker
+# step_2_mainpackage
+RUN sh /tmp/install.sh -s 2 -v ${VERSION} -w ${WEBSERVER_HOME} -d ${DATABASE} -i docker
+# step_3_database
+RUN sh /tmp/install.sh -s 3 -v ${VERSION} -w ${WEBSERVER_HOME} -d ${DATABASE} -i docker
+# step_4_apache
+RUN sh /tmp/install.sh -s 4 -v ${VERSION} -w ${WEBSERVER_HOME} -d ${DATABASE} -i docker
+# step_5_php
+RUN sh /tmp/install.sh -s 5 -v ${VERSION} -w ${WEBSERVER_HOME} -d ${DATABASE} -i docker
+# step 6 : copy jeedom source files
+COPY . ${WEBSERVER_HOME}
+# step_7_jeedom_customization_mariadb
+RUN sh /tmp/install.sh -s 7 -v ${VERSION} -w ${WEBSERVER_HOME} -d ${DATABASE} -i docker
+# step_8_jeedom_customization
+RUN sh /tmp/install.sh -s 8 -v ${VERSION} -w ${WEBSERVER_HOME} -d ${DATABASE} -i docker
+# step_9_jeedom_configuration
+RUN sh /tmp/install.sh -s 9 -v ${VERSION} -w ${WEBSERVER_HOME} -d ${DATABASE} -i docker
+# step_10_jeedom_installation
+RUN sh /tmp/install.sh -s 10 -v ${VERSION} -w ${WEBSERVER_HOME} -d ${DATABASE} -i docker
+# step_11_jeedom_post
+RUN sh /tmp/install.sh -s 11 -v ${VERSION} -w ${WEBSERVER_HOME} -d ${DATABASE} -i docker
+
+# cleanup
+RUN apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
+# this file is a flag to trigger init.sh initialisation
+RUN touch initialisation
 
 EXPOSE 80
 COPY install/OS_specific/Docker/init.sh /root/
-CMD ["sh", "/root/init.sh"]
+CMD ["bash", "/root/init.sh"]
--- install/OS_specific/Docker/init.sh
+++ install/OS_specific/Docker/init.sh
@@ -19,51 +19,40 @@ docker_stop(){
 	echo "${VERT}Stopping Apache gracefully${NORMAL}"
 	service apache2 stop
 	echo "${VERT}Stopping Database gracefully${NORMAL}"
-	service_mariadb stop
+	service mariadb stop
 	echo "${VERT}Stopping ATD gracefully${NORMAL}"
 	service atd stop
+	echo "${VERT}Stopping fail2ban gracefully${NORMAL}"
+	service fail2ban stop
 	echo "${ROUGE}Requesting stop on init.sh${NORMAL}"
 	touch ${FILE_STOP}
 	exit 0
 }
 
-service_mariadb(){
-	service mysql $1
-	if [ $? -ne 0 ]; then
-		service mariadb $1
-		if [ $? -ne 0 ]; then
-			echo "${ROUGE}Cannot start mariadb - Cancelling${NORMAL}"
-			return 1
-		fi
-	fi
-	return 0
-}
-
-echo 'Start init'
+# flag to fail fast on errors
+set -e
 
 # $WEBSERVER_HOME and $VERSION env variables comes from Dockerfile
 
-if [ -f ${WEBSERVER_HOME}/core/config/common.config.php ]; then
-	echo 'Jeedom is already install'
-	JEEDOM_INSTALL=1
-else
-	echo 'Start jeedom installation'
+if [[ -f ${WEBSERVER_HOME}/initialisation ]]; then
+    echo "************************
+Start JEEDOM initialisation !
+************************"
 	JEEDOM_INSTALL=0
-	rm -rf /root/install.sh
-	wget https://raw.githubusercontent.com/jeedom/core/${VERSION}/install/install.sh -O /root/install.sh
-	chmod +x /root/install.sh
-	/root/install.sh -s 6 -v ${VERSION} -w ${WEBSERVER_HOME}
-	if [ $(which mysqld | wc -l) -ne 0 ]; then
-		chown -R mysql:mysql /var/lib/mysql
-		mysql_install_db --user=mysql --basedir=/usr/ --ldata=/var/lib/mysql/
-		service_mariadb restart
-		DB_PASSWORD=$(cat /dev/urandom | tr -cd 'a-f0-9' | head -c 15)
-		echo "DROP USER 'jeedom'@'localhost';" | mysql > /dev/null 2>&1
-		echo  "CREATE USER 'jeedom'@'localhost' IDENTIFIED BY '${DB_PASSWORD}';" | mysql
-		echo  "DROP DATABASE IF EXISTS jeedom;" | mysql
-		echo  "CREATE DATABASE jeedom;" | mysql
-		echo  "GRANT ALL PRIVILEGES ON jeedom.* TO 'jeedom'@'localhost';" | mysql
-	fi
+
+    echo "************************
+Start mariadb service
+************************"
+
+	service mariadb start
+	service mariadb status
+
+	DB_PASSWORD=$(cat /dev/urandom | tr -cd 'a-f0-9' | head -c 15)
+	echo "DROP USER IF EXISTS 'jeedom'@'localhost';" | mysql
+	echo  "CREATE USER 'jeedom'@'localhost' IDENTIFIED BY '${DB_PASSWORD}';" | mysql
+	echo  "DROP DATABASE IF EXISTS jeedom;" | mysql
+	echo  "CREATE DATABASE jeedom;" | mysql
+	echo  "GRANT ALL PRIVILEGES ON jeedom.* TO 'jeedom'@'localhost';" | mysql
 
 	cp ${WEBSERVER_HOME}/core/config/common.config.sample.php ${WEBSERVER_HOME}/core/config/common.config.php
 	sed -i "s/#PASSWORD#/${DB_PASSWORD}/g" ${WEBSERVER_HOME}/core/config/common.config.php
@@ -71,32 +60,42 @@ else
 	sed -i "s/#USERNAME#/${DB_USERNAME:-jeedom}/g" ${WEBSERVER_HOME}/core/config/common.config.php
 	sed -i "s/#PORT#/${DB_PORT:-3306}/g" ${WEBSERVER_HOME}/core/config/common.config.php
 	sed -i "s/#HOST#/${DB_HOST:-localhost}/g" ${WEBSERVER_HOME}/core/config/common.config.php
-	/root/install.sh -s 10 -v ${VERSION} -w ${WEBSERVER_HOME}
-	/root/install.sh -s 11 -v ${VERSION} -w ${WEBSERVER_HOME}
+
+	# remove default fail2ban, contains useless sshd check
+	rm /etc/fail2ban/jail.d/defaults-debian.conf
+
+    echo "************************
+start JEEDOM PHP script installation
+************************"
+
+	php ${WEBSERVER_HOME}/install/install.php mode=force
+
+	# remove the flag file after the first successfull installation
+	rm ${WEBSERVER_HOME}/initialisation
 fi
 
 echo 'Start atd'
 service atd restart
+service atd status
 
-if [ $(which mysqld | wc -l) -ne 0 ]; then
+if [[ $(which mysqld | wc -l) -ne 0 ]]; then
 	echo 'Starting mariadb'
 	chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
-	service_mariadb restart
+	service mariadb restart
 	if [ $? -ne 0 ]; then
 		# That can lead to FATAL corruption of databases
 		# rm /var/lib/mysql/ib_logfile*
-		# service_mariadb restart
 		echo "${ROUGE}Starting Database FAILED${NORMAL}"
 		exit 1
 	fi
 fi
 
-if [ ${JEEDOM_INSTALL} -eq 0 ] && [ ! -z "${RESTOREBACKUP}" ] && [ "${RESTOREBACKUP}" != 'NO' ]; then
+if [[ ${JEEDOM_INSTALL} == 0 ]] && [[ ! -z "${RESTOREBACKUP}" ]] && [[ "${RESTOREBACKUP}" != 'NO' ]]; then
 	echo 'Need restore backup '${RESTOREBACKUP}
 	wget ${RESTOREBACKUP} -O /tmp/backup.tar.gz
 	php ${WEBSERVER_HOME}/install/restore.php backup=/tmp/backup.tar.gz
 	rm /tmp/backup.tar.gz
-	if [ ! -z "${UPDATEJEEDOM}" ] && [ "${UPDATEJEEDOM}" != 'NO' ]; then
+	if [[ ! -z "${UPDATEJEEDOM}" ]] && [[ "${UPDATEJEEDOM}" != 'NO' ]]; then
 		echo 'Need update jeedom'
 		php ${WEBSERVER_HOME}/install/update.php
 	fi
@@ -104,19 +103,24 @@ fi
 
 echo 'All init complete'
 chmod 777 /dev/tty*
-chmod 777 -R /tmp
 chmod 755 -R ${WEBSERVER_HOME}
-chown -R www-data:www-data ${WEBSERVER_HOME}
 
 echo 'Start apache2'
 service apache2 start
+service apache2 status
+
+echo 'Start fail2ban'
+service fail2ban start
+service fail2ban status
 
 echo 'Start CRON daemon'
 cron
+
+# step_12_jeedom_check
+sh /tmp/install.sh -s 12 -v ${VERSION} -w ${WEBSERVER_HOME} -i docker
 
 #TAKE CARE : the init.sh script is running under sh so trap only takes signal_number
 echo 'Add trap docker_stop'
 trap "docker_stop $$ ;" 15
 
-rm "${FILE_STOP}" 1>/dev/null 2>&1
-while [ ! -e "${FILE_STOP}" ]; do sleep 1; done
+while [[ ! -e "${FILE_STOP}" ]]; do sleep 1; done
