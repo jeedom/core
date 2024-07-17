6b4dc0b320a6cc4baba340c660382a850fb0b6f9
Loïc <1536036+zoic21@users.noreply.github.com>
2024-07-17 17:08:01 +0200

    Init queue system #2489

---
+++ core/ajax/queue.ajax.php
@@ -0,0 +1,78 @@
+<?php
+
+/* This file is part of Jeedom.
+ *
+ * Jeedom is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Jeedom is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+try {
+	require_once __DIR__ . '/../php/core.inc.php';
+	include_file('core', 'authentification', 'php');
+
+	if (!isConnect('admin')) {
+		throw new Exception(__('401 - Accès non autorisé', __FILE__));
+	}
+
+	ajax::init();
+
+	if (init('action') == 'save') {
+		unautorizedInDemo();
+		utils::processJsonObject('queue', init('queues'));
+		ajax::success();
+	}
+
+	if (init('action') == 'remove') {
+		unautorizedInDemo();
+		$queue = queue::byId(init('id'));
+		if (!is_object($queue)) {
+			throw new Exception(__('Queue id inconnu', __FILE__));
+		}
+		$queue->remove();
+		ajax::success();
+	}
+
+	if (init('action') == 'all') {
+		$queues = queue::all(true);
+		foreach ($queues as $queue) {
+			$queue->refresh();
+		}
+		ajax::success(utils::o2a($queues));
+	}
+
+	if (init('action') == 'start') {
+		$queue = queue::byId(init('id'));
+		if (!is_object($queue)) {
+			throw new Exception(__('Queue id inconnu', __FILE__));
+		}
+		$queue->run();
+		sleep(1);
+		ajax::success();
+	}
+
+	if (init('action') == 'stop') {
+		$queue = queue::byId(init('id'));
+		if (!is_object($queue)) {
+			throw new Exception(__('Queue id inconnu', __FILE__));
+		}
+		$queue->halt();
+		sleep(1);
+		ajax::success();
+	}
+
+	throw new Exception(__('Aucune méthode correspondante à :', __FILE__) . ' ' . init('action'));
+
+	/*     * *********Catch exeption*************** */
+} catch (Exception $e) {
+	ajax::error(displayException($e), $e->getCode());
+}
--- desktop/js/cron.js
+++ desktop/js/cron.js
@@ -273,6 +273,94 @@ if (!jeeFrontEnd.cron) {
         }
       })
     },
+    printQueue: function() {
+      jeedom.queue.all({
+        success: function(data) {
+          domUtils.showLoading()
+          if (jeeFrontEnd.queue.queueDataTable) {
+            jeeFrontEnd.queue.queueDataTable.destroy()
+            delete jeeFrontEnd.queue.queueDataTable
+          }
+          var table = document.getElementById('table_queue').querySelector('tbody')
+          table.empty()
+          for (var i in data) {
+            let newRow = document.createElement("tr")
+            newRow.innerHTML = jeeP.addqueue(data[i])
+            newRow.setJeeValues(data[i], '.queueAttr')
+            table.appendChild(newRow)
+          }
+          jeeFrontEnd.cron.setQueueTable()
+          jeeFrontEnd.modifyWithoutSave = false
+          setTimeout(function() {
+            jeeFrontEnd.modifyWithoutSave = false
+          }, 1000)
+          domUtils.hideLoading()
+        }
+      })
+    },
+    addQueue: function(_queue) {
+      jeedomUtils.hideAlert()
+      var tr = '<tr>'
+      tr += '<td><span class="queueAttr label label-info" data-l1key="id"></span></td>'
+      tr += '<td>'
+      tr += init(_queue.pid)
+      tr += '</td>'
+      tr += '<td><input class="form-control queueAttr input-sm" data-l1key="queueId" disabled /></td>'
+      tr += '<td><input class="form-control queueAttr input-sm" data-l1key="class" disabled /></td>'
+      tr += '<td><input class="form-control queueAttr input-sm" data-l1key="function" disabled /></td>'
+      tr += '<td>'
+      tr += '<input class="form-control queueAttr input-sm" data-l1key="timeout" />'
+      tr += '</td>'
+      tr += '<td>'
+      tr += init(_queue.lastRun)
+      tr += '</td>'
+      tr += '<td data-sorton="' + init(_queue.runtime, '0') + '">'
+      tr += init(_queue.runtime, '0') + 's'
+      tr += '</td>'
+      tr += '<td>'
+      var label = 'label label-info'
+      var state = init(_queue.state)
+      if (init(_queue.state) == 'run') {
+        label = 'label label-success'
+        state = '{{En cours}}'
+      }
+      if (init(_queue.state) == 'stop') {
+        label = 'label label-danger'
+        state = '{{Arrêté}}'
+      }
+      if (init(_queue.state) == 'starting') {
+        label = 'label label-warning'
+        state = '{{Démarrage}}'
+      }
+      if (init(_queue.state) == 'stoping') {
+        label = 'label label-warning'
+        state = '{{Arrêt}}'
+      }
+      tr += '<span class="' + label + '">' + state + '</span>'
+      tr += '</td>'
+
+      tr += '<td>'
+      if (init(_queue.id) != '') {
+        tr += '<a class="btn btn-xs displayQueue" title="{{Détails de cette tâche}}"><i class="fas fa-file"></i></a> '
+      }
+      tr += '</td>'
+      tr += '</tr>'
+      return tr
+    },
+    setQueueTable: function() {
+      if (jeeFrontEnd.queue.queueDataTable) {
+        jeeFrontEnd.queue.queueDataTable.destroy()
+        delete jeeFrontEnd.queue.queueDataTable
+      }
+      jeeFrontEnd.queue.queueDataTable = new DataTable(jeeFrontEnd.queue.tableQueue, {
+        columns: [
+          { select: 0, sort: "asc" },
+          { select: 8, sortable: false }
+        ],
+        searchable: false,
+        paging: false,
+      })
+    },
   }
 }
 
@@ -401,6 +489,15 @@ document.getElementById('table_cron')?.tBodies[0].addEventListener('click', func
       id: 'jee_modal',
       title: "{{Détails du cron}}",
       contentUrl: 'index.php?v=d&modal=object.display&class=cron&id=' + _target.closest('tr').querySelector('span[data-l1key="id"]').innerHTML
+    })
+    return
+  }
+
+  if (_target = event.target.closest('.displayQueue')) {
+    jeeDialog.dialog({
+      id: 'jee_modal',
+      title: "{{Détails de la queue}}",
+      contentUrl: 'index.php?v=d&modal=object.display&class=queue&id=' + _target.closest('tr').querySelector('span[data-l1key="id"]').innerHTML
     })
     return
   }
