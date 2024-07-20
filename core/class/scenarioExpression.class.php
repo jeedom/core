60a4dc0141f697ccec9bf6d758b781b56a873893
Loïc <1536036+zoic21@users.noreply.github.com>
2024-07-20 10:04:39 +0200

    bugfix scenario action in background

--- core/class/scenarioExpression.class.php
+++ core/class/scenarioExpression.class.php
@@ -1404,10 +1404,7 @@ class scenarioExpression {
 		}
 		$this->checkBackground();
 		if ($this->getOptions('background', 0) == 1) {
-			$key = 'scenarioElement' . config::genKey(10);
-			while (cache::exist($key)) {
-				$key = 'scenarioElement' . config::genKey(10);
-			}
+			$key = 'scenarioElement'.$this->getId() . config::genKey(16).strtotime('now');
 			cache::set($key, array('scenarioExpression' => $this, 'scenario' => $scenario), 60);
 			$cmd = __DIR__ . '/../php/jeeScenarioExpression.php';
 			$cmd .= ' key=' . $key;
