c19f57bfabca2b4f6662979dcd09a8d80067cd57
Salvialf <jeuxalf@live.fr>
2024-08-10 22:02:25 +0200

    install jeeasy on demand

--- desktop/modal/first.use.php
+++ desktop/modal/first.use.php
@@ -22,145 +22,175 @@ if (!isConnect()) {
 $productName = config::byKey('product_name');
 
 try {
-	repo_market::test();
-
-	try {
-		if (!is_object($jeeasy = update::byLogicalId('jeeasy'))) {
-			$jeeasy = (new update)
-				->setLogicalId('jeeasy')
-				->setSource('market')
-				->setConfiguration('version', 'stable');
-			$jeeasy->save();
-			$jeeasy->doUpdate();
-		} else {
-			$jeeasy->checkUpdate();
-			if ($jeeasy->getStatus() == 'update') {
-				$jeeasy->doUpdate();
-			}
-		}
-
-		$jeeasy = plugin::byId('jeeasy');
-		if (!$jeeasy->isActive()) {
-			$jeeasy->setIsEnable(1);
-		}
-	} catch (Exception $e) {
-		echo '<div class="alert alert-danger text-center">';
-		echo "{{Impossible de démarrer l'assistant de configuration.}}";
-		echo '</div>';
-		return;
+	$jeeasy = plugin::byId('jeeasy');
+	if (!$jeeasy->isActive()) {
+		$jeeasy->setIsEnable(1);
 	}
-?>
-	<script>
-		jeeDialog.get('#md_firstUse').close()
-
-		jeeDialog.dialog({
-			id: 'md_firstConfig',
-			title: "{{Configuration de}} <?= $productName ?>",
-			fullScreen: true,
-			onClose: function() {
-				jeeDialog.get('#md_firstConfig').destroy()
-			},
-			contentUrl: 'index.php?v=d&plugin=jeeasy&modal=welcome',
-			callback: function() {
-				jeeDialog.get('#md_firstConfig', 'title').querySelector('button.btClose').remove()
-			}
-		})
-	</script>
-<?php
+	$jeeasy = $jeeasy->getUpdate();
+	$jeeasy->checkUpdate();
+	if ($jeeasy->getStatus() == 'update') {
+		$jeeasy->doUpdate();
+	}
+	echo "<script>jeedomUtils.loadPage('index.php?v=d&p=modaldisplay&plugin=jeeasy&loadmodal=welcome')</script>";
+	die();
 } catch (Exception $e) {
+}
+
+if (config::byKey('jeedom::firstUse') == 1) {
+	echo '<button class="btn btn-xs btn-danger" id="bt_doNotDisplayFirstUse" style="position:absolute;right:15px;">';
+	echo '<i class="fas fa-eye-slash"></i> {{Ne plus afficher}}';
+	echo '</button>';
+}
+
 ?>
-	<div class="text-center">
-		<h3>{{Connexion au Market}}</h3>
 
-		<div class="alert alert-info col-md-10 col-md-offset-1">
-			<strong>{{L' assistant de configuration}} <?= $productName ?> {{nécessite de pouvoir accéder au Market, merci de valider vos identifiants de connexion au Market}} <?= $productName ?>.
-			</strong>
+<div class="text-center">
+
+	<h3 class="first_use">{{Bienvenue dans}} <?= $productName ?></h3>
+	<h3 class="market_connect hidden">{{Connexion au Market}}</h3>
+
+	<div class="alert alert-info col-md-10 col-md-offset-1">
+		<p class="first_use">
+			<?= $productName ?> {{est une solution incontournable dans la gestion du bâtiment intelligent et de l'habitat connecté.}}
 			<br><br>
-			<form class="form-horizontal">
-				<div class="form-group">
-					<label class="control-label col-md-4">{{Utilisateur}} :</label>
-					<input type="text" class="form-control col-md-8 col-xs-12" id="in_username_market" placeholder="{{Nom d'utilisateur Market}}">
-				</div>
-				<div class="form-group">
-					<label class="control-label col-md-4">{{Mot de passe}} :</label>
-					<input type="password" class="form-control col-md-8 col-xs-12" autocomplete="new-password" id="in_password_market" placeholder="{{Mot de passe Market}}">
-				</div>
-			</form>
-			<?php
-			if (config::byKey('doc::base_url', 'core') != '') {
-				echo '<br>';
-				echo '<a class="btn btn-xs btn-default" href="https://market.jeedom.com/index.php?v=d&p=register" target="_blank">';
-				echo '<i class="fas fa-sign-out-alt"></i> {{Pas de compte Market ? En créer un !}}';
-				echo '</a>';
-			}
-			?>
-		</div>
+			{{Cliquez sur le bouton "Installer l'assistant de configuration" pour être accompagné de manière ludique et interactive dans la configuration de votre installation}} <?= $productName ?>.
+		</p>
+		<p class="market_connect hidden">{{L'assistant de configuration}} <?= $productName ?> {{nécessite de pouvoir accéder au Market, merci de valider vos identifiants de connexion au Market}} <?= $productName ?>.
+		</p>
 
-		<button class="btn btn-success" id="bt_validate_market"><i class="fas fa-sign-in-alt"></i> {{Valider}}</button>
-		<?php
-		if (config::byKey('jeedom::firstUse') == 1) {
-			echo '<button class="btn btn-danger" id="bt_doNotDisplayFirstUse"><i class="fas fa-times"></i> {{Ne jamais redemander}}</button>';
-		}
-		if (config::byKey('doc::base_url', 'core') != '') {
-			echo '<br><br>';
-			echo '<div>';
-			echo "{{De plus amples informations sont disponibles à la lecture de}} ";
-			echo '<a href="https://doc.jeedom.com/fr_FR/premiers-pas/" target="_blank">{{la documentation de démarrage}}.</a></div>';
-		}
-		?>
+		<form class="form-horizontal market_connect hidden">
+			<div class="form-group">
+				<label class="control-label col-md-4">{{Utilisateur}}</label>
+				<input type="text" class="form-control col-md-8 col-xs-12" id="in_username_market" placeholder="{{Nom d'utilisateur Market}}">
+			</div>
+			<div class="form-group">
+				<label class="control-label col-md-4">{{Mot de passe}}</label>
+				<input type="password" class="form-control col-md-8 col-xs-12" autocomplete="new-password" id="in_password_market" placeholder="{{Mot de passe Market}}">
+			</div>
+		</form>
 	</div>
-	<script>
-		document.getElementById('md_firstUse').addEventListener('click', function(event) {
-			var _target = null
 
-			if (_target = event.target.closest('#bt_doNotDisplayFirstUse')) {
+	<?php
+	if (config::byKey('doc::base_url', 'core') != '') {
+		echo '<a class="btn btn-default market_connect hidden" href="https://market.jeedom.com/index.php?v=d&p=register" target="_blank">';
+		echo '<i class="fas fa-sign-out-alt"></i> {{Pas de compte Market ? En créer un !}}';
+		echo '</a>';
+	}
+	?>
+	<button class="btn btn-success market_connect hidden" id="bt_validate_market"><i class="fas fa-check"></i> {{Valider les identifiants Market}}</button>
+	<button class="btn btn-success first_use" id="bt_install_jeeasy"><i class="fas fa-sign-in-alt"></i> {{Installer l'assistant de configuration}}</button>
+
+	<hr class="hrPrimary">
+	<?php
+	if (in_array(strtolower(config::byKey('hardware_name')), ['smart', 'atlas', 'luna'])) {
+		echo '<div class="col-md-12">';
+		echo '<a href="https://start.jeedom.com/" target="_blank">';
+		echo '<i class="fas fa-image"></i> {{Retrouvez le guide de démarrage}}';
+		echo '</a>';
+		echo " {{de votre box officielle Jeedom.}}";
+		echo '</div>';
+	}
+	if (config::byKey('doc::base_url', 'core') != '') {
+		echo '<div class="col-md-12">';
+		echo '<a href="https://doc.jeedom.com/fr_FR/premiers-pas/" target="_blank">';
+		echo '<i class="fas fa-book"></i> {{La documentation de démarrage}}';
+		echo '</a>';
+		echo " {{détaille les étapes de mise en service votre box Jeedom.}} ";
+		echo '</div>';
+	}
+	?>
+</div>
+
+<script>
+	document.getElementById('md_firstUse').addEventListener('click', function(event) {
+		var _target = null
+
+		if (_target = event.target.closest('#bt_install_jeeasy')) {
+			jeedom.repo.test({
+				repo: 'market',
+				error: function() {
+					document.querySelectorAll('.first_use').unseen()
+					document.querySelectorAll('.market_connect').removeClass('hidden')
+				},
+				success: function() {
+					jeedom.update.save({
+						update: {
+							'logicalId': 'jeeasy',
+							'source': 'market',
+							'configuration': {
+								'version': 'stable'
+							}
+						},
+						error: function(_error) {
+							jeedomUtils.showAlert({
+								message: _error.message,
+								level: 'danger'
+							})
+						},
+						success: function(_jeeasy) {
+							jeedom.update.do({
+								id: _jeeasy.id,
+								error: function(error) {
+									jeedomUtils.showAlert({
+										message: error.message,
+										level: 'danger'
+									})
+								},
+								success: function() {
+									window.location.reload()
+								}
+							})
+						}
+					})
+				}
+			})
+			return
+		}
+
+		if (_target = event.target.closest('#bt_validate_market')) {
+			username = document.getElementById('in_username_market').value.trim()
+			password = document.getElementById('in_password_market').value.trim()
+
+			if (username != '' && password != '') {
 				jeedom.config.save({
 					configuration: {
-						'jeedom::firstUse': 0
+						'market::username': username,
+						'market::password': password
 					},
-					error: function(error) {
+					error: function(_error) {
 						jeedomUtils.showAlert({
-							message: error.message,
+							message: _error.message,
 							level: 'danger'
 						})
 					},
 					success: function() {
-						jeeDialog.get('#md_firstUse').close()
-						jeedomUtils.showAlert({
-							message: '{{Demande enregistrée}}',
-							level: 'success'
-						})
+						window.location.reload()
 					}
 				})
-				return
 			}
+			return
+		}
 
-			if (_target = event.target.closest('#bt_validate_market')) {
-				username = document.getElementById('in_username_market').value.trim()
-				password = document.getElementById('in_password_market').value.trim()
-
-				if (username != '' && password != '') {
-					jeedom.config.save({
-						configuration: {
-							'jeedom::firstUse': 1,
-							'market::username': username,
-							'market::password': password
-						},
-						error: function(error) {
-							jeedomUtils.showAlert({
-								message: error.message,
-								level: 'danger'
-							})
-						},
-						success: function() {
-							window.location.reload()
-						}
+		if (_target = event.target.closest('#bt_doNotDisplayFirstUse')) {
+			jeedom.config.save({
+				configuration: {
+					'jeedom::firstUse': 0
+				},
+				error: function(_error) {
+					jeedomUtils.showAlert({
+						message: _error.message,
+						level: 'danger'
+					})
+				},
+				success: function() {
+					jeeDialog.get('#md_firstUse').close()
+					jeedomUtils.showAlert({
+						message: '{{Demande enregistrée}}',
+						level: 'success'
 					})
 				}
-				return
-			}
-		})
-	</script>
-<?php
-}
-?>
+			})
+			return
+		}
+	})
+</script>
