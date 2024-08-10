e7199e39a7936a649acd552fbdf461d10cac9c49
Salvialf <jeuxalf@live.fr>
2024-08-10 22:11:27 +0200

    grab line

--- desktop/modal/first.use.php
+++ desktop/modal/first.use.php
@@ -15,7 +15,6 @@
 * You should have received a copy of the GNU General Public License
 * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
 */
-
 if (!isConnect()) {
 	throw new Exception('{{401 - Accès non autorisé}}');
 }
