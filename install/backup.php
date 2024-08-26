4d6bd770fa7c60d404b55c93bdc449940465f2d1
Loïc <1536036+zoic21@users.noreply.github.com>
2024-08-26 10:56:51 +0200

    improve code

--- install/backup.php
+++ install/backup.php
@@ -89,8 +89,7 @@ try {
 	}
 	$tables = DB::Prepare("SHOW TABLES", array(), DB::FETCH_TYPE_ALL);
 	foreach ($tables as $table) {
-		$table = array_values($table);
-		$table = $table[0];
+		$table = array_values($table)[0];
 		if($table == 'event'){
 			continue;
 		}
