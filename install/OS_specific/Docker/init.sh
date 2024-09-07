27f1754f86cd95269da81d9170cc94c8d6002230
Loïc <1536036+zoic21@users.noreply.github.com>
2024-09-07 11:46:07 +0200

    fix docker

--- install/OS_specific/Docker/init.sh
+++ install/OS_specific/Docker/init.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 VERT="\\033[1;32m"
 NORMAL="\\033[0;39m"
 ROUGE="\\033[1;31m"
@@ -9,88 +8,130 @@ BLANC="\\033[0;02m"
 BLANCLAIR="\\033[1;08m"
 JAUNE="\\033[1;33m"
 CYAN="\\033[1;36m"
-
-FILE_STOP="/root/stop_requested"
-
-docker_stop(){
-	echo "${JAUNE}Stopping Jeedom container${NORMAL}"
-	echo "${VERT}Killing CRON${NORMAL}"
-	killall cron
-	echo "${VERT}Stopping Apache gracefully${NORMAL}"
-	service apache2 stop
-	echo "${VERT}Stopping Database gracefully${NORMAL}"
-	service mariadb stop
-	echo "${VERT}Stopping ATD gracefully${NORMAL}"
-	service atd stop
-	echo "${ROUGE}Requesting stop on init.sh${NORMAL}"
-	touch ${FILE_STOP}
-	exit 0
+  
+service_mariadb(){
+  service mysql $1
+  if [ $? -ne 0 ]; then
+    service mariadb $1
+    if [ $? -ne 0 ]; then
+      echo "${ROUGE}Cannot start mariadb - Cancelling${NORMAL}"
+      return 1
+    fi
+  fi
+  return 0
 }
 
-# flag to fail fast on errors
-set -e
+docker_stop() {
+  echo "${JAUNE}Stopping Jeedom container${NORMAL}"
+  echo "${VERT}Killing CRON${NORMAL}"
+  killall cron
+  echo "${VERT}Stopping Apache gracefully${NORMAL}"
+  service apache2 stop
+  if [[ 1 -eq ${ISMARIABDBINSTALLED} ]]; then
+     echo "${VERT}Stopping Database gracefully${NORMAL}"
+     service mariadb stop
+  fi
+  echo "${VERT}Stopping ATD gracefully${NORMAL}"
+  service atd stop
+  echo "${ROUGE}Requesting stop on init.sh${NORMAL}"
+  touch ${FILE_STOP}
+  exit 0
+}
+
+setTimeZone() {
+  [[ ${TZ} == $(</etc/timezone) ]] && return
+  echo "Setting timezone to ${TZ}"
+  ln -fs /usr/share/zoneinfo/${TZ} /etc/localtime
+  dpkg-reconfigure -fnoninteractive tzdata
+}
+
+set_root_password(){
+  #set root password
+  if [[ -z ${ROOT_PASSWD} ]]; then
+    ROOT_PASSWD=$(openssl rand -base64 32 | tr -d /=+ | cut -c1-15)
+    echo "Use generate password : ${ROOT_PASSWD}"
+  fi
+  echo "root:${ROOT_PASSWD}" | chpasswd
+}
+
+apache_setup() {
+  mkdir -p /var/log/apache2/
+  #define ports, activate ssl
+  if [[ 3 -ne $(grep -cP "(${APACHE_HTTP_PORT}|${APACHE_HTTPS_PORT})" /etc/apache2/ports.conf) ]]; then
+    echo "Ports update for apache2: ${APACHE_HTTP_PORT}, ${APACHE_HTTPS_PORT}"
+    echo "Listen ${APACHE_HTTP_PORT}
+	<IfModule ssl_module>
+		Listen ${APACHE_HTTPS_PORT:-443}
+	</IfModule>
+
+	<IfModule mod_gnutls.c>
+		Listen ${APACHE_HTTPS_PORT:-443}
+	</IfModule>" >/etc/apache2/ports.conf
+    sed -i -E "s/\<VirtualHost \*:(.*)\>/VirtualHost \*:${APACHE_HTTP_PORT}/" /etc/apache2/sites-available/000-default.conf
+    sed -i -E "s/\<VirtualHost \*:(.*)\>/VirtualHost \*:${APACHE_HTTPS_PORT}/" /etc/apache2/sites-available/default-ssl.conf
+  fi
+}
+
+echo 'Start init'
 
 # $WEBSERVER_HOME and $VERSION env variables comes from Dockerfile
 
-if [[ -f ${WEBSERVER_HOME}/initialisation ]]; then
-    echo "************************
-Start JEEDOM initialisation !
-************************"
+if [ -f ${WEBSERVER_HOME}/core/config/common.config.php ]; then
+	echo 'Jeedom is already install'
+	JEEDOM_INSTALL=1
+else
+	echo 'Start jeedom installation'
 	JEEDOM_INSTALL=0
-
-    echo "************************
-Start mariadb service
-************************"
-
-	service mariadb start
-	service mariadb status
-
-	DB_PASSWORD=$(cat /dev/urandom | tr -cd 'a-f0-9' | head -c 15)
-	echo "DROP USER IF EXISTS 'jeedom'@'localhost';" | mysql
-	echo  "CREATE USER 'jeedom'@'localhost' IDENTIFIED BY '${DB_PASSWORD}';" | mysql
-	echo  "DROP DATABASE IF EXISTS jeedom;" | mysql
-	echo  "CREATE DATABASE jeedom;" | mysql
-	echo  "GRANT ALL PRIVILEGES ON jeedom.* TO 'jeedom'@'localhost';" | mysql
-
-	cp ${WEBSERVER_HOME}/core/config/common.config.sample.php ${WEBSERVER_HOME}/core/config/common.config.php
-	sed -i "s/#PASSWORD#/${DB_PASSWORD}/g" ${WEBSERVER_HOME}/core/config/common.config.php
-	sed -i "s/#DBNAME#/${DB_NAME:-jeedom}/g" ${WEBSERVER_HOME}/core/config/common.config.php
-	sed -i "s/#USERNAME#/${DB_USERNAME:-jeedom}/g" ${WEBSERVER_HOME}/core/config/common.config.php
-	sed -i "s/#PORT#/${DB_PORT:-3306}/g" ${WEBSERVER_HOME}/core/config/common.config.php
-	sed -i "s/#HOST#/${DB_HOST:-localhost}/g" ${WEBSERVER_HOME}/core/config/common.config.php
-
-    echo "************************
-start JEEDOM PHP script installation
-************************"
-
-	php ${WEBSERVER_HOME}/install/install.php mode=force
-
-	# remove the flag file after the first successfull installation
-	rm ${WEBSERVER_HOME}/initialisation
-fi
-
-echo 'Start atd'
-service atd restart
-service atd status
-
-if [[ $(which mysqld | wc -l) -ne 0 ]]; then
-	echo 'Starting mariadb'
-	chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
-	service mariadb restart
-	if [ $? -ne 0 ]; then
-		# That can lead to FATAL corruption of databases
-		# rm /var/lib/mysql/ib_logfile*
-		echo "${ROUGE}Starting Database FAILED${NORMAL}"
-		exit 1
+	rm -rf /root/install.sh
+	wget https://raw.githubusercontent.com/jeedom/core/${VERSION}/install/install.sh -O /root/install.sh
+	chmod +x /root/install.sh
+	/root/install.sh -s 6 -v ${VERSION} -w ${WEBSERVER_HOME}
+	if [ $(which mysqld | wc -l) -ne 0 ]; then
+		chown -R mysql:mysql /var/lib/mysql
+		mysql_install_db --user=mysql --basedir=/usr/ --ldata=/var/lib/mysql/
+		service_mariadb restart
+		MYSQL_JEEDOM_PASSWD=$(cat /dev/urandom | tr -cd 'a-f0-9' | head -c 15)
+		echo "DROP USER 'jeedom'@'localhost';" | mysql > /dev/null 2>&1
+		echo  "CREATE USER 'jeedom'@'localhost' IDENTIFIED BY '${MYSQL_JEEDOM_PASSWD}';" | mysql
+		echo  "DROP DATABASE IF EXISTS jeedom;" | mysql
+		echo  "CREATE DATABASE jeedom;" | mysql
+		echo  "GRANT ALL PRIVILEGES ON jeedom.* TO 'jeedom'@'localhost';" | mysql
+		cp ${WEBSERVER_HOME}/core/config/common.config.sample.php ${WEBSERVER_HOME}/core/config/common.config.php
+		sed -i "s/#PASSWORD#/${MYSQL_JEEDOM_PASSWD}/g" ${WEBSERVER_HOME}/core/config/common.config.php
+		sed -i "s/#DBNAME#/jeedom/g" ${WEBSERVER_HOME}/core/config/common.config.php
+		sed -i "s/#USERNAME#/jeedom/g" ${WEBSERVER_HOME}/core/config/common.config.php
+		sed -i "s/#PORT#/3306/g" ${WEBSERVER_HOME}/core/config/common.config.php
+		sed -i "s/#HOST#/localhost/g" ${WEBSERVER_HOME}/core/config/common.config.php
+		/root/install.sh -s 10 -v ${VERSION} -w ${WEBSERVER_HOME}
+		/root/install.sh -s 11 -v ${VERSION} -w ${WEBSERVER_HOME}
 	fi
 fi
 
-if [[ ${JEEDOM_INSTALL} == 0 ]] && [[ ! -z "${RESTOREBACKUP}" ]] && [[ "${RESTOREBACKUP}" != 'NO' ]]; then
+setTimeZone
+#setup apache port
+apache_setup
+#setup root passwd
+set_root_password
+
+echo 'Start atd'
+service atd restart
+
+if [ $(which mysqld | wc -l) -ne 0 ]; then
+	echo 'Starting mariadb'
+	chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
+	service_mariadb restart
+	if [ $? -ne 0 ]; then
+		 rm /var/lib/mysql/ib_logfile*
+		 service_mariadb restart
+	fi
+fi
+
+if [ ${JEEDOM_INSTALL} -eq 0 ] && [ ! -z "${RESTOREBACKUP}" ] && [ "${RESTOREBACKUP}" != 'NO' ]; then
 	echo 'Need restore backup '${RESTOREBACKUP}
 	wget ${RESTOREBACKUP} -O /tmp/backup.tar.gz
 	php ${WEBSERVER_HOME}/install/restore.php backup=/tmp/backup.tar.gz
 	rm /tmp/backup.tar.gz
-	if [[ ! -z "${UPDATEJEEDOM}" ]] && [[ "${UPDATEJEEDOM}" != 'NO' ]]; then
+	if [ ! -z "${UPDATEJEEDOM}" ] && [ "${UPDATEJEEDOM}" != 'NO' ]; then
 		echo 'Need update jeedom'
 		php ${WEBSERVER_HOME}/install/update.php
 	fi
@@ -98,19 +139,14 @@ fi
 
 echo 'All init complete'
 chmod 777 /dev/tty*
+chmod 777 -R /tmp
 chmod 755 -R ${WEBSERVER_HOME}
+chown -R www-data:www-data ${WEBSERVER_HOME}
 
 echo 'Start apache2'
 service apache2 start
-service apache2 status
-echo 'Start CRON daemon'
-cron
 
-# step_12_jeedom_check
-sh /tmp/install.sh -s 12 -v ${VERSION} -w ${WEBSERVER_HOME} -i docker
-
-#TAKE CARE : the init.sh script is running under sh so trap only takes signal_number
-echo 'Add trap docker_stop'
+echo "Add trap docker_stop"
 trap "docker_stop $$ ;" 15
 
-while [[ ! -e "${FILE_STOP}" ]]; do sleep 1; done
+cron -f
