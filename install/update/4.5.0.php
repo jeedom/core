a21f3498195f0003c5ead7cd5e8589f1c77c1c06
Loïc <1536036+zoic21@users.noreply.github.com>
2024-09-23 14:04:25 +0200

    fix cache table

--- install/update/4.5.0.php
+++ install/update/4.5.0.php
@@ -5,6 +5,9 @@ try {
     echo 'Empty old cache system...';
     cache::flush();
     echo 'OK';
+    echo 'Empty DB cache table...';
+    DB::prepare('TRUNCATE TABLE `cache`',array(),DB::FETCH_TYPE_ALL);
+    echo 'OK';
 } catch (\Throwable $th) {
     echo 'Error on empty cache : '.$th->getMessage();
 }
