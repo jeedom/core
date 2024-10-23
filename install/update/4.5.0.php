b568493dbfb53648e17e9fdec315a69c0ca48acf
Loïc <1536036+zoic21@users.noreply.github.com>
2024-10-23 15:09:48 +0200

    test

--- install/update/4.5.0.php
+++ install/update/4.5.0.php
@@ -11,5 +11,18 @@ try {
 } catch (\Throwable $th) {
     echo 'Error on empty cache : '.$th->getMessage();
 }
-
+try {
+    echo "Save cache state of cmd and eqLogic...";
+    $data = array('cmd' => array(),'eqLogic' => array());
+    foreach(cmd::all() as  $cmd){
+      $data['cmd'][$cmd->getId()] = $cmd->getCache();
+    }
+    foreach(eqLogic::all() as  $eqLogic){
+      $data['eqLogic'][$eqLogic->getId()] = $eqLogic->getCache();
+    }
+    file_put_contents('/tmp/jeedom/cache.json',json_encode($data));
+    echo "OK\n";
+} catch (Exception $e) {
+    echo '***WARNING***' . $e->getMessage();
+}
 echo shell_exec('php '.__DIR__.'/reloadCache.php');
