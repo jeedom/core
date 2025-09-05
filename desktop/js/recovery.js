b86b04ee17bc2dd2652f291be0bedcceafda1a48
Salvialf <jeuxalf@live.fr>
2025-09-05 14:48:12 +0200

    recovery interface

---
+++ desktop/js/recovery.js
@@ -0,0 +1,245 @@
+/* This file is part of Jeedom.
+ *
+ * Jeedom is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Jeedom is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+"use strict"
+
+if (!jeeFrontEnd.recovery) {
+  jeeFrontEnd.recovery = {
+    title: Node,
+    step: Node,
+    details: Node,
+    progress: Node,
+    buttons: NodeList,
+    hardware: null,
+    inProgress: null,
+    mode: null,
+    usb: null,
+    init: function() {
+      this.title = document.getElementById('recovery-title')
+      this.step = document.getElementById('recovery-step')
+      this.details = document.getElementById('recovery-details')
+      this.progress = document.getElementById('recovery-progress')
+      this.buttons = document.getElementById('recovery-buttons').querySelectorAll('a.btn')
+      this.hardware = jeephp2js.hardware
+      window.jeeP = this
+    },
+    start: function(_mode) {
+      jeeP.clearProgress()
+      jeeP.mode = _mode
+      jeeP.monitorProgress()
+      jeeP.displayButtons('cancel')
+
+      jeedom.recovery.start({
+        global: false,
+        hardware: jeeP.hardware,
+        mode: _mode,
+        success: function(_success) {
+          jeeP.monitorProgress('stop')
+          jeeP.mode = null
+
+          if (_success) {
+            jeeP.displayButtons('reboot', 'halt')
+          } else {
+            jeeP.displayButtons('refresh')
+          }
+        }
+      })
+    },
+    cancel: function() {
+      bootbox.confirm('<div class="text-center alert alert-danger"><i class="fas fa-exclamation-triangle"></i> {{Annuler la procédure de restauration système en cours ?}}</div>', function(ok) {
+        if (ok) {
+          if (jeeP.mode) {
+            jeedom.recovery.cancel({
+              async: false
+            })
+          } else {
+            jeeP.monitorProgress('stop')
+            jeeP.updateProgress({
+              details: "{{Procédure annulée à la demande de l'utilisateur}}",
+              progress: -1
+            })
+          }
+          jeeP.displayButtons('refresh')
+        }
+      })
+    },
+    getProgress: function() {
+      jeedom.recovery.getProgress({
+        async: false,
+        success: function(_data) {
+          if (_data) {
+            jeeP.updateProgress(JSON.parse(_data))
+          }
+        }
+      })
+    },
+    monitorProgress: function(_state = 'start') {
+      if (_state === 'start') {
+        jeeP.inProgress = setInterval(function() {
+          jeeP.getProgress()
+        }, 500)
+      } else if (_state === 'stop') {
+        if (jeeP.inProgress) {
+          clearInterval(jeeP.inProgress)
+          jeeP.inProgress = null
+        }
+      }
+    },
+    updateProgress: function(_data) {
+      if (isset(_data.step)) {
+        jeeP.step.innerText = _data.step
+      }
+      if (isset(_data.details)) {
+        jeeP.details.innerText = _data.details
+      }
+      if (isset(_data.progress)) {
+        if (_data.progress < 0) {
+          jeeP.progress.classList = 'progress-bar ' + (!jeeP.inProgress ? 'progress-bar-warning' : 'progress-bar-danger')
+          jeeP.progress.style.width = '100%'
+          jeeP.progress.setAttribute('aria-valuenow', 100)
+          jeeP.progress.innerText = (!jeeP.inProgress) ? "{{Annulation}}" : '{{Erreur}}'
+          jeeP.displayButtons()
+        } else if (_data.progress >= 100) {
+          jeeP.progress.classList = 'progress-bar progress-bar-success'
+          jeeP.progress.style.width = '100%'
+          jeeP.progress.setAttribute('aria-valuenow', 100)
+          jeeP.progress.innerText = '100%'
+          jeeP.displayButtons()
+        } else {
+          jeeP.progress.classList = 'progress-bar progress-bar-striped progress-bar-animated progress-bar-info active'
+          jeeP.progress.style.width = _data.progress + '%'
+          jeeP.progress.setAttribute('aria-valuenow', _data.progress)
+          jeeP.progress.innerText = _data.progress + '%'
+          if (_data.progress >= 98) {
+            jeeP.displayButtons()
+          }
+          if (!jeeP.progress.isVisible()) {
+            jeeP.progress.parentNode.seen()
+          }
+        }
+      }
+    },
+    clearProgress: function() {
+      jeeP.updateProgress({
+        step: '',
+        details: '',
+        progress: 0
+      })
+    },
+    displayButtons: function(..._buttons) {
+      jeeP.buttons.unseen()
+      _buttons.forEach(_button => {
+        document.getElementById('bt_' + _button).seen()
+      })
+    },
+    usbConnected: function() {
+      jeedom.recovery.usbConnected({
+        async: false,
+        hardware: jeeP.hardware,
+        success: function(_data) {
+          jeeP.usb = _data
+        }
+      })
+      return jeeP.usb
+    },
+    usbDetection: function() {
+      return new Promise(function(resolve, reject) {
+        if (jeeP.usbConnected()) {
+          return resolve()
+        }
+
+        let i = 1
+        jeeP.updateProgress({
+          step: '{{Détection de la clé USB}}',
+          details: "{{Veuillez insérer une clé USB dans le premier port en haut à droite}}",
+          progress: i
+        })
+
+        jeeP.displayButtons('cancel')
+        jeeP.inProgress = setInterval(function() {
+          if (jeeP.usbConnected()) {
+            jeeP.updateProgress({
+              details: '{{Clé USB détectée}}',
+              progress: 100
+            })
+            jeeP.monitorProgress('stop')
+            setTimeout(() => {
+              return resolve()
+            }, 1000)
+          }
+
+          if (i == 100) {
+            jeeP.updateProgress({
+              details: '{{Abandon, clé USB non détectée}}',
+              progress: -1
+            })
+            jeeP.monitorProgress('stop')
+            jeeP.displayButtons('refresh')
+            return reject()
+          }
+
+          i += 1
+          jeeP.updateProgress({
+            progress: i
+          })
+        }, 5000)
+      })
+    }
+  }
+}
+
+jeeFrontEnd.recovery.init()
+
+/**************************BUTTONS***********************************/
+// /*Events delegations
+// */
+document.getElementById('recovery-buttons').addEventListener('click', function(_event) {
+  var _target = null
+  if (_target = _event.target.closest('#bt_auto')) {
+    jeeP.title.innerText = jeeP.title.innerText + ' (AUTO)'
+    jeeP.start('auto')
+    return
+  }
+
+  if (_target = _event.target.closest('#bt_usb')) {
+    jeeP.title.innerText = jeeP.title.innerText + ' (USB)'
+    jeeP.usbDetection().then(() => {
+      jeeP.start('usb')
+    }).catch((_error) => {
+    })
+    return
+  }
+
+  if (_target = _event.target.closest('#bt_cancel')) {
+    jeeP.cancel()
+    return
+  }
+
+  if (_target = _event.target.closest('#bt_reboot')) {
+    jeedomUtils.loadPage('index.php?v=d&p=reboot')
+    return
+  }
+
+  if (_target = _event.target.closest('#bt_halt')) {
+    jeedomUtils.loadPage('index.php?v=d&p=shutdown')
+    return
+  }
+
+  if (_target = _event.target.closest('#bt_refresh')) {
+    window.location.reload()
+    return
+  }
+})
