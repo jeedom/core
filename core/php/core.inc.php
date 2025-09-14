519468553e48d60db69adffff493c9ca6c10eedf
Emmanuel Bernaszuk <ebernaszuk@nodevo.com>
2025-09-14 11:37:07 +0200

    Explicit log setting error

--- core/php/core.inc.php
+++ core/php/core.inc.php
@@ -27,47 +27,43 @@ try {
 	if (isset($configs['timezone'])) {
 		date_default_timezone_set($configs['timezone']);
 	}
-} catch (Exception $e) {
-} catch (Error $e) {
+} catch (\Throwable $throwable) {
+    error_log('Unable to set timezone: ' . $throwable->getMessage());
 }
 
 try {
 	if (isset($configs['log::level'])) {
 		log::define_error_reporting($configs['log::level']);
 	}
-} catch (Exception $e) {
-} catch (Error $e) {
+} catch (\Throwable $throwable) {
+    error_log('Unable to set log level: ' . $throwable->getMessage());
 }
 
+/**
+ * Autoload function for specific Jeedom classes
+ * this function is called after the default Composer autoloader
+ * it will load specific classes such as Cmd, Real, etc.
+ * for plugins that do not use namespaces or Composer autoloading
+ */
 function jeedomAutoload($_classname) {
-	/* core class always in /core/class : */
-	$path = __DIR__ . "/../../core/class/$_classname.class.php";
-	if (file_exists($path)) {
-		include_file('core', $_classname, 'class');
-	} else if (substr($_classname, 0, 4) === 'com_') {
-		/* class com_$1 in /core/com/$1.com.php */
-		include_file('core', substr($_classname, 4), 'com');
-	} else if (substr($_classname, 0, 5) === 'repo_') {
-		/* class repo_$1 in /core/repo/$1.repo.php */
-		include_file('core', substr($_classname, 5), 'repo');
-	} else if (strpos($_classname, '\\') === false && strpos($_classname, '/') === false) {
-		/* autoload for plugins : no namespace */
-		$classname = str_replace(array('Real', 'Cmd'), '', $_classname);
+    if (strpos($_classname, '\\') !== false || strpos($_classname, '/') !== false) {
+        return;
+    }
+
+	/* autoload for plugins : no namespace */
+	$classname = str_replace(array('Real', 'Cmd'), '', $_classname);
+	$plugin_active = config::byKey('active', $classname, null);
+	if (($plugin_active === null || $plugin_active == '' || $plugin_active == 0) && strpos($classname, '_') !== false) {
+		$classname = explode('_', $classname)[0];
 		$plugin_active = config::byKey('active', $classname, null);
-		if (($plugin_active === null || $plugin_active == '' || $plugin_active == 0) && strpos($classname, '_') !== false) {
-			$classname = explode('_', $classname)[0];
-			$plugin_active = config::byKey('active', $classname, null);
-		}
-		if ($plugin_active == 1) {
-			try {
-				include_file('core', $classname, 'class', $classname);
-			} catch (Exception $e) {
-				
-			} catch (Error $e) {
-				
-			}
+	}
+	if ($plugin_active == 1) {
+		try {
+			include_file('core', $classname, 'class', $classname);
+		} catch (\Throwable $throwable) {
+           error_log('Error loading class ' . $classname . ': ' . $throwable->getMessage());
 		}
 	}
 }
 
-spl_autoload_register('jeedomAutoload', true, true);
+spl_autoload_register('jeedomAutoload');
