6c6f33a0bd273c83bc26a7c9f6795d2c330ca27b
Salvialf <jeuxalf@live.fr>
2026-03-18 16:53:31 +0100

    additionnal dependancy check only when previously ok

--- core/class/plugin.class.php
+++ core/class/plugin.class.php
@@ -725,13 +725,15 @@ class plugin {
 			}
 			$return['last_launch'] = config::byKey('lastDependancyInstallTime', $this->getId(), __('Inconnue', __FILE__));
 			$return['auto'] = config::byKey('dependancyAutoMode', $this->getId(), 1);
-			if ($return['state'] != 'in_progress' && method_exists($plugin_id, 'additionnalDependancyCheck')) {
+
+			if ($return['state'] === 'ok' && method_exists($plugin_id, 'additionnalDependancyCheck')) {
 				$additionnal = $plugin_id::additionnalDependancyCheck();
 				if (isset($additionnal['state'])) {
 					$return['state'] = $additionnal['state'];
 				}
 			}
-			if ($return['state'] == 'ok') {
+
+			if ($return['state'] === 'ok') {
 				cache::set('dependancy' . $this->getId(), $return);
 			}
 			return $return;
