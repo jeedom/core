250caba6bc6cb73de28ac9918d2553c5f6878546
Salvialf <jeuxalf@live.fr>
2026-04-27 17:48:06 +0200

    fix(install): remove deprecated MariaDB directives incompatible with MariaDB 10.5+
    - removed query_cache_type, query_cache_size, query_cache_limit, query_cache_min_res_unit
    - removed innodb_large_prefix

--- install/install.sh
+++ install/install.sh
@@ -206,14 +206,9 @@ step_7_jeedom_customization_mariadb() {
     echo "thread_cache_size = 16" >> /etc/mysql/conf.d/jeedom_my.cnf
     echo "tmp_table_size = 48M" >> /etc/mysql/conf.d/jeedom_my.cnf
     echo "max_heap_table_size = 48M" >> /etc/mysql/conf.d/jeedom_my.cnf
-    echo "query_cache_type =1" >> /etc/mysql/conf.d/jeedom_my.cnf
-    echo "query_cache_size = 32M" >> /etc/mysql/conf.d/jeedom_my.cnf
-    echo "query_cache_limit = 2M" >> /etc/mysql/conf.d/jeedom_my.cnf
-    echo "query_cache_min_res_unit=3K" >> /etc/mysql/conf.d/jeedom_my.cnf
     echo "innodb_flush_method = O_DIRECT" >> /etc/mysql/conf.d/jeedom_my.cnf
     echo "innodb_flush_log_at_trx_commit = 2" >> /etc/mysql/conf.d/jeedom_my.cnf
     echo "innodb_log_file_size = 32M" >> /etc/mysql/conf.d/jeedom_my.cnf
-    echo "innodb_large_prefix = on" >> /etc/mysql/conf.d/jeedom_my.cnf
     echo "connect_timeout = 600" >> /etc/mysql/conf.d/jeedom_my.cnf
     echo "wait_timeout = 600" >> /etc/mysql/conf.d/jeedom_my.cnf
     echo "interactive_timeout = 600" >> /etc/mysql/conf.d/jeedom_my.cnf
