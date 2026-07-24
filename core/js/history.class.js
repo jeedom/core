a92d2113290ea8dbd0ab6bd147a73e7c1c72a965
Salvialf <jeuxalf@live.fr>
2026-07-24 17:34:13 +0200

    fix(history.class.js): skip graphUpdate for comparison, grouped and derived charts

--- core/js/history.class.js
+++ core/js/history.class.js
@@ -161,7 +161,13 @@ jeedom.history.graphUpdate = function(_params) {
       continue
     }
     for (const chart in jeedom.history.chart) {
+      if (jeedom.history.chart[chart].comparing) {
+        continue
+      }
       const cmd = jeedom.history.chart[chart].cmd[cmd_id]
+      if (cmd?.option?.groupingType || cmd?.option?.graphDerive) {
+        continue
+      }
       let value = _params[i].value
       if (typeof cmd?.calcul === 'function') {
         value = cmd.calcul(value)
@@ -179,6 +185,7 @@ jeedom.history.graphUpdate = function(_params) {
       for (const serie in jeedom.history.chart[chart].chart.series) {
         if (jeedom.history.chart[chart].chart.series[serie]?.options.id == cmd_id) {
           jeedom.history.chart[chart].chart.series[serie].addPoint([Date.now() + (-new Date().getTimezoneOffset() * 60 * 1000), value])
+          break
         }
       }
     }
