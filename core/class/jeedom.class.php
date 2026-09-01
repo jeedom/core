aa931ec197874c9dd127da8c6f1a44e3fb5b84a0
Salvialf <jeuxalf@live.fr>
2026-09-01 15:46:13 +0200

    fix(jeedom.class): raise minimum required Debian version from 11 to 12
    Closes #3191

--- core/class/jeedom.class.php
+++ core/class/jeedom.class.php
@@ -296,10 +296,10 @@ class jeedom {
 			$version = trim(strtolower(file_get_contents('/etc/debian_version')));
 			$majorVersion = intval($version);
 			if ($majorVersion > 0) {
-				if ($majorVersion < 11 || $majorVersion > 13) {
+				if ($majorVersion < config::byKey('os::min') || $majorVersion > config::byKey('os::max')) {
 					$state = false;
 				}
-			} else if (strpos($version, 'bullseye') === false && strpos($version, 'bookworm') === false && strpos($version, 'trixie') === false) {
+			} else if (strpos($version, 'bookworm') === false && strpos($version, 'trixie') === false) {
 				$state = false;
 			}
 		}
